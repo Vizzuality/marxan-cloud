4320f30e392dd4964997a0a49623d31e529fb110
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2020-11-26 11:57:25 +0100

    Global: tailwindcss working

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import 'styles/tailwind.css';
 
 const App: React.ReactNode = ({ Component, pageProps }: AppProps) => {
   return (
