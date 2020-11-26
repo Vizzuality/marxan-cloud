6ad2225b55d4704243f8d6caf54fc698d3a3af5d
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2020-11-26 16:16:46 +0100

    Global: reorder page tests

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,1 +1,21 @@
-export { default } from './home';
+import React from 'react';
+import Head from 'next/head';
+
+import Header from 'components/header';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header />
+      </main>
+    </>
+  );
+};
+
+export default Home;
