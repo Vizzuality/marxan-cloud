a57f09dce7327d594bb1993aff66daeadbd7f723
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2020-12-01 20:50:59 +0100

    Button: dark theme, controls and hide canvas tab

--- app/components/button/component.stories.tsx
+++ app/components/button/component.stories.tsx
@@ -1,108 +1,32 @@
-import * as React from 'react';
-import Button from './component';
+import React from 'react';
+import { Story } from '@storybook/react/types-6-0';
+import Button, { ButtonProps } from './component';
 
 export default {
   title: 'Components/Button',
   component: Button,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['xs', 's', 'base'],
+      },
+    },
+    theme: {
+      control: {
+        type: 'select',
+        options: ['primary', 'primary-alt', 'secondary', 'secondary-alt'],
+      },
+    },
+  },
 };
 
-export const Primary: React.FC<unknown> = () => {
-  return (
-    <div>
-      <div>
-        <h2>Extra small</h2>
-        <Button size="xs" theme="primary">
-          Button
-        </Button>
-      </div>
-      <div className="pt-5">
-        <h2>Small</h2>
-        <Button size="s" theme="primary">
-          Button
-        </Button>
-      </div>
-      <div className="pt-5">
-        <h2>Base</h2>
-        <Button size="base" theme="primary">
-          Button
-        </Button>
-      </div>
-    </div>
-  );
-};
+const Template: Story<ButtonProps> = ({ children, ...args }: ButtonProps) => <Button {...args}>{children}</Button>;
 
-export const PrimaryAlt: React.FC<unknown> = () => {
-  return (
-    <div>
-      <div>
-        <h2>Extra small</h2>
-        <Button size="xs" theme="primary-alt">
-          Button
-        </Button>
-      </div>
-      <div className="pt-5">
-        <h2>Small</h2>
-        <Button size="s" theme="primary-alt">
-          Button
-        </Button>
-      </div>
-      <div className="pt-5">
-        <h2>Base</h2>
-        <Button size="base" theme="primary-alt">
-          Button
-        </Button>
-      </div>
-    </div>
-  );
-};
+export const Default = Template.bind({});
 
-export const Secondary: React.FC<unknown> = () => {
-  return (
-    <div>
-      <div>
-        <h2>Extra small</h2>
-        <Button size="xs" theme="secondary">
-          Button
-        </Button>
-      </div>
-      <div className="pt-5">
-        <h2>Small</h2>
-        <Button size="s" theme="secondary">
-          Button
-        </Button>
-      </div>
-      <div className="pt-5">
-        <h2>Base</h2>
-        <Button size="base" theme="secondary">
-          Button
-        </Button>
-      </div>
-    </div>
-  );
-};
-
-export const SecondaryAlt: React.FC<unknown> = () => {
-  return (
-    <div>
-      <h1>Sizes</h1>
-      <div>
-        <h2>Extra small</h2>
-        <Button size="xs" theme="secondary-alt">
-          Button
-        </Button>
-      </div>
-      <div className="pt-5">
-        <h2>Small</h2>
-        <Button size="s" theme="secondary-alt">
-          Button
-        </Button>
-      </div>
-      <div className="pt-5">
-        <h2>Base</h2>
-        <Button size="base" theme="secondary-alt">
-          Button
-        </Button>
-      </div>
-    </div>
-  );
+Default.args = {
+  children: 'Button',
+  size: 'base',
+  theme: 'primary',
 };
