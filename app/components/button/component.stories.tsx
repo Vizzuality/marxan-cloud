ad61d6ba291625eaa4cbb80e12225900572ad60b
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2020-12-01 21:08:27 +0100

    Button: show canvas to prevent errors

--- app/components/button/component.stories.tsx
+++ app/components/button/component.stories.tsx
@@ -30,3 +30,103 @@ Default.args = {
   size: 'base',
   theme: 'primary',
 };
+
+export const Primary: React.FC<unknown> = () => {
+  return (
+    <div>
+      <div>
+        <h2 className="mb-2 text-white uppercase text-xxs">Extra small</h2>
+        <Button size="xs" theme="primary">
+          Button
+        </Button>
+      </div>
+      <div className="pt-5">
+        <h2 className="mb-2 text-white uppercase text-xxs">Small</h2>
+        <Button size="s" theme="primary">
+          Button
+        </Button>
+      </div>
+      <div className="pt-5">
+        <h2 className="mb-2 text-white uppercase text-xxs">Base</h2>
+        <Button size="base" theme="primary">
+          Button
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const PrimaryAlt: React.FC<unknown> = () => {
+  return (
+    <div>
+      <div>
+        <h2 className="mb-2 text-white uppercase text-xxs">Extra small</h2>
+        <Button size="xs" theme="primary-alt">
+          Button
+        </Button>
+      </div>
+      <div className="pt-5">
+        <h2 className="mb-2 text-white uppercase text-xxs">Small</h2>
+        <Button size="s" theme="primary-alt">
+          Button
+        </Button>
+      </div>
+      <div className="pt-5">
+        <h2 className="mb-2 text-white uppercase text-xxs">Base</h2>
+        <Button size="base" theme="primary-alt">
+          Button
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const Secondary: React.FC<unknown> = () => {
+  return (
+    <div>
+      <div>
+        <h2 className="mb-2 text-white uppercase text-xxs">Extra small</h2>
+        <Button size="xs" theme="secondary">
+          Button
+        </Button>
+      </div>
+      <div className="pt-5">
+        <h2 className="mb-2 text-white uppercase text-xxs">Small</h2>
+        <Button size="s" theme="secondary">
+          Button
+        </Button>
+      </div>
+      <div className="pt-5">
+        <h2 className="mb-2 text-white uppercase text-xxs">Base</h2>
+        <Button size="base" theme="secondary">
+          Button
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const SecondaryAlt: React.FC<unknown> = () => {
+  return (
+    <div>
+      <div>
+        <h2 className="mb-2 text-white uppercase text-xxs">Extra small</h2>
+        <Button size="xs" theme="secondary-alt">
+          Button
+        </Button>
+      </div>
+      <div className="pt-5">
+        <h2 className="mb-2 text-white uppercase text-xxs">Small</h2>
+        <Button size="s" theme="secondary-alt">
+          Button
+        </Button>
+      </div>
+      <div className="pt-5">
+        <h2 className="mb-2 text-white uppercase text-xxs">Base</h2>
+        <Button size="base" theme="secondary-alt">
+          Button
+        </Button>
+      </div>
+    </div>
+  );
+};
