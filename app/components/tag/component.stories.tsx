31524f16ed2c68e5803f376cf803312f426c50b0
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2020-12-16 18:52:05 +0100

    Components: tag

---
+++ app/components/tag/component.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Story } from '@storybook/react/types-6-0';
+import Tag, { TagProps } from './component';
+
+export default {
+  title: 'Components/Tag',
+  component: Tag,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['s', 'base'],
+      },
+    },
+  },
+};
+
+const Template: Story<TagProps> = ({ children, ...args }: TagProps) => (
+  <Tag {...args}>{children}</Tag>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Source name',
+  removable: true,
+};
