2a9e65808b21a5a0ba647107d1e8f633e4b747fb
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2020-12-23 10:39:38 +0100

    Map: zoom control

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
     },
