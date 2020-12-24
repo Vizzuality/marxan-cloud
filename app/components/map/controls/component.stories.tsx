a1bb6f7bf2987ed57e1ee6c9872e6dbd11ef0e82
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2020-12-24 10:33:03 +0100

    Map: control story fixes

--- app/components/map/controls/component.stories.tsx
+++ app/components/map/controls/component.stories.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Story } from '@storybook/react/types-6-0';
 
 import ZoomControl from 'components/map/controls/zoom';
+import FitBoundsControl from 'components/map/controls/fit-bounds';
 
 import Controls, { ControlsProps } from './component';
 
@@ -18,19 +19,43 @@ const Template: Story<ControlsProps> = (args) => {
   });
 
   return (
-    <Controls {...args}>
-      <ZoomControl
-        viewport={viewport}
-        onZoomChange={(zoom) => {
-          setViewport({
-            ...viewport,
-            zoom,
-          });
-        }}
-      />
-    </Controls>
+    <div className="relative h-24">
+      <Controls {...args}>
+        <ZoomControl
+          viewport={viewport}
+          onZoomChange={(zoom) => {
+            setViewport({
+              ...viewport,
+              zoom,
+            });
+          }}
+        />
+
+        <FitBoundsControl
+          bounds={{
+            bbox: [
+              10.5194091796875,
+              43.6499881760459,
+              10.9588623046875,
+              44.01257086123085,
+            ],
+            options: {
+              padding: 50,
+            },
+            viewportOptions: {
+              transitionDuration: 1500,
+            },
+          }}
+          onFitBoundsChange={(bounds) => {
+            console.info(bounds);
+          }}
+        />
+      </Controls>
+    </div>
   );
 };
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  className: 'w-10 absolute bottom-0 left-0',
+};
