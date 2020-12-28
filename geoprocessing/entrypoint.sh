0fb16f8e7304f97107ec753f3e7d5799792b5a1a
Alicia <alicia.arenzana@gmail.com>
2020-12-28 11:45:52 +0100

    added skeleton for geoprocessing

---
+++ geoprocessing/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+case "$1" in
+    develop)
+        echo "Running Development Server"
+        exec yarn start:dev
+        ;;
+    test-unit)
+        echo "Running Unit Tests"
+        exec yarn test:unit
+        ;;
+    test-e2e)
+        echo "Running e2e Tests"
+        exec yarn test:e2e
+        ;;
+    start)
+        echo "Running Start"
+        exec yarn start
+        ;;
+
+    *)
+        exec "$@"
+esac
