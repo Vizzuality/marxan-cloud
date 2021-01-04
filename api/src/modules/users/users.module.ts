d2a9a63a3bff74553fb54d1e10c63e96446b6c7a
andrea rota <andrea.rota@vizzuality.com>
2021-01-04 20:23:51 +0000

    add JSON:API serialization scaffolding to UsersController

--- api/src/modules/users/users.module.ts
+++ api/src/modules/users/users.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UsersController } from './users.controller';
@@ -6,7 +6,7 @@ import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
