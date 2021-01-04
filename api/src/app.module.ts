98c6fbe660a1bef7b776707ed9b084c4f50cb634
andrea rota <andrea.rota@vizzuality.com>
2021-01-04 20:25:34 +0000

    plug in new modules to app

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectsModule } from './modules/projects/projects.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
+  imports: [TypeOrmModule.forRoot(), ProjectsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
