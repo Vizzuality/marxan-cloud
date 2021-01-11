77d0c1f9c6edfac87156474b059d32deeb0a81a9
andrea rota <andrea.rota@vizzuality.com>
2021-01-11 19:32:19 +0000

    add login endpoint

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Logger,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  AccessToken,
+  AuthenticationService,
+} from 'modules/authentication/authentication.service';
+import { LocalAuthGuard } from 'modules/authentication/local-auth.guard';
+import { User } from 'modules/users/user.entity';
+import { inspect } from 'util';
 import { AppService } from './app.service';
+
+// Request object augmented with user data
+export interface RequestWithAuthenticatedUser extends Request {
+  user: User;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authenticationService: AuthenticationService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @Request() req: RequestWithAuthenticatedUser,
+  ): Promise<AccessToken> {
+    Logger.debug(inspect(req.user));
+    return this.authenticationService.login(req.user);
   }
 }
