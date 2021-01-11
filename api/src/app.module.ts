a77d8c872b70dfa4422e91a76a859672a9c84757
andrea rota <andrea.rota@vizzuality.com>
2021-01-11 19:32:19 +0000

    add login endpoint

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthenticationModule } from 'modules/authentication/authentication.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ProjectsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ProjectsModule,
+    UsersModule,
+    AuthenticationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
