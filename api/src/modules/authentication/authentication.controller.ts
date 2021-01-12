42ca5b22203a7d46e01b940db6850787a94dc8d7
andrea rota <andrea.rota@vizzuality.com>
2021-01-12 17:10:02 +0000

    update controller path

--- api/src/modules/authentication/authentication.controller.ts
+++ api/src/modules/authentication/authentication.controller.ts
@@ -9,7 +9,7 @@ import { ApiTags } from '@nestjs/swagger';
 
 import { AuthenticationService } from 'modules/authentication/authentication.service';
 
-@Controller('/api/v1/auth')
+@Controller('/auth')
 @ApiTags('Authentication')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
