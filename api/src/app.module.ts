dd174bc4f4fc253adbc4f6fabefe268bbe266a8a
andrea rota <andrea.rota@vizzuality.com>
2021-01-13 18:19:10 +0000

    add ping/liveness endpoint

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PingController } from './modules/ping/ping.controller';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), ProjectsModule, UsersModule],
-  controllers: [AppController],
+  controllers: [AppController, PingController],
   providers: [AppService],
 })
 export class AppModule {}
