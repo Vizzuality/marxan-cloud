c4697aecdbb7a8682da6155080b6ca4eef5d7d39
andrea rota <andrea.rota@vizzuality.com>
2021-01-19 16:38:51 +0000

    use generics for wrapper classes for JSON:API document structure
    This will not actually work because TS generics cannot be used by the
    NestJS OpenAPI plugin to figure out class shapes. We may have to
    duplicate some classes, or use some form of templating/postprocessing on
    the generated OpenAPI files.

--- api/src/modules/projects/project.api.entity.ts
+++ api/src/modules/projects/project.api.entity.ts
@@ -1,4 +1,5 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { JSONAPIData } from 'modules/countries/country.entity';
 import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from '../users/user.api.entity';
 
@@ -12,10 +13,19 @@ export class Project {
   @Column('character varying')
   name: string;
 
+  @ApiPropertyOptional()
+  @Column('character varying')
+  description: string;
+
   @ApiProperty({
     type: () => User,
     isArray: true,
   })
   @ManyToMany((_type) => User, (user) => user.projects, { eager: true })
-  users: User[];
+  users: Partial<User>[];
+}
+
+export class ProjectResult {
+  @ApiProperty()
+  data: JSONAPIData<Project>;
 }
