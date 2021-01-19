04198b3c5d94fe2d8e00feff371dd02ab034d597
Alicia <alicia.arenzana@gmail.com>
2021-01-19 12:51:15 +0100

    fixed airflow setup

--- airflow/dags/exampleDag.py
+++ airflow/dags/exampleDag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
-with DAG('example_dag', start_date=datetime(2021, 1, 1)) as dag:
+with DAG('example_dag', start_date=datetime(2016, 1, 1)) as dag:
     op = DummyOperator(task_id='op')
--- airflow/scripts/entrypoint.sh
+++ airflow/scripts/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-airflow initdb
+airflow db init
 airflow webserver
