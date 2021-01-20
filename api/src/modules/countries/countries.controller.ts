eaeed45b52c1a16156c4714a00513a5026d97538
andrea rota <andrea.rota@vizzuality.com>
2021-01-20 22:00:07 +0000

    switch plural GET endpoints to JSONAPIQueryParams decorator

--- api/src/modules/countries/countries.controller.ts
+++ api/src/modules/countries/countries.controller.ts
@@ -8,11 +8,11 @@ import {
   ApiForbiddenResponse,
   ApiOkResponse,
   ApiOperation,
-  ApiQuery,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { apiGlobalPrefixes } from 'api.config';
 import { JwtAuthGuard } from 'guards/jwt-auth.guard';
+import { JSONAPIQueryParams } from 'decorators/json-api-parameters.decorator';
 
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
@@ -33,41 +33,7 @@ export class CountriesController {
     description:
       'The current user does not have suitable permissions for this request.',
   })
-  @ApiQuery({
-    name: 'include',
-    description:
-      'A comma-separated list of relationship paths. Allows the client to customize which related resources should be returned.',
-    type: String,
-    required: false,
-  })
-  @ApiQuery({
-    name: 'fields',
-    description:
-      'A comma-separated list that refers to the name(s) of the fields to be returned. An empty value indicates that no fields should be returned.',
-    type: String,
-    required: false,
-  })
-  @ApiQuery({
-    name: 'sort',
-    description:
-      'A comma-separated list of fields of the primary data according to which the results should be sorted. Sort order is ascending unless the field name is prefixed with a minus (for descending order).',
-    type: String,
-    required: false,
-  })
-  @ApiQuery({
-    name: 'page[size]',
-    description:
-      'Page size for pagination. If not supplied, pagination with default page size of 10 elements will be applied. Specify page[size]=0 to disable pagination.',
-    type: Number,
-    required: false,
-  })
-  @ApiQuery({
-    name: 'page[number]',
-    description:
-      'Page number for pagination. If not supplied, the first page of results will be returned.',
-    type: Number,
-    required: false,
-  })
+  @JSONAPIQueryParams()
   @Get()
   async findAll(): Promise<Country[]> {
     const serializer = new JSONAPISerializer.Serializer('projects', {
