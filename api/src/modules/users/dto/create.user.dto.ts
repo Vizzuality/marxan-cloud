2a0dd81953325bd34482e31ef2e2d04580637aa8
andrea rota <andrea.rota@vizzuality.com>
2021-01-20 19:55:22 +0000

    remove nonsense note
    Salted hashed password field size has no relationship to password
    length. The note may apply to the actual (unencrypted, user-entered)
    password field in the create/update user DTOs.

--- api/src/modules/users/dto/create.user.dto.ts
+++ api/src/modules/users/dto/create.user.dto.ts
@@ -15,6 +15,16 @@ export class CreateUserDTO {
   lname: string | null;
 
   @ApiProperty()
+  /**
+   * 18 UTF-8 characters may be at most 4*18 bytes (72 bytes), which is the
+   * maximum string length that can be compared fully by bcrypt (see
+   * https://www.npmjs.com/package/bcrypt#security-issues-and-concerns).
+   *
+   * @debt I don't think we should really limit this to 18 *characters* though.
+   * If users want to set longer passphrases using mostly alphanumeric
+   * characters then they should welcome to do so, as long as the *effective*
+   * byte count of the chosen passphrase is at most 72.
+   */
   @MaxLength(18)
   password: string;
 }
