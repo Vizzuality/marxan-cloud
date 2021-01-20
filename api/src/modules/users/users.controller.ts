517c0de3e3c6fb613da17b91a400bb8f2ddfc17d
andrea rota <andrea.rota@vizzuality.com>
2021-01-20 22:00:07 +0000

    switch plural GET endpoints to JSONAPIQueryParams decorator

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -7,13 +7,13 @@ import {
   ApiBearerAuth,
   ApiForbiddenResponse,
   ApiOperation,
-  ApiQuery,
   ApiResponse,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { apiGlobalPrefixes } from 'api.config';
 import { JwtAuthGuard } from 'guards/jwt-auth.guard';
 import { RequestWithAuthenticatedUser } from 'app.controller';
+import { JSONAPIQueryParams } from 'decorators/json-api-parameters.decorator';
 
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
@@ -34,41 +34,7 @@ export class UsersController {
     description:
       'The current user does not have suitable permissions for this request.',
   })
-  @ApiQuery({
-    name: 'include',
-    description:
-      'A comma-separated list of relationship paths. Allows the client to customize which related resources should be returned.',
-    type: String,
-    required: false,
-  })
-  @ApiQuery({
-    name: 'fields',
-    description:
-      'A comma-separated list that refers to the name(s) of the fields to be returned. An empty value indicates that no fields should be returned.',
-    type: String,
-    required: false,
-  })
-  @ApiQuery({
-    name: 'sort',
-    description:
-      'A comma-separated list of fields of the primary data according to which the results should be sorted. Sort order is ascending unless the field name is prefixed with a minus (for descending order).',
-    type: String,
-    required: false,
-  })
-  @ApiQuery({
-    name: 'page[size]',
-    description:
-      'Page size for pagination. If not supplied, pagination with default page size of 10 elements will be applied. Specify page[size]=0 to disable pagination.',
-    type: Number,
-    required: false,
-  })
-  @ApiQuery({
-    name: 'page[number]',
-    description:
-      'Page number for pagination. If not supplied, the first page of results will be returned.',
-    type: Number,
-    required: false,
-  })
+  @JSONAPIQueryParams()
   @Get()
   async findAll(): Promise<User[]> {
     const serializer = new JSONAPISerializer.Serializer('users', {
