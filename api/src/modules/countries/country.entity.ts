ceff472e0376789ba2b5c9a853919236e461fee7
andrea rota <andrea.rota@vizzuality.com>
2021-01-20 19:50:14 +0000

    update entities and create DTOs

--- api/src/modules/countries/country.entity.ts
+++ api/src/modules/countries/country.entity.ts
@@ -1,6 +1,24 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
+import { Dictionary } from 'lodash';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+export interface LocalName {
+  /**
+   * Local name of a country.
+   *
+   * E.g. "Italia"
+   */
+  name: string;
+
+  /**
+   * Locale code for this name, composed of the dash-separated two-letter
+   * ISO-639 language code and the two-letter ISO 3166-1 alpha2 code.
+   *
+   * E.g. "it-IT"
+   */
+  locale: string;
+}
 
 @Entity('countries')
 export class Country {
@@ -13,6 +31,10 @@ export class Country {
   @Column('character varying')
   @Transform((_) => fakerStatic.address.country())
   name: string;
+
+  @ApiPropertyOptional()
+  @Column('jsonb')
+  localNames: Dictionary<LocalName>;
 }
 
 export class JSONAPIData<Entity> {
--- api/src/modules/countries/dto/create.country.dto.ts
+++ api/src/modules/countries/dto/create.country.dto.ts
@@ -1,9 +1,25 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Dictionary } from 'lodash';
+import { LocalName } from '../country.entity';
 
 export class CreateCountryDTO {
+  /**
+   * ISO 3166-1 alpha2 code
+   */
   @ApiProperty()
   id: string;
 
+  /**
+   * English name
+   */
   @ApiProperty()
   name: string;
+
+  /**
+   * A dictionary of (name, locale) tuples.
+   *
+   * E.g. ('Italia', 'it-IT')
+   */
+  @ApiPropertyOptional()
+  localNames: Dictionary<LocalName>;
 }
