1313244983aa63f9fe7f145bd70fe60b32173ee6
Alicia <alicia.arenzana@gmail.com>
2021-01-22 16:39:22 +0100

    added file for metadata migration setup in api db

---
+++ api/src/migrations/api/1611329857558-initialDataMetadataEntities.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class initialDataMetadataEntities1611329857558 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(``)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(``)
+    }
+
+}
