8bd40da94545e2524a50c0fb2e95771e654f8dcf
Alicia <alicia.arenzana@gmail.com>
2021-01-26 10:02:03 +0100

    finish renaming stuff

--- api/src/migrations/geoprocessing/1611221157285-initialGeoDBSetup.ts
+++ api/src/migrations/geoprocessing/1611221157285-initialGeoDBSetup.ts
@@ -119,9 +119,9 @@ export class initialGeoDBSetup1611221157285 implements MigrationInterface {
       -- Planning units usage by scenario.
       CREATE TABLE "scenarios_pu_data" (
         "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-        "pu_geomid" uuid NOT NULL REFERENCES "planning_units_geom" ("id"),
+        "pu_geom_id" uuid NOT NULL REFERENCES "planning_units_geom" ("id"),
         "scenario_id" uuid NOT NULL,
-        "puid" int NOT NULL,
+        "pu_id" int NOT NULL,
         "lockin_status" int,
         "xloc" float8,
         "yloc" float8,
@@ -137,7 +137,7 @@ export class initialGeoDBSetup1611221157285 implements MigrationInterface {
 
       CREATE TABLE "output_results_data" (
         "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-        "puid" int REFERENCES "scenarios_pu_data" ("puid"),
+        "puid" int REFERENCES "scenarios_pu_data" ("pu_id"),
         "scenario_id" uuid,
         "run_id" uuid,
         "value" float8,
