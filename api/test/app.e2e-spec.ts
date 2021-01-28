b11d42d91cc35a2057fb2a2ef91aa0f983df63db
andrea rota <andrea.rota@vizzuality.com>
2021-01-28 19:09:36 +0000

    add some initial e2e tests

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -15,7 +15,81 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('🐠');
+  afterEach(async () => {
+    await Promise.all([app.close()]);
+  });
+
+  describe('Authentication', () => {
+    let jwtToken: string;
+
+    it('Retrieves a JWT token when authenticating with valid credentials', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({
+          username: 'aa@example.com',
+          password: 'aauserpassword',
+        })
+        .expect(201);
+
+      jwtToken = response.body.accessToken;
+    });
+
+    it('Fails to authenticate a user with an incorrect password', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'test@example.com', password: 'wrong' })
+        .expect(401);
+
+      expect(response.body.accessToken).not.toBeDefined();
+    });
+
+    it('Fails to authenticate a non-existing user', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'test@example.com', password: 'wrong' })
+        .expect(401);
+
+      expect(response.body.accessToken).not.toBeDefined();
+    });
+
+    it('Gets projects', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/v1/projects')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .expect(200);
+
+      const resources = response.body.data;
+
+      expect(resources[0].type).toBe('projects');
+    });
+
+    let anOrganization: { id: string, type: 'organizations' };
+
+    it('Gets organizations', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/v1/organizations')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .expect(200);
+
+      const resources = response.body.data;
+      anOrganization = resources[0];
+      expect(resources[0].type).toBe('organizations');
+    });
+
+    it('Creates a project', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/v1/projects')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .send({
+          name: 'Test Project 1234',
+          description: 'Description for this awesome project',
+          organizationId: anOrganization.id,
+        })
+        .expect(201);
+
+      const resources = response.body.data;
+
+      expect(resources[0].type).toBe('projects');
+    });
   });
 });
