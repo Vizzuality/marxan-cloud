b5e235761b236dab34655b36b0b46f745d2126fa
andrea rota <andrea.rota@vizzuality.com>
2021-01-29 15:19:53 +0000

    add @debt note

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -12,6 +12,11 @@ case "$1" in
         ;;
     test-e2e)
         echo "Running e2e Tests"
+        # @debt This feels a bit hacky. It slows down the tests, and couples
+        # this script to an Alpine-based image. We should install
+        # `postgresql-client` during image build (in the Dockerfile), maybe via
+        # a `RUN` directive with a conditional shell statement that only
+        # installs the package if a specific build argument or env var is set.
         apk update && apk add postgresql-client
         yarn typeorm migration:run
         psql $API_POSTGRES_URL < ./test/fixtures/test-data.sql
