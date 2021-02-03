780b55056a5333e21c2fc899f7e9e8803dbc9fdb
mluena <maluenarod@gmail.com>
2021-02-03 12:00:41 +0100

    search component using react-aria

--- app/components/search/component.stories.tsx
+++ app/components/search/component.stories.tsx
@@ -8,6 +8,25 @@ import Search, { SearchProps } from './component';
 export default {
   title: 'Components/Search',
   component: Search,
+  argTypes: {
+    theme: {
+      control: {
+        type: 'select',
+        options: ['dark', 'light'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['sm', 'base'],
+      },
+    },
+    labelRef: {
+      control: {
+        disable: true,
+      },
+    },
+  },
 };
 
 const Template: Story<SearchProps> = ({ ...args }: SearchProps) => {
@@ -21,21 +40,15 @@ const Template: Story<SearchProps> = ({ ...args }: SearchProps) => {
 
   return (
     <>
-      <Label ref={labelRef} id="slider-component" className="uppercase sr-only">
+      <Label ref={labelRef} id="search-component" className="uppercase sr-only">
         Search
       </Label>
-      <Search {...args} value={value} onChange={onChange} />
+      <Search id="search-component" labelRef={labelRef} value={value} onChange={onChange} {...args} />
     </>
   );
 };
 
 export const Default = Template.bind({});
 Default.args = {
-  label: 'Search',
   placeholder: 'Search by feature, planning area name...',
-  labelRef: {
-    control: {
-      disable: true,
-    },
-  },
 };
