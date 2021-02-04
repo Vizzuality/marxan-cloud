45f0d167389ea01b16ff6e74f4dd72559361816e
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-02-04 13:54:28 +0100

    fix Table storybook example data

--- app/components/table/component.stories.tsx
+++ app/components/table/component.stories.tsx
@@ -26,8 +26,14 @@ Default.args = {
       },
     ],
     rows: [
-      [{ label: 'Row 1 Value 1', value: 1 }, { label: 'Row 1 Value 2', value: 2 }],
-      [{ label: 'Row 2 Value 1', value: 3 }, { label: 'Row 2 Value 2', value: 4 }],
+      {
+        items: [{ label: 'Row 1 Value 1', value: 1 }, { label: 'Row 1 Value 2', value: 2 }],
+        id: 'row1',
+      },
+      {
+        id: 'row2',
+        items: [{ label: 'Row 2 Value 1', value: 3 }, { label: 'Row 2 Value 2', value: 4 }],
+      },
     ],
   },
 };
