edf71dd0be44a9fef7f8c64cb1ab19eb5a0f0aeb
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-02-05 09:41:32 +0100

    Some styles changes plus cell+row selectable feature

--- app/components/table/component.tsx
+++ app/components/table/component.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
 
-export interface TableDataItem {
+export interface LabelValue {
   label: string;
   value: Object;
 }
 
-export interface TableHeaderItem {
-  label: string;
-  value: Object;
+export interface TableDataItem extends LabelValue {
+  selectable: boolean
+}
+
+export interface TableHeaderItem extends LabelValue {
+  selectable: boolean
 }
 
 export interface TableRow {
@@ -16,39 +19,74 @@ export interface TableRow {
   items: TableDataItem[]
 }
 
-export interface TableData {
-  headers: TableHeaderItem[],
-  rows: TableRow[]
-}
-
 export interface TableProps {
-  data: TableData;
+  headers: TableHeaderItem[],
+  body: TableRow[],
+  rowSelectable: boolean,
+  cellSelectable: boolean,
   className?: string;
 }
 
 export const Table: React.FC<TableProps> = ({
-  data,
+  headers,
+  body,
+  rowSelectable,
+  cellSelectable,
   className,
-}: TableProps) => (
-  <table
-    className={cx({
-      '': true,
-      [className]: !!className,
-    })}
-  >
-    <thead>
-      {data.headers.map((header) => <th key={`header-${header.value}`}>{header.label}</th>)}
-    </thead>
-    <tbody>
-      {data.rows.map((row) => (
-        <tr key={row.id}>
-          {
-                        row.items.map((item) => <td key={`td-${item.value}`}>{item.label}</td>)
-                    }
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+}: TableProps) => {
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [selectedCell, setSelectedCell] = useState(null);
+
+  return (
+    <table
+      className={cx({
+        '': true,
+        [className]: !!className,
+      })}
+      role="grid"
+    >
+      <thead>
+        {headers.map((header) => <th key={`header-${header.value}`}>{header.label}</th>)}
+      </thead>
+      <tbody>
+        {body.map((row, rowIndex) => {
+          const rowIsSelected = selectedRow === row.id;
+          return (
+            <tr
+              key={row.id}
+              className={cx({
+                'bg-gray-100': (!rowIsSelected || cellSelectable) && (rowIndex + 1) % 2 === 0,
+                'bg-primary-500': rowIsSelected && !cellSelectable,
+                'hover:cursor-pointer': rowSelectable,
+              })}
+              onClick={() => setSelectedRow(row.id)}
+            >
+              {
+                row.items.map((item) => {
+                  const cellIsSelected = selectedCell === item.value && selectedRow === row.id;
+                  const handleCellClick = () => cellSelectable && setSelectedCell(item.value);
+                  return (
+                    <td
+                      key={`td-${item.value}`}
+                      className={cx({
+                        'bg-primary-500': cellIsSelected,
+                        'hover:cursor-pointer': cellSelectable,
+                      })}
+                      onClick={handleCellClick}
+                      onKeyPress={handleCellClick}
+                      role="gridcell"
+                    >
+                      {item.label}
+                    </td>
+                  );
+                })
+              }
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
