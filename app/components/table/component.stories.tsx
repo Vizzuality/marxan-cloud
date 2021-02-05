379149303ce605cadf91ea50b3621caef88f4553
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-02-05 09:41:32 +0100

    Some styles changes plus cell+row selectable feature

--- app/components/table/component.stories.tsx
+++ app/components/table/component.stories.tsx
@@ -14,26 +14,38 @@ const Template: Story<TableProps> = ({ ...args }: TableProps) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  data: {
-    headers: [
-      {
-        label: 'Header A',
-        value: 'a',
-      },
-      {
-        label: 'Header B',
-        value: 'b',
-      },
-    ],
-    rows: [
-      {
-        items: [{ label: 'Row 1 Value 1', value: 1 }, { label: 'Row 1 Value 2', value: 2 }],
-        id: 'row1',
-      },
-      {
-        id: 'row2',
-        items: [{ label: 'Row 2 Value 1', value: 3 }, { label: 'Row 2 Value 2', value: 4 }],
-      },
-    ],
-  },
+  headers: [
+    {
+      label: 'Run',
+      value: 'run',
+    },
+    {
+      label: 'Score',
+      value: 'score',
+    },
+    {
+      label: 'Cost',
+      value: 'cost',
+    },
+  ],
+  body: [
+    {
+      items: [{ label: '1', value: 1 }, { label: '170', value: 170 }, { label: '168.0', value: 168 }],
+      id: 'row1',
+    },
+    {
+      id: 'row2',
+      items: [{ label: '2', value: 1 }, { label: '130', value: 130 }, { label: '168.0', value: 168 }],
+    },
+    {
+      id: 'row3',
+      items: [{ label: '3', value: 1 }, { label: '190', value: 190 }, { label: '208.0', value: 208 }],
+    },
+    {
+      id: 'row4',
+      items: [{ label: '4', value: 1 }, { label: '150', value: 150 }, { label: '188.0', value: 188 }],
+    },
+  ],
+  rowSelectable: true,
+  cellSelectable: false,
 };
