2706cc4c9d65236a848d4dd95424e2f666e6d48b
andrea rota <andrea.rota@vizzuality.com>
2021-02-05 20:48:23 +0000

    fix lint warnings/errors

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -52,5 +52,45 @@ describe('AppController (e2e)', () => {
 
       expect(response.body.accessToken).not.toBeDefined();
     });
+
+    it('Gets projects', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/v1/projects')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .expect(200);
+
+      const resources = response.body.data;
+
+      expect(resources[0].type).toBe('projects');
+    });
+
+    let anOrganization: { id: string; type: 'organizations' };
+
+    it('Gets organizations', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/v1/organizations')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .expect(200);
+
+      const resources = response.body.data;
+      anOrganization = resources[0];
+      expect(resources[0].type).toBe('organizations');
+    });
+
+    it('Creates a project', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/v1/projects')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .send({
+          name: 'Test Project 1234',
+          description: 'Description for this awesome project',
+          organizationId: anOrganization.id,
+        })
+        .expect(201);
+
+      const resources = response.body.data;
+
+      expect(resources[0].type).toBe('projects');
+    });
   });
 });
