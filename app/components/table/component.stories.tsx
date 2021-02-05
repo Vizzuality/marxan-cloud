4d10bf5db62f6a9a241853c54f5424492975e359
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-02-05 12:12:08 +0100

    styles improvements, new approach for custom appearance

--- app/components/table/component.stories.tsx
+++ app/components/table/component.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Story } from '@storybook/react/types-6-0';
-import Table, { TableProps } from './component';
+import Button from 'components/button';
+import Icon from 'components/icon';
+import STAR_SVG from 'svgs/ui/star.svg?sprite';
+import Table, { TableProps, TableRow, LabelValue } from './component';
 
 export default {
   title: 'Components/Table',
@@ -16,16 +19,39 @@ export const Default = Template.bind({});
 Default.args = {
   headers: [
     {
-      label: 'Run',
-      value: 'run',
+      label: 'Best',
+      id: 'best',
+      customCell: (row: LabelValue) => {
+        if (row.value) return <Icon className="w-3 h-3" icon={STAR_SVG} />;
+        return '';
+      },
+    },
+    {
+      label: 'RUN',
+      id: 'run',
     },
     {
       label: 'Score',
-      value: 'score',
+      id: 'score',
     },
     {
       label: 'Cost',
-      value: 'cost',
+      id: 'cost',
+    },
+    {
+      label: 'View on map',
+      id: 'view-on-map',
+      customCell: function selectSolutionButton({ isSelected }: TableRow) {
+        return (
+          <Button
+            theme="secondary-alt"
+            size="s"
+            className="flex justify-center w-full"
+          >
+            {isSelected ? 'Selected' : 'Select solution'}
+          </Button>
+        );
+      },
     },
   ],
   body: [
@@ -33,24 +59,32 @@ Default.args = {
       run: { label: '1', value: 1 },
       score: { label: '170', value: 170 },
       cost: { label: '168.0', value: 168 },
+      'view-on-map': { label: '-', value: '-' },
+      best: { label: '-', value: false },
       id: 'row1',
     },
     {
       run: { label: '1', value: 1 },
       score: { label: '170', value: 170 },
       cost: { label: '168.0', value: 168 },
+      'view-on-map': { label: '-', value: '-' },
+      best: { label: '-', value: false },
       id: 'row2',
     },
     {
       run: { label: '1', value: 1 },
       score: { label: '170', value: 170 },
       cost: { label: '168.0', value: 168 },
+      'view-on-map': { label: '-', value: '-' },
+      best: { label: '-', value: true },
       id: 'row3',
     },
     {
       run: { label: '1', value: 1 },
       score: { label: '170', value: 170 },
       cost: { label: '168.0', value: 168 },
+      'view-on-map': { label: '-', value: '-' },
+      best: { label: '-', value: false },
       id: 'row4',
     },
   ],
