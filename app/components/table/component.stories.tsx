aac6be205b47e0f84d9af8eaec4c39ffeae64aeb
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-02-08 11:22:41 +0100

    most comments from PR review addressed here

--- app/components/table/component.stories.tsx
+++ app/components/table/component.stories.tsx
@@ -3,7 +3,8 @@ import { Story } from '@storybook/react/types-6-0';
 import Button from 'components/button';
 import Icon from 'components/icon';
 import STAR_SVG from 'svgs/ui/star.svg?sprite';
-import Table, { TableProps, TableRow, LabelValue } from './component';
+import Table from './component';
+import { TableProps, TableRow } from './types';
 
 export default {
   title: 'Components/Table',
@@ -21,8 +22,8 @@ Default.args = {
     {
       label: 'Best',
       id: 'best',
-      customCell: (row: LabelValue) => {
-        if (row.value) return <Icon className="w-3 h-3" icon={STAR_SVG} />;
+      customCell: (value) => {
+        if (value) return <Icon className="w-3 h-3" icon={STAR_SVG} />;
         return '';
       },
     },
@@ -41,7 +42,7 @@ Default.args = {
     {
       label: 'View on map',
       id: 'view-on-map',
-      customCell: function selectSolutionButton({ isSelected }: TableRow) {
+      customCell: function selectSolutionButton(value, { isSelected } : TableRow) {
         return (
           <Button
             theme="secondary-alt"
@@ -56,35 +57,35 @@ Default.args = {
   ],
   body: [
     {
-      run: { label: '1', value: 1 },
-      score: { label: '170', value: 170 },
-      cost: { label: '168.0', value: 168 },
-      'view-on-map': { label: '-', value: '-' },
-      best: { label: '-', value: false },
+      run: 1,
+      score: 170,
+      cost: 168,
+      'view-on-map': false,
+      best: false,
       id: 'row1',
     },
     {
-      run: { label: '1', value: 1 },
-      score: { label: '170', value: 170 },
-      cost: { label: '168.0', value: 168 },
-      'view-on-map': { label: '-', value: '-' },
-      best: { label: '-', value: false },
+      run: 1,
+      score: 170,
+      cost: 168,
+      'view-on-map': false,
+      best: false,
       id: 'row2',
     },
     {
-      run: { label: '1', value: 1 },
-      score: { label: '170', value: 170 },
-      cost: { label: '168.0', value: 168 },
-      'view-on-map': { label: '-', value: '-' },
-      best: { label: '-', value: true },
+      run: 1,
+      score: 170,
+      cost: 168,
+      'view-on-map': false,
+      best: true,
       id: 'row3',
     },
     {
-      run: { label: '1', value: 1 },
-      score: { label: '170', value: 170 },
-      cost: { label: '168.0', value: 168 },
-      'view-on-map': { label: '-', value: '-' },
-      best: { label: '-', value: false },
+      run: 1,
+      score: 170,
+      cost: 168,
+      'view-on-map': false,
+      best: false,
       id: 'row4',
     },
   ],
