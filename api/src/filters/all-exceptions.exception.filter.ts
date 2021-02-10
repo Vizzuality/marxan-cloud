57b97629308eb02cc7b40a6d57ed30c4f05ea42d
andrea rota <andrea.rota@vizzuality.com>
2021-02-10 13:27:21 +0000

    add error details/stack trace to error responses

--- api/src/filters/all-exceptions.exception.filter.ts
+++ api/src/filters/all-exceptions.exception.filter.ts
@@ -4,24 +4,47 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
+import * as config from 'config';
 
-@Catch()
+/**
+ * Catch-all exception filter.
+ *
+ *
+ */
+@Catch(Error)
 export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
+  catch(exception: Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
+
+    Logger.error(`Error: ${JSON.stringify(exception, null, 2)}`);
 
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
+    // In development environments only, add full error details to the response.
+    const errorDetails =
+      config.util.getEnv('NODE_ENV') === 'development'
+        ? {
+            type: Object.getPrototypeOf(exception)?.name,
+            raw: exception,
+            stack: exception.stack,
+          }
+        : undefined;
+
     response.status(status).json({
       statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
+      error: {
+        message: exception.message,
+        ...errorDetails,
+      },
     });
   }
 }
