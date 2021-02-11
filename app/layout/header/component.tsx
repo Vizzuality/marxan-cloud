e614fc33d6310c7da831b3fe0702d1e651ec97ca
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-02-11 18:02:27 +0100

    Athentication system: Header user component

--- app/layout/header/component.tsx
+++ app/layout/header/component.tsx
@@ -4,15 +4,14 @@ import cx from 'classnames';
 import Link from 'next/link';
 
 import Wrapper from 'layout/wrapper';
+import User from 'layout/header/user';
 
 import Icon from 'components/icon';
 import Button from 'components/button';
-import Avatar from 'components/avatar';
 
 import { useAuth } from 'hooks/authentication';
 
 import LOGO_SVG from 'svgs/logo.svg?sprite';
-import ARROW_DOWN_SVG from 'svgs/ui/arrow-down.svg?sprite';
 import { useRouter } from 'next/router';
 
 export interface HeaderProps {
@@ -32,6 +31,7 @@ export const Header: React.FC<HeaderProps> = ({ size }:HeaderProps) => {
   const auth = useAuth();
   const router = useRouter();
 
+  // Events
   const onClickSignIn = useCallback(() => {
     router.push('/sign-in');
   }, [router]);
@@ -61,15 +61,7 @@ export const Header: React.FC<HeaderProps> = ({ size }:HeaderProps) => {
             </Link>
 
             {auth.user && (
-              <button
-                type="button"
-                className="flex items-center justify-start"
-              >
-                <Avatar className="text-sm text-white uppercase bg-primary-700">
-                  MB
-                </Avatar>
-                <Icon icon={ARROW_DOWN_SVG} className="w-2.5 h-2.5 text-white" />
-              </button>
+              <User />
             )}
 
             {!auth.user && (
