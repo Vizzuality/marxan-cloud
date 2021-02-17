058ff5532c07e662f9a5840c44924d25751b381d
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-02-17 13:14:49 +0100

    Structure: project pages

---
+++ app/pages/projects/[pid]/edit.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Header from 'layout/header';
+import Protected from 'layout/protected';
+
+const EditProjectsPage: React.FC = () => {
+  return (
+    <Protected>
+      <Head>
+        <title>Edit Project</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header size="base" />
+      </main>
+    </Protected>
+  );
+};
+
+export default EditProjectsPage;
