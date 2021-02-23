8c2f1a40e99e8dd7ae25eb764cba1d763ef00990
andrea rota <andrea.rota@vizzuality.com>
2021-02-23 14:56:55 +0000

    rename auth tests, split off projects tests

--- api/test/auth.e2e-spec.ts
+++ api/test/auth.e2e-spec.ts
@@ -21,18 +21,14 @@ describe('AppController (e2e)', () => {
   });
 
   describe('Authentication', () => {
-    let jwtToken: string;
-
     it('Retrieves a JWT token when authenticating with valid credentials', async () => {
-      const response = await request(app.getHttpServer())
+      await request(app.getHttpServer())
         .post('/auth/sign-in')
         .send({
           username: E2E_CONFIG.users.aa.username,
           password: E2E_CONFIG.users.aa.password,
         })
         .expect(201);
-
-      jwtToken = response.body.accessToken;
     });
 
     it('Fails to authenticate a user with an incorrect password', async () => {
@@ -51,46 +47,6 @@ describe('AppController (e2e)', () => {
         .expect(401);
 
       expect(response.body.accessToken).not.toBeDefined();
-    });
-
-    it('Gets projects', async () => {
-      const response = await request(app.getHttpServer())
-        .get('/api/v1/projects')
-        .set('Authorization', `Bearer ${jwtToken}`)
-        .expect(200);
-
-      const resources = response.body.data;
-
-      expect(resources[0].type).toBe('projects');
-    });
-
-    let anOrganization: { id: string; type: 'organizations' };
-
-    it('Gets organizations', async () => {
-      const response = await request(app.getHttpServer())
-        .get('/api/v1/organizations')
-        .set('Authorization', `Bearer ${jwtToken}`)
-        .expect(200);
-
-      const resources = response.body.data;
-      anOrganization = resources[0];
-      expect(resources[0].type).toBe('organizations');
-    });
-
-    it('Creates a project', async () => {
-      const response = await request(app.getHttpServer())
-        .post('/api/v1/projects')
-        .set('Authorization', `Bearer ${jwtToken}`)
-        .send({
-          name: 'Test Project 1234',
-          description: 'Description for this awesome project',
-          organizationId: anOrganization.id,
-        })
-        .expect(201);
-
-      const resources = response.body.data;
-
-      expect(resources.type).toBe('projects');
     });
   });
 });
