5106d430135099083b7e782bfd010323c2af60c8
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-02-23 16:43:02 +0100

    Tooltip: omponent and story

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -2,11 +2,6 @@ import React from 'react';
 
 import { themes } from '@storybook/theming';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { AuthorizationProvider } from 'hooks/authentication';
-
-const queryClient = new QueryClient();
-
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   docs: {
@@ -17,11 +12,9 @@ export const parameters = {
 export const decorators = [
   (Story) => {
     return (
-      <QueryClientProvider client={queryClient}>
-        <AuthorizationProvider>
-          {Story()}
-        </AuthorizationProvider>
-      </QueryClientProvider>
+      <div>
+        {Story()}
+      </div>
     )
   },
 ]
