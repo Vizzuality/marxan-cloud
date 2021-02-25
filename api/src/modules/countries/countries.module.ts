0a93ddf6ff05d532ec4165e7abfc56490ac9a5b4
andrea rota <andrea.rota@vizzuality.com>
2021-02-25 16:14:34 +0000

    finish moving Country entity to geoprocessingDB connection

--- api/src/modules/countries/countries.module.ts
+++ api/src/modules/countries/countries.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CountriesController } from './countries.controller';
-import { Country } from './country.api.entity';
+import { Country } from './country.geo.entity';
 import { CountriesService } from './countries.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Country])],
+  imports: [TypeOrmModule.forFeature([Country], 'geoprocessingDB')],
   providers: [CountriesService],
   controllers: [CountriesController],
   exports: [CountriesService],
