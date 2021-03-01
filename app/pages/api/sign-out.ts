c7e94c93c2df64ea37c04fadff276def92fdea50
David Inga <david.inga@vizzuality.com>
2021-03-01 13:57:15 +0100

    invalidate token when user sign-out

--- app/pages/api/sign-out.ts
+++ app/pages/api/sign-out.ts
@@ -1,7 +1,31 @@
+import { getLoginSession } from 'auth';
 import { removeTokenCookie } from 'auth/cookies';
+import AUTHENTICATION from 'services/authentication';
 
 export default async function signout(req, res) {
-  removeTokenCookie(res);
-  res.writeHead(302, { Location: '/' });
+  const { token } = await getLoginSession(req);
+
+  try {
+    // First, invalidate session token on the API
+    await AUTHENTICATION.request({
+      url: '/sign-out',
+      method: 'POST',
+      data: null,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    // Second, Remove local cookie
+    removeTokenCookie(res);
+
+    // Then redirect to landing page
+    res.writeHead(302, { Location: '/' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).end(error.message);
+  }
+
   res.end();
 }
