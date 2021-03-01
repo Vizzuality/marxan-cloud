9158d556063e87fcf66884e6248d7a97f4259034
David Inga <david.inga@vizzuality.com>
2021-03-01 13:34:29 +0100

    sync expiration date from api token

--- app/auth/cookies.ts
+++ app/auth/cookies.ts
@@ -2,12 +2,9 @@ import { serialize, parse } from 'cookie';
 
 const TOKEN_NAME = 'user';
 
-export const MAX_AGE = 60 * 60 * 8; // 8 hours
-
-export function setTokenCookie(res, token) {
+export function setTokenCookie(res, token, options) {
   const cookie = serialize(TOKEN_NAME, token, {
-    maxAge: MAX_AGE,
-    expires: new Date(Date.now() + MAX_AGE * 1000),
+    ...options,
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     path: '/',
