bb16a772e4702a6b222f11107d13c3416f405882
Clément Prod'homme <contact@clementprodhomme.fr>
2021-03-02 12:03:02 +0100

    Update the Modal component to use React Aria

--- app/components/modal/component.stories.tsx
+++ app/components/modal/component.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Story } from '@storybook/react/types-6-0';
 
 import Button from 'components/button';
@@ -8,43 +8,28 @@ export default {
   title: 'Components/Modal',
   component: Modal,
   parameters: { actions: { argTypesRegex: '^on.*' } },
-  argTypes: {
-    open: {
-      control: {
-        disable: true,
-      },
-    },
-  },
+  argTypes: {},
 };
 
-const Template: Story<ModalProps> = ({ ...args }: ModalProps) => {
-  const [open, setOpen] = useState(false);
-
-  const onOpen = () => setOpen(true);
-  const onClose = (e) => {
-    setOpen(false);
-    args.onClose(e);
-  };
-
-  return (
-    <>
-      <Button theme="primary" size="base" onClick={onOpen}>
+const Template: Story<ModalProps> = ({ ...args }: ModalProps) => (
+  <Modal
+    {...args}
+    trigger={(
+      <Button theme="primary" size="base">
         Open modal
       </Button>
-      <Modal {...args} open={open} onClose={onClose}>
-        <h1 className="mb-5 text-xl font-medium">Modal content</h1>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
-        sodales est, eu imperdiet elit. Suspendisse eget diam accumsan, lacinia
-        odio nec, fringilla ex. Quisque consectetur diam in massa egestas, vitae
-        posuere magna semper. Sed ac iaculis purus, at pretium tellus. Duis non
-        commodo lorem, non tincidunt ex.
-      </Modal>
-    </>
-  );
-};
+    )}
+  >
+    <h1 className="mb-5 text-xl font-medium">Modal content</h1>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
+    sodales est, eu imperdiet elit. Suspendisse eget diam accumsan, lacinia
+    odio nec, fringilla ex. Quisque consectetur diam in massa egestas, vitae
+    posuere magna semper. Sed ac iaculis purus, at pretium tellus. Duis non
+    commodo lorem, non tincidunt ex.
+  </Modal>
+);
 
-export const Default = Template.bind({});
+export const Default: Story<ModalProps> = Template.bind({});
 Default.args = {
   title: 'Modal component',
-  open: false,
 };
