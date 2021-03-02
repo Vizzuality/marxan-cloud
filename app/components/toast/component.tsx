bed3e49dbb416f6d8c6ee85c1d7499ad60f91296
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-03-02 18:06:19 +0100

    Notifications: toast examples on the app

--- app/components/toast/component.tsx
+++ app/components/toast/component.tsx
@@ -87,7 +87,7 @@ export const Toast: React.FC<ToastProps> = ({
         })}
       >
         <div
-          className="flex w-full p-2 text-gray-500 bg-white rounded-2xl"
+          className="flex w-full p-2 text-gray-500 bg-white shadow-md rounded-2xl"
           onMouseEnter={() => {
             controls.stop();
           }}
