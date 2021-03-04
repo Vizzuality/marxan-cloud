5c9979bfce47ee322d36264fb444728bb276f88e
andrea rota <andrea.rota@vizzuality.com>
2021-03-04 15:54:06 +0000

    remove indexes and field which will not be needed

--- geoprocessing/src/migrations/geoprocessing/1614256630000-AddCountriesView.ts
+++ geoprocessing/src/migrations/geoprocessing/1614256630000-AddCountriesView.ts
@@ -4,11 +4,8 @@ export class AddCountriesView1614256630000
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-CREATE UNIQUE INDEX only_countries_by_gid ON admin_regions(gid_0) WHERE (gid_0 IS NOT NULL AND gid_1 IS NULL AND gid_2 IS NULL);
-CREATE UNIQUE INDEX only_countries_by_name ON admin_regions(name_0) WHERE (name_0 IS NOT NULL AND name_1 IS NULL AND name_2 IS NULL);
 CREATE VIEW countries AS (
   SELECT
-    ogc_fid,
     gid_0,
     name_0,
     the_geom
