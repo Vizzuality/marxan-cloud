d367e4cdcb5c52f141f7f4139c2f99fbf1d22888
elpamart <elena.palao@vizzuality.com>
2021-03-10 09:40:45 +0100

    first steps on mvt scaffolding

---
+++ geoprocessing/src/modules/vector-tile/vector-tile.service.ts
@@ -0,0 +1,9 @@
+// to-do: work on cache later
+//import { Cache, defaultCacheOptions} from './cache';
+import { createQueryForTile } from '';
+
+//to-do: add cache options in params
+export async function VectorTileServer<T>({
+  maxZoomLevel = 12,
+  attributes = [],
+});
