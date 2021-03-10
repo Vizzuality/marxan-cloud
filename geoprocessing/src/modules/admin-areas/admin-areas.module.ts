9f620729a8cf251c40bbb50475c74cc4f679b57e
elpamart <elena.palao@vizzuality.com>
2021-03-10 09:40:45 +0100

    first steps on mvt scaffolding

--- geoprocessing/src/modules/admin-areas/admin-areas.module.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.module.ts
@@ -1,18 +1,20 @@
-import { Logger, Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { HttpModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminAreasController } from './admin-areas.controller';
-//import { AdminArea } from './admin-areas.geo.entity';
+import { AdminArea } from './admin-area.geo.entity';
 import { AdminAreasService } from './admin-areas.service';
-import { TileModule } from 'modules/tile/tile.module';
-
-const logger = new Logger('admin-areas');
-
-logger.debug(TileModule);
+import { VectorTileModule } from 'modules/vector-tile/vector-tile.service';
 
 @Module({
-  imports: [TileModule],
+  imports: [
+    TypeOrmModule.forFeature([AdminArea], 'geoprocessingDB'),
+    HttpModule.register({
+      maxContentLength: 100000000,
+    }),
+    VectorTileModule,
+  ],
   providers: [AdminAreasService],
   controllers: [AdminAreasController],
+  exports: [AdminAreasService],
 })
 export class AdminAreasModule {}
