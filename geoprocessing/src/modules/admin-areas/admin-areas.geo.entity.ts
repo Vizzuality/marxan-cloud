e02b56ef859d786693c1bdf9e9f47ffff8ed4139
elpamart <elena.palao@vizzuality.com>
2021-03-10 09:40:45 +0100

    first steps on mvt scaffolding

--- geoprocessing/src/modules/admin-areas/admin-areas.geo.entity.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.geo.entity.ts
@@ -1,12 +1,9 @@
-/**
- * @todo We are replicating the same code that we have in the api. If we update something here we should also replicate it in the api side.
- */
 import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 
-
+//to-do - extend to country
 @Entity('admin-regions')
-export class AdminArea {
+export class AdminAreas {
   /**
    * Country id (ISO 3166-1 alpha-3).
    */
@@ -55,4 +52,17 @@ export class AdminArea {
   @ApiProperty()
   @Column('geometry', { name: 'the_geom' })
   theGeom: any;
+}
+
+export class JSONAPIAdministrativeAreasData {
+  @ApiProperty()
+  type = 'administrative-areas';
+
+  @ApiProperty()
+  attributes: AdminAreas;
+}
+
+export class AdminAreasResult {
+  @ApiProperty()
+  data: JSONAPIAdministrativeAreasData;
 }
