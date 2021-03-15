83cea8bbe9eac024a0d697e16032fc0782efa447
Alicia <alicia.arenzana@gmail.com>
2021-03-15 14:25:00 +0100

    WIP

--- geoprocessing/api.config.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Global prefixes for API routes
- *
- * If adding new API versions, new prefixes should be added here and used in
- * the relevant controllers.
- */
-export const apiGlobalPrefixes = {
-  v1: '/api/v1/geodata',
-};
--- geoprocessing/src/app.module.ts
+++ geoprocessing/src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module, Logger } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { geoprocessingConnections } from './ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+<<<<<<< HEAD
 import { AdminAreasModule } from 'src/modules/admin-areas/admin-areas.module';
+=======
+>>>>>>> WIP
 import { PlanningUnitsModule } from 'src/modules/planning-units/planning-units.module';
+import { AdminAreasModule } from 'src/modules/admin-areas/admin-areas.module';
 
 @Module({
   imports: [
@@ -18,6 +22,7 @@ import { PlanningUnitsModule } from 'src/modules/planning-units/planning-units.m
     }),
     AdminAreasModule,
     PlanningUnitsModule,
+    AdminAreasModule,
   ],
   controllers: [AppController],
   providers: [AppService],
