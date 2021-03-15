5cfe85b27f1a76df6261287c88c738154647f7d2
Alicia <alicia.arenzana@gmail.com>
2021-03-15 14:25:00 +0100

    WIP

--- geoprocessing/src/ormconfig.ts
+++ geoprocessing/src/ormconfig.ts
@@ -1,9 +1,5 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-<<<<<<< HEAD
-import { AppConfig } from 'src/utils/config.utils';
-=======
 import { AppConfig } from 'utils/config.utils';
->>>>>>> WIP
 
 /**
  * @see https://typeorm.io/#/using-ormconfig/using-ormconfigjs
@@ -34,17 +30,12 @@ export const geoprocessingConnections: {
     logging: ['error'],
     cache: false,
     migrations: ['src/migrations/geoprocessing/**/*.ts'],
-<<<<<<< HEAD
     migrationsRun:
       AppConfig.get<string>(
         'postgresApi.runMigrationsOnStartup',
       )?.toLowerCase() !== 'false'
         ? true
         : false,
-=======
-<<<<<<< HEAD
-    migrationsRun: true,
->>>>>>> WIP
     cli: {
       migrationsDir: 'src/migrations/geoprocessing',
     },
@@ -53,11 +44,7 @@ export const geoprocessingConnections: {
     name: 'apiDB',
     synchronize: false,
     type: 'postgres',
-<<<<<<< HEAD
-    url: AppConfig.get('postgresApi.url'),
-=======
     url: config.get('postgresApi.url'),
-=======
     migrationsRun:  AppConfig.get<string>(
       'postgresApi.runMigrationsOnStartup',
     )?.toLowerCase() !== 'false'
@@ -66,25 +53,5 @@ export const geoprocessingConnections: {
     cli: {
       migrationsDir: "src/migrations/geoprocessing"
     }
-  },
-  apiDB: {
-    name: "apiDB",
-    synchronize: false,
-    type: 'postgres',
-    url: AppConfig.get('postgresApi.url'),
->>>>>>> WIP
->>>>>>> WIP
-    ssl: false,
-    entities: ['src/modules/**/*.api.entity.ts'],
-    // Logging may be: ['query', 'error', 'schema', 'warn', 'info', 'log'] Use
-    // 'query' if needing to see the actual generated SQL statements (this should
-    // be limited to `NODE_ENV=development`). Use 'error' for least verbose
-    // logging.
-    logging: ['error'],
-<<<<<<< HEAD
-    cache: false,
-=======
-    cache: false
->>>>>>> WIP
   },
 };
