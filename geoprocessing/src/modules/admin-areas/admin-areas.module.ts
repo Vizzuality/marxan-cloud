972ba17bd17b2989a2a8b57a1c0ea9fd24f91414
elpamart <elena.palao@vizzuality.com>
2021-03-15 09:34:21 +0100

    next steps mvt scaffolding

--- geoprocessing/src/modules/admin-areas/admin-areas.module.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.module.ts
@@ -1,21 +1,21 @@
-import { HttpModule, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { HttpModule, Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AdminAreasController } from './admin-areas.controller';
-import { AdminArea } from './admin-area.geo.entity';
-import { AdminAreasService } from './admin-areas.service';
-import { VectorTileModule } from 'modules/vector-tile/vector-tile.service';
+// //import { AdminAreasController } from './admin-areas.controller';
+// import { AdminArea } from './admin-area.geo.entity';
+// import { AdminAreasService } from './admin-areas.service';
+// import { VectorTileModule } from 'modules/vector-tile/vector-tile.service';
 
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([AdminArea], 'geoprocessingDB'),
-    HttpModule.register({
-      maxContentLength: 100000000,
-    }),
-    VectorTileModule,
-  ],
-  providers: [AdminAreasService],
-  controllers: [AdminAreasController],
-  exports: [AdminAreasService],
-})
-export class AdminAreasModule {}
+// @Module({
+//   imports: [
+//     TypeOrmModule.forFeature([AdminArea], 'geoprocessingDB'),
+//     HttpModule.register({
+//       maxContentLength: 100000000,
+//     }),
+//     VectorTileModule,
+//   ],
+//   providers: [AdminAreasService],
+//   controllers: [AdminAreasController],
+//   exports: [AdminAreasService],
+// })
+// export class AdminAreasModule {}
