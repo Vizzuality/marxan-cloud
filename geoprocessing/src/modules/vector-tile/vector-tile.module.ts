80a4220ac996163a39c350350a52995bd28fd4f9
elpamart <elena.palao@vizzuality.com>
2021-03-15 09:34:21 +0100

    next steps mvt scaffolding

--- geoprocessing/src/modules/vector-tile/vector-tile.module.ts
+++ geoprocessing/src/modules/vector-tile/vector-tile.module.ts
@@ -1,15 +1,15 @@
-import { HttpModule, Module } from '@nestjs/common';
-//import { TypeOrmModule } from '@nestjs/typeorm'; - when to use typeOrmModule vs HttpModule?
+// import { HttpModule, Module } from '@nestjs/common';
+// //import { TypeOrmModule } from '@nestjs/typeorm'; - when to use typeOrmModule vs HttpModule?
 
-import { VectorTileService } from './vector-tile.service';
+// import { TileServer } from './vector-tile.service';
 
-@Module({
-  imports: [
-    HttpModule.register({
-      maxContentLength: 100000000,
-    }),
-  ],
-  providers: [VectorTileService],
-  exports: [VectorTileService],
-})
-export class VectorTileModule {}
+// @Module({
+//   imports: [
+//     HttpModule.register({
+//       maxContentLength: 100000000,
+//     }),
+//   ],
+//   providers: [TileServer],
+//   exports: [TileServer],
+// })
+// export class VectorTileModule {}
