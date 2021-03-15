7837db5743e76f796fd444bac9f08040aecc5e02
Alicia <alicia.arenzana@gmail.com>
2021-03-15 14:25:00 +0100

    WIP

--- geoprocessing/src/app.module.ts
+++ geoprocessing/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module, Logger } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { geoprocessingConnections } from './ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminAreasModule } from 'src/modules/admin-areas/admin-areas.module';
 import { PlanningUnitsModule } from 'src/modules/planning-units/planning-units.module';
+import { AdminAreasModule } from 'src/modules/admin-areas/admin-areas.module';
 
 @Module({
   imports: [
@@ -18,6 +18,7 @@ import { PlanningUnitsModule } from 'src/modules/planning-units/planning-units.m
     }),
     AdminAreasModule,
     PlanningUnitsModule,
+    AdminAreasModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- geoprocessing/src/modules/admin-areas/admin-areas.service.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.service.ts
@@ -1,109 +1,16 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { TileService } from 'src/modules/tile/tile.service';
-// import { Repository, SelectQueryBuilder } from 'typeorm';
-import { IsInt, IsOptional, Max, Min } from 'class-validator';
-// import { AdminArea } from './admin-areas.geo.entity';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Alias } from 'typeorm/query-builder/Alias';
-// import { Transform } from 'class-transformer';
+import { TileServerService } from 'modules/vector-tile/vector-tile.service'
 
-/**
- * Supported admin area levels (sub-national): either level 1 or level 2.
- * @todo change type level to string
- */
-export class AdminAreaLevelFilters {
-  @IsOptional()
-  @IsInt()
-  @Min(0)
-  @Max(2)
-  // @Transform((level: any) => parseInt(level))
-  level: number;
-}
-
+const logger = new Logger('admin-areas-service');
 
 @Injectable()
 export class AdminAreasService {
-  private readonly logger: Logger = new Logger(AdminAreasService.name);
   constructor(
-    // @InjectRepository(AdminArea) private readonly repository: Repository<AdminArea>,
-    private readonly tileService: TileService,
+    private readonly tileServerService: TileServerService,
   ) {}
 
-  /**
-   *
-   * @todo add filters to filster dto
-   */
-
-  // computeFilters(
-  //   query: SelectQueryBuilder<AdminArea>,
-  //   filters?: AdminAreaLevelFilters,
-  //   alias = 'c'
-  // ): SelectQueryBuilder<AdminArea> {
-  //   if (filters?.level === 2) {
-  //     query.andWhere(`${alias}.gid2 IS NOT NULL`);
-  //   }
-
-  //   if (filters?.level === 1) {
-  //     query.andWhere(
-  //       `${alias}.gid1 IS NOT NULL AND ${alias}.gid2 IS NULL`
-  //     );
-  //   }
-
-  //   if (filters?.level === 0) {
-  //     query.andWhere(
-  //       `${alias}.gid0 IS NOT NULL AND ${alias}.gid1 IS NULL AND ${alias}.gid2 IS NULL`
-  //     )
-  //   }
-
-  //   return query;
-  // }
-
-  // public getQueryBuilder(
-  //   alias: string = 'c',
-  // ): SelectQueryBuilder<AdminArea> {
-  //   return this.computeFilters(
-  //     this.repository.createQueryBuilder(alias)
-  //   )
-  // }
-
-
-
-  public findTile(
-    z: number,
-    x: number,
-    y: number,
-    level: number,
-  ): Promise<Buffer> {
-
-    // const customQueryQB = this.getQueryBuilder(
-
-    // )
-    // console.log('customQueryQB', customQueryQB)
-    const table = 'admin_regions';
-    const geometry = 'the_geom';
-    const extent = 4096;
-    const buffer = 256;
-    const maxZoomLevel = 12;
-    /**
-     * @todo get this query from query builder
-     * @todo use the level to generate the cusom query
-     */
-    const customQuery = 'gid_0 is not null and gid_1 is null and gid_2 is null';
-    const attributes = 'gid_0, gid_1, gid_2';
-
-
-    this.logger.debug('test_execute_tile_service');
-    return this.tileService.getTile(
-      z,
-      x,
-      y,
-      table,
-      geometry,
-      extent,
-      buffer,
-      maxZoomLevel,
-      customQuery,
-      attributes,
-    );
+  public findTest(): number {
+    logger.debug('test_execute')
+    return this.tileServerService.getTile()
   }
 }
