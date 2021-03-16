9e435e19910f77308705c731586f31498311484a
elpamart <elena.palao@vizzuality.com>
2021-03-16 13:06:49 +0100

    add query builder to main vl scaffolding

--- geoprocessing/src/modules/admin-areas/admin-areas.service.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.service.ts
@@ -1,16 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { TileServerService } from 'modules/vector-tile/vector-tile.service'
+import { TileServerService } from 'modules/vector-tile/vector-tile.service';
 
 const logger = new Logger('admin-areas-service');
 
 @Injectable()
 export class AdminAreasService {
-  constructor(
-    private readonly tileServerService: TileServerService,
-  ) {}
+  constructor(private readonly tileServerService: TileServerService) {}
 
-  public findTest(): number {
-    logger.debug('test_execute')
-    return this.tileServerService.getTile()
+  public findTile(z: number, x: number, y: number): number {
+    logger.debug('test_execute');
+    return this.tileServerService.getTile(z, x, y);
   }
 }
