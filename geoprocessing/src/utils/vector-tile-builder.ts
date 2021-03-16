53b276902c9c7b7ea37bcc75e9816b59b6ed84dc
elpamart <elena.palao@vizzuality.com>
2021-03-16 18:28:10 +0100

    fix mvt query

--- geoprocessing/src/utils/vector-tile-builder.ts
+++ geoprocessing/src/utils/vector-tile-builder.ts
@@ -64,60 +64,7 @@ export function createQueryForTile({
     ',\n',
   )}\nSELECT ST_AsMVT(tile, 'geom') AS mvt FROM tile`;
 
-  logger.debug('create query for tile:', sql);
+  // logger.debug(`Create query for tile: ${sql}`);
 
   return sql;
 }
-
-// export function createQueryForTile({
-//   z,
-//   x,
-//   y,
-//   maxZoomLevel,
-//   table,
-//   geometry,
-//   sourceLayer,
-//   extent,
-//   attributes,
-//   query,
-//   getBaseQuery = defaultGetBaseQuery,
-//   getTileQuery = defaultGetTileQuery,
-// }: ITileQueryInput): string {
-//   const queryParts: string[] = [];
-//   queryParts.push(
-//     `WITH base_query AS (${getBaseQuery({
-//       x,
-//       y,
-//       z,
-//       table,
-//       geometry,
-//       maxZoomLevel,
-//       attributes: attributesToSelect(attributes),
-//       query,
-//     })})`
-//   );
-
-//   let parentTable = 'base_query';
-//   // to add here the simplicifation by zoom level or pyramiding?
-
-//   queryParts.push(
-//     `tile AS (${getTileQuery({
-//       x,
-//       y,
-//       z,
-//       table: parentTable,
-//       geometry: 'geom',
-//       extent,
-//       attributes: attributesToArray(attributes),
-//     })})`
-//   );
-
-//   const sql: string = `${queryParts.join(
-//     ',\n'
-//   )}\nSELECT ST_AsMVT(tile, '${sourceLayer}', ${extent}, 'geom') AS mvt FROM tile`;
-
-//   // console.log(sql);
-
-//   return sql;
-
-// }
