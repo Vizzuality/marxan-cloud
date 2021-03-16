13ab3f67234a1730d9a88c67dece7ae4ee1fdbb7
elpamart <elena.palao@vizzuality.com>
2021-03-16 13:06:49 +0100

    add query builder to main vl scaffolding

--- geoprocessing/src/types/tileQuery.ts
+++ geoprocessing/src/types/tileQuery.ts
@@ -1,36 +1,36 @@
-// /**
-//  * @description Input interface for the tile query builder
-//  */
-// export interface ITileQuery {
-//   x: number;
-//   y: number;
-//   z: number;
-//   table: string;
-//   geometry: string;
-//   extent: number;
-//   attributes: string;
-// }
+/**
+ * @description Input interface for the tile query builder
+ */
+export interface ITileQuery {
+  x: number;
+  y: number;
+  z: number;
+  table: string;
+  geometry: string;
+  extent: number;
+  // attributes: string;
+}
 
-// /**
-//  * @description The tile query builder callback definition
-//  */
-// export type GetTileQuery = (input: ITileQuery) => string;
+/**
+ * @description The tile query builder callback definition
+ */
+export type GetTileQuery = (input: ITileQuery) => string;
 
-// /**
-//  * @description Input interface for the base query builder
-//  */
-// export interface IBaseQueryInput {
-//   x: number;
-//   y: number;
-//   z: number;
-//   table: string;
-//   geometry: string;
-//   maxZoomLevel: number;
-//   attributes: string;
-//   query: string[];
-// }
+/**
+ * @description Input interface for the base query builder
+ */
+export interface IBaseQueryInput {
+  x: number;
+  y: number;
+  z: number;
+  table: string;
+  geometry: string;
+  maxZoomLevel: number;
+  // attributes: string;
+  // query: string[];
+}
 
-// /**
-//  * @description The base query builder callback definition
-//  */
-// export type GetBaseQuery = (input: IBaseQueryInput) => string;
+/**
+ * @description The base query builder callback definition
+ */
+export type GetBaseQuery = (input: IBaseQueryInput) => string;
