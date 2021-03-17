6e05dc028af4724ef6d6356b38bfaf545ac9fe44
elpamart <elena.palao@vizzuality.com>
2021-03-17 19:19:27 +0100

    add admin areas info and buffer param to query

--- geoprocessing/src/modules/admin-areas/admin-areas.service.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.service.ts
@@ -10,8 +10,26 @@ const logger = new Logger('admin-areas-service');
 export class AdminAreasService {
   constructor(private readonly tileServerService: TileServerService) {}
 
-  public findTile(z: number, x: number, y: number): Promise<Vectortile> {
+  public findTile(
+    z: number,
+    x: number,
+    y: number,
+    table: string,
+    geometry: string,
+    extent: number,
+    buffer: number,
+    maxZoomLevel: number,
+  ): Promise<Vectortile> {
     logger.debug('test_execute');
-    return this.tileServerService.getTile(z, x, y);
+    return this.tileServerService.getTile(
+      z,
+      x,
+      y,
+      (table = 'admin_regions'),
+      (geometry = 'the_geom'),
+      (extent = 4096),
+      (buffer = 256),
+      (maxZoomLevel = 12),
+    );
   }
 }
