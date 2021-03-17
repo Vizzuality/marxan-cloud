4c4dc25708c4e7eb96c09410d07403d654d97ac4
elpamart <elena.palao@vizzuality.com>
2021-03-17 16:09:33 +0100

    adds fecthTile and query builder

--- geoprocessing/src/app.module.ts
+++ geoprocessing/src/app.module.ts
@@ -3,24 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { geoprocessingConnections } from './ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminAreasModule } from 'modules/admin-areas/admin-areas.module'
+import { AdminAreasModule } from 'modules/admin-areas/admin-areas.module';
 
 export const logger = new Logger('app');
-logger.debug(AdminAreasModule)
-
+logger.debug(AdminAreasModule);
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    ...geoprocessingConnections.default,
-    keepConnectionAlive: true,
-  }),
-  TypeOrmModule.forRoot({
-    ...geoprocessingConnections.apiDB,
-    keepConnectionAlive: true,
-  }),
-  AdminAreasModule
-  // forwardRef(() => AdminAreasModule),
-],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...geoprocessingConnections.default,
+      keepConnectionAlive: true,
+    }),
+    TypeOrmModule.forRoot({
+      ...geoprocessingConnections.apiDB,
+      keepConnectionAlive: true,
+    }),
+    AdminAreasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
