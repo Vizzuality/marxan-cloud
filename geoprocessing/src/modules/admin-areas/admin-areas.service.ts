a8d4b5a6974563e13a54c25ed81cb63ac23556f8
elpamart <elena.palao@vizzuality.com>
2021-03-17 16:09:33 +0100

    adds fecthTile and query builder

--- geoprocessing/src/modules/admin-areas/admin-areas.service.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.service.ts
@@ -2,13 +2,15 @@ import { Injectable, Logger } from '@nestjs/common';
 // import { Repository, SelectQueryBuilder } from 'typeorm';
 import { IsInt, IsOptional, Max, Min } from 'class-validator';
 import { TileServerService } from 'modules/vector-tile/vector-tile.service';
+import { Vectortile } from 'types/tileRenderer';
 
 const logger = new Logger('admin-areas-service');
 
 @Injectable()
 export class AdminAreasService {
   constructor(private readonly tileServerService: TileServerService) {}
-  public findTile(z: number, x: number, y: number): number {
+
+  public findTile(z: number, x: number, y: number): Promise<Vectortile> {
     logger.debug('test_execute');
     return this.tileServerService.getTile(z, x, y);
   }
