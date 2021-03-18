2e2bf98df9afa94474dd7a77cf9b88638fd70d0a
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-03-18 18:06:02 +0100

    Head: titles

--- app/pages/projects/[pid]/scenarios/[sid]/edit.tsx
+++ app/pages/projects/[pid]/scenarios/[sid]/edit.tsx
@@ -1,33 +1,20 @@
 import React from 'react';
 import Head from 'next/head';
 
+import Title from 'layout/title/scenario-title';
 import Header from 'layout/header';
 import Protected from 'layout/protected';
-
-import { useProject } from 'hooks/projects';
-import { useScenario } from 'hooks/scenarios';
-import { useRouter } from 'next/router';
 
 import { withProtection, withUser } from 'hoc/auth';
 
 export const getServerSideProps = withProtection(withUser());
 
 const EditScenarioPage: React.FC = () => {
-  const { query } = useRouter();
-  const { pid, sid } = query;
-  const { data: projectData } = useProject(pid);
-  const { data: scenarioData } = useScenario(sid);
-
   return (
     <Protected>
+      <Title title="Edit" />
+
       <Head>
-        <title>
-          {projectData?.name}
-          {' '}
-          -
-          {' Edit '}
-          {scenarioData?.name}
-        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
