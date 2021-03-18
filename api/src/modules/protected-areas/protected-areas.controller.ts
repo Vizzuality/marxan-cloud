9d106433101c25fa0eb246cc3eb328977fbeaa11
andrea rota <andrea.rota@vizzuality.com>
2021-03-18 11:29:02 +0000

    add endpoint: list IUCN categories for protected areas within a given admin area

--- api/src/modules/protected-areas/protected-areas.controller.ts
+++ api/src/modules/protected-areas/protected-areas.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ProtectedAreaResult } from './protected-area.geo.entity';
 import {
   ApiBearerAuth,
@@ -21,6 +28,7 @@ import {
   protectedAreaResource,
   ProtectedAreasService,
 } from './protected-areas.service';
+import { IUCNProtectedAreaCategoryResult } from './dto/iucn-protected-area-category.dto';
 
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
@@ -32,5 +40,36 @@ export class ProtectedAreasController {
   @Get('iucn-categories')
   async listProtectedAreaCategories(): Promise<Array<string | undefined>> {
     return await this.service.listProtectedAreaCategories();
+  }
+
+  @ApiOperation({
+    description:
+      'Find unique IUCN categories among protected areas in a single given administrative area.',
+  })
+  @ApiParam({
+    name: 'adminAreaId',
+    description:
+      'Only protected areas within the given admin area will be considered.',
+    type: String,
+    required: true,
+  })
+  @ApiOkResponse({
+    type: IUCNProtectedAreaCategoryResult,
+  })
+  @ApiUnauthorizedResponse()
+  @ApiForbiddenResponse()
+  @JSONAPIQueryParams()
+  @Get('iucn-categories/:adminAreaId')
+  async listIUCNProtectedAreaCategories(
+    @Param('adminAreaId') adminAreaId: string,
+    @ProcessFetchSpecification() fetchSpecification: FetchSpecification,
+  ): Promise<IUCNProtectedAreaCategoryResult[]> {
+    return await this.service.findAllProtectedAreaCategories(
+      fetchSpecification,
+      undefined,
+      {
+        adminAreaId,
+      },
+    );
   }
 }
