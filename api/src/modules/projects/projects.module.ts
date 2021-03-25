0ebd73830f9d39a20213da0da886f20db564a2e5
andrea rota <andrea.rota@vizzuality.com>
2021-03-25 20:11:33 +0000

    set up imports and dependency injection for new features

--- api/src/modules/projects/projects.module.ts
+++ api/src/modules/projects/projects.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProjectsController } from './projects.controller';
@@ -6,9 +6,17 @@ import { Project } from './project.api.entity';
 import { ProjectsService } from './projects.service';
 import { UsersModule } from 'modules/users/users.module';
 import { ScenariosModule } from 'modules/scenarios/scenarios.module';
+import { AdminAreasModule } from 'modules/admin-areas/admin-areas.module';
+import { CountriesModule } from 'modules/countries/countries.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project]), ScenariosModule, UsersModule],
+  imports: [
+    AdminAreasModule,
+    CountriesModule,
+    forwardRef(() => ScenariosModule),
+    TypeOrmModule.forFeature([Project]),
+    UsersModule,
+  ],
   providers: [ProjectsService],
   controllers: [ProjectsController],
   exports: [ProjectsService],
--- api/src/modules/scenarios/scenarios.module.ts
+++ api/src/modules/scenarios/scenarios.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ScenariosController } from './scenarios.controller';
 import { Scenario } from './scenario.api.entity';
 import { ScenariosService } from './scenarios.service';
 import { UsersModule } from 'modules/users/users.module';
+import { Project } from 'modules/projects/project.api.entity';
+import { ProtectedAreasModule } from 'modules/protected-areas/protected-areas.module';
+import { ProjectsModule } from 'modules/projects/projects.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Scenario]), UsersModule],
+  imports: [
+    ProtectedAreasModule,
+    forwardRef(() => ProjectsModule),
+    TypeOrmModule.forFeature([Project, Scenario]),
+    UsersModule,
+  ],
   providers: [ScenariosService],
   controllers: [ScenariosController],
   exports: [ScenariosService],
