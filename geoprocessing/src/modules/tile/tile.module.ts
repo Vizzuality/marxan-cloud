565e077b737b7de79b73711c5f8e5dddd7b6f006
elpamart <elena.palao@vizzuality.com>
2021-03-26 11:13:13 +0100

    unification of mvt server and testing index html

--- geoprocessing/src/modules/tile/tile.module.ts
+++ geoprocessing/src/modules/tile/tile.module.ts
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 import { Module } from '@nestjs/common';
 import { TileService } from './tile.service';
 
+=======
+import { Logger, Module } from '@nestjs/common';
+import { TileService } from './tile.service';
+
+const logger = new Logger('Vector tile module');
+
+>>>>>>> unification of mvt server and testing index html
 @Module({
   providers: [TileService],
   exports: [TileService],
