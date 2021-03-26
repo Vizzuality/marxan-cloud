57c15e40c50c2db18c94ed52549a999cb6e9dc33
elpamart <elena.palao@vizzuality.com>
2021-03-26 11:13:13 +0100

    unification of mvt server and testing index html

--- geoprocessing/src/modules/admin-areas/admin-areas.controller.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Header } from '@nestjs/common';
 // import { AdminAreasResult } from './admin-areas.geo.entity';
 import { AdminAreasService } from './admin-areas.service';
 import { apiGlobalPrefixes } from 'api.config';
@@ -12,6 +12,9 @@ import {
   // ApiTags,
   // ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
+
+import { Tile } from 'modules/tile/tile.service';
+import { logger } from 'app.module';
 
 @Controller(`${apiGlobalPrefixes.v1}`)
 export class AdminAreasController {
@@ -38,7 +41,19 @@ export class AdminAreasController {
     type: Number,
     required: true,
   })
-  @Get('/wdpa/tiles/:z/:x/:y')
+  @ApiParam({
+    name: 'level',
+    description:
+      'Specific level to filter the administrative areas (0, 1 or 2)',
+    type: Number,
+    required: true,
+  })
+  @Get('/administrative-areas/:level/preview/tiles/:z/:x/:y.mvt')
+  @Header('Access-Control-Allow-Origin', '*')
+  @Header('Content-Type', 'application/x-protobuf')
+  // @Header('Content-Encoding', 'gzip')
+  // @Header('Content-Type', 'application/vnd.mapbox-vector-tile')
+  // @Header('Content-Disposition', 'attachment')
   async getTile(
     @Param('z') z: number,
     @Param('x') x: number,
@@ -48,8 +63,10 @@ export class AdminAreasController {
     @Param('extent') extent: number,
     @Param('buffer') buffer: number,
     @Param('maxZoomLevel') maxZoomLevel: number,
-  ): Promise<void> {
-    await this.service.findTile(
+  ): Promise<any> {
+    // Promise<tile>
+    let response;
+    const tile: Tile = await this.service.findTile(
       z,
       x,
       y,
@@ -59,5 +76,45 @@ export class AdminAreasController {
       buffer,
       maxZoomLevel,
     );
+    if (tile.res >= 0 && tile.data) {
+      response = tile.data; //.toString('base64')
+      // {
+      //   statusCode: 200,
+      //   headers: {
+      //     'Content-Type': 'application/vnd.mapbox-vector-tile',
+      //     'Content-Encoding':"gzip" ,
+      //     'access-control-allow-origin': '*'
+      //  },
+      //   body: tile.data.toString('base64'),
+      //   isBase64Encoded: true
+      // }
+    } else {
+      response = JSON.stringify(tile);
+      // {
+      //   statusCode: 500,
+      //   headers: {
+      //       'Content-Type': 'text/html',
+      //       'access-control-allow-origin': '*',
+      //       'Content-Encoding': 'identity'
+      //   },
+      //   body: JSON.stringify(tile),
+      //   isBase64Encoded: false
+      // }
+    }
+    // logger.debug(tile.data)
+    // logger.debug(tile.data?.toString('base64'))
+    return response; //Promise.resolve(response)
   }
+  // ): Promise<Tile> {
+  //   return await this.service.findTile(
+  //     z,
+  //     x,
+  //     y,
+  //     table,
+  //     geometry,
+  //     extent,
+  //     buffer,
+  //     maxZoomLevel,
+  //   );
+  // }
 }
