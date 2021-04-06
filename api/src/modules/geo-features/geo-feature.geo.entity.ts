f2c7bd035ee84f209ee7d6b91edeedb5533e430d
andrea rota <andrea.rota@vizzuality.com>
2021-04-06 12:30:00 +0100

    initial GeoFeature entity

---
+++ api/src/modules/geo-features/geo-feature.geo.entity.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { BaseServiceResource } from 'types/resource.interface';
+
+export const geoFeatureResource: BaseServiceResource = {
+  className: 'GeoFeature',
+  name: {
+    singular: 'geo_feature',
+    plural: 'geo_features',
+  },
+  moduleControllerPrefix: 'geo-features',
+};
+
+@Entity('features_data')
+export class GeoFeatureGeometry {
+  @ApiProperty()
+  @PrimaryColumn()
+  id: string;
+}
+
+export class JSONAPIGeoFeaturesGeometryData {
+  @ApiProperty()
+  type = geoFeatureResource.name.plural;
+
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  attributes: GeoFeatureGeometry;
+}
+
+export class GeoFeatureResult {
+  @ApiProperty()
+  data: JSONAPIGeoFeaturesGeometryData;
+}
