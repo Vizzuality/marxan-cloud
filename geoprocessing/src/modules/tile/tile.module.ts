2d87a637752ab4b8406be560335aedf87dd9f5ad
Alicia <alicia.arenzana@gmail.com>
2021-04-07 14:20:21 +0200

    fixed problem with proxy url reirection

--- geoprocessing/src/modules/tile/tile.module.ts
+++ geoprocessing/src/modules/tile/tile.module.ts
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import { Module } from '@nestjs/common';
-import { TileService } from './tile.service';
-
-=======
 import { Logger, Module } from '@nestjs/common';
 import { TileService } from './tile.service';
 
 const logger = new Logger('Vector tile module');
 
->>>>>>> unification of mvt server and testing index html
 @Module({
   providers: [TileService],
   exports: [TileService],
