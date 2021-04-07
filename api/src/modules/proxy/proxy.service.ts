3c12f91b834b74654a82e5c37d2e49c69b228327
Alicia <alicia.arenzana@gmail.com>
2021-04-07 14:20:21 +0200

    fixed problem with proxy url reirection

--- api/src/modules/proxy/proxy.service.ts
+++ api/src/modules/proxy/proxy.service.ts
@@ -1,14 +1,18 @@
 import { HttpStatus, Injectable, Logger } from '@nestjs/common';
 
-import config from 'config';
+import { AppConfig } from 'utils/config.utils';
 import { Request, Response } from 'express';
-import Server from 'http-proxy';
+/**
+ * @debt i think this is not properly imported, but it was the only way for it to work
+ */
+import * as Server from 'http-proxy';
 
 @Injectable()
 export class ProxyService {
   private server: Server;
-
+  private readonly logger = new Logger(ProxyService.name);
   constructor() {
+    this.logger.debug(Server)
     this.server = Server.createProxyServer();
   }
 
@@ -16,23 +20,23 @@ export class ProxyService {
     /**
      * @todo change server name in config url
      */
-    const tileTargetURL: string = config.get('geoprocessing.url');
+    const tileTargetURL: string = AppConfig.get('geoprocessing.url') as string;
     // Use `originalUrl` here, as the `/api/v1/geoprocessing` is otherwise stripped
     // away, but we need this because the backend microservice expects that as
     // part of its routes
-    request.url = request.originalUrl;
+    request.url = request.originalUrl.replace('api/v1/geoprocessing', 'api/v1/geodata');
 
     return this.server.web(
       request,
       response,
       { changeOrigin: true, target: tileTargetURL },
       (error) => {
-        Logger.error(
+        this.logger.error(
           `Unexpected exception on proxy to tile service - ${error}`,
         );
         return response
           .status(HttpStatus.BAD_GATEWAY)
-          .send({ message: 'Unexpected exception during request' });
+          .send({ message: `Unexpected exception during request - ${error}` });
       },
     );
   }
