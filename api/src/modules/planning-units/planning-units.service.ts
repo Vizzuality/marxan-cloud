b84e1d1a2920a0800708564742d05281c436dddd
Alicia <alicia.arenzana@gmail.com>
2021-04-08 17:53:05 +0200

    added validation started point

--- api/src/modules/planning-units/planning-units.service.ts
+++ api/src/modules/planning-units/planning-units.service.ts
@@ -1,6 +1,6 @@
 import { Logger, Injectable } from '@nestjs/common';
 
-import { Job, Queue, QueueEvents } from 'bullmq';
+import { Queue, QueueEvents } from 'bullmq';
 import * as config from 'config';
 
 import { CreatePlanningUnitsDTO } from './dto/create.planning-units.dto';
@@ -28,12 +28,34 @@ export class PlanningUnitsService {
     config.get('redisApi'),
   );
   constructor() {
-    this.queueEvents.on('completed', (job) => {
+    this.queueEvents.on('completed', (args) => {
       this.logger.log(
-        `this job ${job.jobId} for ${this.queueName} is completed`,
+        `this job ${args.jobId} for ${this.queueName} is completed with ${args.returnvalue}`,
+      );
+    });
+    this.queueEvents.on('failed', (args) => {
+      this.logger.log(
+        `this job ${args.jobId} for ${this.queueName} has failed ${args.failedReason}`,
       );
     });
   }
+  /** @description A job should only be triggered when:
+   * * An area(extent)/region/custom grid is not supplied -> nothing is triggered
+  * * A custom extent (area) is supplied, but not a planning unit area type + grid size or user grid -> nothing is triggered
+  * * A planning unit area type + grid size is supplied but not an area(extent)/region/custom grid is provided -> nothing is triggered
+  * * An area(extent)/region is provided + a planning unit area type(regular grid hexagon or square or irregular) + grid size -> PU grid generation is triggered.
+  * * A custom grid uploaded by the user is provided -> Regular grid creation should not be triggered, but we should upload that grid as a custom one set the PlanningUnitGridShape.fromShapefile and infer the extent and /or admin regions from there. Also, have a way to retrieve that user data as part of the project.
+  * **the area extent can be also uploaded by the user but is only a geometry that represents a custom extent.
+  * @TODO
+  * Validations:
+  * * If a user has provided also an admin area / EEZ, this extent must be contained in the adm area provided.
+  * * PlanningUnitAreakm2 should be a positive number
+  * * This piece will be triggered only if we require to generate a custom regular grid (not user provided PUs)
+  * * If a user provides a geometry that represents a custom extent, what will happen with adm fields?
+  * * Validation of extent geometry.
+  * * Validation of admin codes(this probably should be at project level)
+  *
+  */
 
   public async create(creationOptions: CreatePlanningUnitsDTO): Promise<void> {
     await this.planningUnitsQueue.add('create-regular-pu', creationOptions);
