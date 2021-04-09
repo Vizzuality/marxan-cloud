a70ffa9956886bdfbbded651c589fc7aae3b1821
Alicia <alicia.arenzana@gmail.com>
2021-04-09 11:21:12 +0200

    Fixed conflicts with develop branch

--- geoprocessing/src/modules/admin-areas/admin-areas.controller.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.controller.ts
@@ -1,20 +1,9 @@
 import { Controller, Get, Param, Header, Res } from '@nestjs/common';
-// import { AdminAreasResult } from './admin-areas.geo.entity';
 import { AdminAreasService } from './admin-areas.service';
-import { apiGlobalPrefixes } from 'api.config';
-import {
-  // ApiBearerAuth,
-  // ApiForbiddenResponse,
-  // ApiOkResponse,
-  ApiOperation,
-  ApiParam,
-  // ApiQuery,
-  // ApiTags,
-  // ApiUnauthorizedResponse,
-} from '@nestjs/swagger';
+import { apiGlobalPrefixes } from 'src/api.config';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
-import { Tile } from 'modules/tile/tile.service';
-import { logger } from 'app.module';
+import { Tile } from 'src/modules/tile/tile.service';
 import { Response } from 'express';
 
 @Controller(`${apiGlobalPrefixes.v1}`)
@@ -84,44 +73,10 @@ export class AdminAreasController {
     );
 
     if (tile.res >= 0 && tile.data) {
-      response.send(tile.data); //.toString('base64')
-      // {
-      //   statusCode: 200,
-      //   headers: {
-      //     'Content-Type': 'application/vnd.mapbox-vector-tile',
-      //     'Content-Encoding':"gzip" ,
-      //     'access-control-allow-origin': '*'
-      //  },
-      //   body: tile.data.toString('base64'),
-      //   isBase64Encoded: true
-      // }
+      response.send(tile.data);
     } else {
       response.send(JSON.stringify(tile));
-      // {
-      //   statusCode: 500,
-      //   headers: {
-      //       'Content-Type': 'text/html',
-      //       'access-control-allow-origin': '*',
-      //       'Content-Encoding': 'identity'
-      //   },
-      //   body: JSON.stringify(tile),
-      //   isBase64Encoded: false
-      // }
     }
-    // logger.debug(tile.data)
-    // logger.debug(tile.data?.toString('base64'))
-    return response; //Promise.resolve(response)
+    return response;
   }
-  // ): Promise<Tile> {
-  //   return await this.service.findTile(
-  //     z,
-  //     x,
-  //     y,
-  //     table,
-  //     geometry,
-  //     extent,
-  //     buffer,
-  //     maxZoomLevel,
-  //   );
-  // }
 }
