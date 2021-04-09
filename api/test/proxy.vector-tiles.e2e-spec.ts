c0a64dd452dd08dd8a066fe5c1729f4eee175a7d
Alicia <alicia.arenzana@gmail.com>
2021-04-09 11:29:52 +0200

    example of test for proxy vector tiles admin area.

--- api/test/proxy.vector-tiles.e2e-spec.ts
+++ api/test/proxy.vector-tiles.e2e-spec.ts
@@ -4,7 +4,6 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { E2E_CONFIG } from './e2e.config';
 
-// import { ProxyService } from '../src/modules/proxy/proxy.service'
 describe('ProxyVectorTilesModule (e2e)', () => {
   let app: INestApplication;
 
@@ -40,7 +39,7 @@ describe('ProxyVectorTilesModule (e2e)', () => {
     await Promise.all([app.close()]);
   });
 
-  describe('Proxy administrative areas', () => {
+  describe('Countries', () => {
     // Make sure we have GADM data for this country in the test data which
     // is used to populate the geodb in CI pipelines.
 
@@ -50,40 +49,6 @@ describe('ProxyVectorTilesModule (e2e)', () => {
         .set('Authorization', `Bearer ${jwtToken}`)
         .expect(200);
     });
-    /**
-     * @todo add error respone in main code
-     */
-    it('Should simulate an error', async () => {
-      const response = await request(app.getHttpServer())
-      .get('/api/v1/geoprocessing/administrative-areas/1/preview/tiles/100/60/30.mvt')
-      .set('Authorization', `Bearer ${jwtToken}`)
-      .expect(400)
-    })
-
-    /**
-     * @todo add level restriction on endpoint
-     */
-    it('Should throw a 400 error if filtering by level other than 0, 1 or 2', async () => {
-      const response = await request(app.getHttpServer())
-      .get('/api/v1/geoprocessing/administrative-areas/3/preview/tiles/6/30/25.mvt')
-      .set('Authorization', `Bearer ${jwtToken}`)
-      .expect(400)
-    })
-
-    /**
-     * @todo add zoom level restriction on endpoint
-     */
-    it('Should throw a 400 error if filtering by  z level greater than 20', async () => {
-      const response = await request(app.getHttpServer())
-      .get('/api/v1/geoprocessing/administrative-areas/3/preview/tiles/21/30/25.mvt')
-      .set('Authorization', `Bearer ${jwtToken}`)
-      .expect(400)
-    })
-
-    /**
-     * @todo add invalid tile test
-     * @todo add mvt format test
-     */
 
   });
 });
