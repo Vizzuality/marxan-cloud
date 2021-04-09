70109ad6a85eb9624fd1a134eae584037145cdf0
Alicia <alicia.arenzana@gmail.com>
2021-04-09 11:42:20 +0200

    added new endpoint to swagger

--- api/src/modules/proxy/proxy.controller.ts
+++ api/src/modules/proxy/proxy.controller.ts
@@ -1,20 +1,61 @@
-import { All, Controller, Req, Res, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Req, Res, UseGuards,Get } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'guards/jwt-auth.guard';
-
+import { apiGlobalPrefixes } from 'api.config';
 import { ProxyService } from './proxy.service';
 import { Request, Response } from 'express';
 
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
-@ApiTags('Microservice Proxy Controllet')
-@Controller('/api/v1')
+@ApiTags('Proxy for vector tiles Controller')
+@Controller(`${apiGlobalPrefixes.v1}`)
 export class ProxyController {
   constructor(private readonly proxyService: ProxyService) {}
 
-  @All('geoprocessing')
-  @ApiOperation({ summary: 'Proxy to geoprocessing tile service' })
-  proxyTile(@Req() request: Request, @Res() response: Response) {
+  @ApiOperation({
+    description: 'Find administrative areas within a given country.',
+  })
+  @ApiOkResponse({
+    type: 'mvt',
+  })
+  @ApiUnauthorizedResponse()
+  @ApiForbiddenResponse()
+  @ApiParam({
+    name: 'z',
+    description: 'The zoom level ranging from 0 - 20',
+    type: Number,
+    required: true,
+  })
+  @ApiParam({
+    name: 'x',
+    description: 'The tile x offset on Mercator Projection',
+    type: Number,
+    required: true,
+  })
+  @ApiParam({
+    name: 'y',
+    description: 'The tile y offset on Mercator Projection',
+    type: Number,
+    required: true,
+  })
+  @ApiParam({
+    name: 'level',
+    description:
+      'Specific level to filter the administrative areas (0, 1 or 2)',
+    type: Number,
+    required: true,
+  })
+  @Get('/geoprocessing/administrative-areas/:level/preview/tiles/:z/:x/:y.mvt')
+  async proxyTile(@Req() request: Request, @Res() response: Response) {
     return this.proxyService.proxyTileRequest(request, response);
   }
 }
