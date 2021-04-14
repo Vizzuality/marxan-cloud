6e6087445f8d5eec4d5cb334e2c042c0687b1fd7
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-04-14 16:39:09 +0200

    Scenario: Feature list

--- app/hooks/features/index.ts
+++ app/hooks/features/index.ts
@@ -5,6 +5,9 @@ import { useSession } from 'next-auth/client';
 
 import orderBy from 'lodash/orderBy';
 
+import { ItemProps as RawItemProps } from 'components/features/raw-item/component';
+import { ItemProps as SelectedItemProps } from 'components/features/selected-item/component';
+
 import FEATURES from 'services/scenarios';
 
 import ITEMS from './mock';
@@ -21,7 +24,7 @@ export function useAllFeatures(filters: UseFeaturesFiltersProps = {}) {
   const [session] = useSession();
   const { search } = filters;
 
-  const query = useQuery(['features'], async () => FEATURES.request({
+  const query = useQuery(['all-features'], async () => FEATURES.request({
     method: 'GET',
     url: '/',
     headers: {
@@ -32,7 +35,7 @@ export function useAllFeatures(filters: UseFeaturesFiltersProps = {}) {
   const { data } = query;
 
   return useMemo(() => {
-    let parsedData = Array.isArray(data?.data) ? ITEMS.map((d) => {
+    let parsedData = Array.isArray(data?.data) ? ITEMS.map((d):RawItemProps => {
       const {
         id, name, description, tags,
       } = d;
@@ -58,6 +61,94 @@ export function useAllFeatures(filters: UseFeaturesFiltersProps = {}) {
 
     // Sort
     parsedData = orderBy(parsedData, ['name'], ['asc']);
+
+    return {
+      ...query,
+      data: parsedData,
+      rawData: data?.data,
+    };
+  }, [query, data?.data, search]);
+}
+
+export function useSelectedFeatures(filters: UseFeaturesFiltersProps = {}) {
+  const [session] = useSession();
+  const { search } = filters;
+
+  const query = useQuery(['selected-features'], async () => FEATURES.request({
+    method: 'GET',
+    url: '/',
+    headers: {
+      Authorization: `Bearer ${session.accessToken}`,
+    },
+  }));
+
+  const { data } = query;
+
+  return useMemo(() => {
+    let parsedData = Array.isArray(data?.data) ? ITEMS.map((d):SelectedItemProps => {
+      const {
+        id, name, type, description,
+      } = d;
+
+      return {
+        id,
+        name,
+        type: type === 'bioregional' ? 'bioregional' : 'species', // TODO: check why this is happening
+        description,
+        // SPLIT
+
+        // splitSelected: 'attribute-1',
+        // splitOptions: [
+        //   { label: 'Attribute 1', value: 'attribute-1' },
+        //   { label: 'Attribute 2', value: 'attribute-2' },
+        //   { label: 'Attribute 3', value: 'attribute-3' },
+        // ],
+        // onSplitSelected: (selected) => console.info(selected),
+
+        // splitFeaturesSelected: [],
+        // splitFeaturesOptions: [
+        //   { label: 'Deserts and Xeric Shrublands', value: 'id-1' },
+        //   {
+        //     label: 'Tropical and Subtropical Grasslands, Savannas and Shrublands',
+        //     value: 'id-2',
+        //   },
+        //   { label: 'Flooded Grasslands and Savannas', value: 'id-3' },
+        //   { label: 'Montane Grasslands and Shrublands', value: 'id-4' },
+        //   {
+        //     label: 'Tropical and Subtropical Moist, Broadleaf Forests',
+        //     value: 'id-5',
+        //   },
+        //   { label: 'Mangroves', value: 'id-6' },
+        // ],
+        // onSplitFeaturesSelected: (selected) => console.info(selected),
+
+        // INTEERESECTION
+
+        // intersectFeaturesSelected: ['id-1', 'id-2'],
+        // intersectFeaturesOptions: [
+        //   { label: 'Deserts and Xeric Shrublands', value: 'id-1' },
+        //   {
+        //     label: 'Tropical and Subtropical Grasslands, Savannas and Shrublands',
+        //     value: 'id-2',
+        //   },
+        // ],
+
+      };
+    }) : [];
+
+    // Filter
+    if (search) {
+      const fuse = new Fuse(parsedData, {
+        keys: ['name'],
+        threshold: 0.25,
+      });
+      parsedData = fuse.search(search).map((f) => {
+        return f.item;
+      });
+    }
+
+    // Sort
+    parsedData = orderBy(parsedData, ['type', 'name'], ['asc', 'asc']);
 
     return {
       ...query,
--- app/layout/scenarios/sidebar/features/list/component.tsx
+++ app/layout/scenarios/sidebar/features/list/component.tsx
@@ -1,98 +1,60 @@
 import React from 'react';
 import cx from 'classnames';
 
+import Loading from 'components/loading';
 import Item from 'components/features/selected-item';
 
-import { useRouter } from 'next/router';
-import { useScenario } from 'hooks/scenarios';
-
-const ITEMS = [
-  {
-    id: 1,
-    name: 'Ecoregions',
-    type: 'bioregional',
-    description:
-      'Donec est ad luctus dapibus sociosqu. Imperdiet platea viverra dui congue orci ad. Turpis a, dictumst eget. Justo potenti morbi iaculis habitasse justo aliquam tortor tellus nostra. Accumsan nunc lorem malesuada, eget sed magna habitasse laoreet rutrum non ante suscipit. Adipiscing quisque justo vel, et tellus suscipit purus. Mattis primis curae;, scelerisque parturient libero dictumst ad! Cras elit condimentum molestie sociis mauris. Pharetra tincidunt habitant imperdiet mauris vitae tempor sollicitudin pulvinar feugiat pharetra scelerisque? Purus erat penatibus adipiscing vestibulum fermentum et platea eros quis ad congue. Porta fringilla enim bibendum per tortor natoque ante suscipit. Congue.',
-
-    splitSelected: 'attribute-1',
-    splitOptions: [
-      { label: 'Attribute 1', value: 'attribute-1' },
-      { label: 'Attribute 2', value: 'attribute-2' },
-      { label: 'Attribute 3', value: 'attribute-3' },
-    ],
-    onSplitSelected: (selected) => console.info(selected),
-
-    splitFeaturesSelected: [],
-    splitFeaturesOptions: [
-      { label: 'Deserts and Xeric Shrublands', value: 'id-1' },
-      {
-        label: 'Tropical and Subtropical Grasslands, Savannas and Shrublands',
-        value: 'id-2',
-      },
-      { label: 'Flooded Grasslands and Savannas', value: 'id-3' },
-      { label: 'Montane Grasslands and Shrublands', value: 'id-4' },
-      {
-        label: 'Tropical and Subtropical Moist, Broadleaf Forests',
-        value: 'id-5',
-      },
-      { label: 'Mangroves', value: 'id-6' },
-    ],
-    onSplitFeaturesSelected: (selected) => console.info(selected),
-  },
-  {
-    id: 2,
-    name: 'Lion',
-    type: 'species',
-    description:
-      'Donec est ad luctus dapibus sociosqu. Imperdiet platea viverra dui congue orci ad. Turpis a, dictumst eget. Justo potenti morbi iaculis habitasse justo aliquam tortor tellus nostra. Accumsan nunc lorem malesuada, eget sed magna habitasse laoreet rutrum non ante suscipit. Adipiscing quisque justo vel, et tellus suscipit purus. Mattis primis curae;, scelerisque parturient libero dictumst ad! Cras elit condimentum molestie sociis mauris. Pharetra tincidunt habitant imperdiet mauris vitae tempor sollicitudin pulvinar feugiat pharetra scelerisque? Purus erat penatibus adipiscing vestibulum fermentum et platea eros quis ad congue. Porta fringilla enim bibendum per tortor natoque ante suscipit. Congue.',
-    intersectFeaturesSelected: ['id-1', 'id-2'],
-    intersectFeaturesOptions: [
-      { label: 'Deserts and Xeric Shrublands', value: 'id-1' },
-      {
-        label: 'Tropical and Subtropical Grasslands, Savannas and Shrublands',
-        value: 'id-2',
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: 'Elephant',
-    type: 'species',
-    description:
-      'Donec est ad luctus dapibus sociosqu. Imperdiet platea viverra dui congue orci ad.',
-  },
-];
+import { useSelectedFeatures } from 'hooks/features';
 
 export interface ScenariosFeaturesListProps {
 }
 
 export const ScenariosFeaturesList: React.FC<ScenariosFeaturesListProps> = () => {
-  const { query } = useRouter();
-  const { sid } = query;
+  const {
+    data: selectedFeaturesData,
+    isFetching: selectedFeaturesIsFetching,
+    isFetched: selectedFeaturesIsFetched,
+  } = useSelectedFeatures({});
 
-  const { data: scenarioData } = useScenario(sid);
-
-  if (!scenarioData) return null;
+  if (selectedFeaturesIsFetching && !selectedFeaturesIsFetched) {
+    return (
+      <Loading
+        visible
+        className="z-40 flex items-center justify-center w-full h-40 bg-transparent bg-opacity-90"
+        iconClassName="w-5 h-5 text-primary-500"
+      />
+    );
+  }
 
   return (
     <div
       className="mt-5"
     >
-      {ITEMS.map((item, i) => {
-        return (
-          <div
-            className={cx({
-              'mt-1.5': i !== 0,
-            })}
-            key={`${item.id}`}
-          >
-            <Item
-              {...item}
-            />
-          </div>
+      {(!selectedFeaturesData || !selectedFeaturesData.length) && (
+        <div className="flex items-center justify-center w-full h-40 text-sm uppercase">
+          No results found
+        </div>
+      )}
 
-        );
-      })}
+      {
+        !!selectedFeaturesData
+        && selectedFeaturesData.length
+        && selectedFeaturesData.map((item, i) => {
+          return (
+            <div
+              className={cx({
+                'mt-1.5': i !== 0,
+              })}
+              key={`${item.id}`}
+            >
+              <Item
+                {...item}
+              />
+            </div>
+
+          );
+        })
+}
     </div>
   );
 };
