c49e692b8bd7b8114729629329b8bc0d2a998ec3
andrea rota <andrea.rota@vizzuality.com>
2021-04-14 15:16:59 +0100

    add GeoFeature entity definition

---
+++ api/src/modules/geo-features/geo-feature.api.entity.ts
@@ -0,0 +1,60 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { BaseServiceResource } from 'types/resource.interface';
+
+export const geoFeatureResource: BaseServiceResource = {
+  className: 'GeoFeature',
+  name: {
+    singular: 'geo_feature',
+    plural: 'geo_features',
+  },
+  moduleControllerPrefix: 'geo-features',
+};
+
+export enum FeatureTags {
+  bioregional = 'bioregional',
+  species = 'species',
+}
+
+@Entity('features')
+export class GeoFeature {
+  @ApiProperty()
+  @PrimaryColumn()
+  id: string;
+
+  @ApiPropertyOptional()
+  @Column('varchar', { name: 'feature_class_name' })
+  featureClassName?: string;
+
+  @ApiPropertyOptional()
+  @Column('varchar')
+  alias?: string;
+
+  @ApiPropertyOptional()
+  @Column('varchar', { name: 'property_name' })
+  propertyName?: string;
+
+  @ApiPropertyOptional()
+  @Column('uuid')
+  intersection?: string[];
+
+  @ApiProperty()
+  @Column('varchar')
+  tag: FeatureTags;
+}
+
+export class JSONAPIGeoFeaturesData {
+  @ApiProperty()
+  type = geoFeatureResource.name.plural;
+
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  attributes: GeoFeature;
+}
+
+export class GeoFeatureResult {
+  @ApiProperty()
+  data: JSONAPIGeoFeaturesData;
+}
