1d1fb57fcdc2fd8783128a3cc5404ca00b56e77b
andrea rota <andrea.rota@vizzuality.com>
2021-04-14 15:18:18 +0100

    plug in fake data for GeoFeatures (singular requests)

--- api/src/modules/geo-features/geo-features.module.ts
+++ api/src/modules/geo-features/geo-features.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GeoFeature } from './geo-feature.api.entity';
 import { GeoFeatureGeometry } from './geo-feature.geo.entity';
 
 import { GeoFeaturesController } from './geo-features.controller';
 import { GeoFeaturesService } from './geo-features.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GeoFeatureGeometry], 'geoprocessingDB')],
+  imports: [
+    TypeOrmModule.forFeature([GeoFeatureGeometry], 'geoprocessingDB'),
+    TypeOrmModule.forFeature([GeoFeature]),
+  ],
   providers: [GeoFeaturesService],
   controllers: [GeoFeaturesController],
   exports: [GeoFeaturesService],
