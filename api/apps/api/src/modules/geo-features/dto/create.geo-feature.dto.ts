52770ff1722afd508d3d3635a83d8d29e7dd1469
andrea rota <andrea.rota@vizzuality.com>
2021-04-16 19:23:27 +0100

    WIP DTO for feature set spec for scenarios

--- api/apps/api/src/modules/geo-features/dto/create.geo-feature.dto.ts
+++ api/apps/api/src/modules/geo-features/dto/create.geo-feature.dto.ts
@@ -1,1 +1,25 @@
-export class CreateGeoFeatureDTO {}
+import { JobStatus } from 'modules/scenarios/scenario.api.entity';
+import {
+  GeoprocessingOpSplitV1,
+  GeoprocessingOpStratificationV1,
+} from '../types/geo-feature.geoprocessing-operations.type';
+import { MarxanSettingsForGeoFeature } from '../types/geo-feature.marxan-settings.type';
+
+interface SpecForPlainGeoFeature {
+  featureId: string;
+  marxanSettings: MarxanSettingsForGeoFeature;
+  geoprocessingOperations?: never;
+}
+
+interface SpecForGeoFeatureWithGeoprocessing {
+  featureId: string;
+  geoprocessingOperations: Array<
+    GeoprocessingOpSplitV1 | GeoprocessingOpStratificationV1
+  >;
+  marxanSettings: never;
+}
+
+export interface CreateGeoFeatureSetDTO {
+  status: JobStatus.draft | JobStatus.created;
+  features: Array<SpecForPlainGeoFeature | SpecForGeoFeatureWithGeoprocessing>;
+}
