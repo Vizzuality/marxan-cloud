71cf1531989e11ac14dfd7131b18e0a9c1f3f3dc
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-04-19 16:21:34 +0200

    Add json-api e2e test file

---
+++ api/test/json-api.e2e-specs.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { E2E_CONFIG } from './e2e.config';
+
+describe('ExceptionFilter (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await Promise.all([app.close()]);
+  });
+
+  
+});
