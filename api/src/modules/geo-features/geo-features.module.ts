df58b723a299fe4159f31c6d695e79a7b405a7cb
andrea rota <andrea.rota@vizzuality.com>
2021-04-20 15:20:32 +0100

    extend results for GeoFeatures with properties data from geodb

--- api/src/modules/geo-features/geo-features.module.ts
+++ api/src/modules/geo-features/geo-features.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GeoFeature } from './geo-feature.api.entity';
-import { GeoFeatureGeometry } from './geo-feature.geo.entity';
+import {
+  GeoFeatureGeometry,
+  GeoFeaturePropertySet,
+} from './geo-feature.geo.entity';
 
 import { GeoFeaturesController } from './geo-features.controller';
 import { GeoFeaturesService } from './geo-features.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([GeoFeatureGeometry], 'geoprocessingDB'),
+    TypeOrmModule.forFeature(
+      [GeoFeatureGeometry, GeoFeaturePropertySet],
+      'geoprocessingDB',
+    ),
     TypeOrmModule.forFeature([GeoFeature]),
   ],
   providers: [GeoFeaturesService],
