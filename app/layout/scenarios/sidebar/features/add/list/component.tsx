68bf0ba7b5618bcf4af9d7be0a05b4a0cde4df07
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-04-20 18:01:00 +0200

    Scenario: Feature all scroll

--- app/layout/scenarios/sidebar/features/add/list/component.tsx
+++ app/layout/scenarios/sidebar/features/add/list/component.tsx
@@ -38,12 +38,12 @@ export const ScenariosFeaturesAddList: React.FC<ScenariosFeaturesAddListProps> =
   }
 
   return (
-    <div className="relative h-full overflow-hidden">
+    <div className="relative flex flex-col flex-grow overflow-hidden">
       <div className="absolute left-0 z-10 w-full h-6 -top-1 bg-gradient-to-b from-white via-white" />
 
       <div
         className={cx({
-          'bg-white divide-y divide-black divide-dashed divide-opacity-20 h-full overflow-y-auto overflow-x-hidden px-8': true,
+          'bg-white divide-y divide-black divide-dashed divide-opacity-20 overflow-y-auto overflow-x-hidden px-8': true,
         })}
       >
         {(!allFeaturesData || !allFeaturesData.length) && (
