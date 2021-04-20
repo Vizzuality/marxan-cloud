b17ca411fabb7a7deda15193151f2e3dfee907f6
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-04-20 17:01:11 +0200

    Add all-exceptions.exception.filter spec file

---
+++ api/src/filters/all-exceptions.exception.filter.spec.ts
@@ -0,0 +1,7 @@
+import { AllExceptionsFilter } from './all-exceptions.exception.filter';
+
+describe('AllExceptionFilter class spec (unit)', () => {
+  it('should be defined', () => {
+    expect(new AllExceptionsFilter()).toBeDefined();
+  });
+});
