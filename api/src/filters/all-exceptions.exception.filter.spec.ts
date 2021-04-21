ba7de72719333e014fa7a74ba0626cc989774387
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-04-21 16:16:04 +0200

    all-exception filter coverage. tests passing

--- api/src/filters/all-exceptions.exception.filter.spec.ts
+++ api/src/filters/all-exceptions.exception.filter.spec.ts
@@ -1,43 +1,67 @@
 import { AllExceptionsFilter } from './all-exceptions.exception.filter';
-import { HttpException, ArgumentsHost } from '@nestjs/common';
-import * as JSONAPISerializer from 'jsonapi-serializer';
-import { after } from 'lodash';
-jest.mock('jsonapi-serializer');
+import { HttpException, ArgumentsHost, HttpStatus } from '@nestjs/common';
 
-afterEach(() => {
-  jest.clearAllMocks();
-});
 describe('AllExceptionFilter class spec (unit)', () => {
   const allExceptionsFilter = new AllExceptionsFilter();
+
+  /**
+   * Common Mocks
+   */
+  const responseMock = {
+    status: jest.fn(function () {
+      return this;
+    }),
+    json: jest.fn(),
+    url: 'fakeUrl',
+  };
+
+  const hostMock = {
+    switchToHttp: () => {
+      return {
+        getResponse: () => responseMock,
+        getRequest: () => {
+          return { url: 1 };
+        },
+      };
+    },
+  } as ArgumentsHost;
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(new AllExceptionsFilter()).toBeDefined();
   });
-  it('should call Andoni to help', () => {
-    (JSONAPISerializer as jest.Mock).mockReturnValue({});
 
-    const exceptionMock = ({
-      getStatus: jest.fn(() => 500),
+  it('should get HTTP status through exception Object', () => {
+    const exceptionObjectMock = ({
+      getStatus: jest.fn(() => 404),
       message: 'Mock Message',
+      stack: 'Fake Error Stack',
     } as unknown) as HttpException;
 
-    const responseMock = {
-      status: jest.fn(function () {
-        return this;
-      }),
-      json: jest.fn(),
-    };
+    /**
+     * Prototype pollution to mock 'instanceof'
+     */
+    const exception = Object.create(HttpException.prototype);
+    const exceptionMock = Object.assign(exception, exceptionObjectMock);
 
-    const hostMock = {
-      switchToHttp: () => {
-        return {
-          getResponse: () => responseMock,
-          getRequest: () => {
-            return { url: 1 };
-          },
-        };
-      },
-    } as ArgumentsHost;
+    allExceptionsFilter.catch(exception, hostMock);
+
+    expect(responseMock.status).toHaveBeenCalledWith(exceptionMock.getStatus());
+    expect(responseMock.json).toHaveBeenCalled();
+  });
+
+  it('should get HTTP status through HttpStatus', () => {
+    const exceptionMock = ({
+      message: 'Mock Message',
+      stack: 'Fake Error Stack',
+    } as unknown) as Error;
     allExceptionsFilter.catch(exceptionMock, hostMock);
-    expect(responseMock.json).toHaveBeenCalledWith({ erros: [{}] });
+
+    expect(responseMock.status).toHaveBeenCalledWith(
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+    expect(responseMock.json).toHaveBeenCalled();
   });
 });
