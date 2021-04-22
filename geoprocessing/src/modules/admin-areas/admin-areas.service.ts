8cd64eab2bca92ffb520f04409eb3b96d87e817d
elpamart <elena.palao@vizzuality.com>
2021-04-22 12:53:43 +0200

    add validator in admin areas controller

--- geoprocessing/src/modules/admin-areas/admin-areas.service.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.service.ts
@@ -1,19 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { TileService } from 'src/modules/tile/tile.service';
-import { IsInt, IsIn } from 'class-validator';
+import { TileService, TileRequest } from 'src/modules/tile/tile.service';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsInt,
+  Max,
+  Min,
+} from 'class-validator';
 import { Transform } from 'class-transformer';
-const logger = new Logger('test-interface');
-/**
- * Supported admin area levels (national and sub-national): either level 0, level 1 or level 2.
- * @todo different from the adminAreaLevel in the api side
- * @todo check transformation from string to int that doesn't work.
- */
-export class AdminAreaLevelFilters {
-  // @IsIn([0, 1, 2])
-  // @IsInt()
-  @Transform((level: string) => parseInt(level))
+
+export class TileSpecification extends TileRequest{
+  @ApiProperty()
+  @Min(0)
+  @Max(2)
+  @IsInt()
+  @Transform(value =>  Number.parseInt(value))
   level: number;
 }
+
 
 @Injectable()
 export class AdminAreasService {
@@ -25,16 +28,15 @@ export class AdminAreasService {
    * @todo generate the custom queries using query builder and the entity data.
    * @todo move the string to int transformation to the AdminAreaLevelFilters class
    */
-  buildAdminAreaQuery(level: AdminAreaLevelFilters): string {
+  buildAdminAreaQuery(level: number): string {
     let customsql: string = '';
-    const adminAreaLevel = parseInt(`${level.level}`, 10);
-    if (adminAreaLevel === 0) {
+    if (level === 0) {
       customsql += `gid_0 IS NOT NULL AND gid_1 IS NULL AND gid_2 IS NULL`;
     }
-    if (adminAreaLevel === 1) {
+    if (level === 1) {
       customsql += `gid_1 IS NOT NULL AND gid_2 IS NULL`;
     }
-    if (adminAreaLevel === 2) {
+    if (level === 2) {
       customsql += `gid_2 IS NOT NULL`;
     }
     return customsql;
@@ -44,8 +46,8 @@ export class AdminAreasService {
    *
    * @todo find another way of parsing the attributes
    */
-  getAttributes(level: AdminAreaLevelFilters): string {
-    return `gid_${level.level}`;
+  getAttributes(level: number): string {
+    return `gid_${level}`;
   }
 
   /**
@@ -54,23 +56,26 @@ export class AdminAreasService {
    * @todo update the function to include guid apiQuery param
    */
   public findTile(
-    z: number,
-    x: number,
-    y: number,
-    level: number,
+    {
+      z,
+      x,
+      y,
+      level
+    }: {
+      z: number,
+      x: number,
+      y: number,
+      level: number
+    }
   ): Promise<Buffer> {
-    // if (parseInt(`${level}`, 10) > 2) {
-    //   throw new BadRequestException(
-    //     'An invalid level may have been provided. Level should be lower or equal to 2'
-    //     );
-    // }
-    const customQuery = this.buildAdminAreaQuery({ level });
-    const attributes = this.getAttributes({ level });
+    const customQuery = this.buildAdminAreaQuery(level);
+    const attributes = this.getAttributes(level);
     const table = 'admin_regions';
     const geometry = 'the_geom';
     const extent = 4096;
     const buffer = 256;
     const maxZoomLevel = 12;
+    console.log('params', level)
 
     return this.tileService.getTile(
       z,
