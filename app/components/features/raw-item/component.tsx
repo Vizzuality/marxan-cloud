7f981907af800c1cdc351d75fb15b296d2a70a03
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-04-23 10:00:03 +0200

    Scenario: Feature all infiniteQuery invalidation

--- app/components/features/raw-item/component.tsx
+++ app/components/features/raw-item/component.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { MutableRefObject } from 'react';
 import cx from 'classnames';
 
 import Button from 'components/button';
 import Tag from 'components/tag';
+import InfoButton from 'components/info-button';
+
+import { useInView } from 'react-intersection-observer';
 
 export interface ItemProps {
   id: string | number;
@@ -12,6 +15,7 @@ export interface ItemProps {
   description: string;
   tag?: string;
   source?: string;
+  scrollRoot?: MutableRefObject<HTMLDivElement | null>;
   selected?: boolean;
   onToggleSelected?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
@@ -23,19 +27,38 @@ export const Item: React.FC<ItemProps> = ({
   description,
   tag,
   source,
+  scrollRoot,
   selected,
   onToggleSelected,
 }: ItemProps) => {
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0,
+    root: scrollRoot.current,
+  });
+
   return (
     <div
+      ref={ref}
       className={cx({
         'bg-white px-0 py-6 text-black': true,
         [className]: !!className,
+        invisible: !inView,
       })}
     >
       <header className="flex items-baseline justify-between">
-        <div className="flex divide-x">
+        <div className="flex space-x-2">
           <h2 className="text-sm font-medium font-heading">{name}</h2>
+
+          {description && (
+            <InfoButton
+              theme="secondary"
+            >
+              <div className="text-sm opacity-50">
+                {description}
+              </div>
+            </InfoButton>
+          )}
 
           {!!categories && (
             <div className="pl-2 ml-2 text-sm underline">
@@ -82,8 +105,6 @@ export const Item: React.FC<ItemProps> = ({
           </div>
         )}
       </div>
-
-      <div className="mt-4 text-sm opacity-50 clamp-2">{description}</div>
     </div>
   );
 };
