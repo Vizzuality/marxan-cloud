99636404862f521c1452e7661670f2be2f14382e
andrea rota <andrea.rota@vizzuality.com>
2021-04-27 14:19:17 +0100

    quick&dirty suggestion on a possible way to test behaviour in different NOD_ENVs

--- api/test/json-api.e2e-spec.ts
+++ api/test/json-api.e2e-spec.ts
@@ -18,40 +18,89 @@ describe('JSON API Specs (e2e)', () => {
   afterAll(async () => {
     await Promise.all([app.close()]);
   });
-  it('should return a response shaped as JSON:API Error spec', async () => {
-    const jsonApiErrorResponse = {
-      id: null,
-      links: null,
-      status: null,
-      code: null,
-      source: null,
-      title: null,
-      meta: {
-        timestamp: null,
-        path: null,
-        type: null,
-        rawError: null,
-        stack: null,
-      },
-    };
-    const response = await request(app.getHttpServer())
-      .post('/auth/sign-in')
-      .send({
-        username: 'fakeuser@example.com',
-        password: 'fakePassword',
-      });
-    response.body.errors.forEach((err: any) => {
-      expect(Object.keys(jsonApiErrorResponse)).toEqual(
-        expect.arrayContaining(Object.keys(err)),
-      );
-      if (process.env.NODE_ENV != 'development') {
-        expect(Object.keys(err)).toEqual(
-          expect.arrayContaining(Object.keys(jsonApiErrorResponse.meta)),
-        );
-      }
-      expect(Object.keys(jsonApiErrorResponse.meta)).toEqual(
-        expect.arrayContaining(Object.keys(err.meta)),
-      );
+  describe(`Running with NODE_ENV=development`, () => {
+    beforeAll(() => {
+      process.env.NODE_ENV = 'development';
     });
-  });
+
+    it('should return a response shaped as JSON:API Error spec', async () => {
+      const jsonApiErrorResponse = {
+        id: null,
+        links: null,
+        status: null,
+        code: null,
+        source: null,
+        title: null,
+        meta: {
+          timestamp: null,
+          path: null,
+          type: null,
+          rawError: null,
+          stack: null,
+        },
+      };
+      const response = await request(app.getHttpServer())
+        .post('/auth/sign-in')
+        .send({
+          username: 'fakeuser@example.com',
+          password: 'fakePassword',
+        });
+      response.body.errors.forEach((err: any) => {
+        expect(Object.keys(jsonApiErrorResponse)).toEqual(
+          expect.arrayContaining(Object.keys(err)),
+        );
+        if (process.env.NODE_ENV != 'development') {
+          expect(Object.keys(err)).toEqual(
+            expect.arrayContaining(Object.keys(jsonApiErrorResponse.meta)),
+          );
+        }
+        expect(Object.keys(jsonApiErrorResponse.meta)).toEqual(
+          expect.arrayContaining(Object.keys(err.meta)),
+        );
+      });
+    });
+  })
+
+  describe(`Running with NODE_ENV=production`, () => {
+    beforeAll(() => {
+      process.env.NODE_ENV = 'production';
+    });
+
+    it('should return a response shaped as JSON:API Error spec', async () => {
+      const jsonApiErrorResponse = {
+        id: null,
+        links: null,
+        status: null,
+        code: null,
+        source: null,
+        title: null,
+        meta: {
+          timestamp: null,
+          path: null,
+          type: null,
+          rawError: null,
+          stack: null,
+        },
+      };
+      const response = await request(app.getHttpServer())
+        .post('/auth/sign-in')
+        .send({
+          username: 'fakeuser@example.com',
+          password: 'fakePassword',
+        });
+      response.body.errors.forEach((err: any) => {
+        expect(Object.keys(jsonApiErrorResponse)).toEqual(
+          expect.arrayContaining(Object.keys(err)),
+        );
+        if (process.env.NODE_ENV != 'development') {
+          expect(Object.keys(err)).toEqual(
+            expect.arrayContaining(Object.keys(jsonApiErrorResponse.meta)),
+          );
+        }
+        expect(Object.keys(jsonApiErrorResponse.meta)).toEqual(
+          expect.arrayContaining(Object.keys(err.meta)),
+        );
+      });
+    });
+  })
 });
