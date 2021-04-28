dbfe7f20a104622e216137b0fd4cf110b81a9f82
Alicia <alicia.arenzana@gmail.com>
2021-04-28 11:23:59 +0200

    fixed way we build the query

--- geoprocessing/src/modules/admin-areas/admin-areas.service.ts
+++ geoprocessing/src/modules/admin-areas/admin-areas.service.ts
@@ -1,53 +1,52 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { TileService, TileRequest, TileRenderer } from 'src/modules/tile/tile.service';
-import { ApiProperty } from '@nestjs/swagger';
+import { Injectable, Logger, Inject } from '@nestjs/common';
 import {
-  IsInt,
-  Max,
-  Min,
-} from 'class-validator';
+  TileService,
+  TileRequest,
+  TileRenderer,
+} from 'src/modules/tile/tile.service';
+import { ApiProperty } from '@nestjs/swagger';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { IsInt, Max, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
-
-export class TileSpecification extends TileRequest{
+import { AdminArea } from 'src/modules/admin-areas/admin-areas.geo.entity';
+import { inspect } from 'util';
+export class TileSpecification extends TileRequest {
   @ApiProperty()
   @Min(0)
   @Max(2)
   @IsInt()
-  @Transform(value =>  Number.parseInt(value))
+  @Transform((value) => Number.parseInt(value))
   level: number;
 }
 
-
 @Injectable()
-export class AdminAreasService<T> {
+export class AdminAreasService {
   private readonly logger: Logger = new Logger(AdminAreasService.name);
-  constructor(private readonly tileService: TileService<T>) {}
+  constructor(
+    @InjectRepository(AdminArea)
+    private readonly adminAreasRepository: Repository<AdminArea>,
+    @Inject(TileService)
+    private readonly tileService: TileService,
+  ) {}
 
   /**
    *
    * @todo generate the custom queries using query builder and the entity data.
    * @todo move the string to int transformation to the AdminAreaLevelFilters class
    */
-  buildAdminAreaQuery(level: number): string {
-    let customsql: string = '';
+  buildAdminAreaWhereQuery(level: number): string {
+    let whereQuery = '';
     if (level === 0) {
-      customsql += `gid_0 IS NOT NULL AND gid_1 IS NULL AND gid_2 IS NULL`;
+      whereQuery = `gid_0 IS NOT NULL AND gid_1 IS NULL AND gid_2 IS NULL`;
     }
     if (level === 1) {
-      customsql += `gid_1 IS NOT NULL AND gid_2 IS NULL`;
+      whereQuery = `gid_1 IS NOT NULL AND gid_2 IS NULL`;
     }
     if (level === 2) {
-      customsql += `gid_2 IS NOT NULL`;
+      whereQuery = `gid_2 IS NOT NULL`;
     }
-    return customsql;
-  }
-
-  /**
-   *
-   * @todo find another way of parsing the attributes
-   */
-  getAttributes(level: number): string {
-    return `gid_${level}`;
+    return whereQuery;
   }
 
   /**
@@ -55,18 +54,24 @@ export class AdminAreasService<T> {
    * @todo apply validation on the controller
    * @todo update the function to include guid apiQuery param
    */
-  public findTile(
-    tileSpecification: TileSpecification
-  ): Promise<Buffer> {
+  public findTile(tileSpecification: TileSpecification): Promise<Buffer> {
     const { z, x, y, level } = tileSpecification;
-    const customQuery = this.buildAdminAreaQuery(level);
-    const attributes = this.getAttributes(level);
-    const table = 'admin_regions';
+    const attributes = 'name_0';
+    const table = this.adminAreasRepository.metadata.tableName;
     const geometry = 'the_geom';
     const extent = 4096;
     const buffer = 256;
     const maxZoomLevel = 12;
-
+    const customQuery = this.buildAdminAreaWhereQuery(level);
+    this.logger.debug(
+      inspect(
+        this.adminAreasRepository.metadata.ownColumns.map((column) => {
+          if (column.isSelect) {
+            return column.entityMetadata.targetName;
+          }
+        }),
+      ),
+    );
     return this.tileService.getTile({
       z,
       x,
