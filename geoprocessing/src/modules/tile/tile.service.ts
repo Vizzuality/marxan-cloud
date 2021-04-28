a728a24a953756c026cb817f0b45e3c97a90d913
Alicia <alicia.arenzana@gmail.com>
2021-04-28 12:40:08 +0200

    improved query simplification capabilities

--- geoprocessing/src/modules/tile/tile.service.ts
+++ geoprocessing/src/modules/tile/tile.service.ts
@@ -3,15 +3,7 @@ import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { getConnection } from 'typeorm';
 import * as zlib from 'zlib';
 import { Transform } from 'class-transformer';
-<<<<<<< HEAD
-import { IsInt, Max, Min, } from 'class-validator';
-import { TileSpecification } from '../admin-areas/admin-areas.service';
-import { async } from 'rxjs';
-import { query } from 'express';
-import { result } from 'lodash';
-=======
 import { IsInt, Max, Min } from 'class-validator';
->>>>>>> fixed way we build the query
 
 /**
  * @description The specification of the tile request
@@ -70,11 +62,7 @@ export interface TileInput<T> extends TileRequest {
   /**
    * @description Custom query for the different entities
    */
-<<<<<<< HEAD
-  customQuery: string,
-=======
   customQuery: T;
->>>>>>> fixed way we build the query
   /**
    * @description attributes to be retrieved from the different entities
    */
@@ -87,11 +75,6 @@ export interface TileInput<T> extends TileRequest {
  */
 export type TileRenderer<TileInput> = (args: TileInput) => Promise<Buffer>;
 
-
-
-/**
- * @todo add generic type
- */
 @Injectable()
 export class TileService {
   /**
@@ -107,63 +90,7 @@ export class TileService {
    * connection is open to a minimum. This reduces the risk for the database-instance to run out of connections.
    * @param query the actual query to be sent to the database engine
    * @return the tile-data as Buffer wrapped in a promise.
-   *  WITH tile AS (
-    SELECT
-      ${tileInput.attributes},
-      ST_AsMVTGeom(
-        -- Geometry from table
-        ST_Transform(ST_RemoveRepeatedPoints(${tileInput.geometry}, 0.1), 3857),
-        -- MVT tile boundary
-        ST_TileEnvelope(${tileInput.z}, ${tileInput.x}, ${tileInput.y}),
-        -- Extent
-        ${tileInput.extent},
-        -- Buffer
-        ${tileInput.buffer},
-        -- Clip geom
-        true
-      ) AS mvt_geom
-    FROM ${tileInput.table}
-    WHERE
-      ST_Intersects(ST_Transform(ST_TileEnvelope(${tileInput.z}, ${tileInput.x}, ${tileInput.y}), 4326), ${tileInput.geometry})
-      and ${tileInput.customQuery}
-      and not gid_0 ='ATA'
-  )
-  SELECT ST_AsMVT(tile, 'layer0',  ${tileInput.extent}, 'mvt_geom') AS mvt FROM tile
    */
-<<<<<<< HEAD
-  buildQuery(
-    { z, x, y }: Pick<TileSpecification, 'z' | 'x' | 'y'>,
-    table: string,
-    geometry: string,
-    extent: number,
-    buffer: number,
-    maxZoomLevel: number,
-    customQuery: string,
-    attributes: string,
-  ): string {
-    let query = '';
-
-    try {
-      query = this.createQueryForTile({
-        z,
-        x,
-        y,
-        maxZoomLevel,
-        table,
-        geometry,
-        extent,
-        buffer,
-        customQuery,
-        attributes,
-      });
-      this.logger.debug(`Create query for tile: ${query}`);
-    } catch (error) {
-      this.logger.error(`Error getting the query: ${error}`);
-    }
-    return query;
-  }
-=======
->>>>>>> fixed way we build the query
 
   async fetchTileFromDatabase({
     z,
@@ -182,10 +109,18 @@ export class TileService {
       .createQueryBuilder()
       .select(`ST_AsMVT(tile, 'layer0', ${extent}, 'mvt_geom')`, 'mvt')
       .from((subQuery) => {
+        if (z > 7) {
+          subQuery.select(
+            `${attributes}, ST_AsMVTGeom(ST_Transform(${geometry}, 3857), ST_TileEnvelope(${z}, ${x}, ${y}), ${extent}, ${buffer}, true) AS mvt_geom`,
+          );
+        } else {
+          subQuery.select(
+            `${attributes}, ST_AsMVTGeom(ST_Transform(ST_RemoveRepeatedPoints(${geometry}, ${
+              0.1 / z
+            }), 3857), ST_TileEnvelope(${z}, ${x}, ${y}), ${extent}, ${buffer}, true) AS mvt_geom`,
+          );
+        }
         return subQuery
-          .select(
-            `${attributes}, ST_AsMVTGeom(ST_Transform(ST_RemoveRepeatedPoints(${geometry}, 0.1), 3857), ST_TileEnvelope(${z}, ${x}, ${y}), ${extent}, ${buffer}, true) AS mvt_geom`,
-          )
           .from(table, '')
           .where(
             `ST_Intersects(ST_Transform(ST_TileEnvelope(:z, :x, :y), 4326), ${geometry} )`,
@@ -194,12 +129,12 @@ export class TileService {
           .andWhere(customQuery)
           .andWhere("gid_0 != 'ATA'");
       }, 'tile');
-    this.logger.debug(query.getSql());
     const result = await query.getRawMany();
 
     if (result) {
       return result;
     } else {
+      this.logger.debug(query.getSql());
       throw new Error("Property 'mvt' does not exist in res.rows[0]");
     }
   }
@@ -229,48 +164,6 @@ export class TileService {
    * @todo Add tile server config type to getTile function
    * @todo add tile render type to promise
    */
-<<<<<<< HEAD
-  async getTile({
-    z,
-    x,
-    y,
-    table,
-    geometry,
-    extent,
-    buffer,
-    maxZoomLevel,
-    customQuery,
-    attributes,
-  }:TileInput<T>): Promise<Buffer> {
-    const query = this.buildQuery(
-      {
-        z,
-        x,
-        y
-      },
-      table,
-      geometry,
-      extent,
-      buffer,
-      maxZoomLevel,
-      customQuery,
-      attributes,
-    );
-    this.logger.debug('Query created');
-    // return async ({}: TileInput<T>) -- result - const queryResult --tile: wait this.zip(queryResult[0].mvt);
-    let data: Buffer = Buffer.from('');
-    if (query) {
-      try {
-        const queryResult: Record<'mvt', Buffer> [] = await this.fetchTileFromDatabase(query);
-        this.logger.debug('Data succesfully retrieved from database');
-        // zip data
-        data = await this.zip(queryResult[0].mvt);
-    this.logger.debug('Data compressed');
-      } catch (error) {
-        this.logger.error(`Database error: ${error.message}`);
-        return data;
-      }
-=======
   async getTile(tileInput: TileInput<string>): Promise<Buffer> {
     let data: Buffer = Buffer.from('');
     try {
@@ -278,14 +171,11 @@ export class TileService {
         'mvt',
         Buffer
       >[] = await this.fetchTileFromDatabase(tileInput);
-      this.logger.debug('Data succesfully retrieved from database');
       // zip data
       data = await this.zip(queryResult[0].mvt);
-      this.logger.debug('Data compressed');
     } catch (error) {
       this.logger.error(`Database error: ${error.message}`);
       throw new BadRequestException(error.message);
->>>>>>> fixed way we build the query
     }
     return data;
   }
