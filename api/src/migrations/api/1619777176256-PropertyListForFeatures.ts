07ec61d1c44139939774f6af2ac6fa57e164ce36
Alicia <alicia.arenzana@gmail.com>
2021-04-30 17:36:37 +0200

    added properties keys to features and added a better simplification algorithm

---
+++ api/src/migrations/api/1619777176256-PropertyListForFeatures.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class PropertyListForFeatures1619777176256
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    ALTER TABLE features
+    ADD COLUMN list_property_keys jsonb;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+     await queryRunner.query(`
+     ALTER TABLE features
+     DROP COLUMN list_property_keys;
+     `);
+  }
+}
