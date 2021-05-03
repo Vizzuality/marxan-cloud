82cb20c1f6c5dd5fb8f7676001415f752da06b69
alexeh <37807221+alexeh@users.noreply.github.com>
2021-05-03 08:00:07 +0200

    marxan 89 add test post patch data shapes (#143)
    - Add test to check POST/PATCH response shape
    -Add test to check paginated responses have pagination metadata

--- api/test/json-api.e2e-spec.ts
+++ api/test/json-api.e2e-spec.ts
@@ -3,9 +3,22 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { omit } from 'lodash';
+import { E2E_CONFIG } from './e2e.config';
+import { ProjectsTestUtils } from './utils/projects.test.utils';
+import { Organization } from 'modules/organizations/organization.api.entity';
+import { OrganizationsTestUtils } from './utils/organizations.test.utils';
+import * as JSONAPISerializer from 'jsonapi-serializer';
+import { Project } from 'modules/projects/project.api.entity';
 
 describe('JSON API Specs (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
+  let fakeOrganization: Organization;
+  let fakeProject: Project;
+  const fakeCountry: string = 'ESP';
+  const Deserializer = new JSONAPISerializer.Deserializer({
+    keyForAttribute: 'camelCase',
+  });
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,6 +27,37 @@ describe('JSON API Specs (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    /**
+     * Login User
+     */
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/sign-in')
+      .send({
+        username: E2E_CONFIG.users.basic.aa.username,
+        password: E2E_CONFIG.users.basic.aa.password,
+      })
+      .expect(201);
+
+    jwtToken = response.body.accessToken;
+
+    /**
+     * Create Scenario
+     */
+
+    fakeOrganization = await OrganizationsTestUtils.createOrganization(
+      app,
+      jwtToken,
+      E2E_CONFIG.organizations.valid.minimal(),
+    ).then(async (response) => await Deserializer.deserialize(response));
+
+    fakeProject = await ProjectsTestUtils.createProject(app, jwtToken, {
+      ...E2E_CONFIG.projects.valid.minimalInGivenAdminArea({
+        countryCode: fakeCountry,
+      }),
+      organizationId: fakeOrganization.id,
+    }).then(async (response) => await Deserializer.deserialize(response));
   });
 
   afterAll(async () => {
@@ -36,12 +80,11 @@ describe('JSON API Specs (e2e)', () => {
         stack: null,
       },
     };
+
     const response = await request(app.getHttpServer())
-      .post('/auth/sign-in')
-      .send({
-        username: 'fakeuser@example.com',
-        password: 'fakePassword',
-      });
+      .get('/api/v1/projects/fakeProject/features?q=fakeFeature')
+      .set('Authorization', `Bearer ${jwtToken}`);
+
     response.body.errors.forEach((err: any) => {
       expect(Object.keys(jsonApiErrorResponse)).toEqual(
         expect.arrayContaining(Object.keys(err)),
@@ -62,6 +105,54 @@ describe('JSON API Specs (e2e)', () => {
           Object.keys(jsonApiErrorResponse.meta),
         );
       }
+    });
+  });
+
+  it('should return a object with a "data" prop as a response to a POST request', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/api/v1/projects')
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send({
+        ...E2E_CONFIG.projects.valid.minimalInGivenAdminArea({
+          countryCode: fakeCountry,
+        }),
+        organizationId: fakeOrganization.id,
+      });
+
+    expect(response.body).toMatchObject({ data: expect.any(Object) });
+  });
+  it('should return a object with a "data" prop as a response to a PATCH request', async () => {
+    const response = await request(app.getHttpServer())
+      .patch(`/api/v1/projects/${fakeProject.id}`)
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send({
+        ...E2E_CONFIG.projects.valid.minimalInGivenAdminArea({
+          countryCode: fakeCountry,
+        }),
+        organizationId: fakeOrganization.id,
+      });
+
+    expect(response.body).toMatchObject({ data: expect.any(Object) });
+  });
+
+  it('should include pagination metadata as a paginated response', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/api/v1/projects/${fakeProject.id}/features`)
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send({
+        ...E2E_CONFIG.projects.valid.minimalInGivenAdminArea({
+          countryCode: fakeCountry,
+        }),
+        organizationId: fakeOrganization.id,
+      });
+
+    expect(response.body).toMatchObject({
+      meta: {
+        totalItems: expect.any(Number),
+        totalPages: expect.any(Number),
+        size: expect.any(Number),
+        page: expect.any(Number),
+      },
     });
   });
 });
