b1a61e51ffc6f65beb49b271315573894f60a190
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-05-06 12:09:50 +0200

    Get file in geoprocessing

--- geoprocessing/src/modules/planning-units/planning-units.controller.ts
+++ geoprocessing/src/modules/planning-units/planning-units.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Logger,
+  Get,
+  Req,
+  Param,
+  UploadedFile,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller('planning-units')
-export class PlanningUnitsController {}
+import { apiGlobalPrefixes } from 'src/api.config';
+
+@Controller(`${apiGlobalPrefixes.v1}/scenarios`)
+export class PlanningUnitsController {
+  private readonly logger: Logger = new Logger(PlanningUnitsController.name);
+  constructor() {}
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('/:id/planning-unit-lock-in')
+  async getShapeFile(
+    @Param('id') scenarioId: string,
+    @UploadedFile() file: any,
+  ) {
+    this.logger.log('GEOPROCESSING PLANNING UNITS CONTROLLER');
+    this.logger.log(scenarioId);
+    this.logger.log(file.buffer.toString());
+    return true;
+  }
+}
