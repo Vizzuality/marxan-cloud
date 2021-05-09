e23dafeaca8dac50666a3df86951c5c0a53c7cf9
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-05-09 17:06:46 +0200

    Mapshaper Buffer output from shp

--- geoprocessing/src/modules/shapefiles/shapefiles.service.ts
+++ geoprocessing/src/modules/shapefiles/shapefiles.service.ts
@@ -1,26 +1,84 @@
 import { Injectable, Logger } from '@nestjs/common';
 
 import multer from 'multer';
-import unzipper from 'unzipper';
-import path from 'path';
+
 import { createReadStream, readdirSync } from 'fs';
 const mapshaper = require('mapshaper');
+const unzipper = require('unzipper');
+const path = require('path');
 
 @Injectable()
 export class ShapeFileService {
   private readonly logger: Logger = new Logger(ShapeFileService.name);
   constructor() {}
 
-  private async _unzipShapefile(path: string) {
-    return await createReadStream(path).pipe(
-      unzipper.Extract({ path: '/../tmp/' + path.replace('.zip', '') }),
-    );
+  private async _unzipShapefile(fileInfo: Express.Multer.File): Promise<void> {
+    this.logger.log(fileInfo.filename);
+    await createReadStream(fileInfo.path)
+      .pipe(
+        unzipper.Extract({
+          path: fileInfo.destination + fileInfo.filename.replace('.zip', ''),
+        }),
+      )
+      .on('close', async function () {
+        console.log(
+          'FILE READ RESULT',
+          readdirSync(
+            path.join(
+              __dirname,
+              '..',
+              '..',
+              '..',
+              '..',
+              '..',
+              'tmp',
+              `${fileInfo.filename.replace('.zip', '')}`,
+            ),
+          ),
+        );
+        await mapshaper.applyCommands(
+          `-i ${fileInfo.path.replace(
+            '.zip',
+            '',
+          )}/POLYGON.shp -o format=geojson output.geojson`,
+
+          (err: any, output: any) => console.log(output),
+        );
+        console.log('COMMAND RUN');
+      });
   }
 
-  private async shapeFileToGeoJson(path: string) {}
-  getGeoJson(shapeFile: any) {
+  private async _shapeFileToGeoJson(fileInfo: Express.Multer.File) {
+    this.logger.log(fileInfo.path);
+    this.logger.log(
+      readdirSync(
+        path.join(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          '..',
+          'tmp',
+          `${fileInfo.filename.replace('.zip', '')}/layers`,
+        ),
+      ),
+    );
+
+    const _geoJson = await mapshaper.applyCommands(
+      `-i ${fileInfo.path.replace(
+        '.zip',
+        '',
+      )}/layers/POLYGON.shp -o format=geojson`,
+    );
+    this.logger.log(_geoJson);
+  }
+
+  async getGeoJson(shapeFile: any) {
     this.logger.log('SHAPEFILE SERVICE ');
-    console.log('SHAPEFILE FILE INFO', shapeFile);
+    const res = await this._unzipShapefile(shapeFile);
+    //this._shapeFileToGeoJson(shapeFile);
+
     return { message: 'SHAPEFILE SERVICE RESPONSE' };
   }
 }
