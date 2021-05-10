dfc9fa35702e65afc9a0918a9eec3399cf83df73
andrea rota <andrea.rota@vizzuality.com>
2021-05-10 10:17:59 +0100

    add composite index to allow for index scans

--- geoprocessing/src/migrations/geoprocessing/1620408879000-AddUniqueConstraintsToAdminRegionsTable.ts
+++ geoprocessing/src/migrations/geoprocessing/1620408879000-AddUniqueConstraintsToAdminRegionsTable.ts
@@ -7,6 +7,7 @@ export class AddUniqueConstraintsToAdminRegionsTable1620408879000
 CREATE UNIQUE INDEX unique_l0_regions ON admin_regions (gid_0, level) where level = 'country';
 CREATE UNIQUE INDEX unique_l1_regions ON admin_regions (gid_1, level) where level = 'adm_1';
 CREATE UNIQUE INDEX unique_l2_regions ON admin_regions (gid_2, level) where level = 'adm_2';
+CREATE INDEX admin_regions_l012_ids ON admin_regions (gid_0, gid_1, gid_2);
     `);
   }
 
@@ -15,6 +16,7 @@ CREATE UNIQUE INDEX unique_l2_regions ON admin_regions (gid_2, level) where leve
 DROP INDEX unique_l0_regions;
 DROP INDEX unique_l1_regions;
 DROP INDEX unique_l2_regions;
+DROP INDEX admin_regions_l012_ids;
      `);
   }
 }
