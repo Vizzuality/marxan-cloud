23de7c86695d26eb167126a700ba16e83cc8b13b
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-05-10 09:50:26 +0200

    Parsing GeoJSON

--- geoprocessing/src/modules/shapefiles/shapefiles.service.ts
+++ geoprocessing/src/modules/shapefiles/shapefiles.service.ts
@@ -9,7 +9,7 @@ export class ShapeFileService {
   private readonly logger: Logger = new Logger(ShapeFileService.name);
   constructor() {}
 
-  private _unzipShapefile(fileInfo: Express.Multer.File): Promise<String> {
+  private _unzipShapefile(fileInfo: Express.Multer.File): Promise<string> {
     return new Promise((resolve, reject) => {
       createReadStream(fileInfo.path)
         .pipe(
@@ -27,12 +27,12 @@ export class ShapeFileService {
   }
 
   private async _shapeFileToGeoJson(fileInfo: Express.Multer.File) {
+    const outputKey = `shapefile-${new Date().getTime()}.gejson`;
     const _geoJson = await mapshaper.applyCommands(
-      `-i ${fileInfo.path.replace('.zip', '')}/*.shp -o format=geojson`,
+      `-i ${fileInfo.path.replace('.zip', '')}/*.shp -o ${outputKey}`,
     );
-    this.logger.log(_geoJson);
 
-    return _geoJson;
+    return JSON.parse(_geoJson[outputKey].toString('utf-8'));
   }
 
   private wipeOutFolder(path: string): void {
@@ -42,9 +42,13 @@ export class ShapeFileService {
   async getGeoJson(shapeFile: any) {
     const res = await this._unzipShapefile(shapeFile);
     this.logger.log(res);
-    const geoJson = await this._shapeFileToGeoJson(shapeFile).then(() => {
-      this.wipeOutFolder(shapeFile.path.replace('.zip', ''));
-    });
+    const geoJson = await this._shapeFileToGeoJson(shapeFile).then(
+      (geoJson) => {
+        this.wipeOutFolder(shapeFile.path.replace('.zip', ''));
+        return geoJson;
+      },
+    );
+    this.logger.log(geoJson);
 
     return { message: 'SHAPEFILE SERVICE RESPONSE', data: geoJson };
   }
