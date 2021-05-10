92f6722ec51498e735e577e3e7c2c2c2d38551af
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-05-10 14:59:55 +0200

    returning geojson

--- geoprocessing/src/utils/file.utils.ts
+++ geoprocessing/src/utils/file.utils.ts
@@ -3,28 +3,25 @@
  */
 
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
-import { createReadStream, readdirSync } from 'fs';
+import { Request } from 'express';
 
 /**
  * ES5 import to avoid TS complaining
  */
 const multer = require('multer');
 
-/**
- * Options for Multer
- */
 export const uploadOptions: MulterOptions = {
   storage: multer.diskStorage({
     destination: function (
-      req: any,
-      file: any,
+      req: Request,
+      file: Express.Multer.File,
       cb: (error: Error | null, destination: string) => void,
     ) {
       cb(null, '/../tmp/');
     },
     filename: function (
-      req: any,
-      file: any,
+      req: Request,
+      file: Express.Multer.File,
       cb: (error: Error | null, fieldname: string) => void,
     ) {
       cb(null, Date.now() + '-' + file.originalname);
