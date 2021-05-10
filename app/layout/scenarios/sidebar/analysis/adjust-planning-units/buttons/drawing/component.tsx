5c4334726765aa59c7c10e94568f1f0b4c082a70
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-05-10 16:28:59 +0200

    Map: draw form

--- app/layout/scenarios/sidebar/analysis/adjust-planning-units/buttons/drawing/component.tsx
+++ app/layout/scenarios/sidebar/analysis/adjust-planning-units/buttons/drawing/component.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 
 import cx from 'classnames';
 
 import Button from 'components/button';
 import Icon from 'components/icon';
 
+import { Form as FormRFF, Field as FieldRFF } from 'react-final-form';
+
 import { useRouter } from 'next/router';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getScenarioSlice } from 'store/slices/scenarios/edit';
 
 import DRAW_SHAPE_SVG from 'svgs/ui/draw.svg?sprite';
@@ -25,90 +27,126 @@ export const AnalysisAdjustDrawing: React.FC<AnalysisAdjustDrawingProps> = ({
 
   const scenarioSlice = getScenarioSlice(sid);
   const { setDrawing, setDrawingGeo } = scenarioSlice.actions;
-
   const dispatch = useDispatch();
+  const { drawingGeo } = useSelector((state) => state[`/scenarios/${sid}/edit`]);
 
+  const INITIAL_VALUES = useMemo(() => {
+    return {
+      drawingGeo,
+    };
+  }, [drawingGeo]);
+
+  // Effects
   useEffect(() => {
-    dispatch(setDrawing('polygon'));
+    if (selected) {
+      dispatch(setDrawing('polygon'));
+    }
 
+    if (!selected) {
+      dispatch(setDrawing(null));
+      dispatch(setDrawingGeo(null));
+    }
+
+    // Unmount
     return () => {
       dispatch(setDrawing(null));
       dispatch(setDrawingGeo(null));
     };
-  }, []); // eslint-disable-line
+  }, [selected]); // eslint-disable-line
+
+  // Callbacks
+  const onSubmit = useCallback((values) => {
+    // Save current drawn shape
+    console.info(values);
+  }, []);
 
   return (
-    <div
-      key="drawing"
-      role="presentation"
-      className={cx({
-        'text-sm py-2.5 focus:outline-none relative transition rounded-3xl px-10 cursor-pointer': true,
-        'bg-gray-600 text-gray-200 opacity-50': !selected,
-        'bg-gray-600 text-white': selected,
-      })}
-      onClick={() => onSelected('drawing')}
+    <FormRFF
+      key="features-list"
+      onSubmit={onSubmit}
+      initialValues={INITIAL_VALUES}
     >
-      <header className="relative flex justify-between w-full">
-        <div
-          className={cx({
-            'text-center': !selected,
-            'text-left': selected,
-          })}
-        >
-          Draw shape on map
-        </div>
-
-        {!selected && (
-          <Icon
-            className="absolute right-0 w-5 h-5 transform -translate-y-1/2 top-1/2"
-            icon={DRAW_SHAPE_SVG}
-          />
-        )}
-
-        {selected && (
-        <div className="flex items-center space-x-3 divide-x-2">
-          <Button
-            theme="secondary-alt"
-            size="s"
-            onClickCapture={() => {
-              onSelected(null);
-            }}
+      {({ handleSubmit }) => (
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <FieldRFF
+            name="drawingGeo"
           >
-            Cancel
-          </Button>
+            {({ input }) => (<input type="hidden" {...input} />)}
+          </FieldRFF>
 
-          <Button
-            type="submit"
-            theme="primary"
-            size="s"
+          <div
+            key="drawing"
+            role="presentation"
+            className={cx({
+              'text-sm py-2.5 focus:outline-none relative transition rounded-3xl px-10 cursor-pointer': true,
+              'bg-gray-600 text-gray-200 opacity-50': !selected,
+              'bg-gray-600 text-white': selected,
+            })}
+            onClick={() => onSelected('drawing')}
           >
-            Save
-          </Button>
-          {/* <button
-            type="button"
-            className="flex items-center justify-center h-5 pl-5 pr-1 focus:outline-none"
-            onClickCapture={() => {
-              setSelected(null);
-            }}
-          >
-            <Icon
-              className="w-3 h-3 text-primary-500"
-              icon={ARROW_UP_SVG}
-            />
-          </button> */}
-        </div>
-        )}
-      </header>
+            <header className="relative flex justify-between w-full">
+              <div
+                className={cx({
+                  'text-center': !selected,
+                  'text-left': selected,
+                })}
+              >
+                Draw shape on map
+              </div>
 
-      {selected && (
-        <div className="pt-2">
-          <div className="flex w-full">
-            <p className="text-sm text-gray-300">Click over the map, and select planning units.</p>
+              {!selected && (
+                <Icon
+                  className="absolute right-0 w-5 h-5 transform -translate-y-1/2 top-1/2"
+                  icon={DRAW_SHAPE_SVG}
+                />
+              )}
+
+              {selected && (
+                <div className="flex items-center space-x-3 divide-x-2">
+                  <Button
+                    theme="secondary-alt"
+                    size="s"
+                    onClickCapture={() => {
+                      onSelected(null);
+                    }}
+                  >
+                    Cancel
+                  </Button>
+
+                  <Button
+                    type="submit"
+                    theme="primary"
+                    size="s"
+                  >
+                    Save
+                  </Button>
+                  {/* <button
+                    type="button"
+                    className="flex items-center justify-center h-5 pl-5 pr-1 focus:outline-none"
+                    onClickCapture={() => {
+                      setSelected(null);
+                    }}
+                  >
+                    <Icon
+                      className="w-3 h-3 text-primary-500"
+                      icon={ARROW_UP_SVG}
+                    />
+                  </button> */}
+                </div>
+              )}
+            </header>
+
+            {selected && (
+              <div className="pt-2">
+                <div className="flex w-full">
+                  <p className="text-sm text-gray-300">Click over the map, and select planning units.</p>
+                </div>
+              </div>
+            )}
           </div>
-
-        </div>
+        </form>
       )}
-    </div>
+    </FormRFF>
   );
 };
 
