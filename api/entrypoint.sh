bbe45df1eb374d1d4758446ceed7b97627051df4
Alicia <alicia.arenzana@gmail.com>
2021-05-10 10:24:59 +0200

    test entrypoint env set inside api docker container

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -15,8 +15,8 @@ case "$1" in
         exec yarn test:e2e
         ;;
     run-migrations-for-e2e-tests)
-        echo "Running migrations for e2e Tests"
-        sleep 15
+        echo "\u001b[34m Running migrations for e2e Tests in Api \u001b[0m"
+        echo $(GEO_POSTGRES_PASSWORD)
         yarn typeorm migration:run
         ;;
     start)
