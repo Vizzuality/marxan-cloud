5cd508de0d2022a9836ad756ce4ad9a77d31431e
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-05-11 09:30:15 +0200

    Add DTP for better typing
    Add swaggerdocs in API and geoprocessing

--- geoprocessing/src/modules/planning-units/planning-units.controller.ts
+++ geoprocessing/src/modules/planning-units/planning-units.controller.ts
@@ -8,16 +8,25 @@ import {
   ParseUUIDPipe,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBody, ApiConsumes, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 import { apiGlobalPrefixes } from 'src/api.config';
 import { uploadOptions } from 'src/utils/file.utils';
+import { ShapefileGeoJSONResponseDTO } from '../shapefiles/dto/shapefile.geojson.response.dto';
 import { ShapeFileService } from '../shapefiles/shapefiles.service';
 
 @Controller(`${apiGlobalPrefixes.v1}/planning-units`)
 export class PlanningUnitsController {
   private readonly logger: Logger = new Logger(PlanningUnitsController.name);
   constructor(public shapefileService: ShapeFileService) {}
+  @ApiOperation({
+    description: 'Upload Zip file containing .shp, .dbj, .prj and .shx files',
+  })
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     schema: {
@@ -30,15 +39,15 @@ export class PlanningUnitsController {
       },
     },
   })
-  @ApiOperation({
-    description: 'Upload Zip file containing .shp, .dbj, .prj and .shx files',
+  @ApiResponse({
+    type: ShapefileGeoJSONResponseDTO,
   })
   @UseInterceptors(FileInterceptor('file', uploadOptions))
   @Post('/:id/planning-unit-shapefile')
   async getShapeFile(
     @Param('id', ParseUUIDPipe) scenarioId: string,
     @UploadedFile() file: Express.Multer.File,
-  ) {
+  ): Promise<ShapefileGeoJSONResponseDTO> {
     return this.shapefileService.getGeoJson(file);
   }
 }
