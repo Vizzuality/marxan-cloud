0bd0187ef3ca138d547ea48ffcd7f653f04311ba
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-05-11 12:07:24 +0200

    Gap analysis

--- app/layout/scenarios/sidebar/analysis/gap-analysis/component.tsx
+++ app/layout/scenarios/sidebar/analysis/gap-analysis/component.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { motion } from 'framer-motion';
 
 import Icon from 'components/icon';
 
-import ARROW_LEFT_SVG from 'svgs/ui/arrow-left.svg?sprite';
+import ARROW_LEFT_SVG from 'svgs/ui/arrow-right-2.svg?sprite';
 
-export interface ScenariosSidebarAnalysisSectionsProps {
+import Toolbar from 'layout/scenarios/sidebar/analysis/gap-analysis/toolbar';
+import List from 'layout/scenarios/sidebar/analysis/gap-analysis/list';
+
+export interface ScenariosGapAnalysisProps {
   onChangeSection: (s: string) => void;
 }
 
-export const ScenariosSidebarAnalysisSections: React.FC<ScenariosSidebarAnalysisSectionsProps> = ({
+export const ScenariosGapAnalysis: React.FC<ScenariosGapAnalysisProps> = ({
   onChangeSection,
-}: ScenariosSidebarAnalysisSectionsProps) => {
+}: ScenariosGapAnalysisProps) => {
+  const [search, setSearch] = useState(null);
+
+  const onSearch = useCallback((s) => {
+    setSearch(s);
+  }, []);
+
   return (
     <motion.div
       key="gap-analysis"
@@ -21,22 +30,28 @@ export const ScenariosSidebarAnalysisSections: React.FC<ScenariosSidebarAnalysis
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <button
-        type="button"
-        className="flex items-center w-full py-5 space-x-1 text-left"
-        onClick={() => {
-          onChangeSection(null);
-        }}
-      >
-        <Icon icon={ARROW_LEFT_SVG} className="w-3 h-3" />
-        <h4 className="text-xs uppercase font-heading">Gap analysis</h4>
-      </button>
+      <header className="mb-5">
+        <button
+          type="button"
+          className="flex items-center w-full pt-5 pb-1 space-x-2 text-left focus:outline-none"
+          onClick={() => {
+            onChangeSection(null);
+          }}
+        >
+          <Icon icon={ARROW_LEFT_SVG} className="w-3 h-3 transform rotate-180 text-primary-500" />
+          <h4 className="text-xs uppercase font-heading">Gap analysis</h4>
+        </button>
+      </header>
 
-      <div>
-        List of features
+      <div className="relative flex flex-col flex-grow w-full min-h-0 overflow-hidden">
+        <Toolbar search={search} onSearch={onSearch} />
+
+        <List
+          search={search}
+        />
       </div>
     </motion.div>
   );
 };
 
-export default ScenariosSidebarAnalysisSections;
+export default ScenariosGapAnalysis;
