bd5b33512fbb3c36f582360b9d5dded3c5c5b269
Kamil Gajowy <kgajowy@users.noreply.github.com>
2021-05-12 09:20:02 +0200

    feat(api): scenarios: wdpa calculation trigger
    * feat(api): scenarios: wdpa calculation trigger
    
    * feat(api): scenarios: wdpa calculation trigger - update conditions
    
    * chore(api): scenarios: wdpa-area-calculation rename

---
+++ api/src/modules/scenarios/__mocks__/scenario.data.ts
@@ -0,0 +1,29 @@
+import { JobStatus, Scenario, ScenarioType } from '../scenario.api.entity';
+import { User } from '../../users/user.api.entity';
+import { IUCNCategory } from '../../protected-areas/protected-area.geo.entity';
+
+const scenarioBase = (): Scenario => ({
+  createdAt: new Date('2021-05-10T10:25:11.959Z'),
+  lastModifiedAt: new Date('2021-05-10T10:25:11.959Z'),
+  createdBy: '00000000-0000-0000-0000-000000000000',
+  createdByUser: {} as User,
+  id: '00000000-0000-0000-0000-000000000000',
+  name: `Scenario Name`,
+  projectId: '00000000-0000-0000-0000-000000000000',
+  status: JobStatus.done,
+  type: ScenarioType.marxan,
+  users: [],
+  wdpaThreshold: undefined,
+  wdpaIucnCategories: undefined,
+  protectedAreaFilterByIds: undefined,
+});
+
+export const scenarioWithRequiredWatchedEmpty = (): Scenario => ({
+  ...scenarioBase(),
+  wdpaThreshold: undefined,
+});
+
+export const scenarioWithAllWatchedPresent = (): Scenario => ({
+  ...scenarioBase(),
+  wdpaThreshold: 40,
+});
