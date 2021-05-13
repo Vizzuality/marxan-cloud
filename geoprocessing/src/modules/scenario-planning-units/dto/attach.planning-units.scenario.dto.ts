9a12ef785ffd098eae6085d230defbd597bb0ca8
Alicia <alicia.arenzana@gmail.com>
2021-05-13 12:11:12 +0200

    added dtos to geoprocessing of the expected data to be shared from the api job setup

--- geoprocessing/src/modules/scenario-planning-units/dto/attach.planning-units.scenario.dto.ts
+++ geoprocessing/src/modules/scenario-planning-units/dto/attach.planning-units.scenario.dto.ts
@@ -1,44 +1,5 @@
 import {
   IsOptional,
-<<<<<<< HEAD
-  IsEnum,
-  IsUUID,
-  Min,
-  Max,
-  IsInt,
-  IsArray,
-} from 'class-validator';
-import { PlanningUnitsJob } from 'src/modules/planning-units/dto/create.regular.planning-units.dto';
-import { IUCNCategory } from 'src/modules/protected-areas/protected-areas.geo.entity';
-
-/**
- * @description
- * With the scenario, we can easily retrieve the pus so we can attache them to the scenario
- * Also if wdpaThreshold is in the data shared with the job we should concatenate it with the
- * calculation of level of protection of the PUs
- * @todo
- * We have this dto partially duplicated in the api service in
- * @file api/src/modules/scenarios/dto/create.scenario.dto.ts
- */
-export class PlanningUnitsForScenarioJob extends PlanningUnitsJob {
-  @IsUUID(4)
-  scenarioId!: string;
-
-  @IsOptional()
-  @IsArray()
-  @IsEnum(IUCNCategory, { each: true })
-  wdpaIucnCategories?: IUCNCategory[];
-
-  @IsOptional()
-  @IsUUID(4, { each: true })
-  customProtectedAreaIds?: string[];
-
-  @IsInt()
-  @IsOptional()
-  @Min(0)
-  @Max(100)
-  wdpaThreshold?: number;
-=======
   IsISO31661Alpha3,
   IsEnum,
   IsPositive,
@@ -88,5 +49,4 @@ export class PlanningUnitsForScenarioJob {
   // TODO (debt) there is no validation happening even with decorator
   @ValidateNested()
   extent?: Polygon;
->>>>>>> wip
 }
--- geoprocessing/src/modules/scenario-planning-units/scenario-planning-units.job.ts
+++ geoprocessing/src/modules/scenario-planning-units/scenario-planning-units.job.ts
@@ -4,14 +4,10 @@ import { geoprocessingConnections } from 'src/ormconfig';
 import { createConnection } from 'typeorm';
 import { validate } from 'class-validator';
 
-<<<<<<< HEAD
-import { PlanningUnitsForScenarioJob } from './dto/attach.planning-units.scenario.dto';
-=======
 import {
   PlanningUnitsForScenarioJob,
   PlanningUnitGridShape,
 } from './dto/attach.planning-units.scenario.dto';
->>>>>>> wip
 import { plainToClass } from 'class-transformer';
 
 const logger = new Logger('planning-units-job-processor');
@@ -48,17 +44,7 @@ const attachPlanningUnitGridToScenarioJobSpec = async (
     try {
       /**
        * @todo
-<<<<<<< HEAD
-       * From the Project
-       * * Get the extent/admin-region used to generate the geometries of the PU
-       * * Get the size and type of the pu
-       * After that Insert into the table scenarios_pu_data.
-       * Need to trigger the calcs for area protection if:
-       * * iucn_cat or wdpaids are seted
-       * * And threshold is defined by user.
-=======
        * TBD this will also need to trigger if is already defined the calcs for area protection
->>>>>>> wip
        */
       return 0;
     } catch (err) {
