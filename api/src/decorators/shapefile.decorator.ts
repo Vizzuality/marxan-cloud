30e41220122da4b38fe1598fd8167dcfd46d3fe5
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-05-13 14:55:50 +0200

    chore(api): scenario cost surfaces shell

--- api/src/decorators/shapefile.decorator.ts
+++ api/src/decorators/shapefile.decorator.ts
@@ -8,6 +8,26 @@ import {
 
 import { ShapefileGeoJSONResponseDTO } from 'modules/scenarios/dto/shapefile.geojson.response.dto';
 
+export function ApiConsumesShape() {
+  return applyDecorators(
+    ApiOperation({
+      description: 'Upload Zip file containing .shp, .dbj, .prj and .shx files',
+    }),
+    ApiConsumes('multipart/form-data'),
+    ApiBody({
+      schema: {
+        type: 'object',
+        properties: {
+          file: {
+            type: 'Zip file containing .shp, .dbj, .prj and .shx files',
+            format: 'binary',
+          },
+        },
+      },
+    }),
+  );
+}
+
 export function ApiConsumesShapefile() {
   return applyDecorators(
     ApiOperation({
