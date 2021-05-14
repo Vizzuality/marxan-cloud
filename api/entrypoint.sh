7817e3160f168e5e749c722afbe7f038961aae47
andrea rota <andrea.rota@vizzuality.com>
2021-05-14 18:20:10 +0100

    remove debug output

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -16,7 +16,6 @@ case "$1" in
         ;;
     run-migrations-for-e2e-tests)
         echo "(ESC)[44m Running migrations (api db) for e2e Tests in Api (ESC)[0m"
-        env | grep -v SECRET | grep -v PASSWORD | grep -v KEY
         exec yarn typeorm migration:run
         ;;
     start)
