5670c6a7b5a1342afa5dfaa972ab32a4777bba18
andrea rota <andrea.rota@vizzuality.com>
2021-05-14 17:38:58 +0100

    backport fix to remove not applicable header in exception handler

--- geoprocessing/src/filters/all-exceptions.exception.filter.ts
+++ geoprocessing/src/filters/all-exceptions.exception.filter.ts
@@ -68,6 +68,7 @@ export class AllExceptionsFilter implements ExceptionFilter {
         : errorData,
     );
 
+    response.removeHeader('content-encoding');
     response
       .status(status)
       .header('Content-Type', 'application/json')
