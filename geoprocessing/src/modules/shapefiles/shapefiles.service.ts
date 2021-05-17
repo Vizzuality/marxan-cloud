3476d520f75c7d61e6c79b00611c72bcd20dce3d
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-05-17 15:03:15 +0200

    Create standalone FileService

--- geoprocessing/src/modules/shapefiles/shapefiles.service.ts
+++ geoprocessing/src/modules/shapefiles/shapefiles.service.ts
@@ -1,35 +1,13 @@
 import { Injectable, Logger } from '@nestjs/common';
 
 import { createReadStream, rmdirSync, unlinkSync } from 'fs';
-import { Extract } from 'unzipper';
+
 const mapshaper = require('mapshaper');
 
 @Injectable()
 export class ShapeFileService {
   constructor(private readonly logger: Logger) {
     this.logger.setContext(ShapeFileService.name);
-  }
-
-  private unzipShapefile(fileInfo: Express.Multer.File): Promise<string> {
-    return new Promise((resolve, reject) => {
-      createReadStream(fileInfo.path)
-        .pipe(
-          Extract({
-            path: `${fileInfo.destination}/${fileInfo.filename.replace(
-              '.zip',
-              '',
-            )}`,
-          }),
-        )
-        .on('close', () =>
-          resolve(`${fileInfo.filename} extracted successfully`),
-        )
-        .on('error', (error: Error) =>
-          reject(
-            new Error(`${fileInfo.filename} could not be extracted: ` + error),
-          ),
-        );
-    });
   }
 
   private async shapeFileToGeoJson(fileInfo: Express.Multer.File) {
@@ -40,15 +18,6 @@ export class ShapeFileService {
     );
 
     return JSON.parse(_geoJson[outputKey].toString('utf-8'));
-  }
-
-  private deleteShapefileData(path: string): void {
-    if (path.startsWith('/tmp')) {
-      unlinkSync(path);
-      rmdirSync(path.replace('.zip', ''), { recursive: true });
-    } else {
-      throw new Error(`Could not complete deletion: ${path} is not in /tmp`);
-    }
   }
 
   async getGeoJson(shapeFile: Express.Multer.File) {
--- geoprocessing/src/utils/file.utils.ts
+++ geoprocessing/src/utils/file.utils.ts
@@ -2,10 +2,13 @@
  * File Utils
  */
 
+import { Injectable } from '@nestjs/common';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { Request } from 'express';
 import { diskStorage } from 'multer';
 import * as tempDirectory from 'temp-dir';
+import { createReadStream, rmdirSync, unlinkSync } from 'fs';
+import { Extract } from 'unzipper';
 
 export const uploadOptions: Readonly<MulterOptions> = {
   storage: diskStorage({
@@ -25,3 +28,37 @@ export const uploadOptions: Readonly<MulterOptions> = {
     },
   }),
 };
+
+@Injectable()
+export class FileSevice {
+  unzipFile(fileInfo: Express.Multer.File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      createReadStream(fileInfo.path)
+        .pipe(
+          Extract({
+            path: `${fileInfo.destination}/${fileInfo.filename.replace(
+              '.zip',
+              '',
+            )}`,
+          }),
+        )
+        .on('close', () =>
+          resolve(`${fileInfo.filename} extracted successfully`),
+        )
+        .on('error', (error: Error) =>
+          reject(
+            new Error(`${fileInfo.filename} could not be extracted: ` + error),
+          ),
+        );
+    });
+  }
+
+  private deleteDataFromFS(path: string): void {
+    if (path.startsWith('/tmp')) {
+      unlinkSync(path);
+      rmdirSync(path.replace('.zip', ''), { recursive: true });
+    } else {
+      throw new Error(`Could not complete deletion: ${path} is not in /tmp`);
+    }
+  }
+}
