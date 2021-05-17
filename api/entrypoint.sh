ea9dce20320a67bd68dca429c9956b44fcb23917
andrea rota <andrea.rota@vizzuality.com>
2021-05-17 09:47:02 +0100

    (test) run each test suite separately

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -12,7 +12,19 @@ case "$1" in
         ;;
     test-e2e)
         echo "Running e2e Tests"
-        exec yarn test:e2e
+        # exec yarn test:e2e
+        exec yarn test:e2e auth
+        exec yarn test:e2e countries
+        exec yarn test:e2e geo-features
+        exec yarn test:e2e json-api
+        exec yarn test:e2e organizations
+        exec yarn test:e2e planning-units
+        exec yarn test:e2e plan.s01e02
+        exec yarn test:e2e projects
+        exec yarn test:e2e protected-areas
+        exec yarn test:e2e proxy.vector-tiles
+        exec yarn test:e2e scenarios
+        exec yarn test:e2e users
         ;;
     run-migrations-for-e2e-tests)
         echo "(ESC)[44m Running migrations (api db) for e2e Tests in Api (ESC)[0m"
