c68e09a941e5178142b3a04351dd4303a8a4eb2c
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-05-19 13:39:36 +0200

    feat(api): product testing - raise timeout

--- e2e-product-testing/cypress/integration/login.ts
+++ e2e-product-testing/cypress/integration/login.ts
@@ -1,18 +1,30 @@
 describe("MVP Flow", () => {
   it("Create project", () => {
-    cy.visit("https://marxan.vercel.app/");
+    cy.visit("https://marxan.vercel.app/", {
+      timeout: 10000,
+    });
     cy.contains("Sign in").click();
 
     cy.get("#login-username").type("aa@example.com");
     cy.get("#login-password").type("aauserpassword");
 
-    cy.get("main > div").contains("Sign in").click();
+    cy.get("main > div").contains("Sign in").click({
+      timeout: 20000,
+    });
+    cy.screenshot();
 
-    cy.contains("Create new project").click();
+    cy.contains("Create new project").click({
+      timeout: 10000,
+    });
 
     cy.get("#name").type(`My Test Project ${new Date().getTime()}`);
     cy.get("#description").type(`Lorem Ipsum`);
 
-    cy.contains("Save").click();
+    cy.contains("Save").click({
+      timeout: 10000,
+    });
+
+    cy.wait(2000);
+    cy.screenshot();
   });
 });
