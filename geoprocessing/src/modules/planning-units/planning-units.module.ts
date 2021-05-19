0b91f5cc556c378dde226ea1b992c701b0d6f25b
Alex Larranaga <alex.larranaga@vizzuality.com>
2021-05-19 12:19:12 +0200

    Solve conflicts

--- geoprocessing/src/modules/planning-units/planning-units.module.ts
+++ geoprocessing/src/modules/planning-units/planning-units.module.ts
@@ -9,6 +9,7 @@ import { PlanningUnitsService } from './planning-units.service';
 import { FileService } from '../files/files.service';
 
 import { WorkerModule } from '../worker/worker.module';
+import { FileService } from 'src/utils/file.utils';
 
 @Module({
   imports: [
