933d4d8fb2e5a4f4230b6f90b9816284c0ed0081
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-05-19 12:45:45 +0200

    feat(api): product testing

---
+++ e2e-product-testing/cypress/integration/login.ts
@@ -0,0 +1,18 @@
+describe("My First Test", () => {
+  it("Visits the Kitchen Sink", () => {
+    cy.visit("https://marxan.vercel.app/");
+    cy.contains("Sign in").click();
+
+    cy.get("#login-username").type("aa@example.com");
+    cy.get("#login-password").type("aauserpassword");
+
+    cy.get("main > div").contains("Sign in").click();
+
+    cy.contains("Create new project").click();
+
+    cy.get("#name").type(`My Test Project ${new Date().getTime()}`);
+    cy.get("#description").type(`Lorem Ipsum`);
+
+    cy.contains("Save").click();
+  });
+});
