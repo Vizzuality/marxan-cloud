659c82a5b23e0dab11217cc109e728a98e6936f6
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-05-19 16:18:34 +0200

    feat(api): product testing - gherkin syntax

--- e2e-product-testing/cypress/plugins/index.js
+++ e2e-product-testing/cypress/plugins/index.js
@@ -1,22 +1,12 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const browserify = require("@cypress/browserify-preprocessor");
+const cucumber = require("cypress-cucumber-preprocessor").default;
+const resolve = require("resolve");
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  const options = {
+    ...browserify.defaultOptions,
+    typescript: resolve.sync("typescript", { baseDir: config.projectRoot }),
+  };
+
+  on("file:preprocessor", cucumber(options));
+};
