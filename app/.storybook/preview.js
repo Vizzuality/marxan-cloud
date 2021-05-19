ea131fe6dd62c4c2f0ed56eb4eae03b7c7bd5471
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-05-19 17:50:40 +0200

    Docs: structure

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -5,6 +5,15 @@ import { OverlayProvider } from '@react-aria/overlays';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  options: {
+    storySort: {
+      order: [
+        'Intro',
+        'Docs', ['Install', 'Deploy', 'Authentication', 'Fetching', 'Media', 'Tests'],
+        'Components'
+      ],
+    },
+  },
   docs: {
     theme: themes.dark,
   },
