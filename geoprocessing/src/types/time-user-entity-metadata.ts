3e35a5786c3fbd3d5de34c20b5e957320d1656f0
Kamil Gajowy <kgajowy@users.noreply.github.com>
2021-05-20 19:39:00 +0200

    chore(geoprocessing): wdpa - update entity with projectId
    * chore(geoprocessing): wdpa - update entity to reflect current model schema
    
    * chore(geoprocessing): wdpa - add project id
    
    * chore(geoprocessing): wdpa - update entity to match internal names of api entity reflection
    
    * chore(geoprocessing): wdpa - move srid to project const/type

---
+++ geoprocessing/src/types/time-user-entity-metadata.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+/**
+ * duplicated in API - except the relations
+ */
+export abstract class TimeUserEntityMetadata {
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp without time zone',
+    nullable: true,
+  })
+  createdAt?: Date | null;
+
+  @Column('uuid', { name: 'created_by', nullable: true })
+  createdBy?: string | null;
+
+  @UpdateDateColumn({
+    name: 'last_modified_at',
+    type: 'timestamp without time zone',
+    nullable: true,
+  })
+  lastModifiedAt?: Date | null;
+}
