4f9fa835925668e089754fc42ea35f68c01b4b98
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-05-31 11:09:37 +0200

    Project new: scroll

--- app/layout/projects/new/form/component.tsx
+++ app/layout/projects/new/form/component.tsx
@@ -80,104 +80,109 @@ const ProjectForm: React.FC<ProjectFormProps> = () => {
         <form
           onSubmit={handleSubmit}
           autoComplete="off"
-          className="flex flex-col justify-between w-full py-8 pl-8"
+          className="flex flex-col justify-between flex-grow w-full overflow-hidden"
         >
-          <div>
-            <h1 className="max-w-xs text-2xl text-white font-heading">
-              Name your project and define a planning area:
-            </h1>
+          <div className="relative flex flex-col flex-grow min-h-0 overflow-hidden">
+            <div className="absolute top-0 left-0 z-10 w-full h-6 bg-gradient-to-b from-gray-700 via-gray-700" />
 
-            {/* NAME */}
-            <div className="mt-8">
-              <FieldRFF
-                name="name"
-                validate={composeValidators([{ presence: true }])}
-              >
-                {(fprops) => (
-                  <Field id="name" {...fprops}>
-                    <Label theme="dark" className="mb-3 uppercase">Project Name</Label>
-                    <Input theme="dark" type="text" placeholder="Write project name..." />
-                  </Field>
-                )}
-              </FieldRFF>
-            </div>
+            <div className="flex flex-col flex-grow p-8 overflow-auto">
+              <h1 className="max-w-xs text-2xl text-white font-heading">
+                Name your project and define a planning area:
+              </h1>
 
-            {/* DESCRIPTION */}
-            <div className="mt-8">
-              <FieldRFF
-                name="description"
-                validate={composeValidators([{ presence: true }])}
-              >
-                {(fprops) => (
-                  <Field id="description" {...fprops}>
-                    <Label theme="dark" className="mb-3 uppercase">Description</Label>
-                    <Textarea rows={4} placeholder="Write your project description..." />
-                  </Field>
-                )}
-              </FieldRFF>
-            </div>
-
-            {/* TEMPORARILY HIDDEN, it will be implemented in the future
-            <h2 className="mt-12 text-white font-heading">
-              Do you have a planning region shapefile?
-            </h2> */}
-
-            {/* PLANNING AREA */}
-            <div className="flex items-center justify-between mt-6">
-              <div className="flex items-center">
-                <Label theme="dark" className="mr-2 uppercase text-xxs">Planning area</Label>
-                <InfoButton>
-                  <span>Planning area info button.</span>
-                </InfoButton>
-              </div>
-              {/* TEMPORARILY HIDDEN, it will be implemented in the future */}
-              <div className="hidden">
-                <Button
-                  className="w-20 h-6 mr-4"
-                  size="xs"
-                  theme={!hasPlanningArea ? 'white' : 'secondary'}
-                  onClick={() => setHasPlanningArea(false)}
+              {/* NAME */}
+              <div className="mt-8">
+                <FieldRFF
+                  name="name"
+                  validate={composeValidators([{ presence: true }])}
                 >
-                  No
-                </Button>
-                <Button
-                  className="w-20 h-6"
-                  size="xs"
-                  theme={hasPlanningArea ? 'white' : 'secondary'}
-                  onClick={() => setHasPlanningArea(true)}
-                >
-                  Yes
-                </Button>
+                  {(fprops) => (
+                    <Field id="name" {...fprops}>
+                      <Label theme="dark" className="mb-3 uppercase">Project Name</Label>
+                      <Input theme="dark" type="text" placeholder="Write project name..." />
+                    </Field>
+                  )}
+                </FieldRFF>
               </div>
-            </div>
 
-            {!hasPlanningArea && (
-              <PlanningAreaSelector
-                area={DEFAULT_AREA}
-                values={values}
-                onChange={(value) => console.info('Planning area change: ', value)}
-              />
-            )}
+              {/* DESCRIPTION */}
+              <div className="mt-8">
+                <FieldRFF
+                  name="description"
+                  validate={composeValidators([{ presence: true }])}
+                >
+                  {(fprops) => (
+                    <Field id="description" {...fprops}>
+                      <Label theme="dark" className="mb-3 uppercase">Description</Label>
+                      <Textarea rows={4} placeholder="Write your project description..." />
+                    </Field>
+                  )}
+                </FieldRFF>
+              </div>
 
-            {hasPlanningArea && (
-              <Button
-                className="flex w-full mt-4"
-                theme="secondary"
-                size="base"
-                onClick={() => console.info('Upload shapefile')}
-              >
-                <span className="w-full">
-                  Upload shapefile
-                </span>
-                <Icon
-                  icon={UPLOAD_SHAPEFILE_SVG}
+              {/* TEMPORARILY HIDDEN, it will be implemented in the future
+              <h2 className="mt-12 text-white font-heading">
+                Do you have a planning region shapefile?
+              </h2> */}
+
+              {/* PLANNING AREA */}
+              <div className="flex items-center justify-between mt-6">
+                <div className="flex items-center">
+                  <Label theme="dark" className="mr-2 uppercase text-xxs">Planning area</Label>
+                  <InfoButton>
+                    <span>Planning area info button.</span>
+                  </InfoButton>
+                </div>
+                {/* TEMPORARILY HIDDEN, it will be implemented in the future */}
+                <div className="hidden">
+                  <Button
+                    className="w-20 h-6 mr-4"
+                    size="xs"
+                    theme={!hasPlanningArea ? 'white' : 'secondary'}
+                    onClick={() => setHasPlanningArea(false)}
+                  >
+                    No
+                  </Button>
+                  <Button
+                    className="w-20 h-6"
+                    size="xs"
+                    theme={hasPlanningArea ? 'white' : 'secondary'}
+                    onClick={() => setHasPlanningArea(true)}
+                  >
+                    Yes
+                  </Button>
+                </div>
+              </div>
+
+              {!hasPlanningArea && (
+                <PlanningAreaSelector
+                  area={DEFAULT_AREA}
+                  values={values}
+                  onChange={(value) => console.info('Planning area change: ', value)}
                 />
-              </Button>
-            )}
+              )}
+
+              {hasPlanningArea && (
+                <Button
+                  className="flex w-full mt-4"
+                  theme="secondary"
+                  size="base"
+                  onClick={() => console.info('Upload shapefile')}
+                >
+                  <span className="w-full">
+                    Upload shapefile
+                  </span>
+                  <Icon
+                    icon={UPLOAD_SHAPEFILE_SVG}
+                  />
+                </Button>
+              )}
+            </div>
+            <div className="absolute bottom-0 left-0 z-10 w-full h-6 bg-gradient-to-t from-gray-700 via-gray-700" />
           </div>
 
           {/* BUTTON BAR */}
-          <div className="flex mt-12">
+          <div className="flex px-8 pb-8 mt-4">
             <Link href="/projects" passHref>
               <a
                 href="/projects"
