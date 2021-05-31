db337ebf02c35fe7ab5164d0b05a4099698777d7
alexeh <alexurola@gmail.com>
2021-05-31 14:33:40 +0200

    Delete unused files

--- api/src/pipes/zipfile-validation.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
-
-export class ZipfileValidationPipe implements PipeTransform {
-  transform(value: Express.Multer.File, metadata: ArgumentMetadata): any {
-    console.log('value', value);
-    console.log('metadata', metadata);
-    return value;
-  }
-}
