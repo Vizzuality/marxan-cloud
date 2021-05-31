7419df624c96f873cd819328f7d54d21e93e04d0
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-05-31 12:55:02 +0200

    feat(geoprocessing): scenario planning units: use integrity rule

--- api/apps/geoprocessing/src/modules/surface-cost/application/surface-cost-processor.ts
+++ api/apps/geoprocessing/src/modules/surface-cost/application/surface-cost-processor.ts
@@ -6,8 +6,9 @@ import { CostSurfaceJobInput } from '../cost-surface-job-input';
 
 import { CostSurfacePersistencePort } from '../ports/persistence/cost-surface-persistence.port';
 import { PuExtractorPort } from '../ports/pu-extractor/pu-extractor.port';
-import { ArePuidsAllowedPort } from '../ports/pu-validator/are-puuids-allowed.port';
+import { GetAvailablePlanningUnits } from '../ports/available-planning-units/get-available-planning-units';
 import { ShapefileConverterPort } from '../ports/shapefile-converter/shapefile-converter.port';
+import { lockPlanningUnits } from '@marxan/scenarios-planning-unit';
 
 @Injectable()
 export class SurfaceCostProcessor
@@ -15,16 +16,20 @@ export class SurfaceCostProcessor
   constructor(
     private readonly repo: CostSurfacePersistencePort,
     private readonly puExtractor: PuExtractorPort,
-    private readonly puValidator: ArePuidsAllowedPort,
+    private readonly availablePlanningUnits: GetAvailablePlanningUnits,
     private readonly shapefileConverter: ShapefileConverterPort,
   ) {}
 
   async process(job: Job<CostSurfaceJobInput, true>): Promise<true> {
     const geoJson = await this.shapefileConverter.convert(job.data.shapefile);
     const surfaceCosts = this.puExtractor.extract(geoJson);
-    const { errors } = await this.puValidator.validate(
-      job.data.scenarioId,
+    const planningUnitsIds = (
+      await this.availablePlanningUnits.get(job.data.scenarioId)
+    ).ids;
+
+    const { errors } = lockPlanningUnits(
       surfaceCosts.map((cost) => cost.planningUnitId),
+      planningUnitsIds,
     );
     if (errors.length > 0) {
       throw new Error(errors.join('.'));
