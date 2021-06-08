f5cb20e30fa2f71636f677fc7a96ef1edd97b3a6
Alicia <alicia.arenzana@gmail.com>
2021-06-08 11:06:40 +0200

    fixed rebase issues with queue import module in planning units

--- api/apps/api/src/modules/planning-units/planning-units.module.ts
+++ api/apps/api/src/modules/planning-units/planning-units.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProxyService } from '@marxan-api/modules/proxy/proxy.service';
 import { PlanningUnitsController } from './planning-units.controller';
 import { PlanningUnitsService } from './planning-units.service';
+import { QueueModule } from '@marxan-api/modules/queue/queue.module';
 
 @Module({
+  imports: [
+    QueueModule.register({
+      name: 'planning-units',
+    }),
+  ],
   providers: [PlanningUnitsService, ProxyService],
   exports: [PlanningUnitsService],
   controllers: [PlanningUnitsController],
