60433ea01f126e0a002e4881acfc6e54102919ad
andrea rota <andrea.rota@vizzuality.com>
2021-06-22 17:44:57 +0100

    (wip) geofeatures geoprocessing recipes validation

--- api/apps/api/src/modules/geo-features/dto/create.geo-feature-set.dto.ts
+++ api/apps/api/src/modules/geo-features/dto/create.geo-feature-set.dto.ts
@@ -1,25 +1,67 @@
-import { JobStatus } from '@marxan-api/modules/scenarios/scenario.api.entity';
+import { SimpleJobStatus } from '@marxan-api/modules/scenarios/scenario.api.entity';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsArray, IsEnum, IsUUID, ValidateNested } from 'class-validator';
 import {
+  GeoprocessingOp,
   GeoprocessingOpSplitV1,
   GeoprocessingOpStratificationV1,
 } from '../types/geo-feature.geoprocessing-operations.type';
 import { MarxanSettingsForGeoFeature } from '../types/geo-feature.marxan-settings.type';
 
-class SpecForPlainGeoFeature {
+export abstract class SpecForGeofeature {
+  @IsUUID()
+  @ApiProperty()
   featureId!: string;
+}
+
+export class SpecForPlainGeoFeature extends SpecForGeofeature {
+  @ValidateNested()
+  @ApiProperty()
   marxanSettings!: MarxanSettingsForGeoFeature;
+
   geoprocessingOperations?: never;
 }
 
-class SpecForGeoFeatureWithGeoprocessing {
+export class SpecForGeoFeatureWithGeoprocessing extends SpecForGeofeature {
+  @IsUUID()
+  @ApiProperty()
   featureId!: string;
+
+  @ValidateNested({ each: true })
+  @Type(() => GeoprocessingOp, {
+    keepDiscriminatorProperty: true,
+    discriminator: {
+      property: 'kind',
+      subTypes: [
+        { value: GeoprocessingOpSplitV1, name: 'split/v1' },
+        { value: GeoprocessingOpStratificationV1, name: 'stratification/v1' },
+      ]
+    }
+  })
+  @ApiPropertyOptional()
   geoprocessingOperations?: Array<
     GeoprocessingOpSplitV1 | GeoprocessingOpStratificationV1
   >;
+
   marxanSettings?: never;
 }
 
 export class CreateGeoFeatureSetDTO {
-  status!: JobStatus.draft | JobStatus.created;
+  @ApiProperty()
+  @IsEnum(Object.keys(SimpleJobStatus))
+  status!: SimpleJobStatus;
+
+  @ApiProperty()
+  @ValidateNested({ each: true })
+  @Type(() => SpecForGeofeature, {
+    discriminator: {
+      property: 'kind',
+      subTypes: [
+        { value: SpecForPlainGeoFeature, name: 'plain' },
+        { value: SpecForGeoFeatureWithGeoprocessing, name: 'withGeoprocessing' },
+      ]
+    }
+  })
   features!: Array<SpecForPlainGeoFeature | SpecForGeoFeatureWithGeoprocessing>;
 }
