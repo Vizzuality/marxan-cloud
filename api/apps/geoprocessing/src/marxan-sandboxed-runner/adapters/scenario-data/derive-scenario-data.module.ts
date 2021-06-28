29e78a436fbaf8691961fc6eda7ae71f5cfd6560
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-06-28 17:11:09 +0200

    feat(marxan-runner): pipe assets to input files

--- api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters/scenario-data/derive-scenario-data.module.ts
+++ api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters/scenario-data/derive-scenario-data.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { InputFiles } from '../../ports/input-files';
 import { DeriveScenarioFacade } from './derive-scenario.facade';
 
 @Module({
+  imports: [HttpModule],
   providers: [
     {
       provide: InputFiles,
