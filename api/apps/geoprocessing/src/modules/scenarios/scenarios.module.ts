c5ad203d7c8dd4293f2aed5956ff69b1cfc17fad
Dyostiq <15174395+Dyostiq@users.noreply.github.com>
2021-06-29 15:41:09 +0200

    fix(geoprocessing): add missing entity in scenario module

--- api/apps/geoprocessing/src/modules/scenarios/scenarios.module.ts
+++ api/apps/geoprocessing/src/modules/scenarios/scenarios.module.ts
@@ -1,15 +1,15 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ScenariosController } from './scenarios.controller';
 import { ScenariosService } from './scenarios.service';
 import { TileModule } from '@marxan-geoprocessing/modules/tile/tile.module';
-import { ScenariosPuPaDataGeo } from '@marxan/scenarios-planning-unit/scenarios-pu-pa-data.geo.entity';
-
+import { ScenariosPuPaDataGeo } from '@marxan/scenarios-planning-unit';
+import { PlanningUnitsGeom } from '@marxan-jobs/planning-unit-geometry';
 
 @Module({
   imports: [
-    forwardRef(() =>TypeOrmModule.forFeature([ScenariosPuPaDataGeo])),
+    TypeOrmModule.forFeature([ScenariosPuPaDataGeo, PlanningUnitsGeom]),
     TileModule,
   ],
   providers: [ScenariosService],
