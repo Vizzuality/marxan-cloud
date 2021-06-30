5f99239d8c30af04abeb1419c3bac5ea57ebed0d
anamontiaga <amontiaga@gmail.com>
2021-06-30 17:10:45 +0200

    fix: about styles

--- app/layout/statics/hero/component.tsx
+++ app/layout/statics/hero/component.tsx
@@ -1,38 +1,36 @@
 import React from 'react';
 
-import cx from 'classnames';
 import Wrapper from 'layout/wrapper';
 
-export interface ExploreHeroProps {
+export interface StaticHeroProps {
   section: string;
   title: string;
   description: string,
 }
 
-export const ExploreHero: React.FC<ExploreHeroProps> = ({
+export const StaticHero: React.FC<StaticHeroProps> = ({
   section,
   title,
   description,
-}: ExploreHeroProps) => {
+}: StaticHeroProps) => {
   return (
     <Wrapper>
       <div className="w-full max-w-5xl py-32 mx-auto">
-        <p className="max-w-3xl text-lg font-semibold opacity-40">
+        <h1 className="max-w-3xl text-lg font-semibold opacity-40">
           {section}
-        </p>
-        <h1
+        </h1>
+        <h2
           className="pb-16 mt-3 font-semibold leading-tight text-left max-w-max bg-clip-text text-7xl"
         >
           {title}
-        </h1>
+        </h2>
 
-        <div className="text-lg leading-8 text-justify max-w-max">
+        <p className="text-lg leading-8 text-justify max-w-max font-heading">
           {description}
-        </div>
-        <div className={cx({ 'h-px w-full opacity-20 bg-white mt-24': true })} />
+        </p>
       </div>
     </Wrapper>
   );
 };
 
-export default ExploreHero;
+export default StaticHero;
--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 
-import Contact from 'layout/explore/contact';
+import Contact from 'layout/statics/contact';
 import Header from 'layout/header';
-import Hero from 'layout/explore/hero';
+import Hero from 'layout/statics/hero';
 import Footer from 'layout/footer';
+import Utilities from 'layout/about/utilities';
 
 import { withUser } from 'hoc/auth';
 
@@ -23,13 +24,9 @@ const About: React.FC = () => {
         <Hero
           section="About"
           title="Spatial conservation planning with Marxan."
-          description="Marxan is a suite of open-source decision-support tools that help structure, design and evaluate spatial planning projects for land, freshwater and ocean conservation. Marxan helps decision-making by balancing objectives such as protecting biodiversity and the benefits it provides to people, with socio-economic, cultural and political realities. As the analytic engine behind major planning projects, such as the Great Barrier Reef’s Rezoning Plan and Mongolia’s National Protected Area portfolio, Marxan leads the world in providing robust and flexible systematic decision-support to governments, practitioners and planners around the world"
+          description="Marxan is a suite of open-source decision-support tools that help structure, design and evaluate spatial planning projects for land, freshwater and ocean conservation. Marxan helps decision-making by balancing objectives such as protecting biodiversity and the benefits it provides to people, with socio-economic, cultural and political realities. As the analytic engine behind major planning projects, such as the Great Barrier Reef’s Rezoning Plan and Mongolia’s National Protected Area portfolio, Marxan leads the world in providing robust and flexible systematic decision-support to governments, practitioners and planners around the world."
         />
-        <section>
-          <h1 className="pb-10 text-5xl font-semibold leading-tight text-transparent max-w-max bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text">
-            What can I do with this Marxan planning platform?
-          </h1>
-        </section>
+        <Utilities />
         <Contact />
         <Footer />
       </main>
