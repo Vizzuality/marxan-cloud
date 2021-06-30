0c262df881004327f4506a9f71a30c213686fa3f
andrea rota <andrea.rota@vizzuality.com>
2021-06-30 15:45:30 +0100

    wip: add trigger to precompute property lists for geo features

--- api/apps/geoprocessing/src/migrations/geoprocessing/1625042393000-RefactorExtractDistinctPropsFromFeatures.ts
+++ api/apps/geoprocessing/src/migrations/geoprocessing/1625042393000-RefactorExtractDistinctPropsFromFeatures.ts
@@ -25,6 +25,25 @@ CREATE INDEX idx_feature_properties_key ON feature_properties(key);
 INSERT INTO feature_properties
 SELECT feature_id, id, (jsonb_each(properties)).key, (jsonb_each(properties)).value, ST_Envelope(the_geom) FROM features_data;
 
+CREATE OR REPLACE FUNCTION precompute_feature_property_list()
+  RETURNS TRIGGER AS $BODY$
+  BEGIN
+    DELETE FROM feature_properties
+      WHERE feature_data_id = NEW.id;
+
+    INSERT INTO feature_properties
+      SELECT feature_id, id, (jsonb_each(properties)).key, (jsonb_each(properties)).value, ST_Envelope(the_geom)
+      FROM features_data
+      WHERE id = NEW.id;
+
+    RETURN NULL;
+  END
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_precompute_feature_property_list
+  AFTER INSERT OR UPDATE ON features_data
+  FOR EACH ROW EXECUTE
+  PROCEDURE precompute_feature_property_list();
     `);
   }
 
