ab0824ad1fbcf77336f724b709a6258434c6af9b
anamontiaga <amontiaga@gmail.com>
2021-07-01 15:10:52 +0200

    add card values

--- app/layout/about/values/constants.ts
+++ app/layout/about/values/constants.ts
@@ -1,38 +1,30 @@
-export const UTILITIES = [
+export const VALUES = [
   {
-    order: '01',
-    description: 'Design and evaluate conservation and zoning plans.',
+    order: '01.',
+    title: 'Free and open',
+    description: 'We remain committed to free and open data and technology.',
+    hypertext: 'Know more',
+    hyperlink: '/https://github.com/Marxan-source-code',
   },
   {
-    order: '02',
-    description: 'Integrate, host and share existing Marxan projects.',
+    order: '02.',
+    title: 'Collaborative',
+    description: 'We partner with organizations, companies and institutions who share our commitment to advance biodiversity and sustainability through conservation technology.',
+    hypertext: 'Know more',
+    hyperlink: '/partners',
   },
   {
-    order: '03',
-    description: 'Collaborate and manage projects with your teams, stakeholders and decision-makers.',
+    order: '03.',
+    title: 'Inclusive',
+    description: 'we are dedicated to developing technical capacity and expertise in Marxan around the world.',
+    hypertext: 'Know more',
+    hyperlink: 'https://marxansolutions.org/getting-started',
   },
   {
-    order: '04',
-    description: 'Compare and map scenarios across projects.',
-  },
-  {
-    order: '05',
-    description: 'Access and store key spatial data sets.',
-  },
-  {
-    order: '06',
-    description: 'Conduct gap analyses.',
-  },
-  {
-    order: '07',
-    description: 'Schedule portfolio investments.',
-  },
-  {
-    order: '08',
-    description: 'Understand trade-offs between different objectives.',
-  },
-  {
-    order: '09',
-    description: 'Download reports.',
+    order: '04.',
+    title: 'Community-led',
+    description: 'Marxan supports a global community of over 6000 users in 100 countries who help us advance our software and tools, train and support new users, and integrate Marxan into planning processes that deliver outcomes on the ground.',
+    hypertext: 'Know more',
+    hyperlink: 'https://marxansolutions.org/community',
   },
 ];
