97460d6b6d007dbe97c35355017f1bcc525e8357
anamontiaga <amontiaga@gmail.com>
2021-07-01 10:21:38 +0200

    add values to about layout

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -6,6 +6,7 @@ import Header from 'layout/header';
 import Hero from 'layout/statics/hero';
 import Footer from 'layout/footer';
 import Utilities from 'layout/about/utilities';
+import Values from 'layout/about/values';
 
 import { withUser } from 'hoc/auth';
 
@@ -27,6 +28,7 @@ const About: React.FC = () => {
           description="Marxan is a suite of open-source decision-support tools that help structure, design and evaluate spatial planning projects for land, freshwater and ocean conservation. Marxan helps decision-making by balancing objectives such as protecting biodiversity and the benefits it provides to people, with socio-economic, cultural and political realities. As the analytic engine behind major planning projects, such as the Great Barrier Reef’s Rezoning Plan and Mongolia’s National Protected Area portfolio, Marxan leads the world in providing robust and flexible systematic decision-support to governments, practitioners and planners around the world."
         />
         <Utilities />
+        <Values />
         <Contact />
         <Footer />
       </main>
