49e4ef578cdcb7f4c5e53d664c46688b2969fb1e
andrea rota <andrea.rota@vizzuality.com>
2021-07-05 20:06:03 +0100

    add GeoFeatureSet service and serializer

---
+++ api/apps/api/src/modules/geo-features/geo-feature-set.serializer.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PaginationMeta } from '@marxan-api/utils/app-base.service';
+import { GeoFeatureSetService } from './geo-feature-set.service';
+import { CreateGeoFeatureSetDTO } from './dto/create.geo-feature-set.dto';
+
+@Injectable()
+export class GeoFeatureSetSerializer {
+  constructor(private readonly geoFeatureSetsService: GeoFeatureSetService) {}
+
+  async serialize(
+    entities: Partial<CreateGeoFeatureSetDTO> | (Partial<CreateGeoFeatureSetDTO> | undefined)[],
+    paginationMeta?: PaginationMeta,
+  ): Promise<any> {
+    return this.geoFeatureSetsService.serialize(entities, paginationMeta);
+  }
+}
---
+++ api/apps/api/src/modules/geo-features/geo-feature-set.service.ts
@@ -0,0 +1,29 @@
+import { JSONAPISerializerConfig, PaginationMeta } from "@marxan-api/utils/app-base.service";
+import { Injectable } from "@nestjs/common";
+import { CreateGeoFeatureSetDTO } from "./dto/create.geo-feature-set.dto";
+import * as JSONAPISerializer from 'jsonapi-serializer';
+
+@Injectable()
+export class GeoFeatureSetService {
+  get serializerConfig(): JSONAPISerializerConfig<CreateGeoFeatureSetDTO> {
+    return {
+      attributes: [
+        'status',
+        'features'
+      ],
+      keyForAttribute: 'camelCase',
+    };
+  }
+
+  async serialize(
+    entities: Partial<CreateGeoFeatureSetDTO> | (Partial<CreateGeoFeatureSetDTO> | undefined)[],
+    paginationMeta?: PaginationMeta,
+  ): Promise<any> {
+    const serializer = new JSONAPISerializer.Serializer('GeoFeatureSets', {
+      ...this.serializerConfig,
+      meta: paginationMeta,
+    });
+
+    return serializer.serialize(entities);
+  }
+}
