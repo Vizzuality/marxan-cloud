10c42bcb9f5a32aab7fdbfac16d55e97b572abea
andrea rota <andrea.rota@vizzuality.com>
2021-07-05 20:06:28 +0100

    sync update DTO to create DTO

--- api/apps/api/src/modules/geo-features/dto/update.geo-feature-set.dto.ts
+++ api/apps/api/src/modules/geo-features/dto/update.geo-feature-set.dto.ts
@@ -1,6 +1,3 @@
-import { PartialType } from '@nestjs/swagger';
 import { CreateGeoFeatureSetDTO } from './create.geo-feature-set.dto';
 
-export class UpdateGeoFeatureSetDTO extends PartialType(
-  CreateGeoFeatureSetDTO,
-) {}
+export class UpdateGeoFeatureSetDTO extends CreateGeoFeatureSetDTO {};
