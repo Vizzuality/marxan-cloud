33eccb14b140c40cdbb5be91403096da99b73144
andrea rota <andrea.rota@vizzuality.com>
2021-07-05 20:08:04 +0100

    prepare to move from entity to non-entity class

--- api/apps/api/src/modules/geo-features/geo-feature-set.api.entity.ts
+++ api/apps/api/src/modules/geo-features/geo-feature-set.api.entity.ts
@@ -1,9 +1,7 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { JobStatus } from '@marxan-api/modules/scenarios/scenario.api.entity';
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, PrimaryColumn } from 'typeorm';
 import { BaseServiceResource } from '@marxan-api/types/resource.interface';
-import { GeoFeature } from './geo-feature.api.entity';
-import { IsNumber, IsUUID, ValidateNested } from 'class-validator';
 import {
   SpecForGeoFeatureWithGeoprocessing,
   SpecForPlainGeoFeature,
@@ -28,18 +26,11 @@ export interface GeoFeatureCategory {
   distinctValues: string[];
 }
 
-@Entity('feature_sets')
 export class GeoFeatureSet {
   @ApiProperty()
-  @PrimaryColumn()
-  id!: string;
-
-  @ApiProperty()
-  @Column('varchar')
   status!: JobStatus | 'draft';
 
   @ApiPropertyOptional()
-  @Column('jsonb', { array: true })
   features?: Array<SpecForPlainGeoFeature | SpecForGeoFeatureWithGeoprocessing>;
 }
 
