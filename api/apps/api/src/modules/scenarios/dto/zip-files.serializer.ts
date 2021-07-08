8dac958ac77a2ac802d37949b4922fc4ebde07b7
K Gajowy <kamil.gajowy@gmail.com>
2021-07-08 14:22:24 +0200

    refactor(marxan-run): output archive mapper simplified

--- api/apps/api/src/modules/scenarios/dto/zip-files.serializer.ts
+++ api/apps/api/src/modules/scenarios/dto/zip-files.serializer.ts
@@ -15,17 +15,18 @@ export class ZipFilesSerializer {
   serialize(response: Either<OutputZipFailure, Buffer>): Buffer {
     if (isRight(response)) {
       return response.right;
-    } else {
-      const error = response.left;
-      if (error === metadataNotFound) {
+    }
+    const error = response.left;
+    switch (error) {
+      case metadataNotFound:
         throw new NotFoundException(`Marxan was not yet executed.`);
-      } else if (error === outputZipNotYetAvailable) {
+      case outputZipNotYetAvailable:
         throw new NotFoundException(
           `Marxan has not yet finished or finished with error.`,
         );
-      } else {
-        throw new InternalServerErrorException();
-      }
+      default:
+        const _exhaustiveCheck: never = error;
+        throw _exhaustiveCheck;
     }
   }
 }
