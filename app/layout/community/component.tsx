1626d76f525d6be318aae3fc008d8facb6d03488
anamontiaga <amontiaga@gmail.com>
2021-07-08 13:53:00 +0200

    fix target contact

--- app/layout/community/component.tsx
+++ app/layout/community/component.tsx
@@ -31,7 +31,7 @@ export const CommunityInfo: React.FC<CommunityInfoProps> = () => {
       <div className="flex flex-col items-center w-full max-w-5xl py-16 mx-auto gap-x-36 ">
         <div className="flex flex-col border-white border-opacity-20 w-96">
           <h4 className="mb-2">Reach out with comments or suggestions.</h4>
-          <a className="text-left text-blue-500 underline" href="mailto:marxancloud@gmail.com">Contact us</a>
+          <a className="text-left text-blue-500 underline" href="mailto:marxancloud@gmail.com" rel="noopener noreferrer" target="_blank">Contact us</a>
         </div>
       </div>
       <div className="flex flex-col items-center w-full max-w-5xl py-16 mx-auto border-t border-white gap-x-36 border-opacity-20">
