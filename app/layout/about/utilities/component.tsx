c74c8726cf07d5a1d4c8bf02af030839fffad697
anamontiaga <amontiaga@gmail.com>
2021-07-08 13:35:37 +0200

    link to marxan homepage on abaout static page

--- app/layout/about/utilities/component.tsx
+++ app/layout/about/utilities/component.tsx
@@ -17,7 +17,7 @@ export const AboutUtilities: React.FC<AboutUtilitiesProps> = () => {
           title="What can I do with this Marxan planning platform?"
           subtitle="This platform hosts the Marxan software suite which includes Marxan, Marxan with Zones, and Marxan with Connectivity on a stable Azure cloud environment. You can use this platform to:"
           caption="Learn more"
-          href="/"
+          href="https://marxansolutions.org"
         />
         <div className="flex flex-col items-start gap-10">
           {UTILITIES.map((u) => (
