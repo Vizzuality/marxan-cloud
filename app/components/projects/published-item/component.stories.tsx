9567be7f10f6a79e02ed3ecf4392c227b1d44d98
anamontiaga <amontiaga@gmail.com>
2021-07-09 12:10:32 +0200

    add published item component to sb

--- app/components/projects/published-item/component.stories.tsx
+++ app/components/projects/published-item/component.stories.tsx
@@ -1,39 +1,30 @@
 import React from 'react';
 import { Story } from '@storybook/react/types-6-0';
-import Item, { PublishedItemProps } from './component';
+import PublishedItem, { PublishedItemProps } from './component';
 
 export default {
-  title: 'Components/Projects/Item',
-  component: Item,
+  title: 'Components/Projects/Published Item',
+  component: PublishedItem,
   argTypes: {},
 };
 
 const Template: Story<PublishedItemProps> = ({ ...args }: PublishedItemProps) => (
-  <Item {...args} />
+  <PublishedItem {...args} />
 );
 
 export const Default = Template.bind({});
 Default.args = {
   id: 1,
-  area: 'Planning area name',
-  name: 'Project Name 1',
+  area: 'Kenya',
+  name: 'Kenya Project',
   description:
     'Donec est ad luctus dapibus sociosqu. Imperdiet platea viverra dui congue orci ad. Turpis a, dictumst eget. Justo potenti morbi iaculis habitasse justo aliquam tortor tellus nostra. Accumsan nunc lorem malesuada, eget sed magna habitasse laoreet rutrum non ante suscipit. Adipiscing quisque justo vel, et tellus suscipit purus. Mattis primis curae;, scelerisque parturient libero dictumst ad! Cras elit condimentum molestie sociis mauris. Pharetra tincidunt habitant imperdiet mauris vitae tempor sollicitudin pulvinar feugiat pharetra scelerisque? Purus erat penatibus adipiscing vestibulum fermentum et platea eros quis ad congue. Porta fringilla enim bibendum per tortor natoque ante suscipit. Congue.',
   contributors: [
-    { id: 1, name: 'Miguel Barrenechea', bgImage: '/images/avatar.png' },
-    { id: 2, name: 'Ariadna Martínez', bgImage: '/images/avatar.png' },
-    { id: 3, name: 'Alicia Arenzana', bgImage: '' },
+    { id: 1, name: 'Miguel Barrenechea' },
+    { id: 2, name: 'Ariadna Martínez' },
   ],
-  onDownload: (e) => {
-    console.info('onDownload', e);
-  },
+  timesDuplicated: 120654689745867,
   onDuplicate: (e) => {
     console.info('onDuplicate', e);
-  },
-  onDelete: (e) => {
-    console.info('onDelete', e);
-  },
-  onClick: (e) => {
-    console.info('onClick', e);
   },
 };
