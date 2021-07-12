18350e6aaa6a0c9cc22083895672cf4942b9e9ff
anamontiaga <amontiaga@gmail.com>
2021-07-12 12:23:27 +0200

    fix gradient carousel section

--- app/layout/home/carousel-section/component.tsx
+++ app/layout/home/carousel-section/component.tsx
@@ -11,22 +11,19 @@ export interface HomeCarouselProps {
 
 export const HomeCarousel: React.FC<HomeCarouselProps> = () => {
   return (
-    <>
-      <div className="w-full mt-64 bg-black h-96" />
-      <div className="w-full h-48 bg-gray-700" />
+    <div style={{
+      background: 'linear-gradient(to bottom, #000 75%, #151515 25%)',
+    }}
+    >
       <Wrapper>
-
-        <div className="relative w-full max-w-5xl mx-auto">
-
-          <div className="absolute flex justify-center bottom-px">
-            <Carousel
-              images={IMAGES}
-              initial={0}
-            />
-          </div>
+        <div className="flex justify-center w-full max-w-5xl px-10 mx-auto">
+          <Carousel
+            images={IMAGES}
+            initial={0}
+          />
         </div>
       </Wrapper>
-    </>
+    </div>
   );
 };
 
