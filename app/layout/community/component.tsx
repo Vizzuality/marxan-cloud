2acb8a5f927f0c4304322db5a83ec68e53dabd41
anamontiaga <amontiaga@gmail.com>
2021-07-13 09:20:54 +0200

    no string filters case

--- app/layout/community/component.tsx
+++ app/layout/community/component.tsx
@@ -31,13 +31,13 @@ export const CommunityInfo: React.FC<CommunityInfoProps> = () => {
       <div className="flex flex-col items-center w-full max-w-5xl py-16 mx-auto gap-x-36 ">
         <div className="flex flex-col border-white border-opacity-20 w-96">
           <h4 className="mb-2">Reach out with comments or suggestions.</h4>
-          <a className="text-left text-blue-500 underline" href="mailto:marxancloud@gmail.com">Contact us</a>
+          <a className="text-left text-blue-500 underline" href="mailto:marxancloud@gmail.com" rel="noopener noreferrer" target="_blank">Contact us</a>
         </div>
       </div>
       <div className="flex flex-col items-center w-full max-w-5xl py-16 mx-auto border-t border-white gap-x-36 border-opacity-20">
         <div className="flex flex-col border-white border-opacity-20 w-96">
           <h4 className="mb-2">Follow us on twitter and join in the conversation.</h4>
-          <a className="text-left text-blue-500 underline" href="https://twitter.com/marxan_planning?">@Marxan_Planning</a>
+          <a className="text-left text-blue-500 underline" href="https://twitter.com/marxan_planning?" rel="noopener noreferrer" target="_blank">@Marxan_Planning</a>
         </div>
       </div>
       <div className="flex flex-col items-center w-full max-w-5xl py-16 mx-auto border-t border-white gap-x-36 border-opacity-20">
