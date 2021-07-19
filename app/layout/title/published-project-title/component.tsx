321d1df01831b97301b2345fc5e1bdf07841a6e8
anamontiaga <amontiaga@gmail.com>
2021-07-19 14:35:58 +0200

    copy title for published one

---
+++ app/layout/title/published-project-title/component.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+import { useRouter } from 'next/router';
+import { useProject } from 'hooks/projects';
+
+export interface PublishedProjectTitleProps {
+  title?: string;
+}
+
+export const PublishedProjectTitle: React.FC<PublishedProjectTitleProps> = ({
+  title,
+}:PublishedProjectTitleProps) => {
+  const { query } = useRouter();
+  const { pid } = query;
+  const { data: projectData } = useProject(pid);
+
+  return (
+    <Head>
+      <title>
+        Projects
+        {(projectData?.name || title) && ':'}
+        {' '}
+        {title}
+        {' '}
+        {projectData?.name}
+      </title>
+    </Head>
+  );
+};
+
+export default PublishedProjectTitle;
