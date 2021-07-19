c270fd06fa7990e754c678825b2ef20626ebc995
anamontiaga <amontiaga@gmail.com>
2021-07-19 14:40:35 +0200

    add non data cases and head on meta tags component

--- app/layout/meta-tags/component.tsx
+++ app/layout/meta-tags/component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 
 export interface MetaTagsProps {
   name: string,
@@ -36,23 +37,23 @@ export const MetaTags: React.FC<MetaTagsProps> = ({
   twitterSite,
 }: MetaTagsProps) => {
   return (
-    <>
-      <meta property="og:site_name" content={name} key="name" />
-      <meta property="og:title" content={title} key="title" />
-      <meta property="og:description" content={description} key="description" />
-      <meta property="og:url" content={url} key="url" />
-      <meta property="og:type" content={type} key="type" />
-      <meta property="article:publisher" content={publisher} key="publisher" />
-      <meta property="article:section" content={section} key="section" />
-      <meta property="article:tag" content={tag} key="tag" />
-      <meta property="og:image" content={image} key="image" />
-      <meta property="og:image:secure_url" content={imageURL} key="imageURL" />
-      <meta property="og:image:width" content={imageWidth} key="imageWidth" />
-      <meta property="og:image:height" content={imageHeight} key="imageHeight" />
-      <meta property="twitter:card" content={twitterCard} key="twitterCard" />
-      <meta property="twitter:image" content={twitterImage} key="twitterImage" />
-      <meta property="twitter:site" content={twitterSite} key="twitterSite" />
-    </>
+    <Head>
+      {name && <meta property="og:site_name" content={name} key="name" />}
+      {title && <meta property="og:title" content={title} key="title" />}
+      {description && <meta property="og:description" content={description} key="description" />}
+      {url && <meta property="og:url" content={url} key="url" />}
+      {type && <meta property="og:type" content={type} key="type" />}
+      {publisher && <meta property="article:publisher" content={publisher} key="publisher" />}
+      {section && <meta property="article:section" content={section} key="section" />}
+      {tag && <meta property="article:tag" content={tag} key="tag" />}
+      {image && <meta property="og:image" content={image} key="image" />}
+      {imageURL && <meta property="og:image:secure_url" content={imageURL} key="imageURL" />}
+      {imageWidth && <meta property="og:image:width" content={imageWidth} key="imageWidth" />}
+      {imageHeight && <meta property="og:image:height" content={imageHeight} key="imageHeight" />}
+      {twitterCard && <meta property="twitter:card" content={twitterCard} key="twitterCard" />}
+      {twitterImage && <meta property="twitter:image" content={twitterImage} key="twitterImage" />}
+      {twitterSite && <meta property="twitter:site" content={twitterSite} key="twitterSite" />}
+    </Head>
   );
 };
 
