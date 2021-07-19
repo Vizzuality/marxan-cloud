29931444ba558aeeb28574cd3b69b0612360f047
K Gajowy <kamil.gajowy@gmail.com>
2021-07-19 08:02:42 +0200

    feat(marxan-run): prevent directory traversing during cleanup

--- api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters/workspace/shared-storage.ts
+++ api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters/workspace/shared-storage.ts
@@ -1,31 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { v4 } from 'uuid';
 import { promises } from 'fs';
 import { resolve } from 'path';
-
-import { AppConfig } from '@marxan-geoprocessing/utils/config.utils';
-import { assertDefined } from '@marxan/utils';
 
 import { TemporaryDirectory } from './ports/temporary-directory';
 import { WorkingDirectory } from '../../ports/working-directory';
 import { MarxanDirectory } from '../marxan-directory.service';
 
+export const SharedStoragePath = Symbol('shared storage temporary directory');
+
 @Injectable()
 export class SharedStorage implements TemporaryDirectory {
-  readonly #tempDirectory: string;
-
-  constructor(private readonly marxanDirectory: MarxanDirectory) {
-    const storagePath = AppConfig.get<string>(
-      'storage.sharedFileStorage.localPath',
-    );
-    assertDefined(storagePath);
-    this.#tempDirectory = storagePath;
-  }
+  constructor(
+    @Inject(SharedStoragePath) private readonly tempDirectory: string,
+    private readonly marxanDirectory: MarxanDirectory,
+  ) {}
 
   async cleanup(directory: string): Promise<void> {
     if (this.#hasPoisonNullByte(directory)) {
       throw new Error(`Hacking is not allowed.`);
     }
+
+    if (this.#isDirectoryTraversal(directory, this.tempDirectory)) {
+      throw new Error(`Directory traversal is not allowed.`);
+    }
+
     await promises.rm(directory, {
       recursive: true,
       force: true,
@@ -35,17 +34,17 @@ export class SharedStorage implements TemporaryDirectory {
 
   async get(): Promise<WorkingDirectory> {
     const directory = v4();
-    const fullPath = resolve(
-      this.#tempDirectory,
-      directory,
-    ) as WorkingDirectory;
+    const fullPath = resolve(this.tempDirectory, directory) as WorkingDirectory;
 
-    await promises.mkdir(resolve(this.#tempDirectory, directory));
+    await promises.mkdir(fullPath);
 
     return fullPath as WorkingDirectory;
   }
 
   #hasPoisonNullByte = (path: string): boolean => path.indexOf('\0') !== -1;
+
+  #isDirectoryTraversal = (fullPath: string, rootDirectory: string) =>
+    !fullPath.startsWith(rootDirectory);
 
   async createOutputDirectory(inside: WorkingDirectory): Promise<void> {
     const outputPath = this.marxanDirectory.get('OUTPUTDIR', inside);
--- api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters/workspace/workspace.module.ts
+++ api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters/workspace/workspace.module.ts
@@ -4,12 +4,13 @@ import { WorkspaceBuilder } from '../../ports/workspace-builder';
 import { LinkMarxan } from './ports/link-marxan';
 import { TemporaryDirectory } from './ports/temporary-directory';
 import { SymlinkBinary } from './symlink-binary';
-import { SharedStorage } from './shared-storage';
+import { SharedStorage, SharedStoragePath } from './shared-storage';
 
 import { WorkspaceService } from './workspace.service';
 import { MarxanConfig } from '../../marxan-config';
 import { MarxanDirectory } from '../marxan-directory.service';
 import { FileReader } from '../../adapters/file-reader';
+import { AppConfig } from '@marxan-geoprocessing/utils/config.utils';
 
 @Module({
   providers: [
@@ -25,6 +26,12 @@ import { FileReader } from '../../adapters/file-reader';
     {
       provide: WorkspaceBuilder,
       useClass: WorkspaceService,
+    },
+    {
+      provide: SharedStoragePath,
+      useFactory: () => {
+        return AppConfig.get<string>('storage.sharedFileStorage.localPath');
+      },
     },
     WorkspaceService,
     FileReader,
