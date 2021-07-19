7fd1515022a794069d47b67d8e6bf4dab9581709
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-07-19 10:06:41 +0200

    New project: info buttons and help

--- app/layout/projects/new/form/component.tsx
+++ app/layout/projects/new/form/component.tsx
@@ -102,60 +102,56 @@ const ProjectForm: React.FC<ProjectFormProps> = () => {
         >
 
           <div className="grid h-full grid-cols-1 gap-0 overflow-hidden bg-gray-700 md:grid-cols-2 rounded-3xl">
-            <div id="project-new-form" className="flex flex-col flex-grow overflow-hidden">
-              <div className="relative flex flex-col flex-grow min-h-0">
-                <div className="absolute top-0 left-0 z-10 w-full h-6 pointer-events-none bg-gradient-to-b from-gray-700 via-gray-700" />
-                <HelpBeacon
-                  id="project-new-overview"
-                  title="New project basic information"
-                  subtitle=""
-                  content={(
-                    <div>
-                      Start by adding a name, description and planning area
-                      to your new project.
+            <HelpBeacon
+              id="project-new-overview"
+              title="New project basic information"
+              subtitle=""
+              content={(
+                <div>
+                  Start by adding a name, description and planning area
+                  to your new project.
 
-                    </div>
-                  )}
-                  modifiers={['flip']}
-                  tooltipPlacement="left"
-                >
+                </div>
+                )}
+              modifiers={['flip']}
+              tooltipPlacement="right"
+            >
+              <div id="project-new-form" className="flex flex-col flex-grow overflow-hidden">
+                <div className="relative flex flex-col flex-grow min-h-0">
+                  <div className="absolute top-0 left-0 z-10 w-full h-6 pointer-events-none bg-gradient-to-b from-gray-700 via-gray-700" />
                   <div className="flex flex-col flex-grow p-8 overflow-auto">
                     <h1 className="max-w-xs text-2xl text-white font-heading">
                       Name your project and define a planning area:
                     </h1>
-                    <HelpBeacon
-                      id="project-new-name_description"
-                      title="New project name and description"
-                      subtitle=""
-                      content={(
-                        <div>
-                          A generic name for the project is
-                          recommended (eg: the name of the planning region)
-                          as well as a generic overview for the description.
-                          In each Scenario you will be able
-                          to provide specific details.
 
-                        </div>
-                  )}
-                      modifiers={['flip', 'hide']}
-                      tooltipPlacement="right"
-                    >
-                      {/* NAME */}
+                    {/* NAME */}
+                    <div className="mt-8">
+                      <FieldRFF
+                        name="name"
+                        validate={composeValidators([{ presence: true }])}
+                      >
+                        {(fprops) => (
+                          <Field id="name" {...fprops}>
+                            <div className="flex items-center mb-3 space-x-2">
+                              <Label theme="dark" className="uppercase" id="name">
+                                Project Name
+                              </Label>
 
-                      <div className="mt-8">
-                        <FieldRFF
-                          name="name"
-                          validate={composeValidators([{ presence: true }])}
-                        >
-                          {(fprops) => (
-                            <Field id="name" {...fprops}>
-                              <Label theme="dark" className="mb-3 uppercase">Project Name</Label>
-                              <Input theme="dark" type="text" placeholder="Write project name..." />
-                            </Field>
-                          )}
-                        </FieldRFF>
-                      </div>
-                    </HelpBeacon>
+                              <InfoButton>
+                                <span>
+                                  A generic name for the project is
+                                  recommended (eg: the name of the planning region)
+                                  as well as a generic overview for the description.
+                                  In each Scenario you will be able
+                                  to provide specific details.
+                                </span>
+                              </InfoButton>
+                            </div>
+                            <Input theme="dark" type="text" placeholder="Write project name..." />
+                          </Field>
+                        )}
+                      </FieldRFF>
+                    </div>
                     {/* DESCRIPTION */}
                     <div className="mt-8">
                       <FieldRFF
@@ -171,69 +167,43 @@ const ProjectForm: React.FC<ProjectFormProps> = () => {
                       </FieldRFF>
                     </div>
 
-                    {/* PLANNING AREA */}
-                    <HelpBeacon
-                      id="project-new-planning-area"
-                      title="New project planning area"
-                      subtitle=""
-                      content={(
-                        <div>
-
-                          Decide the outer boundary of
-                          your planning area and grid here.
-                          The platform provides a workflow to select an
-                          administrative
-                          boundary as planning area
-                          (eg: countries or municipalities)
-                          but you can also upload your
-                          own geometry.
-
-                        </div>
-                  )}
-                      modifiers={['flip', 'hide']}
-                      tooltipPlacement="right"
-                    >
-                      <div className="flex items-center justify-between mt-6">
-
-                        <div className="flex items-center">
-
-                          <Label theme="dark" className="mr-2 uppercase text-xxs">Planning area</Label>
-                          <InfoButton>
-                            <span>
-                              The planning area (or study region) is
-                              the outer boundary
-                              of the region where you want to create a
-                              plan. These regions
-                              often represent administrative units
-                              (such as countries or
-                              smaller regions), but can represent any
-                              planning area.
-                            </span>
-                          </InfoButton>
-                        </div>
-                        {/* TEMPORARILY HIDDEN, it will be implemented in the future */}
-                        <div className="hidden">
-                          <Button
-                            className="w-20 h-6 mr-4"
-                            size="xs"
-                            theme={!hasPlanningArea ? 'white' : 'secondary'}
-                            onClick={() => setHasPlanningArea(false)}
-                          >
-                            No
-                          </Button>
-                          <Button
-                            className="w-20 h-6"
-                            size="xs"
-                            theme={hasPlanningArea ? 'white' : 'secondary'}
-                            onClick={() => setHasPlanningArea(true)}
-                          >
-                            Yes
-                          </Button>
-
-                        </div>
+                    <div className="flex items-center justify-between mt-6">
+                      <div className="flex items-center space-x-2">
+                        <Label theme="dark" className="uppercase text-xxs">Planning area</Label>
+                        <InfoButton>
+                          <span>
+                            The planning area (or study region) is
+                            the outer boundary
+                            of the region where you want to create a
+                            plan. These regions
+                            often represent administrative units
+                            (such as countries or
+                            smaller regions), but you can also upload your
+                            own geometry.
+                          </span>
+                        </InfoButton>
+                      </div>
+                      {/* TEMPORARILY HIDDEN, it will be implemented in the future */}
+                      <div className="hidden">
+                        <Button
+                          className="w-20 h-6 mr-4"
+                          size="xs"
+                          theme={!hasPlanningArea ? 'white' : 'secondary'}
+                          onClick={() => setHasPlanningArea(false)}
+                        >
+                          No
+                        </Button>
+                        <Button
+                          className="w-20 h-6"
+                          size="xs"
+                          theme={hasPlanningArea ? 'white' : 'secondary'}
+                          onClick={() => setHasPlanningArea(true)}
+                        >
+                          Yes
+                        </Button>
 
                       </div>
-                    </HelpBeacon>
+                    </div>
 
                     {!hasPlanningArea && (
                       <PlanningAreaSelector
@@ -257,44 +227,60 @@ const ProjectForm: React.FC<ProjectFormProps> = () => {
                       </Button>
                     )}
                   </div>
-                </HelpBeacon>
-                <div className="absolute bottom-0 left-0 z-10 w-full h-6 pointer-events-none bg-gradient-to-t from-gray-700 via-gray-700" />
+                  <div className="absolute bottom-0 left-0 z-10 w-full h-6 pointer-events-none bg-gradient-to-t from-gray-700 via-gray-700" />
 
-              </div>
+                </div>
 
-              {/* BUTTON BAR */}
-              <div className="flex px-8 pb-8 mt-4">
-                <Link href="/projects" passHref>
-                  <a
-                    href="/projects"
-                  >
-                    <Button
-                      theme="secondary"
-                      size="xl"
+                {/* BUTTON BAR */}
+                <div className="flex px-8 pb-8 mt-4">
+                  <Link href="/projects" passHref>
+                    <a
+                      href="/projects"
                     >
-                      Cancel
-                    </Button>
-                  </a>
-                </Link>
-                <Button
-                  className="ml-6"
-                  theme="primary"
-                  size="xl"
-                  type="submit"
-                >
-                  Save
-                </Button>
+                      <Button
+                        theme="secondary"
+                        size="xl"
+                      >
+                        Cancel
+                      </Button>
+                    </a>
+                  </Link>
+                  <Button
+                    className="ml-6"
+                    theme="primary"
+                    size="xl"
+                    type="submit"
+                  >
+                    Save
+                  </Button>
+                </div>
               </div>
-            </div>
+            </HelpBeacon>
 
-            <ProjectNewMap
-              country={values.countryId}
-              region={values.adminAreaLevel1Id}
-              subregion={values.adminAreaLevel2Id}
-              planningUnitGridShape={values.planningUnitGridShape}
-              planningUnitAreakm2={values.planningUnitAreakm2}
+            <HelpBeacon
+              id="project-new-overview"
+              title="New project basic information"
+              subtitle=""
+              content={(
+                <div>
+                  Start by adding a name, description and planning area
+                  to your new project.
 
-            />
+                </div>
+                )}
+              modifiers={['flip']}
+              tooltipPlacement="left"
+            >
+              <div className="w-full h-full">
+                <ProjectNewMap
+                  country={values.countryId}
+                  region={values.adminAreaLevel1Id}
+                  subregion={values.adminAreaLevel2Id}
+                  planningUnitGridShape={values.planningUnitGridShape}
+                  planningUnitAreakm2={values.planningUnitAreakm2}
+                />
+              </div>
+            </HelpBeacon>
 
           </div>
 
