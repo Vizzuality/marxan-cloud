9bc650eaeb20c30d4bd31fe26047fa168e312274
anamontiaga <amontiaga@gmail.com>
2021-07-19 10:32:18 +0200

    add new png logos to partners list

--- app/layout/partners/component.tsx
+++ app/layout/partners/component.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import Wrapper from 'layout/wrapper';
 
-import { FOUNDING_LOGOS, SUPPORTING_LOGOS } from './constants';
+import {
+  FOUNDING_LOGOS, PARTNERSHIP_LOGOS, ADDITIONAL_SUPPORT_LOGOS, INITIATIVE_LOGOS,
+} from './constants';
 
 export interface PartnersListProps {
 
@@ -12,12 +14,24 @@ export const PartnersList: React.FC<PartnersListProps> = () => {
   return (
     <div className="bg-white">
       <Wrapper>
-        <div className="flex flex-col items-center w-full max-w-5xl pt-12 pb-40 mx-auto">
+        <div className="flex flex-col items-center w-full max-w-5xl pt-12 mx-auto">
           <h2 className="pb-20 text-2xl leading-relaxed text-black font-heading max-w-max bg-clip-text">
-            Founding partners:
+            Brought to you by:
           </h2>
-          <div className="grid w-full grid-cols-1 py-10 md:grid-cols-3 gap-x-56 gap-y-24">
+          <div className="grid w-full grid-cols-1 py-10 md:grid-cols-2 gap-x-56 gap-y-24">
             {FOUNDING_LOGOS.map((fl) => (
+              <a href={fl.hyperlink} key={fl.id} rel="noreferrer" target="_blank" className="place-self-center">
+                <img alt={fl.alt} src={fl.src} />
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col items-center w-full max-w-5xl pt-12 mx-auto">
+          <h2 className="pb-20 text-2xl leading-relaxed text-black font-heading max-w-max bg-clip-text">
+            In partnership with:
+          </h2>
+          <div className="grid w-full grid-cols-1 py-10 md:grid-cols-2 gap-x-56 gap-y-24">
+            {PARTNERSHIP_LOGOS.map((fl) => (
               <a href={fl.hyperlink} key={fl.id} rel="noreferrer" target="_blank" className="place-self-center">
                 <img alt={fl.alt} src={fl.src} />
               </a>
@@ -27,11 +41,26 @@ export const PartnersList: React.FC<PartnersListProps> = () => {
         <div className="relative flex flex-col items-center w-full max-w-5xl pt-24 pb-20 mx-auto border-t border-black border-opacity-20">
           <div className="absolute flex justify-center h-10 bg-white w-80 -top-5">
             <h2 className="pb-20 text-2xl leading-relaxed text-black font-heading max-w-max bg-clip-text">
-              Supporting partners:
+              With additional support from:
             </h2>
           </div>
-          <div className="grid w-full grid-cols-1 py-10 md:grid-cols-3 gap-x-56 gap-y-24">
-            {SUPPORTING_LOGOS.map((sl) => (
+          <div className="grid w-full grid-cols-1 py-10 md:grid-cols-5 gap-x-16 gap-y-24">
+            {ADDITIONAL_SUPPORT_LOGOS.map((sl) => (
+              <a href={sl.hyperlink} key={sl.id} rel="noreferrer" target="_blank" className="place-self-center">
+                <img alt={sl.alt} src={sl.src} />
+              </a>
+            ))}
+          </div>
+
+        </div>
+        <div className="relative flex flex-col items-center w-full max-w-5xl pt-24 pb-20 mx-auto border-t border-black border-opacity-20">
+          <div className="absolute flex justify-center h-10 bg-white w-80 -top-5">
+            <h2 className="pb-20 text-2xl leading-relaxed text-black font-heading max-w-max bg-clip-text">
+              Supported by an initiative of the:
+            </h2>
+          </div>
+          <div className="grid w-full grid-cols-1 py-10 md:grid-cols-1 gap-x-16 gap-y-24">
+            {INITIATIVE_LOGOS.map((sl) => (
               <a href={sl.hyperlink} key={sl.id} rel="noreferrer" target="_blank" className="place-self-center">
                 <img alt={sl.alt} src={sl.src} />
               </a>
--- app/layout/partners/constants.ts
+++ app/layout/partners/constants.ts
@@ -1,67 +1,81 @@
-import MICROSOFT_SVG from 'svgs/partners/microsoft_logo.svg';
-import THE_NATURE_CONSERVANCY_SVG from 'svgs/partners/the-nature-conservancy_logo.svg';
-import UNIVERSITY_QUEENSLAND_SVG from 'svgs/partners/university-queensland_logo.svg';
-import EU_SVG from 'svgs/partners/eu_logo.svg';
-import BIOPAMA_SVG from 'svgs/partners/biopama_logo.svg';
-import VIZZUALITY_SVG from 'svgs/partners/vizzuality_logo.svg';
-import UN_ENVIRONMENT_SVG from 'svgs/partners/un-environment_logo.svg';
-import MAPBOX_SVG from 'svgs/partners/mapbox_logo.svg';
-import PACMARA_SVG from 'svgs/partners/pacmara_logo.svg';
+import MICROSOFT_LOGO from 'images/partners/microsoft_logo.png';
+import THE_NATURE_CONSERVANCY_LOGO from 'images/partners/the-nature-conservancy_logo.png';
+import UNIVERSITY_QUEENSLAND_LOGO from 'images/partners/university-queensland_logo.png';
+import EU_COMISSION_LOGO from 'images/partners/european-commission_logo.png';
+import BIOPAMA_LOGO from 'images/partners/biopama_logo.png';
+import VIZZUALITY_LOGO from 'images/partners/vizzuality_logo.png';
+import UN_ENVIRONMENT_LOGO from 'images/partners/un-environment_logo.png';
+import MAPBOX_LOGO from 'images/partners/mapbox_logo.png';
+import PACMARA_LOGO from 'images/partners/pacmara_logo.png';
+import ALLEN_CORAL_LOGO from 'images/partners/allen-coral_logo.png';
 
 export const FOUNDING_LOGOS = [
   {
     id: 1,
-    alt: 'Microsoft logo',
-    hyperlink: 'https://www.microsoft.com',
-    src: MICROSOFT_SVG,
+    alt: 'The Nature Conservancy logo',
+    hyperlink: 'https://www.nature.org',
+    src: THE_NATURE_CONSERVANCY_LOGO,
   },
   {
     id: 2,
-    alt: 'The Nature Conservancy logo',
-    hyperlink: 'https://www.nature.org',
-    src: THE_NATURE_CONSERVANCY_SVG,
-  },
+    alt: 'Microsoft logo',
+    hyperlink: 'https://www.microsoft.com',
+    src: MICROSOFT_LOGO,
+  }];
+
+export const PARTNERSHIP_LOGOS = [
   {
     id: 3,
-    alt: 'The University of Queensland logo',
-    hyperlink: 'https://www.uq.edu.au',
-    src: UNIVERSITY_QUEENSLAND_SVG,
+    alt: 'Biopama logo',
+    hyperlink: 'https://biopama.org',
+    src: BIOPAMA_LOGO,
   },
   {
     id: 4,
-    alt: 'European Commission logo',
-    hyperlink: 'https://ec.europa.eu',
-    src: EU_SVG,
+    alt: 'The University of Queensland logo',
+    hyperlink: 'https://www.uq.edu.au',
+    src: UNIVERSITY_QUEENSLAND_LOGO,
   },
+];
+
+export const ADDITIONAL_SUPPORT_LOGOS = [
   {
     id: 5,
-    alt: 'Biopama logo',
-    hyperlink: 'https://biopama.org',
-    src: BIOPAMA_SVG,
-  }];
-
-export const SUPPORTING_LOGOS = [
-  {
-    id: 6,
     alt: 'Vizzuality logo',
     hyperlink: 'https://www.vizzuality.com',
-    src: VIZZUALITY_SVG,
+    src: VIZZUALITY_LOGO,
+  },
+  {
+    id: 6,
+    alt: 'Mapbox logo',
+    hyperlink: 'https://www.mapbox.com',
+    src: MAPBOX_LOGO,
   },
   {
     id: 7,
     alt: 'United Nations Environment Programme logo',
     hyperlink: 'https://www.unep.org',
-    src: UN_ENVIRONMENT_SVG,
-  },
-  {
-    id: 8,
-    alt: 'Mapbox logo',
-    hyperlink: 'https://www.mapbox.com',
-    src: MAPBOX_SVG,
+    src: UN_ENVIRONMENT_LOGO,
   },
   {
     id: 9,
+    alt: 'Allen Coral logo',
+    hyperlink: 'https://allencoralatlas.org',
+    src: ALLEN_CORAL_LOGO,
+  },
+  {
+    id: 10,
     alt: 'Pacmara logo',
     hyperlink: 'https://pacmara.org',
-    src: PACMARA_SVG,
-  }];
+    src: PACMARA_LOGO,
+  },
+];
+
+export const INITIATIVE_LOGOS = [
+  {
+    id: 11,
+    alt: 'European Commission logo',
+    hyperlink: 'https://ec.europa.eu',
+    src: EU_COMISSION_LOGO,
+  },
+];
