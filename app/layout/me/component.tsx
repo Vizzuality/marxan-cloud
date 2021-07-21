9cc3077d38f724032abee1492a4003f29fb7e729
anamontiaga <amontiaga@gmail.com>
2021-07-21 18:54:59 +0200

    mobile fixes

--- app/layout/me/component.tsx
+++ app/layout/me/component.tsx
@@ -10,11 +10,11 @@ export interface MeProps {
 
 export const Me: React.FC<MeProps> = () => {
   return (
-    <div className="flex w-full h-full">
-      <div className="w-1/3 bg-white p-14">
+    <div className="w-full h-full md:flex">
+      <div className="bg-white md:w-1/3 p-14">
         <Profile />
       </div>
-      <div className="w-2/3 py-24 bg-gray-100">
+      <div className="py-24 bg-gray-100 md:w-2/3">
         <div className="py-10 border-b border-gray-200 px-36">
           <ChangePassword />
         </div>
