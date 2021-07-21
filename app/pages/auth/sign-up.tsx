5c5bab91172bfa64c19fae708ffd5ff361a2c439
anamontiaga <amontiaga@gmail.com>
2021-07-21 10:46:45 +0200

    comment guide request

--- app/pages/auth/sign-up.tsx
+++ app/pages/auth/sign-up.tsx
@@ -6,8 +6,8 @@ import { useRouter } from 'next/router';
 import Header from 'layout/header';
 import MetaTags from 'layout/meta-tags';
 
-// import SignUp from 'layout/sign-up';
-import GuideRequest from 'layout/sign-up/guide-request';
+import SignUp from 'layout/sign-up';
+// import GuideRequest from 'layout/sign-up/guide-request';
 
 import MARXAN_SOCIAL_MEDIA_IMG from 'images/social-media/marxan-social-media.png';
 
@@ -41,9 +41,9 @@ const SignUpPage: React.FC = () => {
 
         <div className="flex flex-col h-full md:flex-grow">
           <div className="flex items-center justify-center h-full py-10 bg-white">
-            {/* <SignUp /> */}
+            <SignUp />
 
-            <GuideRequest />
+            {/* <GuideRequest /> */}
 
           </div>
         </div>
