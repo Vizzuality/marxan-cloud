2b021fb2cfb4dd92395b404e27de0f62969a61f5
anamontiaga <amontiaga@gmail.com>
2021-07-21 16:08:27 +0200

    add confirm sign up on sign up process

--- app/layout/sign-up/component.tsx
+++ app/layout/sign-up/component.tsx
@@ -7,7 +7,6 @@ import Wrapper from 'layout/wrapper';
 
 import Link from 'next/link';
 import Button from 'components/button';
-import Modal from 'components/modal';
 import Loading from 'components/loading';
 
 import { Form as FormRFF, Field as FieldRFF } from 'react-final-form';
@@ -36,10 +35,11 @@ export interface SignUpProps {
 export const SignUp: React.FC<SignUpProps> = () => {
   const [submitting, setSubmitting] = useState(false);
   const { addToast } = useToasts();
-  const [modal, setModal] = useState(false);
+  const [confirm, setConfirm] = useState(false);
 
   const handleSubmit = useCallback(async (data) => {
     setSubmitting(true);
+    setConfirm(true);
 
     try {
       const signUpResponse = await AUTHENTICATION
@@ -51,6 +51,7 @@ export const SignUp: React.FC<SignUpProps> = () => {
       // There is an inconsistency on the API
       // where username is used for log in instead of email
       if (signUpResponse.status === 201) {
+        setConfirm(false);
         await signIn('credentials', { ...data, username: data.email });
       }
     } catch (error) {
@@ -84,100 +85,96 @@ export const SignUp: React.FC<SignUpProps> = () => {
       >
         {(props) => (
           <form onSubmit={props.handleSubmit} autoComplete="off" className="relative flex items-center justify-center h-full">
-            <div className="w-full max-w-xs">
-              <h2 className="mb-5 text-lg font-medium text-center text-gray-600 font-heading">Get Started!</h2>
+            {!confirm && (
+              <div className="w-full max-w-xs">
+                <h2 className="mb-5 text-lg font-medium text-center text-gray-600 font-heading">Get Started!</h2>
 
-              <Loading
-                visible={submitting}
-                className="absolute top-0 bottom-0 left-0 right-0 z-40 flex items-center justify-center w-full h-full bg-white bg-opacity-90"
-                iconClassName="w-10 h-10 text-primary-500"
-              />
+                <Loading
+                  visible={submitting}
+                  className="absolute top-0 bottom-0 left-0 right-0 z-40 flex items-center justify-center w-full h-full bg-white bg-opacity-90"
+                  iconClassName="w-10 h-10 text-primary-500"
+                />
 
-              {/* DISPLAY NAME */}
-              <div>
-                <FieldRFF
-                  name="displayName"
-                  validate={composeValidators([{ presence: true }])}
-                >
-                  {(fprops) => (
-                    <Field id="login-displayName" {...fprops}>
-                      <Label theme="light" className="mb-3 uppercase">Name</Label>
-                      <Input theme="light" icon={USER_SVG} />
-                    </Field>
-                  )}
-                </FieldRFF>
+                {/* DISPLAY NAME */}
+                <div>
+                  <FieldRFF
+                    name="displayName"
+                    validate={composeValidators([{ presence: true }])}
+                  >
+                    {(fprops) => (
+                      <Field id="login-displayName" {...fprops}>
+                        <Label theme="light" className="mb-3 uppercase">Name</Label>
+                        <Input theme="light" icon={USER_SVG} />
+                      </Field>
+                    )}
+                  </FieldRFF>
+                </div>
+
+                {/* EMAIL */}
+                <div className="mt-5">
+                  <FieldRFF
+                    name="email"
+                    validate={composeValidators([{ presence: true, email: true }])}
+                  >
+                    {(fprops) => (
+                      <Field id="login-username" {...fprops}>
+                        <Label theme="light" className="mb-3 uppercase">Email</Label>
+                        <Input theme="light" type="email" icon={EMAIL_SVG} />
+                      </Field>
+                    )}
+                  </FieldRFF>
+                </div>
+
+                {/* PASSWORD */}
+                <div className="mt-5">
+                  <FieldRFF
+                    name="password"
+                    validate={composeValidators([{ presence: true }])}
+                  >
+                    {(fprops) => (
+                      <Field id="login-password" {...fprops}>
+                        <Label theme="light" className="mb-3 uppercase">Password</Label>
+                        <Input theme="light" type="password" icon={PASSWORD_SVG} />
+                      </Field>
+                    )}
+                  </FieldRFF>
+                </div>
+
+                <div className="mt-7">
+                  <FieldRFF
+                    name="checkbox"
+                    type="checkbox"
+                    validate={composeValidators([booleanValidator])}
+                  >
+                    {(fprops) => (
+                      <Field className="flex mt-2" id="form-checkbox" {...fprops}>
+                        <Checkbox theme="light" />
+                        <Label theme="light" className="pr-20 ml-2 -mt-1 font-sans text-sm">I accept the Terms of service and privacy policy</Label>
+                      </Field>
+                    )}
+                  </FieldRFF>
+                </div>
+
+                <div className="mt-10">
+                  <Button theme="primary" size="lg" type="submit" disabled={submitting} className="w-full">
+                    Sign up
+                  </Button>
+                </div>
+
+                <div className="mt-5 text-sm text-center text-black">
+                  Already registered.
+                  {' '}
+                  <Link href="/auth/sign-in"><a href="/auth/sign-in" className="underline">Sign in</a></Link>
+                </div>
               </div>
-
-              {/* EMAIL */}
-              <div className="mt-5">
-                <FieldRFF
-                  name="email"
-                  validate={composeValidators([{ presence: true, email: true }])}
-                >
-                  {(fprops) => (
-                    <Field id="login-username" {...fprops}>
-                      <Label theme="light" className="mb-3 uppercase">Email</Label>
-                      <Input theme="light" type="email" icon={EMAIL_SVG} />
-                    </Field>
-                  )}
-                </FieldRFF>
-              </div>
-
-              {/* PASSWORD */}
-              <div className="mt-5">
-                <FieldRFF
-                  name="password"
-                  validate={composeValidators([{ presence: true }])}
-                >
-                  {(fprops) => (
-                    <Field id="login-password" {...fprops}>
-                      <Label theme="light" className="mb-3 uppercase">Password</Label>
-                      <Input theme="light" type="password" icon={PASSWORD_SVG} />
-                    </Field>
-                  )}
-                </FieldRFF>
-              </div>
-
-              <div className="mt-7">
-                <FieldRFF
-                  name="checkbox"
-                  type="checkbox"
-                  validate={composeValidators([booleanValidator])}
-                >
-                  {(fprops) => (
-                    <Field className="flex mt-2" id="form-checkbox" {...fprops}>
-                      <Checkbox theme="light" />
-                      <Label theme="light" className="pr-20 ml-2 -mt-1 font-sans text-sm">I accept the Terms of service and privacy policy</Label>
-                    </Field>
-                  )}
-                </FieldRFF>
-              </div>
-
-              <div className="mt-10">
-                <Button theme="primary" size="lg" type="submit" disabled={submitting} className="w-full">
-                  Sign up
-                </Button>
-              </div>
-
-              <div className="mt-5 text-sm text-center text-black">
-                Already registered.
-                {' '}
-                <Link href="/auth/sign-in"><a href="/auth/sign-in" className="underline">Sign in</a></Link>
-              </div>
-            </div>
+            )}
           </form>
         )}
       </FormRFF>
 
-      <Modal
-        title="Hello"
-        open={modal}
-        size="narrow"
-        onDismiss={() => setModal(false)}
-      >
+      {confirm && (
         <ConfirmSignUp />
-      </Modal>
-
+      )}
     </Wrapper>
   );
 };
