d94d5ce78668a81992b9e810a06d219bbadd5a0b
andrea rota <andrea.rota@vizzuality.com>
2021-08-02 16:13:40 +0100

    plug in minimal linking of plain features to scenarios

--- api/apps/api/src/modules/geo-features/geo-feature-set.service.ts
+++ api/apps/api/src/modules/geo-features/geo-feature-set.service.ts
@@ -18,6 +18,7 @@ import { GeoFeaturePropertySetService } from './geo-feature-property-sets.servic
 import { ScenarioFeaturesData } from '@marxan/features';
 import { flattenDeep } from 'lodash';
 import { GeoprocessingOpSplitV1 } from './types/geo-feature.geoprocessing-operations.type';
+import { RemoteFeaturesData } from '../scenarios-features/entities/remote-features-data.geo.entity';
 
 export const EntityManagerToken = Symbol();
 
@@ -69,7 +70,7 @@ export class GeoFeatureSetService {
     const scenario = await this.scenarioRepository.findOneOrFail(id);
     await this.scenarioRepository.update(id, { featureSet: dto });
     // @todo: move to async job - this was just for simple tests
-    // await this.createFeaturesForScenario(id, dto.features);
+    await this.createFeaturesForScenario(id, dto.features);
     return await this.geoFeaturePropertySetService.extendGeoFeatureProcessingSpecification(
       dto,
       scenario,
@@ -115,33 +116,43 @@ export class GeoFeatureSetService {
     scenarioId: string,
     featureSpecification: SpecForGeofeature[],
   ): Promise<
-    ({
+    {
       scenarioId: string;
       featuresDataId: string;
-      fpf: number;
-      prop: number | undefined;
-    } & ScenarioFeaturesData)[]
+      fpf?: number | undefined;
+      prop?: number | undefined;
+    }[][]
   > {
     const repository = transactionalEntityManager.getRepository(
       ScenarioFeaturesData,
     );
-    return Promise.all(
+    const scenarioFeaturesData = Promise.all(
       featureSpecification
         .filter(
           (feature): feature is SpecForPlainGeoFeature =>
             feature.kind === 'plain',
         )
-        .map((feature) => {
-          return repository.save({
-            scenarioId,
-            featuresDataId: feature.featureId,
-            fpf:
-              feature.marxanSettings?.fpf ??
-              MarxanFeaturesMetadata.defaults.fpf,
-            prop: feature.marxanSettings?.prop,
-          });
+        .map(async (feature) => {
+          const featuresDataRepo = transactionalEntityManager.getRepository(
+            RemoteFeaturesData,
+          );
+          const featuresData = await featuresDataRepo
+            .find({ featureId: feature.featureId })
+            .then((result) =>
+              result.map((fd) => ({
+                scenarioId,
+                featuresDataId: fd.id,
+                fpf:
+                  feature.marxanSettings?.fpf ??
+                  MarxanFeaturesMetadata.defaults.fpf,
+                prop: feature.marxanSettings?.prop,
+              })),
+            );
+
+          return repository.save(featuresData);
         }),
     );
+    return scenarioFeaturesData;
   }
 
   /**
