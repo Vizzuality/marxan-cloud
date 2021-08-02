f172f92d02699a385114bdea5fa7d10fd75cd0de
Pedro Pimenta <pedro@pimenta.co>
2021-08-02 16:40:53 +0200

    Change Home Features section

--- app/layout/home/features/component.tsx
+++ app/layout/home/features/component.tsx
@@ -13,30 +13,37 @@ export interface HomeFeaturesProps {
 
 export const HomeFeatures: React.FC<HomeFeaturesProps> = () => {
   return (
-    <Wrapper>
-      <div className="w-full pb-32">
-        <div className="grid max-w-5xl grid-cols-1 gap-20 mx-auto sm:grid-cols-2 lg:grid-cols-3">
-          {FEATURES.map((F) => {
-            const {
-              id, name, description, icon, iconClassName,
-            } = F;
+    <div className="bg-white">
+      <Wrapper>
+        <div className="w-full py-32">
+          <div className="grid max-w-5xl grid-cols-1 gap-20 mx-auto sm:grid-cols-2 lg:grid-cols-3">
+            {FEATURES.map((F) => {
+              const {
+                id, name, description, icon, iconClassName,
+              } = F;
 
-            return (
-              <div key={`${id}`}>
-                <div className="flex items-center justify-center w-16 h-16 bg-gray-500 rounded-full">
-                  <Icon icon={icon} className={iconClassName} />
+              return (
+                <div key={`${id}`}>
+                  <div className="flex items-center justify-center w-16 h-16 bg-primary-50 rounded-2xl">
+                    <Icon icon={icon} className={iconClassName} />
+                  </div>
+                  <h2 className="mt-6 mb-10 text-gray-800 text-2xl font-medium font-heading">{name}</h2>
+                  <p className="text-gray-300">{description}</p>
                 </div>
-                <h2 className="mt-8 mb-10 text-2xl font-medium font-heading">{name}</h2>
-                <p className="text-gray-300">{description}</p>
-              </div>
-            );
-          })}
-          <div className="w-full mt-20 place-self-center">
-            <ButtonLink caption="Explore planning Examples" href="/community/projects" />
+              );
+            })}
+            <div className="w-full mt-20 place-self-center">
+              {/* <Link href="/community/projects">
+                <a href="/community/projects" className="mt-2">
+                  Explore planning Examples
+                </a>
+              </Link> */}
+              <ButtonLink caption="Explore planning Examples" href="/community/projects" />
+            </div>
           </div>
         </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </div>
   );
 };
 
--- app/layout/statics/button-link/component.tsx
+++ app/layout/statics/button-link/component.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import Icon from 'components/icon';
 import Link from 'next/link';
+
+import Icon from 'components/icon';
 
 import ARROW_RIGHT_2_SVG from 'svgs/ui/arrow-right-2.svg?sprite';
 
@@ -18,9 +19,9 @@ export const StaticButtonLink: React.FC<StaticButtonLinkProps> = ({
 }: StaticButtonLinkProps) => {
   const children = (
     <>
-      <p className="mr-4 text-lg text-primary-500 hover:underline">{caption}</p>
-      <div className="flex items-center justify-center bg-transparent border rounded-full h-7 w-7 border-primary-500">
-        <Icon icon={ARROW_RIGHT_2_SVG} className="w-3 h-3 text-primary-500" />
+      <p className="w-48 mr-4 text-lg text-gray-600 hover:text-gray-800 underline">{caption}</p>
+      <div className="flex items-center justify-center bg-transparent border rounded-full h-7 w-7 border-gray-600 hover:border-gray-800">
+        <Icon icon={ARROW_RIGHT_2_SVG} className="w-3 h-3 text-gray-600" />
       </div>
     </>
   );
@@ -28,13 +29,13 @@ export const StaticButtonLink: React.FC<StaticButtonLinkProps> = ({
   return (
     <>
       {external && (
-        <a className="flex flex-row items-center cursor-pointer" href={href} rel="noopener noreferrer" target="_blank">
+        <a className="flex flex-row items-center justify-between w-full cursor-pointer" href={href} rel="noopener noreferrer" target="_blank">
           {children}
         </a>
       )}
       {!external && (
         <Link href={href}>
-          <div className="flex flex-row items-center cursor-pointer">
+          <div className="flex flex-row items-center justify-between w-full cursor-pointer">
             {children}
           </div>
         </Link>
