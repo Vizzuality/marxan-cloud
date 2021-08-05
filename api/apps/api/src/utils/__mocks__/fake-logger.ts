a73eb8146b43d7da5778ea1290a3d16b96a01875
K Gajowy <kamil.gajowy@gmail.com>
2021-08-05 17:14:36 +0200

    feat(specification): determine features application logic

--- api/apps/api/src/utils/__mocks__/fake-logger.ts
+++ api/apps/api/src/utils/__mocks__/fake-logger.ts
@@ -1,4 +1,8 @@
-export class FakeLogger {
+import { LoggerService } from '@nestjs/common';
+
+export class FakeLogger implements LoggerService {
+  constructor(public readonly context?: string) {}
+
   debug = jest.fn();
   error = jest.fn();
   log = jest.fn();
