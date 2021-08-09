b42691745f255b30ac3c05a456210021ce35fec3
alicia <alicia.arenzana@gmail.com>
2021-08-09 10:38:39 +0200

    updated demo bots

--- data/bots/demo-brazil/bot.ts
+++ data/bots/demo-brazil/bot.ts
@@ -1,87 +1,151 @@
-import axiod from "https://deno.land/x/axiod/mod.ts";
-import Process from 'https://deno.land/std@0.103.0/node/process.ts';
+import axiod from "https://deno.land/x/axiod@0.22/mod.ts";
+import Process from "https://deno.land/std@0.103.0/node/process.ts";
+import {
+  dirname,
+  fromFileUrl,
+  relative,
+} from "https://deno.land/std@0.103.0/path/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
+import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
 
-const settings = {
-    apiUrl: 'http://localhost:3030',
-    credentials: {
-        "username": "aa@example.com",
-        "password": "aauserpassword"
-    }
-};
+const scriptPath = dirname(relative(Deno.cwd(), fromFileUrl(import.meta.url)));
 
-const jwt = await axiod.post(`${settings.apiUrl}/auth/sign-in/`, settings.credentials)
-    .then(result => result.data.accessToken);
-
-const botClient = axiod.create({
-    baseURL: settings.apiUrl + '/api/v1',
-    headers: {
-        'Authorization': 'Bearer ' + jwt
-    }
+const { API_URL, USERNAME, PASSWORD, POSTGRES_URL } = config({
+  path: scriptPath + "/.env",
 });
 
-const organization = await botClient.post('/organizations', {
-    name: "aliquip nulla ut " + crypto.randomUUID(),
+const settings = {
+  apiUrl: API_URL,
+  credentials: {
+    username: USERNAME,
+    password: PASSWORD,
+  },
+};
+
+const jwt = await axiod
+  .post(`${settings.apiUrl}/auth/sign-in/`, settings.credentials)
+  .then((result) => result.data.accessToken);
+
+const pgClient = new Client(POSTGRES_URL);
+
+const botClient = axiod.create({
+  baseURL: settings.apiUrl + "/api/v1",
+  headers: {
+    Authorization: "Bearer " + jwt,
+  },
+});
+
+async function sendData(url: string, data: Blob) {
+  const formData = new FormData();
+  formData.append("file", data, "corsika.zip");
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: formData,
+    headers: [["authorization", "Bearer " + jwt]],
+  });
+
+  return response;
+}
+
+const organization = await botClient
+  .post("/organizations", {
+    name: "Brazil - Atlantic forest " + crypto.randomUUID(),
     description: "Duis aliquip nostrud sint",
-    metadata: {}
-}).then(result => result.data).catch(e => { console.log(e) });
+    metadata: {},
+  })
+  .then((result) => result.data)
+  .catch((e) => {
+    console.log(e);
+  });
 
 console.log(organization);
 
-const project = await botClient.post('/projects', {
-    name: 'test project ' + crypto.randomUUID(),
+const planningAreaFile = await (
+  await sendData(
+    API_URL + "/api/v1/projects/planning-area/shapefile",
+    new Blob([await Deno.readFile(scriptPath + "/planning-area.zip")])
+  )
+).json();
+
+console.log(planningAreaFile);
+
+const planningUnitAreakm2 = 50;
+
+const project = await botClient
+  .post("/projects", {
+    name: "Brazil " + crypto.randomUUID(),
     organizationId: organization.data.id,
-    countryId: 'BWA',
-    adminAreaLevel1Id: 'BWA.12_1',
-    adminAreaLevel2Id: 'BWA.12.1_1',
-    planningUnitGridShape: 'hexagon',
-    planningUnitAreakm2: 16,
-}).then(result => result.data).catch(e => { console.log(e) });;
+    planningUnitGridShape: "hexagon",
+    planningUnitAreakm2: planningUnitAreakm2,
+    planningAreaId: planningAreaFile.id,
+  })
+  .then((result) => result.data)
+  .catch((e) => {
+    console.log(e);
+  });
 
 console.log(project);
 
 // wait a bit for async job to be picked up and processed
 // @DEBT we should check the actual job status
-await new Promise(r => setTimeout(r, 30e3))
+// await new Promise((r) => setTimeout(r, 30e3));
 
 const scenarioStart = Process.hrtime();
 
-const scenario = await botClient.post('/scenarios', {
-    name: `test scenario in project ${project.data.attributes.name}`,
+const scenario = await botClient
+  .post("/scenarios", {
+    name: `Corsika ${project.data.attributes.name}`,
     type: "marxan",
     projectId: project.data.id,
-    description: "eu et sit",
-    wdpaIucnCategories: ['Not Applicable'],
-    wdpaThreshold: 30
-}).then(result => result.data).catch(e => { console.log(e); });
+    description: "A Corsika scenario",
+    wdpaIucnCategories: ["Not Applicable"],
+    wdpaThreshold: 50,
+  })
+  .then((result) => result.data)
+  .catch((e) => {
+    console.log(e);
+  });
 
 const scenarioTook = Process.hrtime(scenarioStart);
 console.log(`Scenario creation done in ${scenarioTook[0]} seconds`);
 
 console.log(scenario);
 
-const pantheraPardusFeature = await botClient.get(`/projects/${project.data.id}/features?q=pantherapardus`)
-  .then(result => result.data).catch(e => { console.log(e); });
+const pantheraPardusFeature = await botClient
+  .get(`/projects/${project.data.id}/features?q=pantherapardus`)
+  .then((result) => result.data)
+  .catch((e) => {
+    console.log(e);
+  });
 
 console.log(pantheraPardusFeature);
 
 const geoFeatureSpecStart = Process.hrtime();
 
-const geoFeatureSpec = await botClient.post(`/scenarios/${scenario.data.id}/features/specification`, {
-    status: 'created',
+const geoFeatureSpec = await botClient
+  .post(`/scenarios/${scenario.data.id}/features/specification`, {
+    status: "created",
     features: [
       {
         kind: "plain",
         featureId: pantheraPardusFeature.data[0].id,
         marxanSettings: {
-            prop: 0.3,
-            fpf: 1
-        }
+          prop: 0.3,
+          fpf: 1,
+        },
       },
-    ]
-  }).then(result => result.data).catch(e => { console.log(e); });
+    ],
+  })
+  .then((result) => result.data)
+  .catch((e) => {
+    console.log(e);
+  });
 
-  const geoFeatureSpecTook = Process.hrtime(geoFeatureSpecStart);
+const geoFeatureSpecTook = Process.hrtime(geoFeatureSpecStart);
 
-  console.log(`Processing of features for scenario done in ${geoFeatureSpecTook[0]} seconds`);
+console.log(
+  `Processing of features for scenario done in ${geoFeatureSpecTook[0]} seconds`
+);
 
-  console.log(geoFeatureSpec);
+console.log(geoFeatureSpec);
