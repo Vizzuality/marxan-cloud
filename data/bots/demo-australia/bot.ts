bba2ce7ee08efcee96c28459667f56f6fa343eaa
alicia <alicia.arenzana@gmail.com>
2021-08-09 19:27:55 +0200

    fixed issue with custom area upload

--- data/bots/demo-australia/bot.ts
+++ data/bots/demo-australia/bot.ts
@@ -7,6 +7,7 @@ import {
 } from "https://deno.land/std@0.103.0/path/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
+import { sleep } from "https://deno.land/x/sleep/mod.ts";
 
 const scriptPath = dirname(relative(Deno.cwd(), fromFileUrl(import.meta.url)));
 
@@ -26,7 +27,7 @@ const jwt = await axiod
   .post(`${settings.apiUrl}/auth/sign-in/`, settings.credentials)
   .then((result) => result.data.accessToken);
 
-const pgClient = new Client(POSTGRES_URL);
+// const pgClient = new Client(POSTGRES_URL);
 
 const botClient = axiod.create({
   baseURL: settings.apiUrl + "/api/v1",
@@ -37,7 +38,7 @@ const botClient = axiod.create({
 
 async function sendData(url: string, data: Blob) {
   const formData = new FormData();
-  formData.append("file", data, "planning-area.zip");
+  formData.append("file", data, "test_mata.zip");
 
   const response = await fetch(url, {
     method: "POST",
@@ -50,7 +51,7 @@ async function sendData(url: string, data: Blob) {
 
 const organization = await botClient
   .post("/organizations", {
-    name: "University of adelaide " + crypto.randomUUID(),
+    name: "Australia - Kimberley " + crypto.randomUUID(),
     description: "Duis aliquip nostrud sint",
     metadata: {},
   })
@@ -63,18 +64,18 @@ console.log(organization);
 
 const planningAreaFile = await (
   await sendData(
-    API_URL + "/api/v1/projects/planning-area/shapefile",
-    new Blob([await Deno.readFile(scriptPath + "/planning-area.zip")])
+    settings.apiUrl + "/api/v1/projects/planning-area/shapefile",
+    new Blob([await Deno.readFile(scriptPath + "/kimberley.zip")])
   )
 ).json();
 
 console.log(planningAreaFile);
 
-const planningUnitAreakm2 = 25;
+const planningUnitAreakm2 = 50;
 
 const project = await botClient
   .post("/projects", {
-    name: "Kimberley " + crypto.randomUUID(),
+    name: "Australia - Kimberley " + crypto.randomUUID(),
     organizationId: organization.data.id,
     planningUnitGridShape: "hexagon",
     planningUnitAreakm2: planningUnitAreakm2,
@@ -86,18 +87,28 @@ const project = await botClient
   });
 
 console.log(project);
+
 // wait a bit for async job to be picked up and processed
 // @DEBT we should check the actual job status
 // await new Promise((r) => setTimeout(r, 30e3));
 
 const scenarioStart = Process.hrtime();
 
+await sleep(5)
+
+// get the list of protected areas in the region and use all of them
+const paCategories = await botClient.get(`/protected-areas/iucn-categories?filter%5BcustomAreaId%5D=${planningAreaFile.id}`).then((result) => result.data)
+.catch((e) => {
+  console.log(e);
+});
+console.log(paCategories);
+
 const scenario = await botClient
   .post("/scenarios", {
-    name: `Kimberley test 1 ${project.data.attributes.name}`,
+    name: `Kimberley ${project.data.attributes.name}`,
     type: "marxan",
     projectId: project.data.id,
-    description: "A Kimberley scenario planning",
+    description: "An Australia scenario",
     wdpaIucnCategories: ["Not Applicable"],
     wdpaThreshold: 50,
   })
@@ -110,27 +121,44 @@ const scenarioTook = Process.hrtime(scenarioStart);
 console.log(`Scenario creation done in ${scenarioTook[0]} seconds`);
 
 console.log(scenario);
-
-const featureList: string[] = []
-
-const pantheraPardusFeature = await botClient
-  .get(`/projects/${project.data.id}/features?q=pantherapardus`)
+const featureList = [
+       "demo_ecoregions_new_class_split",
+       "demo_buteogallus_urubitinga",
+       "demo_caluromys_philander",
+       "demo_chiroxiphia_caudata",
+       "demo_leopardus_pardalis",
+       "demo_megarynchus_pitangua",
+       "demo_phyllodytes_tuberculosus",
+       "demo_tapirus_terrestris",
+       "demo_thalurania_glaucopis",
+]
+const features = await botClient
+  .get(`/projects/${project.data.id}/features?q=demo`)
   .then((result) => result.data)
   .catch((e) => {
     console.log(e);
   });
 
-console.log(pantheraPardusFeature);
+console.log(features);
 
 const geoFeatureSpecStart = Process.hrtime();
 
+// const featureRecipe = features.map( (x: Object) => { return {
+//     kind: "plain",
+//     featureId: x.id,
+//     marxanSettings: {
+//       prop: 0.3,
+//       fpf: 1,
+//     },
+//   }})
+//   console.log(featureRecipe);
 const geoFeatureSpec = await botClient
   .post(`/scenarios/${scenario.data.id}/features/specification`, {
     status: "created",
     features: [
       {
         kind: "plain",
-        featureId: pantheraPardusFeature.data[0].id,
+        featureId: features.data[0].id,
         marxanSettings: {
           prop: 0.3,
           fpf: 1,
