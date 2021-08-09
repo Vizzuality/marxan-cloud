701e921ac9e0bac58d3f4b53b394f44487351b57
kgajowy <kamil.gajowy@gmail.com>
2021-08-09 13:28:42 +0200

    refactor: remove unnecessary context and constructor from fake-logger util

--- api/apps/api/src/utils/__mocks__/fake-logger.ts
+++ api/apps/api/src/utils/__mocks__/fake-logger.ts
@@ -1,8 +1,6 @@
 import { LoggerService } from '@nestjs/common';
 
 export class FakeLogger implements LoggerService {
-  constructor(public readonly context?: string) {}
-
   debug = jest.fn();
   error = jest.fn();
   log = jest.fn();
