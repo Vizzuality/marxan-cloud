510839e4b991b0ff63a702281ce6b57b73c0b7d5
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-08-13 16:18:08 +0100

    feat(upload): make geofeature model extend base entity

--- api/apps/api/src/modules/geo-features/geo-feature.api.entity.ts
+++ api/apps/api/src/modules/geo-features/geo-feature.api.entity.ts
@@ -1,5 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
 import { BaseServiceResource } from '@marxan-api/types/resource.interface';
 import { FeatureTag } from '@marxan/features/domain';
 import { JobStatus } from '../scenarios/scenario.api.entity';
@@ -19,7 +19,7 @@ export interface GeoFeatureProperty {
 }
 
 @Entity('features')
-export class GeoFeature {
+export class GeoFeature extends BaseEntity {
   @ApiProperty()
   @PrimaryColumn()
   id!: string;
