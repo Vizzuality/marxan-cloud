88f76fdc221ecfde81c28087759dfe60556869a8
alicia <alicia.arenzana@gmail.com>
2021-08-13 17:22:57 +0200

    finish connecting worker and processor to geoprocessing

--- api/apps/api/src/migrations/api/1627979898685-AddNewProcessingEvents.ts
+++ api/apps/api/src/migrations/api/1627979898685-AddNewProcessingEvents.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddProcessingEvents1627979898681 implements MigrationInterface {
+export class AddNewProcessingEvents1627979898685 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       INSERT INTO api_event_kinds (id) values
       ('scenario.planningAreaProtectedCalculation.finished/v1/alpha'),
-      ('scenario.planningAreaProtectedCalculation.failed/v1/alpha'),
+      ('scenario.planningAreaProtectedCalculation.failed/v1/alpha');
     `);
   }
 
