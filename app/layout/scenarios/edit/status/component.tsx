1c09186bbe8c545db2c15b9f051d72d8a63045be
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-08-17 16:57:55 +0200

    Scenario: asyn jobs track

--- app/layout/scenarios/edit/status/component.tsx
+++ app/layout/scenarios/edit/status/component.tsx
@@ -6,28 +6,39 @@ import { motion } from 'framer-motion';
 
 import { useScenarioStatus } from 'hooks/scenarios';
 
+import Button from 'components/button';
+
 export interface ScenarioStatusProps {
 }
+
+const TEXTS = {
+  planningAreaProtectedCalculation: () => 'The scenario is calculating the protected areas percentages',
+  geofeatureCopy: () => 'The scenario is processing the features',
+  planningUnitsInclusion: () => 'The scenario is processing the inclusion/exclusion planning units',
+  run: () => 'The scenario is running Marxan',
+};
 
 export const ScenarioStatus: React.FC<ScenarioStatusProps> = () => {
   const { query } = useRouter();
   const { pid, sid } = query;
 
   const { data } = useScenarioStatus(pid, sid);
-  console.log(data);
 
-  const runJob = useMemo(() => {
+  const JOB = useMemo(() => {
     const { jobs = [] } = data || {};
     return jobs.find((j) => j.status === 'running');
   }, [data]);
 
-  if (runJob) {
-    console.info(runJob);
-  }
+  const TEXT = useMemo(() => {
+    if (JOB && TEXTS[JOB.kind]) {
+      return TEXTS[JOB.kind]();
+    }
+    return null;
+  }, [JOB]);
 
   return (
     <div className="absolute top-0 left-0 z-50 flex flex-col justify-end w-full h-full pointer-events-none">
-      {runJob && (
+      {JOB && (
         <motion.div
           className="absolute top-0 left-0 z-10 w-full h-full pointer-events-auto bg-blur"
           key="status-overlay"
@@ -36,15 +47,25 @@ export const ScenarioStatus: React.FC<ScenarioStatusProps> = () => {
         />
       )}
 
-      {runJob && (
+      {JOB && (
         <motion.div
           className="absolute z-10 pointer-events-auto top-1/2 left-1/2"
           key="status-text"
           initial={{ opacity: 0, y: '-60%', x: '-50%' }}
           animate={{ opacity: 1, y: '-50%', x: '-50%' }}
         >
-          <div className="w-full max-w-xs p-10 bg-gray-500">
-            The scenario is processing XXX
+          <div className="w-full max-w-md p-10 text-center">
+            {TEXT}
+
+            <div className="flex justify-center mt-5 ">
+              <Button
+                theme="primary-alt"
+                size="base"
+                href={`/projects/${pid}`}
+              >
+                Go to project
+              </Button>
+            </div>
           </div>
         </motion.div>
       )}
