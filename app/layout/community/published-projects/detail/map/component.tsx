cdc2eae444727ea92607e710d3a99950f13dba1b
anamontiaga <amontiaga@gmail.com>
2021-08-23 11:23:20 +0200

    published project: controls

--- app/layout/community/published-projects/detail/map/component.tsx
+++ app/layout/community/published-projects/detail/map/component.tsx
@@ -9,12 +9,16 @@ import { useRouter } from 'next/router';
 import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
 import { LayerManager, Layer } from '@vizzuality/layer-manager-react';
 import { AnimatePresence, motion } from 'framer-motion';
+import { useSession } from 'next-auth/client';
 
 import { usePUGridLayer } from 'hooks/map';
 import { usePublishedProject } from 'hooks/projects';
 import { useScenarios } from 'hooks/scenarios';
 
 import Map from 'components/map';
+import Controls from 'components/map/controls';
+import FitBoundsControl from 'components/map/controls/fit-bounds';
+import ZoomControl from 'components/map/controls/zoom';
 
 export interface PublishedProjectMapProps {
 }
@@ -22,6 +26,7 @@ export interface PublishedProjectMapProps {
 export const PublishedProjectMap: React.FC<PublishedProjectMapProps> = () => {
   const [viewport, setViewport] = useState({});
   const [bounds, setBounds] = useState(null);
+  const [session] = useSession();
 
   const { query } = useRouter();
   const { pid } = query;
@@ -62,7 +67,7 @@ export const PublishedProjectMap: React.FC<PublishedProjectMapProps> = () => {
       .filter((s) => !!s);
   }, [scenariosData]);
 
-  const sid = scenariosIsFetched && scenariosData && !!scenariosData.length ? firstSidRunned || `${scenariosData[0].id}` : null;
+  const sid = scenariosIsFetched && scenariosData && !!scenariosData.length ? `${firstSidRunned[0].id}` || `${scenariosData[0].id}` : null;
 
   const PUGridLayer = usePUGridLayer({
     active: scenariosIsFetched && scenariosData && !!scenariosData.length,
@@ -95,12 +100,42 @@ export const PublishedProjectMap: React.FC<PublishedProjectMapProps> = () => {
   }, [bbox]);
 
   const handleViewportChange = useCallback((vw) => {
+    console.log('VIEWPORT', vw);
     setViewport(vw);
   }, []);
 
+  const handleZoomChange = useCallback(
+    (zoom) => {
+      setViewport({
+        ...viewport,
+        zoom,
+        transitionDuration: 500,
+      });
+    },
+    [viewport],
+  );
+
+  const handleFitBoundsChange = useCallback((b) => {
+    console.log('BOUNDS', b);
+    setBounds(b);
+  }, []);
+
+  const handleTransformRequest = (url) => {
+    if (url.startsWith(process.env.NEXT_PUBLIC_API_URL)) {
+      return {
+        url,
+        headers: {
+          Authorization: `Bearer ${session.accessToken}`,
+        },
+      };
+    }
+
+    return null;
+  };
+
   return (
     <AnimatePresence>
-      {id && (
+      {id && scenariosIsFetched && (
         <motion.div
           key="project-map"
           className="relative w-full h-full col-span-5 overflow-hidden pointer-events-none rounded-4xl"
@@ -108,30 +143,52 @@ export const PublishedProjectMap: React.FC<PublishedProjectMapProps> = () => {
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: -10, opacity: 0 }}
         >
-          <Map
-            bounds={bounds}
-            width="100%"
-            height="100%"
-            minZoom={minZoom}
-            maxZoom={maxZoom}
-            viewport={viewport}
-            mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
-            mapStyle="mapbox://styles/marxan/ckn4fr7d71qg817kgd9vuom4s"
-            onMapViewportChange={handleViewportChange}
-          // doubleClickZoom={false}
-          // dragPan={false}
-          >
-            {(map) => {
-              return (
-                <LayerManager map={map} plugin={PluginMapboxGl}>
-                  {LAYERS.map((l) => (
-                    <Layer key={l.id} {...l} />
-                  ))}
-                </LayerManager>
-              );
-            }}
+          <div className="w-full h-full">
+            <Map
+              bounds={bounds}
+              width="100%"
+              height="100%"
+              minZoom={minZoom}
+              maxZoom={maxZoom}
+              viewport={viewport}
+              mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+              mapStyle="mapbox://styles/marxan/ckn4fr7d71qg817kgd9vuom4s"
+              onMapViewportChange={handleViewportChange}
+              transformRequest={handleTransformRequest}
+            >
+              {(map) => {
+                return (
+                  <LayerManager map={map} plugin={PluginMapboxGl}>
+                    {LAYERS.map((l) => (
+                      <Layer key={l.id} {...l} />
+                    ))}
+                  </LayerManager>
+                );
+              }}
 
-          </Map>
+            </Map>
+          </div>
+
+          <Controls>
+            <ZoomControl
+              viewport={{
+                ...viewport,
+                minZoom,
+                maxZoom,
+              }}
+              onZoomChange={handleZoomChange}
+            />
+
+            <FitBoundsControl
+              bounds={{
+                ...bounds,
+                viewportOptions: {
+                  transitionDuration: 1500,
+                },
+              }}
+              onFitBoundsChange={handleFitBoundsChange}
+            />
+          </Controls>
 
         </motion.div>
       )}
