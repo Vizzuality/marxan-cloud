512f73e75106b376607a5ecab81fb021cf407cc8
Dyostiq <15174395+Dyostiq@users.noreply.github.com>
2021-08-23 10:09:06 +0200

    feat(api): add events for the entire specification

--- api/apps/api/src/modules/specification/adapters/specification-adapters.module.ts
+++ api/apps/api/src/modules/specification/adapters/specification-adapters.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ApiEventsModule } from '@marxan-api/modules/api-events/api-events.module';
+
 import { DbSpecificationRepository } from './specification.repository';
 import { SpecificationApiEntity } from './specification.api.entity';
 import { SpecificationFeatureConfigApiEntity } from './specification-feature-config.api.entity';
 import { SpecificationFeatureApiEntity } from './specification-feature.api.entity';
+import { SpecificationCandidateCreatedHandler } from './specification-candidate-created.handler';
 
 import { SpecificationRepository } from '../application/specification.repository';
 
@@ -15,12 +18,14 @@ import { SpecificationRepository } from '../application/specification.repository
       SpecificationFeatureConfigApiEntity,
       SpecificationFeatureApiEntity,
     ]),
+    ApiEventsModule,
   ],
   providers: [
     {
       provide: SpecificationRepository,
       useClass: DbSpecificationRepository,
     },
+    SpecificationCandidateCreatedHandler,
   ],
   exports: [SpecificationRepository],
 })
