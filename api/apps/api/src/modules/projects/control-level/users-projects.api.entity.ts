8d7401e67b072ef6a31ceb5d60bdd69cbb49c22f
kgajowy <kamil.gajowy@gmail.com>
2021-08-24 11:00:45 +0200

    feat(projects): find returns only projects created by user

---
+++ api/apps/api/src/modules/projects/control-level/users-projects.api.entity.ts
@@ -0,0 +1,55 @@
+import { Role, Roles } from '@marxan-api/modules/users/role.api.entity';
+import { Check, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Project } from '@marxan-api/modules/projects/project.api.entity';
+import { User } from '@marxan-api/modules/users/user.api.entity';
+
+@Entity(`users_projects`)
+export class UsersProjectsApiEntity {
+  @PrimaryColumn({
+    type: `uuid`,
+    name: `user_id`,
+  })
+  userId!: string;
+
+  @PrimaryColumn({
+    type: `uuid`,
+    name: `project_id`,
+  })
+  projectId!: string;
+
+  @Check(`role_id`, `LIKE 'project_%'`)
+  @PrimaryColumn({
+    type: `varchar`,
+    name: `role_id`,
+  })
+  roleName!: Roles.project_user | Roles.project_admin | Roles.project_owner;
+
+  @ManyToOne(() => Project, {
+    onDelete: 'CASCADE',
+    primary: true,
+  })
+  @JoinColumn({
+    name: `project_id`,
+    referencedColumnName: `id`,
+  })
+  project?: Project;
+
+  @ManyToOne(() => User, {
+    onDelete: 'CASCADE',
+    primary: true,
+  })
+  @JoinColumn({
+    name: `user_id`,
+    referencedColumnName: `id`,
+  })
+  user?: User;
+
+  @ManyToOne(() => Role, {
+    primary: true,
+  })
+  @JoinColumn({
+    name: `role_id`,
+    referencedColumnName: `name`,
+  })
+  role?: Role;
+}
