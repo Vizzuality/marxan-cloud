32f41a932903812cd0e8d2b513b2e987034ddd45
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-08-25 10:44:32 +0200

    Gap analysis: pre and post hook

--- app/hooks/gap-analysis/index.ts
+++ app/hooks/gap-analysis/index.ts
@@ -18,7 +18,7 @@ import {
 
 interface AllItemProps extends RawItemProps {}
 
-export function useGapAnalysis(sId, options: UseFeaturesOptionsProps = {}) {
+export function usePreGapAnalysis(sId, options: UseFeaturesOptionsProps = {}) {
   const placeholderDataRef = useRef({
     pages: [],
     pageParams: [],
@@ -41,7 +41,7 @@ export function useGapAnalysis(sId, options: UseFeaturesOptionsProps = {}) {
 
   const fetchFeatures = ({ pageParam = 1 }) => SCENARIOS.request({
     method: 'GET',
-    url: `/${sId}/features`,
+    url: `/${sId}/features/gap-data`,
     headers: {
       Authorization: `Bearer ${session.accessToken}`,
     },
@@ -57,7 +57,100 @@ export function useGapAnalysis(sId, options: UseFeaturesOptionsProps = {}) {
     },
   });
 
-  const query = useInfiniteQuery(['gap-analysis', sId, JSON.stringify(options)], fetchFeatures, {
+  const query = useInfiniteQuery(['pre-gap-analysis', sId, JSON.stringify(options)], fetchFeatures, {
+    placeholderData: placeholderDataRef.current,
+    getNextPageParam: (lastPage) => {
+      const { data: { meta } } = lastPage;
+      const { page, totalPages } = meta;
+
+      const nextPage = page + 1 > totalPages ? null : page + 1;
+      return nextPage;
+    },
+  });
+
+  const { data } = query;
+  const { pages } = data || {};
+
+  if (data) {
+    placeholderDataRef.current = data;
+  }
+
+  return useMemo(() => {
+    const parsedData = Array.isArray(pages) ? flatten(pages.map((p) => {
+      const { data: { data: pageData } } = p;
+
+      return pageData.map((d):AllItemProps => {
+        const {
+          id,
+          name,
+        } = d;
+
+        console.log(d);
+
+        return {
+          id,
+          name: name || 'Metadata name',
+          current: {
+            percent: 0.5,
+            value: 400,
+            unit: 'km2',
+          },
+          target: {
+            percent: 0.40,
+            value: 350,
+            unit: 'km2',
+          },
+        };
+      });
+    })) : [];
+
+    return {
+      ...query,
+      data: parsedData,
+    };
+  }, [query, pages]);
+}
+
+export function usePostGapAnalysis(sId, options: UseFeaturesOptionsProps = {}) {
+  const placeholderDataRef = useRef({
+    pages: [],
+    pageParams: [],
+  });
+  const [session] = useSession();
+
+  const {
+    filters = {},
+    search,
+    sort,
+  } = options;
+
+  const parsedFilters = Object.keys(filters)
+    .reduce((acc, k) => {
+      return {
+        ...acc,
+        [`filter[${k}]`]: filters[k],
+      };
+    }, {});
+
+  const fetchFeatures = ({ pageParam = 1 }) => SCENARIOS.request({
+    method: 'GET',
+    url: `/${sId}/marxan/solutions/gap-data`,
+    headers: {
+      Authorization: `Bearer ${session.accessToken}`,
+    },
+    params: {
+      'page[number]': pageParam,
+      ...parsedFilters,
+      ...search && {
+        q: search,
+      },
+      ...sort && {
+        sort,
+      },
+    },
+  });
+
+  const query = useInfiniteQuery(['post-gap-analysis', sId, JSON.stringify(options)], fetchFeatures, {
     placeholderData: placeholderDataRef.current,
     getNextPageParam: (lastPage) => {
       const { data: { meta } } = lastPage;
