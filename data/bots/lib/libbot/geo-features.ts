5fa6fa91cc5612cf7b3fefbed50e3d6310b60172
andrea rota <andrea.rota@vizzuality.com>
2021-08-31 18:21:57 +0200

    add WIP libbot modules

---
+++ data/bots/lib/libbot/geo-features.ts
@@ -0,0 +1,19 @@
+import { BotHttpClient, getJsonApiDataFromResponse } from './marxan-bot.ts';
+import { logError } from './logger.ts';
+
+export class GeoFeatures {
+  private baseHttpClient;
+
+  constructor(httpClient: BotHttpClient) {
+    this.baseHttpClient = httpClient.baseHttpClient;
+  }
+
+  async getIdFromQueryStringInProject(
+    projectId: string,
+    name: string
+  ) {
+    return await this.baseHttpClient.get(`/projects/${projectId}/features?q=${name}&fields=id`)
+      .then(getJsonApiDataFromResponse)
+      .catch(logError);
+  }
+}
