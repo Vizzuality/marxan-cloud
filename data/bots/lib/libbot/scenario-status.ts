360f87b1dd0b63686272f1cd510cef0b905bb0e4
andrea rota <andrea.rota@vizzuality.com>
2021-08-31 18:31:14 +0200

    add utility functions for scenario status checks

--- data/bots/lib/libbot/scenario-status.ts
+++ data/bots/lib/libbot/scenario-status.ts
@@ -2,8 +2,29 @@ import { BotHttpClient, getJsonApiDataFromResponse } from './marxan-bot.ts';
 import { sleep } from "https://deno.land/x/sleep@v1.2.0/mod.ts";
 import _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
 import { ms } from 'https://deno.land/x/ms@v0.1.0/ms.ts';
+import { logError, logInfo, logDebug } from './logger.ts';
 
 const DEFAULT_WATCH_TIMEOUT = 1800;
+
+type WaitKinds = 'short' | 'some' | 'long';
+
+export const WaitForTime:  Record<WaitKinds, RetryOptions> = {
+  short: {
+    delay: '10s',
+    interval: '10s',
+    maxTries: 60,
+  },
+  some: {
+    delay: '30s',
+    interval: '30s',
+    maxTries: 60,
+  },
+  long: {
+    delay: '60s',
+    interval: '30s',
+    maxTries: 120,
+  }
+};
 
 export enum ScenarioJobKinds {
   geoFeatureCopy = 'geofeatureCopy',
@@ -11,7 +32,7 @@ export enum ScenarioJobKinds {
   specification = 'specification',
 }
 
-enum JobStatuses {
+export enum JobStatuses {
   running = 'running',
   done = 'done',
 }
@@ -54,22 +75,54 @@ export class ScenarioJobStatus {
   async get(job: JobSpecification): Promise<JobStatuses | undefined> {
     const projectStatus: ProjectStatus = await this.baseHttpClient.get(`/projects/${job.forProject}/scenarios/status`)
       .then(getJsonApiDataFromResponse)
-      .catch(e => console.log(e));
-    return projectStatus.scenarios.find(i => i.id === job.forScenario)?.jobs.find(i => i.kind === job.jobKind)?.status;
+      .then(data => data.attributes)
+      .catch(logError);
+    logDebug(projectStatus);
+    return projectStatus?.scenarios.find(i => i.id === job.forScenario)?.jobs.find(i => i.kind === job.jobKind)?.status;
   }
 
-  async waitFor(job: JobSpecification, until: JobStatuses, retryOptions: RetryOptions): Promise<void> {
+  async waitFor(job: JobSpecification, until: JobStatuses, retryOptions: RetryOptions): Promise<boolean> {
+    logInfo(`Polling for ${job.jobKind} until status is ${until} for scenario ${job.forScenario}...`);
+
     if(retryOptions?.delay) {
       const delay = ms(retryOptions.delay ?? '0') as number;
-      sleep(delay / 10e3);
+      logDebug(`Waiting for ${delay / 10e2} seconds before starting to poll status...`)
+      await sleep(delay / 10e2);
     }
 
     const interval = ms(retryOptions.interval) as number;
 
-    for(const i of [...Array(retryOptions.maxTries)]) {
+    for(const i of [...Array(retryOptions.maxTries).keys()]) {
+      logInfo(`Retry ${i} of ${retryOptions.maxTries}...`);
       const status = await this.get(job);
-      if(status === until) { return; }
-      sleep(interval / 10e3);
+      if(status === until) {
+        logInfo(`Current status is ${status}.`);
+        return true;
+      }
+      logInfo(`Current status is ${status}: waiting for ${interval / 10e2}s`);
+      await sleep(interval / 10e2);
     }
+
+    return false;
+  }
+
+  async waitForPlanningAreaProtectedCalculationFor(projectId: string, scenarioId: string, waitForTime: keyof typeof WaitForTime = 'short'): Promise<boolean> {
+    return await this.waitFor({
+      jobKind: ScenarioJobKinds.planningAreaProtectedCalculation,
+      forProject: projectId,
+      forScenario: scenarioId,
+    },
+    JobStatuses.done,
+    WaitForTime[waitForTime]);
+  }
+
+  async waitForFeatureSpecificationCalculationFor(projectId: string, scenarioId: string, waitForTime: keyof typeof WaitForTime = 'some'): Promise<boolean> {
+    return await this.waitFor({
+      jobKind: ScenarioJobKinds.specification,
+      forProject: projectId,
+      forScenario: scenarioId,
+    },
+    JobStatuses.done,
+    WaitForTime[waitForTime]);
   }
 }
