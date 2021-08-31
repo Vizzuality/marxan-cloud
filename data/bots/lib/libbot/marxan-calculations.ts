7dcf8693475fed76735e15d268c8ccfeec96ec67
andrea rota <andrea.rota@vizzuality.com>
2021-08-31 18:28:14 +0200

    add marxan runner module

---
+++ data/bots/lib/libbot/marxan-calculations.ts
@@ -0,0 +1,16 @@
+import { BotHttpClient, getJsonApiDataFromResponse } from './marxan-bot.ts';
+import { logError } from './logger.ts';
+
+export class MarxanCalculations {
+  private baseHttpClient;
+
+  constructor(httpClient: BotHttpClient) {
+    this.baseHttpClient = httpClient.baseHttpClient;
+  }
+
+  async runForScenario(scenarioId: string) {
+    return await this.baseHttpClient.post(`/scenarios/${scenarioId}/marxan`)
+      .then(getJsonApiDataFromResponse)
+      .catch(logError);
+  }
+}
