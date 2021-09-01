df0c3b7f0e5433d0e57141d7eb180f21417af3cb
andrea rota <andrea.rota@vizzuality.com>
2021-09-01 14:51:24 +0200

    port Australia bot to libbot

--- data/bots/demo-australia/bot.ts
+++ data/bots/demo-australia/bot.ts
@@ -1,13 +1,13 @@
-import axiod from "https://deno.land/x/axiod@0.22/mod.ts";
-import Process from "https://deno.land/std@0.103.0/node/process.ts";
 import {
   dirname,
   fromFileUrl,
   relative,
 } from "https://deno.land/std@0.103.0/path/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
-import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
-import { sleep } from "https://deno.land/x/sleep/mod.ts";
+import { sleep } from "https://deno.land/x/sleep@v1.2.0/mod.ts";
+import { createBot } from '../lib/libbot/init.ts';
+import { SpecificationStatus} from '../lib/libbot/geo-feature-specifications.ts';
+import { getDemoFeatureSpecificationFromFeatureNamesForProject } from './lib.ts';
 
 const scriptPath = dirname(relative(Deno.cwd(), fromFileUrl(import.meta.url)));
 
@@ -22,178 +22,73 @@ const settings = {
     password: PASSWORD,
   },
 };
+const bot = await createBot(settings);
 
-const jwt = await axiod
-  .post(`${settings.apiUrl}/auth/sign-in/`, settings.credentials)
-  .then((result) => result.data.accessToken);
-
-// const pgClient = new Client(POSTGRES_URL);
-
-const botClient = axiod.create({
-  baseURL: settings.apiUrl + "/api/v1",
-  headers: {
-    Authorization: "Bearer " + jwt,
-  },
+const organization = await bot.organizations.create({
+  name: '[demo] Australia - Kimberley ' + crypto.randomUUID(),
+  description: '',
 });
-
-async function sendData(url: string, data: Blob) {
-  const formData = new FormData();
-  formData.append("file", data, "test_mata.zip");
-
-  const response = await fetch(url, {
-    method: "POST",
-    body: formData,
-    headers: [["authorization", "Bearer " + jwt]],
-  });
-
-  return response;
-}
-
-const organization = await botClient
-  .post("/organizations", {
-    name: "Australia - Kimberley organization",
-    description: "Duis aliquip nostrud sint",
-    metadata: {},
-  })
-  .then((result) => result.data)
-  .catch((e) => {
-    console.log(e);
-  });
-
-console.log(organization);
-
-const planningAreaFile = await (
-  await sendData(
-    settings.apiUrl + "/api/v1/projects/planning-area/shapefile",
-    new Blob([await Deno.readFile(scriptPath + "/kimberley.zip")])
-  )
-).json();
-
-console.log(planningAreaFile);
 
 const planningUnitAreakm2 = 50;
 
-const project = await botClient
-  .post("/projects", {
-    name: "Australia - Kimberley project",
-    organizationId: organization.data.id,
-    planningUnitGridShape: "hexagon",
-    planningUnitAreakm2: planningUnitAreakm2,
-    planningAreaId: planningAreaFile.id,
-  })
-  .then((result) => result.data)
-  .catch((e) => {
-    console.log(e);
-  });
+const planningAreaId = await bot.planningAreaUploader.uploadFromFile(`${scriptPath}/kimberley.zip`);
+const project = await bot.projects.createInOrganization(organization.id, {
+  name: 'Australia - Kimberley ' + crypto.randomUUID(),
+  description: '',
+  planningAreaId: planningAreaId,
+  planningUnitGridShape: 'hexagon',
+  planningUnitAreakm2,
+});
 
-console.log(project);
+// We don't expose status of planning unit grid calculations yet so we cannot
+// poll for completion of this task, but a reasonable, project-specific wait
+// here will do when there is nothing else keeping the API and PostgreSQL busy.
+await sleep(30);
 
-// wait a bit for async job to be picked up and processed
-// @DEBT we should check the actual job status
-// await new Promise((r) => setTimeout(r, 30e3));
+// Scenario creation with the bare minimum; From there we need to be setting
+// other traits via patch.
+const scenario = await bot.scenarios.createInProject(project.id, {
+  name: `Kimberley - scenario 01`,
+  type: "marxan",
+  description: "Demo scenario",
+  metadata: bot.metadata.analysisPreview(),
+});
 
-const scenarioStart = Process.hrtime();
-
-await sleep(5)
-
-const paCategories:{data:Array<{id:string, type:string, attributes:object}>} = await botClient.get(`/protected-areas/iucn-categories?filter%5BcustomAreaId%5D=${planningAreaFile.id}`)
-          .then((result) =>  result.data)
-          .catch((e) => {
-            console.log(e);
-          });
-
-console.log(paCategories);
-
-
-const scenario = await botClient
-  .post("/scenarios", {
-    name: `Kimberley scenario`,
-    type: "marxan",
-    projectId: project.data.id,
-    description: "An Australia scenario",
-    metadata: {
-      scenarioEditingMetadata: {
-        status: {
-          'protected-areas': 'draft',
-          features: 'draft',
-        },
-        tab: 'analysis',
-        subtab: 'analysis-preview',
-      }
-    }
-  })
-  .then((result) => result.data)
-  .catch((e) => {
-    console.log(e);
-  });
-  await sleep(5)
 // get the list of protected areas in the region and use all of them
+const paCategories = await bot.protectedAreas.getIucnCategoriesForPlanningAreaWithId(planningAreaId);
 
-await botClient
-  .patch(`/scenarios/${scenario!.data!.id}`, {
-    wdpaIucnCategories: paCategories!.data.map((i: {id:string, type:string, attributes:object}): string => i.id),
-  }).catch((e) => {
-    console.log(e);
-  });
+await bot.scenarios.update(scenario.id, {
+  wdpaIucnCategories: paCategories,
+});
 
-console.log(scenario);
+await bot.scenarios.update(scenario.id, {
+  wdpaThreshold: 50,
+  metadata: bot.metadata.analysisPreview(),
+});
 
-await sleep(20)
+await bot.scenarioStatus.waitForPlanningAreaProtectedCalculationFor(project.id, scenario.id, 'short');
 
-await botClient
-  .patch(`/scenarios/${scenario!.data!.id}`, {
-    wdpaThreshold: 50,
-  }).catch((e) => {
-    console.log(e);
-  });
+//Setup features in the project
+const wantedFeatures = [
+  // "demo_ecoregions_new_class_split",
+  // "calidris_(erolia)_ferruginea",
+  // "chlamydosaurus_kingii",
+  // "erythrura_(chloebia)_gouldiae",
+  // "haliaeetus_(pontoaetus)_leucogaster",
+  // "malurus_(malurus)_coronatus",
+  // "mesembriomys_macrurus",
+  // "onychogalea_unguifera",
+  // "pseudechis_australis",
+  // "wyulda_squamicaudata",
+  "zyzomys_woodwardi",
+];
 
-const scenarioTook = Process.hrtime(scenarioStart);
-console.log(`Scenario creation done in ${scenarioTook[0]} seconds`);
+const featuresForSpecification = await getDemoFeatureSpecificationFromFeatureNamesForProject(project.id, bot, wantedFeatures);
 
-await botClient.get(`/scenarios/${scenario!.data!.id}`)
-    .then((result) =>  console.log(result.data))
-    .catch((e) => {
-    console.log(e);
-  });
+await bot.geoFeatureSpecifications.submitForScenario(scenario.id, featuresForSpecification, SpecificationStatus.created);
 
-await sleep(5)
+await bot.scenarioStatus.waitForFeatureSpecificationCalculationFor(project.id, scenario.id, 'some');
 
-// Setup features in the project
+await bot.marxanExecutor.runForScenario(scenario.id);
 
-const features = await botClient
-  .get(`/projects/${project.data.id}/features?q=demo`)
-  .then((result) => result.data)
-  .catch((e) => {
-    console.log(e);
-  });
-
-console.log(features);
-
-const geoFeatureSpecStart = Process.hrtime();
-
-const featureRecipe = features!.data.map((x: {id:string, type:string, attributes:object}) => { return {
-    kind: "plain",
-    featureId: x.id,
-    marxanSettings: {
-      prop: 0.3,
-      fpf: 1,
-    },
-  }})
-console.log(featureRecipe);
-const geoFeatureSpec = await botClient
-  .post(`/scenarios/${scenario.data.id}/features/specification`, {
-    status: "draft",
-    features: featureRecipe
-  })
-  .then((result) => result.data)
-  .catch((e) => {
-    console.log(e);
-  });
-
-const geoFeatureSpecTook = Process.hrtime(geoFeatureSpecStart);
-
-console.log(
-  `Processing of features for scenario done in ${geoFeatureSpecTook[0]} seconds`
-);
-
-// console.log(geoFeatureSpec);
+await bot.scenarioStatus.waitForMarxanCalculationsFor(project.id, scenario.id, 'some');
