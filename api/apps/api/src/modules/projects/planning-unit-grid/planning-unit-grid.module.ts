be3aef5b600e412e6526801ae45058abf09ceb6e
kgajowy <kamil.gajowy@gmail.com>
2021-09-10 15:08:43 +0200

    feat(custom-pu-grid): set project grid shape once geometries were set

--- api/apps/api/src/modules/projects/planning-unit-grid/planning-unit-grid.module.ts
+++ api/apps/api/src/modules/projects/planning-unit-grid/planning-unit-grid.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QueueApiEventsModule } from '@marxan-api/modules/queue-api-events';
 import { ApiEventsModule } from '@marxan-api/modules/api-events';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
+
+import { Project } from '@marxan-api/modules/projects/project.api.entity';
 
 import {
   setPlanningUnitGridEventsFactoryProvider,
@@ -10,15 +13,24 @@ import {
 } from './queue.providers';
 import { PlanningUnitGridEventsHandler } from './planning-unit-grid-events.handler';
 import { PlanningUnitGridService } from './planning-unit-grid.service';
+import { PlanningUnitGridSetSaga } from './planning-unit-grid-set.saga';
+import { SetProjectGridFromShapefileHandler } from './set-project-grid-from-shapefile.handler';
 
 @Module({
-  imports: [QueueApiEventsModule, ApiEventsModule, CqrsModule],
+  imports: [
+    QueueApiEventsModule,
+    ApiEventsModule,
+    CqrsModule,
+    TypeOrmModule.forFeature([Project]),
+  ],
   providers: [
     setPlanningUnitGridQueueProvider,
     setPlanningUnitGridQueueEventsProvider,
     setPlanningUnitGridEventsFactoryProvider,
     PlanningUnitGridEventsHandler,
     PlanningUnitGridService,
+    PlanningUnitGridSetSaga,
+    SetProjectGridFromShapefileHandler,
   ],
   exports: [PlanningUnitGridService],
 })
