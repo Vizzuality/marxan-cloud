cc1516295f8793047815cd14c68b490fe3e24b32
kgajowy <kamil.gajowy@gmail.com>
2021-10-01 08:13:25 +0200

    specs(marxan-run): ensure first run is selected when minimum score is a tie

--- api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters/solutions-output/__tests__/solutions.ts
+++ api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters/solutions-output/__tests__/solutions.ts
@@ -18,53 +18,61 @@ const defaults = (): Omit<
   distinctFive: false,
 });
 
-export const lowestScoreRunId = 7;
+export const lowestScoreRunId = 1;
+export const secondLowestScoreRunId = 8;
 
 export const subjectRows: ResultRow[] = [
   {
-    runId: 1,
-    score: 1000,
+    runId: lowestScoreRunId,
+    score: 10,
     cost: 5,
     planningUnits: 4,
     ...defaults(),
   },
   {
     runId: 2,
+    score: 1000,
+    cost: 5,
+    planningUnits: 4,
+    ...defaults(),
+  },
+  {
+    runId: 3,
     score: 2000,
     cost: 200,
     planningUnits: 5,
     ...defaults(),
   },
   {
-    runId: 3,
+    runId: 4,
     score: 5000,
     cost: 50,
     planningUnits: 1,
     ...defaults(),
   },
   {
-    runId: 4,
+    runId: 5,
     score: 6000,
     cost: 50,
     planningUnits: 14,
     ...defaults(),
   },
   {
-    runId: 5,
+    runId: 6,
     score: 15000,
     cost: 75,
     planningUnits: 4,
     ...defaults(),
   },
   {
-    runId: 6,
+    runId: 7,
     score: 2000,
     cost: 95,
     planningUnits: 4,
     ...defaults(),
   },
   {
-    runId: lowestScoreRunId,
+    runId: secondLowestScoreRunId,
     score: 10,
     cost: 5,
     planningUnits: 4,
