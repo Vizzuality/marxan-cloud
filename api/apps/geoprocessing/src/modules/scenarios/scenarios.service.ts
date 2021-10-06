b37c075356de77d2c895dd7deeed16b3170a3456
kgajowy <kamil.gajowy@gmail.com>
2021-10-06 19:42:23 +0200

    feat(tiles): use feature_list for tile scenario features

--- api/apps/geoprocessing/src/modules/scenarios/scenarios.service.ts
+++ api/apps/geoprocessing/src/modules/scenarios/scenarios.service.ts
@@ -106,9 +106,6 @@ export class ScenariosService {
      */
     const sql = this.selectJoins(
       id,
-      z,
-      x,
-      y,
       this.ScenariosPlanningUnitGeoEntityRepository.createQueryBuilder('test')
         .addSelect('plan.the_geom')
         .leftJoin('planning_units_geom', 'plan', `test.pu_geom_id = plan.id`)
@@ -135,9 +132,6 @@ export class ScenariosService {
    */
   private selectJoins(
     id: string,
-    z: number,
-    x: number,
-    y: number,
     qB: SelectQueryBuilder<ScenariosPuPaDataGeo>,
     _filters?: ScenariosPUFilters,
   ): SelectQueryBuilder<ScenariosPuPaDataGeo> {
@@ -147,20 +141,11 @@ export class ScenariosService {
           qB.addSelect(includeSelections[element].select!);
         }
         if (element == 'features') {
-          includeSelections.features.table = `(select pu.scenario_id,
-            pu.id AS scenario_pu_id,
-            string_agg(DISTINCT species.feature_id::text, ','::text) AS feature_list from (SELECT sfd.scenario_id,
-                    (st_dump(fd.the_geom)).geom AS the_geom,
-                    fd.feature_id
-                   FROM scenario_features_data sfd
-                   inner JOIN features_data fd ON sfd.feature_class_id = fd.id) species, (SELECT pug.the_geom,
-                    spd.id,
-                    spd.scenario_id
-                   FROM planning_units_geom pug
-                     inner JOIN scenarios_pu_data spd ON pug.id = spd.pu_geom_id
-                  ORDER BY spd.puid) as pu where st_intersects(species.the_geom, pu.the_geom)
-                 and species.scenario_id='${id}'::uuid
-                and st_intersects(pu.the_geom, st_transform(ST_TileEnvelope(${z}, ${x}, ${y}),4326)) group by 1,2)`;
+          includeSelections.features.table = `(select spd.scenario_id,
+       spd.id,
+       array_to_string(spd.feature_list, ','::text)
+from scenarios_pu_data spd
+where spd.scenario_id = '${id}')`;
         }
         if (includeSelections[element].table) {
           qB.leftJoin(
