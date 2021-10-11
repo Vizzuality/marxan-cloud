7423ec9e069452600a83a86fede06829d691f9a4
Dyostiq <15174395+Dyostiq@users.noreply.github.com>
2021-10-11 19:58:27 +0200

    fix(api): handle NotFoundException when event is not found

--- api/apps/api/src/modules/scenarios/project-checker.service.ts
+++ api/apps/api/src/modules/scenarios/project-checker.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { In } from 'typeorm';
 
 import { API_EVENT_KINDS } from '@marxan/api-events';
@@ -9,27 +9,38 @@ export class ProjectChecker {
   constructor(private readonly apiEvents: ApiEventsService) {}
 
   async isProjectReady(projectId: string): Promise<boolean> {
-    const planningUnitEvent = await this.apiEvents.getLatestEventForTopic({
-      topic: projectId,
-      kind: In([
-        API_EVENT_KINDS.project__planningUnits__failed__v1__alpha,
-        API_EVENT_KINDS.project__planningUnits__finished__v1__alpha,
-        API_EVENT_KINDS.project__planningUnits__submitted__v1__alpha,
-      ]),
-    });
-    const gridEvent = await this.apiEvents.getLatestEventForTopic({
-      topic: projectId,
-      kind: In([
-        API_EVENT_KINDS.project__grid__failed__v1__alpha,
-        API_EVENT_KINDS.project__grid__finished__v1__alpha,
-        API_EVENT_KINDS.project__grid__submitted__v1__alpha,
-      ]),
-    });
+    const planningUnitEvent = await this.apiEvents
+      .getLatestEventForTopic({
+        topic: projectId,
+        kind: In([
+          API_EVENT_KINDS.project__planningUnits__failed__v1__alpha,
+          API_EVENT_KINDS.project__planningUnits__finished__v1__alpha,
+          API_EVENT_KINDS.project__planningUnits__submitted__v1__alpha,
+        ]),
+      })
+      .catch(this.createNotFoundHandler());
+    const gridEvent = await this.apiEvents
+      .getLatestEventForTopic({
+        topic: projectId,
+        kind: In([
+          API_EVENT_KINDS.project__grid__failed__v1__alpha,
+          API_EVENT_KINDS.project__grid__finished__v1__alpha,
+          API_EVENT_KINDS.project__grid__submitted__v1__alpha,
+        ]),
+      })
+      .catch(this.createNotFoundHandler());
     return (
       planningUnitEvent?.kind ===
         API_EVENT_KINDS.project__planningUnits__finished__v1__alpha &&
       (gridEvent === undefined ||
         gridEvent.kind === API_EVENT_KINDS.project__grid__finished__v1__alpha)
     );
+  }
+
+  private createNotFoundHandler() {
+    return (error: unknown) => {
+      if (!(error instanceof NotFoundException)) throw error;
+      return undefined;
+    };
   }
 }
