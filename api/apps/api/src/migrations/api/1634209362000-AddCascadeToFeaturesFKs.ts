9c16f1976dfec7850966042a19966ab80d5b99d9
andrea rota <andrea.rota@vizzuality.com>
2021-10-14 12:33:49 +0100

    add migration for cascade-deletes of features

---
+++ api/apps/api/src/migrations/api/1634209362000-AddCascadeToFeaturesFKs.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddCascadeToFeaturesFKs1634209362000
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    ALTER TABLE public.features DROP CONSTRAINT features_project_id_fkey;
+    ALTER TABLE public.features DROP CONSTRAINT features_created_by_fkey;
+    ALTER TABLE public.features ADD CONSTRAINT features_project_id_fkey FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE;
+    ALTER TABLE public.features ADD CONSTRAINT features_created_by_fkey FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    ALTER TABLE public.features DROP CONSTRAINT features_project_id_fkey;
+    ALTER TABLE public.features DROP CONSTRAINT features_created_by_fkey;
+    ALTER TABLE public.features ADD CONSTRAINT features_project_id_fkey FOREIGN KEY (project_id) REFERENCES projects(id);
+    ALTER TABLE public.features ADD CONSTRAINT features_created_by_fkey FOREIGN KEY (created_by) REFERENCES users(id);
+    `);
+  }
+}
