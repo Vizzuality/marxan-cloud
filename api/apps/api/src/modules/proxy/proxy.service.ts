072ce7627a0a137f40476a577fec2748491bdbba
andrea rota <andrea.rota@vizzuality.com>
2021-10-14 19:45:55 +0100

    use plain node http module for proxying requests

--- api/apps/api/src/modules/proxy/proxy.service.ts
+++ api/apps/api/src/modules/proxy/proxy.service.ts
@@ -1,67 +1,38 @@
-import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 import { AppConfig } from '@marxan-api/utils/config.utils';
 import { Request, Response } from 'express';
-/**
- * @debt i think this is not properly imported, but it was the only way for it to work
- */
-import * as Server from 'http-proxy';
+import { get } from 'http';
 
 @Injectable()
 export class ProxyService {
   private geoprocessingServiceUrl: string = AppConfig.get(
     'geoprocessing.url',
   ) as string;
-  private server: Server;
   private readonly logger = new Logger(ProxyService.name);
-  constructor() {
-    this.server = Server.createProxyServer();
-  }
-  /**
-   *
-   * @todo refactor to a more general function for the parts that can change for the other proxied URL's
-   */
 
-  proxyTileRequest(request: Request, response: Response) {
-    return this.server.web(
-      request,
-      response,
-      { changeOrigin: true, target: this.geoprocessingServiceUrl },
-      (error) => {
-        this.logger.error(
-          `Unexpected exception on proxy to tile service - ${error}`,
-        );
-        return response
-          .status(HttpStatus.BAD_GATEWAY)
-          .send({ message: `Unexpected exception during request - ${error}` });
-      },
-    );
+  async proxyTileRequest(req: Request, res: Response) {
+    const url =
+      this.geoprocessingServiceUrl +
+      req.path +
+      new URL(req.originalUrl, this.geoprocessingServiceUrl).search;
+    get(url, { headers: req.headers }, (response) => {
+      response.pipe(res);
+    });
   }
 
-  async proxyUploadShapeFile(
-    request: Request,
-    response: Response,
-  ): Promise<any> {
-    request.url = request.originalUrl.replace(
+  async proxyUploadShapeFile(req: Request, res: Response): Promise<any> {
+    req.url = req.originalUrl.replace(
       'api/v1/scenarios',
       'api/v1/planning-units',
     );
 
-    return this.server.web(
-      request,
-      response,
-      {
-        changeOrigin: true,
-        target: this.geoprocessingServiceUrl,
-      },
-      (error) => {
-        this.logger.error(
-          `Unexpected exception on proxy for shapefile upload service - ${error}`,
-        );
-        return response
-          .status(HttpStatus.BAD_GATEWAY)
-          .send({ message: `Unexpected exception during request - ${error}` });
-      },
-    );
+    const url =
+      this.geoprocessingServiceUrl +
+      req.path +
+      new URL(req.originalUrl, this.geoprocessingServiceUrl).search;
+    get(url, { headers: req.headers }, (response) => {
+      response.pipe(res);
+    });
   }
 }
