0ea95e14edc78e3af81d256f4338a05d905948d1
anamontiaga <amontiaga@gmail.com>
2021-10-20 12:37:26 +0200

    feat(front): add new selector to selected PA type

--- app/layout/projects/new/form/constants.ts
+++ app/layout/projects/new/form/constants.ts
@@ -4,3 +4,18 @@ export const DEFAULT_AREA = {
   planningUnitAreakm2: 10,
   planningUnitGridShape: PlanningUnit.HEXAGON,
 };
+
+export const PA_OPTIONS = [
+  {
+    label: 'Yes, I have a shapefile with a planning region and a grid',
+    value: 'customPAshapefileGrid',
+  },
+  {
+    label: 'Yes, I have a shapefile with a planning region without grid ',
+    value: 'customPAshapefile',
+  },
+  {
+    label: 'No, I don’t have any shapefiles',
+    value: 'default',
+  },
+];
