b4eb6d31174bf74cccb70f4053cc03d2963a9463
andrea rota <andrea.rota@vizzuality.com>
2021-11-05 19:16:35 +0000

    add libbot-based bot for Okavango demo scenario

---
+++ data/bots/core-demos/demo-okavango/bot.ts
@@ -0,0 +1,23 @@
+import {
+  dirname,
+  fromFileUrl,
+  relative,
+} from "https://deno.land/std@0.103.0/path/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
+import { runBot } from "./core.ts";
+
+const scriptPath = dirname(relative(Deno.cwd(), fromFileUrl(import.meta.url)));
+
+const { API_URL, USERNAME, PASSWORD } = config({
+  path: scriptPath + "/.env",
+});
+
+const settings = {
+  apiUrl: API_URL,
+  credentials: {
+    username: USERNAME,
+    password: PASSWORD,
+  },
+};
+
+await runBot(settings);
