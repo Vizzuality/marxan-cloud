8a18866869c91040b855abbf7c7aeab7eb910570
Ruben Vallejo <ralexanderv89@gmail.com>
2021-11-11 12:26:52 +0100

    feat: add role management to project-acl service

--- api/apps/api/src/modules/projects-acl/project-acl.service.ts
+++ api/apps/api/src/modules/projects-acl/project-acl.service.ts
@@ -5,26 +5,33 @@ import { intersection } from 'lodash';
 import { UsersProjectsApiEntity } from '@marxan-api/modules/projects/control-level/users-projects.api.entity';
 import { Roles } from '@marxan-api/modules/users/role.api.entity';
 import { Injectable } from '@nestjs/common';
+import { AccessControlService } from '../access-control/access-control-service.interface';
 
 /**
  * Debt: neither UsersProjectsApiEntity should belong to projects
  * nor the Roles should belong to users
  */
 @Injectable()
-export class ProjectAclService {
-  private readonly canPublishRoles = [
-    Roles.project_admin,
+export class ProjectAclService extends AccessControlService {
+  private readonly canCreateProjectRoles = [Roles.organization_owner];
+  private readonly canCreateScenarioRoles = [Roles.project_owner];
+  private readonly canEditScenarioRoles = [Roles.project_owner];
+  private readonly canViewSolutionRoles = [Roles.project_owner];
+  private readonly canPublishProjectRoles = [Roles.project_owner];
+  private readonly canViewProjectRoles = [
     Roles.project_owner,
-    Roles.organization_admin,
-    Roles.organization_owner,
+    Roles.project_admin,
+    Roles.project_user,
   ];
 
   constructor(
     @InjectRepository(UsersProjectsApiEntity)
     private readonly roles: Repository<UsersProjectsApiEntity>,
-  ) {}
+  ) {
+    super();
+  }
 
-  async canPublish(userId: string, projectId: string): Promise<boolean> {
+  async canCreateProject(userId: string, projectId: string): Promise<boolean> {
     const roles = (
       await this.roles.find({
         where: {
@@ -34,6 +41,69 @@ export class ProjectAclService {
       })
     ).flatMap((role) => role.roleName);
 
-    return intersection(roles, this.canPublishRoles).length > 0;
+    return intersection(roles, this.canCreateProjectRoles).length > 0;
+  }
+
+  async canViewProject(userId: string, projectId: string): Promise<boolean> {
+    const roles = (
+      await this.roles.find({
+        where: {
+          projectId,
+          userId,
+        },
+      })
+    ).flatMap((role) => role.roleName);
+
+    return intersection(roles, this.canViewProjectRoles).length > 0;
+  }
+
+  async canPublishProject(userId: string, projectId: string): Promise<boolean> {
+    const roles = (
+      await this.roles.find({
+        where: {
+          projectId,
+          userId,
+        },
+      })
+    ).flatMap((role) => role.roleName);
+
+    return intersection(roles, this.canPublishProjectRoles).length > 0;
+  }
+
+  async canCreateScenario(userId: string, projectId: string): Promise<boolean> {
+    const roles = (
+      await this.roles.find({
+        where: {
+          projectId,
+          userId,
+        },
+      })
+    ).flatMap((role) => role.roleName);
+
+    return intersection(roles, this.canCreateScenarioRoles).length > 0;
+  }
+  async canEditScenario(userId: string, projectId: string): Promise<boolean> {
+    const roles = (
+      await this.roles.find({
+        where: {
+          projectId,
+          userId,
+        },
+      })
+    ).flatMap((role) => role.roleName);
+
+    return intersection(roles, this.canEditScenarioRoles).length > 0;
+  }
+  async canViewSolutions(userId: string, projectId: string): Promise<boolean> {
+    const roles = (
+      await this.roles.find({
+        where: {
+          projectId,
+          userId,
+        },
+      })
+    ).flatMap((role) => role.roleName);
+
+    return intersection(roles, this.canViewSolutionRoles).length > 0;
   }
 }
