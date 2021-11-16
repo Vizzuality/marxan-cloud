2d6c2d87804a3168c206d0f3d336df2678971093
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-11-16 13:30:24 +0100

    tests(protected-areas): fix missing file

--- api/libs/protected-areas/src/add-protected-area-input.ts
+++ api/libs/protected-areas/src/add-protected-area-input.ts
@@ -1,4 +1,4 @@
-import { IsString, IsUUID, ValidateNested } from 'class-validator';
+import { IsOptional, IsString, IsUUID, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class Shapefile {
@@ -21,6 +21,7 @@ export class JobInput {
   @Type(() => Shapefile)
   shapefile!: Shapefile;
 
+  @IsOptional()
   @IsString()
   name?: string;
 }
