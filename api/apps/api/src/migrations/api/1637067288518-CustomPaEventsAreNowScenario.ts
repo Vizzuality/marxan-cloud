ff05e89b1f7a391af3f338b4e6d3d553ed579aa4
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-11-16 14:01:33 +0100

    fix(scenarios): protected area api events values

---
+++ api/apps/api/src/migrations/api/1637067288518-CustomPaEventsAreNowScenario.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CustomPaEventsAreNowScenario1637067288518
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      INSERT INTO api_event_kinds (id) values
+      ('scenario.protectedAreas.submitted/v1/alpha'),
+      ('scenario.protectedAreas.finished/v1/alpha'),
+      ('scenario.protectedAreas.failed/v1/alpha');
+    `);
+
+    await queryRunner.query(`
+      UPDATE api_events set kind = 'scenario.protectedAreas.submitted/v1/alpha' where kind = 'project.protectedAreas.submitted/v1/alpha';
+    `);
+
+    await queryRunner.query(`
+      UPDATE api_events set kind = 'scenario.protectedAreas.finished/v1/alpha' where kind = 'project.protectedAreas.finished/v1/alpha';
+    `);
+
+    await queryRunner.query(`
+      UPDATE api_events set kind = 'scenario.protectedAreas.failed/v1/alpha' where kind = 'project.protectedAreas.failed/v1/alpha';
+    `);
+
+    await queryRunner.query(
+      `DELETE FROM api_event_kinds WHERE id like 'project.protectedAreas.%';`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DELETE FROM api_event_kinds WHERE id like 'scenario.protectedAreas.%';`,
+    );
+  }
+}
