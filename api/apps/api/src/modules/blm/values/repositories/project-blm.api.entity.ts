2b3387cd7671c87405562db09223f7d6c1a884b5
Aaron Perez <aarperper@gmail.com>
2021-11-22 14:18:57 +0000

    remove API decorators from ProjectBLM entity

--- api/apps/api/src/modules/blm/values/repositories/project-blm.api.entity.ts
+++ api/apps/api/src/modules/blm/values/repositories/project-blm.api.entity.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, OneToOne, PrimaryColumn } from 'typeorm';
 import { BaseServiceResource } from '@marxan-api/types/resource.interface';
 import { TimeUserEntityMetadata } from '@marxan/utils';
@@ -15,22 +14,18 @@ export const projectBlmResource: BaseServiceResource = {
 
 @Entity('project_blms')
 export class ProjectBlm extends TimeUserEntityMetadata {
-  @ApiProperty({ isArray: true })
   @Column('decimal', { array: true, default: [0.001, 100] })
   range!: [number, number];
 
-  @ApiProperty({ isArray: true })
   @Column('decimal', { array: true, default: [] })
   values!: number[];
 
-  @ApiProperty({ isArray: true })
   @Column('decimal', { array: true, default: [] })
   defaults!: number[];
 
   /**
    * The project to which this BLM Values belongs.
    */
-  @ApiProperty()
   @PrimaryColumn('uuid')
   @OneToOne(() => Project)
   id!: string;
