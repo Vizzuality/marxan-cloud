d44c6b336ba0d6b14ba3a353fe6a4a3224cee0b1
anamontiaga <amontiaga@gmail.com>
2021-11-23 18:49:13 +0100

    styles: change text in panel when calculating PAs percetanges

--- app/layout/scenarios/edit/status/constants.ts
+++ app/layout/scenarios/edit/status/constants.ts
@@ -1,5 +1,5 @@
 export const TEXTS_RUNNING = {
-  planningAreaProtectedCalculation: () => 'Calculating the protected areas percentages...',
+  planningAreaProtectedCalculation: () => 'Calculating planning unit conservation coverage...',
   features: () => 'Processing the features...',
   planningUnitsInclusion: () => 'Processing inclusion/exclusion of planning units...',
   costSurface: () => 'Processing cost surface...',
@@ -7,9 +7,9 @@ export const TEXTS_RUNNING = {
 };
 
 export const TEXTS_FAILURE = {
-  planningAreaProtectedCalculation: () => 'Fail Calculating the protected areas percentages',
-  features: () => 'Fail Processing the features',
-  planningUnitsInclusion: () => 'Fail Processing inclusion/exclusion of planning units',
-  costSurface: () => 'Fail Processing cost surface',
-  run: () => 'Fail Running Marxan',
+  planningAreaProtectedCalculation: () => 'Fail calculating planning unit conservation coverage',
+  features: () => 'Fail processing the features',
+  planningUnitsInclusion: () => 'Fail processing inclusion/exclusion of planning units',
+  costSurface: () => 'Fail processing cost surface',
+  run: () => 'Fail running Marxan',
 };
