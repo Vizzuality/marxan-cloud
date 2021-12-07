cf8f93fd419c5a966a2177a8a1b9c79766a92f49
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-12-07 16:08:07 +0000

    feat: migration for scenario locks table

---
+++ api/apps/api/src/migrations/api/1638891599444-AddScenarioLocksTable.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddScenarioLocksTable1638891599444 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "scenario_locks" (
+              scenario_id uuid NOT NULL,
+              user_id uuid NOT NULL,
+              grab_date timestamptz NOT NULL
+             );`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "scenario_locks" ADD CONSTRAINT scenario_locks_fk FOREIGN KEY (scenario_id) REFERENCES scenarios(id) ON DELETE CASCADE;`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "scenario_locks" ADD CONSTRAINT scenario_locks_fk_1 FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "scenario_locks" ADD CONSTRAINT scenario_locks_pk PRIMARY KEY (scenario_id, user_id);`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "scenario_locks" DROP CONSTRAINT scenario_locks_pk;`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "scenario_locks" DROP CONSTRAINT scenario_locks_fk_1;`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "scenario_locks" DROP CONSTRAINT scenario_locks_fk;`,
+    );
+
+    await queryRunner.query(`DROP TABLE "scenario_locks";`);
+  }
+}
