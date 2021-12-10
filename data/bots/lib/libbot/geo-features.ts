98f2a0184412677725d51e83b6fbda41f62dcb30
andrea rota <andrea.rota@vizzuality.com>
2021-12-10 11:05:01 +0000

    make function do what it should, and express it through its name

--- data/bots/lib/libbot/geo-features.ts
+++ data/bots/lib/libbot/geo-features.ts
@@ -1,5 +1,7 @@
+import Process from "https://deno.land/std@0.103.0/node/process.ts";
 import { BotHttpClient, getJsonApiDataFromResponse } from "./marxan-bot.ts";
-import { logError } from "./logger.ts";
+import { logError, logInfo } from "./logger.ts";
+import { tookMs } from "./util/perf.ts";
 
 export class GeoFeatures {
   private baseHttpClient;
@@ -8,14 +10,33 @@ export class GeoFeatures {
     this.baseHttpClient = httpClient.baseHttpClient;
   }
 
-  async getIdFromQueryStringInProject(
+  async getIdOfFeaturesBySubstringMatchOnName(
     projectId: string,
     name: string,
-  ) {
-    return await this.baseHttpClient.get(
+  ): Promise<string[]> {
+    const opStart = Process.hrtime();
+
+    const id = await this.baseHttpClient.get(
       `/projects/${projectId}/features?q=${name}&fields=id`,
     )
       .then(getJsonApiDataFromResponse)
+      .then((data: { id: string; type: string }[]) => {
+        return data.map((i) => i.id);
+      })
       .catch(logError);
+
+    logInfo(
+      `Id of feature matching query string ${name} retrieved in ${
+        tookMs(Process.hrtime(opStart))
+      }ms.`,
+    );
+
+    if (!id || id.length === 0) {
+      throw new Error(
+        "No feature found with given name: this is probably a bug in this bot",
+      );
+    }
+
+    return id;
   }
 }
