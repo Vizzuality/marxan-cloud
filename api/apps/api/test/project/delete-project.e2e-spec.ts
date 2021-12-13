0667f4d3ec679c3f609f2b627ce64e29a0fac691
Ruben Vallejo <ralexanderv89@gmail.com>
2021-12-13 18:03:20 +0100

    feat: adds delete e2e tests for projects

---
+++ api/apps/api/test/project/delete-project.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { FixtureType } from '@marxan/utils/tests/fixture-type';
+import { getFixtures } from './delete-project.fixtures';
+
+let fixtures: FixtureType<typeof getFixtures>;
+
+beforeEach(async () => {
+  fixtures = await getFixtures();
+});
+
+afterEach(async () => {
+  await fixtures?.cleanup();
+});
+
+test(`deleting a project should work`, async () => {
+  await fixtures.GivenProjectWasCreated();
+
+  await fixtures.WhenProjectIsDeleted();
+
+  await fixtures.ThenProjectIsNotFound();
+});
+
+test(`deleting a project does not work if user is not in project`, async () => {
+  await fixtures.GivenProjectWasCreated();
+
+  const request = await fixtures.WhenProjectIsDeletedAsNotIncludedUser();
+
+  await fixtures.ThenForbiddenIsReturned(request);
+});
