a1ae9fe77b45889de1d72446b38ace6b6723fb28
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-12-13 09:29:01 +0100

    refactor(cloning: replace raw string with resource-id vo

--- api/apps/api/src/modules/clone/import/domain/events/piece-import-requested.event.ts
+++ api/apps/api/src/modules/clone/import/domain/events/piece-import-requested.event.ts
@@ -1,10 +1,10 @@
 import { IEvent } from '@nestjs/cqrs';
-import { ComponentId, ClonePiece } from '@marxan/cloning/domain';
+import { ComponentId, ClonePiece, ResourceId } from '@marxan/cloning/domain';
 
 export class PieceImportRequested implements IEvent {
   constructor(
     public readonly id: ComponentId,
     public readonly piece: ClonePiece,
-    public readonly resourceId: string,
+    public readonly resourceId: ResourceId,
   ) {}
 }
