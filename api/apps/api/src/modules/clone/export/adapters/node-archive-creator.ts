ad25fadfd331621899bc40992b8bcf7e5bacd61e
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-12-15 09:45:08 +0100

    feat(cloning): add file repository for export archive creation

--- api/apps/api/src/modules/clone/export/adapters/node-archive-creator.ts
+++ api/apps/api/src/modules/clone/export/adapters/node-archive-creator.ts
@@ -1,13 +1,23 @@
-import { ArchiveLocation } from '@marxan/cloning/domain';
 import { Injectable } from '@nestjs/common';
+import { Either, right } from 'fp-ts/Either';
 
-import { ArchiveCreator } from '../application/archive-creator.port';
+import { ArchiveLocation } from '@marxan/cloning/domain';
+import { FileRepository } from '@marxan/files-repository';
+
+import {
+  ArchiveCreationError,
+  ArchiveCreator,
+} from '../application/archive-creator.port';
 
 @Injectable()
 export class NodeArchiveCreator extends ArchiveCreator {
+  constructor(private readonly fileRepository: FileRepository) {
+    super();
+  }
+
   async zip(
     files: { uri: string; relativeDestination: string }[],
-  ): Promise<ArchiveLocation> {
-    return new ArchiveLocation('.zip');
+  ): Promise<Either<ArchiveCreationError, ArchiveLocation>> {
+    return right(new ArchiveLocation('.zip'));
   }
 }
