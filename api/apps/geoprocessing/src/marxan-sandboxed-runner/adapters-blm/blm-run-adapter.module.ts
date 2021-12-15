1f43eaddad481c95ff00437884ea1e844428e61d
Daute Rodríguez Rodríguez <daute@acidtango.com>
2021-12-15 14:21:17 +0000

    del: unused imports and providers

--- api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters-blm/blm-run-adapter.module.ts
+++ api/apps/geoprocessing/src/marxan-sandboxed-runner/adapters-blm/blm-run-adapter.module.ts
@@ -14,23 +14,14 @@ import { AssetFactory } from './asset-factory.service';
 
 import { InputFilesFs } from '../adapters-single/scenario-data/input-files-fs';
 import { AssetsModule } from '../adapters-shared';
-import { GeoOutputModule } from '@marxan-geoprocessing/marxan-sandboxed-runner/adapters-single/solutions-output/geo-output';
-import { ResultParserService } from '@marxan-geoprocessing/marxan-sandboxed-runner/adapters-single/solutions-output/result-parser.service';
-import { MostDifferentService } from '@marxan-geoprocessing/marxan-sandboxed-runner/adapters-single/solutions-output/most-different.service';
-import { BestSolutionService } from '@marxan-geoprocessing/marxan-sandboxed-runner/adapters-single/solutions-output/best-solution.service';
-import { MarxanDirectory } from '@marxan-geoprocessing/marxan-sandboxed-runner/adapters-single/marxan-directory.service';
 import { SandboxRunnerOutputHandler } from '@marxan-geoprocessing/marxan-sandboxed-runner/sandbox-runner-output-handler';
 
 export const blmSandboxRunner = Symbol(`blm sandbox runner`);
 
 @Module({
-  imports: [WorkspaceModule, AssetsModule, GeoOutputModule],
+  imports: [WorkspaceModule, AssetsModule],
   providers: [
-    ResultParserService,
-    MostDifferentService,
-    BestSolutionService,
     MarxanConfig,
-    MarxanDirectory,
     {
       provide: sandboxRunnerToken,
       useClass: MarxanSandboxBlmRunnerService,
