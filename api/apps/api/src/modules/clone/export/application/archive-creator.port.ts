b30ae8e63e00676f016874971f2f34a38a409df9
Kamil Gajowy <kamil.gajowy@gmail.com>
2021-12-15 12:04:09 +0100

    feat(cloning): save final archive

--- api/apps/api/src/modules/clone/export/application/archive-creator.port.ts
+++ api/apps/api/src/modules/clone/export/application/archive-creator.port.ts
@@ -2,8 +2,15 @@ import { Either } from 'fp-ts/Either';
 import { ArchiveLocation } from '@marxan/cloning/domain';
 
 export const unknownError = Symbol(`unknown error`);
+export const cannotGetFile = Symbol(`cannot get a file`);
+export const cannotCreateArchive = Symbol(`cannot create archive`);
+export const cannotStoreArchive = Symbol(`cannot store archive`);
 
-export type ArchiveCreationError = typeof unknownError;
+export type ArchiveCreationError =
+  | typeof unknownError
+  | typeof cannotGetFile
+  | typeof cannotCreateArchive
+  | typeof cannotStoreArchive;
 
 export abstract class ArchiveCreator {
   abstract zip(
