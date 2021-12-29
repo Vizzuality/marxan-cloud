c1db13b98c3c5712a535ac669f35bc10a7abffdc
Simao Rodrigues <arodrigues.simao@gmail.com>
2021-12-29 13:00:04 +0000

    Improve homepage partners section responsiveness on mobile

--- app/layout/home/partners/component.tsx
+++ app/layout/home/partners/component.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 import Wrapper from 'layout/wrapper';
 
-import {
-  FOUNDING_LOGOS, PARTNERSHIP_LOGOS, ADDITIONAL_SUPPORT_LOGOS, INITIATIVE_LOGOS,
-} from './constants';
+import { PARTNER_LOGOS } from './constants';
 
 export interface PartnersListProps {
 
@@ -12,63 +10,27 @@ export interface PartnersListProps {
 
 export const PartnersList: React.FC<PartnersListProps> = () => {
   return (
-    <div className="relative pt-4 pb-24 text-base text-gray-800 bg-blue-500">
+    <div className="relative py-10 md:py-24 text-base text-gray-800 bg-blue-500">
       <Wrapper>
-        <div className="space-y-32 md:space-y-20">
-          <div className="absolute w-screen h-px top-2/4 bg-gradient-to-r from-gray-200 opacity-20 to-gray-200 via-black" />
-
-          <div className="flex flex-col w-full max-w-5xl mx-auto">
-            <h2 className="pb-10 leading-relaxed text-center md:pb-6 md:text-left bg-clip-text">
-              In partnership with:
-            </h2>
-            <div className="grid w-full grid-cols-1 md:grid-cols-7 gap-y-20 gap-x-20">
-              {FOUNDING_LOGOS.map((fl) => (
-                <a href={fl.hyperlink} key={fl.id} rel="noreferrer" target="_blank" className="col-span-2 place-self-center md:place-self-start">
-                  <img alt={fl.alt} src={fl.src} height="auto" width="auto" />
-                </a>
-              ))}
-            </div>
-          </div>
-
-          <div className="flex flex-col w-full max-w-5xl pb-20 mx-auto">
-            <h2 className="pb-10 leading-relaxed text-center md:pb-6 bg-clip-text md:text-left">
-              In partnership with:
-            </h2>
-            <div className="grid w-full grid-cols-1 md:grid-cols-4 gap-y-24 md:gap-y-0 md:gap-x-20">
-              {PARTNERSHIP_LOGOS.map((fl) => (
-                <a href={fl.hyperlink} key={fl.id} rel="noreferrer" target="_blank" className="place-self-center md:place-self-start">
-                  <img alt={fl.alt} src={fl.src} height="auto" width="auto" />
-                </a>
-              ))}
-            </div>
-          </div>
-
-          <div className="flex flex-col w-full max-w-5xl pt-10 mx-auto md:pt-20">
-            <h2 className="pb-10 leading-relaxed text-center md:pb-6 bg-clip-text md:text-left">
-              With additional support from:
-            </h2>
-            <div className="grid w-full grid-cols-1 md:grid-cols-4 gap-y-24 md:gap-y-0 md:gap-x-20">
-              {ADDITIONAL_SUPPORT_LOGOS.map((sl) => (
-                <a href={sl.hyperlink} key={sl.id} rel="noreferrer" target="_blank" className="place-self-center md:place-self-start">
-                  <img alt={sl.alt} src={sl.src} height="auto" width="auto" />
-                </a>
-              ))}
-            </div>
-          </div>
-
-          <div className="relative flex flex-col w-full max-w-5xl mx-auto">
-            <h2 className="pb-10 leading-relaxed text-center md:pb-6 bg-clip-text md:text-left">
-              Supported by an initiative of the:
-            </h2>
-            <div className="grid w-full grid-cols-1 md:grid-cols-4">
-              {INITIATIVE_LOGOS.map((sl) => (
-                <a href={sl.hyperlink} key={sl.id} rel="noreferrer" target="_blank" className="place-self-center md:place-self-start">
-                  <img alt={sl.alt} src={sl.src} height="auto" width="auto" />
-                </a>
-              ))}
-            </div>
-          </div>
-
+        <div className="space-y-16 md:space-y-20">
+          {PARTNER_LOGOS.map((section) => (
+            section.separator ? (
+              <div className="w-screen h-px bg-gradient-to-r from-gray-200 opacity-20 to-gray-200 via-black" />
+            ) : (
+              <div key={section.id} className="flex flex-col w-full max-w-5xl mx-auto">
+                <h2 className="pb-10 leading-relaxed text-center md:pb-6 md:text-left bg-clip-text">
+                  {section.title}
+                </h2>
+                <div className="grid w-full grid-cols-1 md:grid-cols-8 gap-y-10 md:gap-y-20 gap-x-20">
+                  {section.logos.map((logo) => (
+                    <a href={logo.hyperlink} key={logo.id} rel="noreferrer" target="_blank" className="col-span-2 place-self-center md:place-self-start">
+                      <img className="max-h-12" alt={logo.alt} src={logo.src} height="auto" width="auto" />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )
+          ))}
         </div>
       </Wrapper>
     </div>
--- app/layout/home/partners/constants.ts
+++ app/layout/home/partners/constants.ts
@@ -8,67 +8,87 @@ import UN_ENVIRONMENT_LOGO from 'images/partners/un-environment_logo.png';
 import UNIVERSITY_QUEENSLAND_LOGO from 'images/partners/university-queensland_logo.png';
 import VIZZUALITY_LOGO from 'images/partners/vizzuality_logo.png';
 
-export const FOUNDING_LOGOS = [
+export const PARTNER_LOGOS = [
   {
-    id: 1,
-    alt: 'Microsoft logo',
-    hyperlink: 'https://www.microsoft.com',
-    src: MICROSOFT_LOGO,
-  },
-  {
-    id: 2,
-    alt: 'The Nature Conservancy logo',
-    hyperlink: 'https://www.nature.org',
-    src: THE_NATURE_CONSERVANCY_LOGO,
-  }];
-
-export const PARTNERSHIP_LOGOS = [
-  {
-    id: 3,
-    alt: 'The University of Queensland logo',
-    hyperlink: 'https://www.uq.edu.au',
-    src: UNIVERSITY_QUEENSLAND_LOGO,
-  },
-  {
-    id: 4,
-    alt: 'Biopama logo',
-    hyperlink: 'https://biopama.org',
-    src: BIOPAMA_LOGO,
-  },
-];
-
-export const ADDITIONAL_SUPPORT_LOGOS = [
-  {
-    id: 5,
-    alt: 'Vizzuality logo',
-    hyperlink: 'https://www.vizzuality.com',
-    src: VIZZUALITY_LOGO,
-  },
-  {
-    id: 6,
-    alt: 'Mapbox logo',
-    hyperlink: 'https://www.mapbox.com',
-    src: MAPBOX_LOGO,
-  },
-  {
-    id: 7,
-    alt: 'United Nations Environment Programme logo',
-    hyperlink: 'https://www.unep.org',
-    src: UN_ENVIRONMENT_LOGO,
-  },
-  {
-    id: 8,
-    alt: 'Pacmara logo',
-    hyperlink: 'https://pacmara.org',
-    src: PACMARA_LOGO,
-  },
-];
-
-export const INITIATIVE_LOGOS = [
-  {
-    id: 11,
-    alt: 'European Commission logo',
-    hyperlink: 'https://ec.europa.eu',
-    src: EU_COMISSION_LOGO,
+    // FOUNDING LOGOS
+    id: 'founding',
+    title: 'In partnership with:',
+    logos: [
+      {
+        id: 1,
+        alt: 'Microsoft logo',
+        hyperlink: 'https://www.microsoft.com',
+        src: MICROSOFT_LOGO,
+      },
+      {
+        id: 2,
+        alt: 'The Nature Conservancy logo',
+        hyperlink: 'https://www.nature.org',
+        src: THE_NATURE_CONSERVANCY_LOGO,
+      }],
+  }, {
+    // PARTNERSHIP LOGOS
+    id: 'partnership',
+    title: 'In partnership with:',
+    logos: [
+      {
+        id: 3,
+        alt: 'The University of Queensland logo',
+        hyperlink: 'https://www.uq.edu.au',
+        src: UNIVERSITY_QUEENSLAND_LOGO,
+      },
+      {
+        id: 4,
+        alt: 'Biopama logo',
+        hyperlink: 'https://biopama.org',
+        src: BIOPAMA_LOGO,
+      },
+    ],
+  }, {
+    // SEPARATOR
+    id: 'separator',
+    separator: true,
+  }, {
+    // ADDITIONAL SUPPORT LOGOS
+    id: 'additional-support',
+    title: 'With additional support from:',
+    logos: [
+      {
+        id: 5,
+        alt: 'Vizzuality logo',
+        hyperlink: 'https://www.vizzuality.com',
+        src: VIZZUALITY_LOGO,
+      },
+      {
+        id: 6,
+        alt: 'Mapbox logo',
+        hyperlink: 'https://www.mapbox.com',
+        src: MAPBOX_LOGO,
+      },
+      {
+        id: 7,
+        alt: 'United Nations Environment Programme logo',
+        hyperlink: 'https://www.unep.org',
+        src: UN_ENVIRONMENT_LOGO,
+      },
+      {
+        id: 8,
+        alt: 'Pacmara logo',
+        hyperlink: 'https://pacmara.org',
+        src: PACMARA_LOGO,
+      },
+    ],
+  }, {
+    // INITIATIVE LOGOS
+    id: 'initiative',
+    title: 'Supported by an initiative of the:',
+    logos: [
+      {
+        id: 11,
+        alt: 'European Commission logo',
+        hyperlink: 'https://ec.europa.eu',
+        src: EU_COMISSION_LOGO,
+      },
+    ],
   },
 ];
