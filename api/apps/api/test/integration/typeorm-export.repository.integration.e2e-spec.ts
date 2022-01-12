2f93593a8a40bbbde21c58d97654b2cc7fbbbc09
Aaron Perez <aarperper@gmail.com>
2022-01-12 15:12:24 +0000

    test: refactorizes tests to comply with fixtures

--- api/apps/api/test/integration/typeorm-export.repository.integration.e2e-spec.ts
+++ api/apps/api/test/integration/typeorm-export.repository.integration.e2e-spec.ts
@@ -1,132 +1,124 @@
 import { ClonePiece, ComponentId, ResourceKind } from '@marxan/cloning/domain';
-import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { v4 } from 'uuid';
 import { ResourceId } from '../../src/modules/clone/export';
-import { ComponentLocationEntity } from '../../src/modules/clone/export/adapters/entities/component-locations.api.entity';
-import { ExportComponentEntity } from '../../src/modules/clone/export/adapters/entities/export-components.api.entity';
 import { ExportEntity } from '../../src/modules/clone/export/adapters/entities/exports.api.entity';
-import { TypeormExportRepository } from '../../src/modules/clone/export/adapters/typeorm-export.repository';
 import { ComponentLocation } from '../../src/modules/clone/export/application/complete-piece.command';
 import { ExportRepository } from '../../src/modules/clone/export/application/export-repository.port';
 import { Export, ExportId } from '../../src/modules/clone/export/domain';
-import { apiConnections } from '../../src/ormconfig';
+import { bootstrapApplication } from '../utils/api-application';
+import { FixtureType } from '@marxan/utils/tests/fixture-type';
 
 describe('Typeorm export repository', () => {
-  let module: TestingModule;
+  let fixtures: FixtureType<typeof getFixtures>;
   let repo: ExportRepository;
 
   beforeEach(async () => {
-    module = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot(apiConnections.default),
-        TypeOrmModule.forFeature([
-          ExportEntity,
-          ExportComponentEntity,
-          ComponentLocationEntity,
-        ]),
-      ],
-      providers: [
-        {
-          provide: ExportRepository,
-          useClass: TypeormExportRepository,
-        },
-      ],
-    }).compile();
-
-    repo = module.get<ExportRepository>(ExportRepository);
+    fixtures = await getFixtures();
   }, 100000);
 
   afterAll(async () => {
-    const connection = module.get<Connection>(Connection);
-    const exportRepo = connection.getRepository(ExportEntity);
-
-    await exportRepo.delete({});
+    await fixtures.cleanup();
   });
 
   it('should expose methods for getting an export by id and storing exports', async () => {
-    expect(repo.find(new ExportId(v4()))).resolves.toBe(undefined);
+    await fixtures.WhenSavingAnExportItShouldNotFail();
 
-    const resourceId = new ResourceId(v4());
-    const componentId = new ComponentId(v4());
-    const componentLocationUri = '/foo/bar/project-metadata.json';
-    const componentLocationRelativePath = 'project-metadata.json';
-    const exportInstance = Export.newOne(resourceId, ResourceKind.Project, [
-      {
-        finished: false,
-        piece: ClonePiece.ProjectMetadata,
-        resourceId: resourceId.value,
-        id: componentId,
-        uris: [
-          new ComponentLocation(
-            componentLocationUri,
-            componentLocationRelativePath,
-          ),
-        ],
-      },
-    ]);
-    await repo.save(exportInstance);
-
-    const result = await repo.find(exportInstance.id);
-
-    expect(result).toBeDefined();
-    expect(result!.id.value).toBe(exportInstance.id.value);
-    expect(result!.resourceKind).toBe(ResourceKind.Project);
-    expect(result!.resourceId.value).toBe(resourceId.value);
-
-    const resultSnapshot = result!.toSnapshot();
-
-    expect(resultSnapshot.exportPieces).toHaveLength(1);
-
-    const [exportComponent] = resultSnapshot.exportPieces;
-
-    expect(exportComponent.finished).toBe(false);
-    expect(exportComponent.piece).toBe(ClonePiece.ProjectMetadata);
-    expect(exportComponent.resourceId).toBe(resourceId.value);
-    expect(exportComponent.uris).toHaveLength(1);
-    expect(exportComponent.uris![0].uri).toBe(componentLocationUri);
-    expect(exportComponent.uris![0].relativePath).toBe(
-      componentLocationRelativePath,
-    );
+    await fixtures.ThenWhenReadingItFromRepositoryExportDataShouldBeOk({
+      componentIsCompleted: false,
+    });
   });
 
   it('should update nested entities', async () => {
-    const resourceId = new ResourceId(v4());
-    const componentId = new ComponentId(v4());
-    const componentLocationUri = '/foo/bar/project-metadata.json';
-    const componentLocationRelativePath = 'project-metadata.json';
-    const componentLocation = new ComponentLocation(
-      componentLocationUri,
-      componentLocationRelativePath,
-    );
-    const exportInstance = Export.newOne(resourceId, ResourceKind.Project, [
-      {
-        finished: false,
-        piece: ClonePiece.ProjectMetadata,
-        resourceId: resourceId.value,
-        id: componentId,
-        uris: [],
-      },
-    ]);
-
-    await repo.save(exportInstance);
-
-    exportInstance.completeComponent(componentId, [componentLocation]);
-
-    await repo.save(exportInstance);
-
-    const storedExport = await repo.find(exportInstance.id);
-
-    expect(storedExport).toBeDefined();
-
-    const snapshot = storedExport!.toSnapshot();
-
-    expect(snapshot.exportPieces[0].finished).toBe(true);
-    expect(snapshot.exportPieces[0].uris).toHaveLength(1);
-    expect(snapshot.exportPieces[0].uris[0].uri).toBe(componentLocationUri);
-    expect(snapshot.exportPieces[0].uris[0].relativePath).toBe(
-      componentLocationRelativePath,
-    );
+    await fixtures.WhenSavingAnExportItShouldNotFail();
+    await fixtures.ThenWhenCompletingAComponent();
+    await fixtures.ThenWhenReadingItFromRepositoryExportDataShouldBeOk({
+      componentIsCompleted: true,
+    });
   });
 });
+
+const getFixtures = async () => {
+  let exportId: ExportId;
+  let resourceId: ResourceId;
+  let componentId: ComponentId;
+  let componentLocationUri: string;
+  let componentLocationRelativePath: string;
+
+  const app = await bootstrapApplication();
+  const repo = app.get<ExportRepository>(ExportRepository);
+
+  return {
+    cleanup: async () => {
+      const connection = app.get<Connection>(Connection);
+      const exportRepo = connection.getRepository(ExportEntity);
+
+      await exportRepo.delete({});
+      await app.close();
+    },
+    WhenSavingAnExportItShouldNotFail: async () => {
+      resourceId = new ResourceId(v4());
+      componentId = new ComponentId(v4());
+      componentLocationUri = '/foo/bar/project-metadata.json';
+      componentLocationRelativePath = 'project-metadata.json';
+      const exportInstance = Export.newOne(resourceId, ResourceKind.Project, [
+        {
+          finished: false,
+          piece: ClonePiece.ProjectMetadata,
+          resourceId: resourceId.value,
+          id: componentId,
+          uris: [
+            new ComponentLocation(
+              componentLocationUri,
+              componentLocationRelativePath,
+            ),
+          ],
+        },
+      ]);
+      exportId = exportInstance.id;
+      await repo.save(exportInstance);
+    },
+    ThenWhenCompletingAComponent: async () => {
+      const componentLocation = new ComponentLocation(
+        componentLocationUri,
+        componentLocationRelativePath,
+      );
+      const exportInstance = (await repo.find(exportId)) as Export;
+      expect(exportInstance).toBeDefined();
+
+      exportInstance.completeComponent(componentId, [componentLocation]);
+      await repo.save(exportInstance);
+    },
+    ThenWhenReadingItFromRepositoryExportDataShouldBeOk: async ({
+      componentIsCompleted,
+    }: {
+      componentIsCompleted: boolean;
+    }) => {
+      const result = (await repo.find(exportId)) as Export;
+
+      expect(result).toBeDefined();
+      expect(result.id.value).toBe(exportId.value);
+      expect(result.resourceKind).toBe(ResourceKind.Project);
+      expect(result.resourceId.value).toBe(resourceId.value);
+
+      const resultSnapshot = result.toSnapshot();
+
+      expect(resultSnapshot.exportPieces).toHaveLength(1);
+
+      const [exportComponent] = resultSnapshot.exportPieces;
+
+      expect(exportComponent.finished).toBe(componentIsCompleted);
+      expect(exportComponent.piece).toBe(ClonePiece.ProjectMetadata);
+      expect(exportComponent.resourceId).toBe(resourceId.value);
+
+      if (componentIsCompleted) {
+        expect(exportComponent.uris).toBeDefined();
+        expect(exportComponent.uris).toHaveLength(1);
+        expect(exportComponent.uris[0].uri).toBe(componentLocationUri);
+        expect(exportComponent.uris[0].relativePath).toBe(
+          componentLocationRelativePath,
+        );
+      }
+    },
+  };
+};
