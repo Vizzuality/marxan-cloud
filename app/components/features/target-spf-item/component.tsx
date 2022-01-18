e2f17b6f32999e2a06c1aa8d2ded442ceb4c5a06
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-01-18 10:29:10 +0000

    Features' Target & SPF redesign

--- app/components/features/target-spf-item/component.tsx
+++ app/components/features/target-spf-item/component.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
+
 import cx from 'classnames';
+
 import Button from 'components/button';
-import Slider from 'components/forms/slider';
-import Label from 'components/forms/label';
 import Input from 'components/forms/input';
+import Label from 'components/forms/label';
+import Slider from 'components/forms/slider';
 
 import { TargetSPFItemProps, Type } from './types';
 
@@ -22,6 +24,8 @@ export const TargetSPFItem: React.FC<TargetSPFItemProps> = ({
   onChangeFPF,
 }: TargetSPFItemProps) => {
   const [targetValue, setTargetValue] = useState((target || defaultTarget) / 100);
+  const [inputTargetValue, setInputTargetValue] = useState(targetValue);
+  const [targetInputFocused, setTargetInputFocused] = useState(false);
   const [FPFValue, setFPFValue] = useState(fpf || defaultFPF);
   const sliderLabelRef = useRef(null);
 
@@ -32,6 +36,11 @@ export const TargetSPFItem: React.FC<TargetSPFItemProps> = ({
   useEffect(() => {
     if (typeof fpf !== 'undefined') setFPFValue(fpf);
   }, [fpf]);
+
+  useEffect(() => {
+    const percentValue = Math.round(targetValue * 100);
+    setInputTargetValue(percentValue);
+  }, [targetValue]);
 
   return (
     <div
@@ -50,54 +59,84 @@ export const TargetSPFItem: React.FC<TargetSPFItemProps> = ({
         })}
       />
       <div className="flex items-start justify-between pb-2 pr-2">
-        <span className="pr-5 text-sm font-medium font-heading">{isAllTargets ? 'Set target and SPF in all features' : name}</span>
-        {!isAllTargets && (
-          <Button
-            className="flex-shrink-0 text-xs"
-            theme="secondary"
-            size="xs"
-            onClick={() => onRemove && onRemove(id)}
-          >
-            Remove
-          </Button>
-        )}
-      </div>
-      <div className="flex">
-        <div className="relative flex-col w-full pr-4">
-          <Label ref={sliderLabelRef} className="mb-1 uppercase">
-            <span>{isAllTargets ? 'ALL TARGETS' : 'TARGET'}</span>
-          </Label>
-          <Slider
-            labelRef={sliderLabelRef}
-            minValue={0}
-            maxValue={1}
-            value={targetValue}
-            step={0.01}
-            onChange={(sliderValue) => {
-              setTargetValue(sliderValue);
-              if (onChangeTarget) onChangeTarget(+(sliderValue * 100).toFixed(0));
-            }}
-          />
-          <div className="flex justify-between w-full text-gray-400">
-            <span>0%</span>
-            <span>100%</span>
+        <div className="flex flex-col flex-grow">
+          <div className="w-full pr-5 text-sm font-medium font-heading">
+            {isAllTargets ? 'Set target and SPF in all features' : name}
           </div>
-        </div>
-        <div className="flex flex-col justify-between w-24 px-4 border-l">
-          <span>{isAllTargets ? 'ALL SPF' : 'SPF'}</span>
-          <div className="w-10 mb-6">
-            <Input
-              className="px-0 py-1"
-              theme="dark"
-              mode="dashed"
-              type="number"
-              value={FPFValue}
-              onChange={({ target: { value: inputValue } }) => {
-                setFPFValue(Number(inputValue));
-                if (onChangeFPF) onChangeFPF(Number(inputValue));
+          <div className="flex justify-between items-center mt-4">
+            <div className="flex items-center">
+              <Label ref={sliderLabelRef} className="uppercase w-16">
+                {isAllTargets ? 'ALL TARGETS' : 'TARGET'}
+              </Label>
+              <Input
+                className="w-16 text-center text-sm"
+                theme="dark"
+                type={targetInputFocused ? 'number' : 'text'}
+                min={0}
+                max={100}
+                step={1}
+                value={targetInputFocused ? inputTargetValue : `${inputTargetValue}%`}
+                onFocusChange={setTargetInputFocused}
+                onChange={((event) => {
+                  const percentValue = parseInt(event.target.value, 10);
+                  if (percentValue < 0 || percentValue > 100) return;
+                  setInputTargetValue(percentValue);
+                })}
+                onBlur={() => {
+                  // Prevent changing all targets if user didn't actually change it
+                  // (despite clicking on the input)
+                  if (Math.round(targetValue * 100) === inputTargetValue) return;
+                  setTargetValue(inputTargetValue / 100);
+                  if (onChangeTarget) onChangeTarget(inputTargetValue);
+                }}
+              />
+            </div>
+            <div className="flex items-center">
+              <span>{isAllTargets ? 'ALL SPF' : 'SPF'}</span>
+              <Input
+                className="w-16 ml-3 text-center text-sm"
+                theme="dark"
+                type="number"
+                value={FPFValue}
+                onChange={({ target: { value: inputValue } }) => {
+                  setFPFValue(Number(inputValue));
+                  if (onChangeFPF) onChangeFPF(Number(inputValue));
+                }}
+              />
+            </div>
+          </div>
+          <div className="relative flex flex-col">
+            <Slider
+              labelRef={sliderLabelRef}
+              minValue={0}
+              maxValue={1}
+              value={targetValue}
+              step={0.01}
+              showValue={false}
+              onChange={(sliderValue) => {
+                const percentValue = +(sliderValue * 100).toFixed(0);
+                setTargetValue(sliderValue);
+                setInputTargetValue(percentValue);
+                if (onChangeTarget) onChangeTarget(percentValue);
               }}
             />
+            <div className="flex justify-between -mt-3 text-gray-400">
+              <span>0%</span>
+              <span>100%</span>
+            </div>
           </div>
+        </div>
+        <div className="flex justify-end w-28">
+          {!isAllTargets && (
+            <Button
+              className="flex-shrink-0 text-xs"
+              theme="secondary"
+              size="xs"
+              onClick={() => onRemove && onRemove(id)}
+            >
+              Remove
+            </Button>
+          )}
         </div>
       </div>
     </div>
