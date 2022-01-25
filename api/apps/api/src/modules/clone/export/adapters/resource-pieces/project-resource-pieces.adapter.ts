673070547559d23d0493a3997c894c46b9e78702
Daute Rodríguez Rodríguez <94375891+aciddaute@users.noreply.github.com>
2022-01-25 14:38:12 +0000

    feat: project pieces resolver includes scenario pieces (#779)

--- api/apps/api/src/modules/clone/export/adapters/resource-pieces/project-resource-pieces.adapter.ts
+++ api/apps/api/src/modules/clone/export/adapters/resource-pieces/project-resource-pieces.adapter.ts
@@ -5,18 +5,41 @@ import {
   ResourceKind,
 } from '@marxan/cloning/domain';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { v4 } from 'uuid';
+import { Scenario } from '../../../../scenarios/scenario.api.entity';
 import { ResourcePieces } from '../../application/resource-pieces.port';
 import { ExportComponentSnapshot } from '../../domain';
 import { ResourcePiecesProvider } from '../resource-pieces.adapter';
+import { ScenarioResourcePiecesAdapter } from './scenario-resource-pieces.adapter';
 
 @Injectable()
 @ResourcePiecesProvider(ResourceKind.Project)
 export class ProjectResourcePiecesAdapter implements ResourcePieces {
+  constructor(
+    @InjectRepository(Scenario)
+    private readonly scenarioRepository: Repository<Scenario>,
+    private readonly scenarioResourcePieces: ScenarioResourcePiecesAdapter,
+  ) {}
+
   async resolveFor(
     id: ResourceId,
     kind: ResourceKind,
   ): Promise<ExportComponentSnapshot[]> {
+    const scenarios = await this.scenarioRepository.find({
+      where: { projectId: id.value },
+    });
+
+    const scenarioPieces = await Promise.all(
+      scenarios.map((scenario) =>
+        this.scenarioResourcePieces.resolveFor(
+          new ResourceId(scenario.id),
+          ResourceKind.Scenario,
+        ),
+      ),
+    );
+
     return [
       {
         id: new ComponentId(v4()),
@@ -53,6 +76,7 @@ export class ProjectResourcePiecesAdapter implements ResourcePieces {
         finished: false,
         uris: [],
       },
+      ...scenarioPieces.flat(),
     ];
   }
 }
--- api/apps/api/src/modules/clone/export/adapters/resource-pieces/scenario-resource-pieces.adapter.ts
+++ api/apps/api/src/modules/clone/export/adapters/resource-pieces/scenario-resource-pieces.adapter.ts
@@ -1,5 +1,11 @@
-import { ResourceId, ResourceKind } from '@marxan/cloning/domain';
+import {
+  ClonePiece,
+  ComponentId,
+  ResourceId,
+  ResourceKind,
+} from '@marxan/cloning/domain';
 import { Injectable } from '@nestjs/common';
+import { v4 } from 'uuid';
 import { ResourcePieces } from '../../application/resource-pieces.port';
 import { ExportComponentSnapshot } from '../../domain';
 import { ResourcePiecesProvider } from '../resource-pieces.adapter';
@@ -11,7 +17,14 @@ export class ScenarioResourcePiecesAdapter implements ResourcePieces {
     id: ResourceId,
     kind: ResourceKind,
   ): Promise<ExportComponentSnapshot[]> {
-    // TODO
-    return [];
+    return [
+      {
+        id: new ComponentId(v4()),
+        resourceId: id.value,
+        piece: ClonePiece.ScenarioMetadata,
+        finished: false,
+        uris: [],
+      },
+    ];
   }
 }
