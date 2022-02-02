6f3f31d34f5b35d67e8de3747880726d8b3e0f84
Ángel Higuera Vaquero <angelhiguera@acidtango.com>
2022-02-02 14:43:18 +0100

    feat: surface-cost.event-handler

--- api/apps/geoprocessing/src/modules/surface-cost/application/surface-cost-worker.ts
+++ api/apps/geoprocessing/src/modules/surface-cost/application/surface-cost-worker.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { EventBus } from '@nestjs/cqrs';
-import { Job, Worker } from 'bullmq';
+import { Worker } from 'bullmq';
 
-import { API_EVENT_KINDS } from '@marxan/api-events';
 import { JobInput, surfaceCostQueueName } from '@marxan/scenario-cost-surface';
-import { ApiEvent } from '@marxan-geoprocessing/modules/api-events';
 import { WorkerBuilder } from '@marxan-geoprocessing/modules/worker';
 
 import { SurfaceCostProcessor } from './surface-cost-processor';
@@ -19,30 +17,5 @@ export class SurfaceCostWorker {
     private readonly eventBus: EventBus,
   ) {
     this.#worker = wrapper.build(surfaceCostQueueName, processor);
-    this.#worker.on('completed', ({ data }: Job<JobInput>) => {
-      this.eventBus.publish(
-        new ApiEvent(
-          data.scenarioId,
-          API_EVENT_KINDS.scenario__costSurface__finished__v1_alpha1,
-        ),
-      );
-    });
-    this.#worker.on(
-      'failed',
-      ({ data, failedReason, attemptsMade, opts }: Job<JobInput>) => {
-        if (attemptsMade !== opts.attempts) {
-          return;
-        }
-        this.eventBus.publish(
-          new ApiEvent(
-            data.scenarioId,
-            API_EVENT_KINDS.scenario__costSurface__costUpdateFailed__v1_alpha1,
-            {
-              error: failedReason,
-            },
-          ),
-        );
-      },
-    );
   }
 }
