9478d7366ee066db24d538fa60a6d59fc757ed9c
Ángel Higuera Vaquero <angelhiguera@acidtango.com>
2022-02-02 14:43:18 +0100

    feat: surface-cost.event-handler

---
+++ api/apps/api/src/modules/scenarios/cost-surface/infra/surface-cost.events-handler.ts
@@ -0,0 +1,59 @@
+import { CreateApiEventDTO } from '@marxan-api/modules/api-events/dto/create.api-event.dto';
+import {
+  CreateWithEventFactory,
+  EventData,
+  EventFactory,
+  QueueEventsAdapter,
+} from '@marxan-api/modules/queue-api-events';
+import { API_EVENT_KINDS } from '@marxan/api-events';
+import { JobInput } from '@marxan/scenario-cost-surface';
+import { Inject, Injectable } from '@nestjs/common';
+import { surfaceCostEventsFactoryToken } from './surface-cost-queue.provider';
+
+type EventKind = { event: API_EVENT_KINDS };
+
+@Injectable()
+export class SurfaceCostEventsHandler implements EventFactory<JobInput, true> {
+  private queueEvents: QueueEventsAdapter<JobInput, true>;
+
+  private failEventsMapper: EventKind = {
+    event: API_EVENT_KINDS.scenario__costSurface__costUpdateFailed__v1_alpha1,
+  };
+
+  private successEventsMapper: EventKind = {
+    event: API_EVENT_KINDS.scenario__costSurface__finished__v1_alpha1,
+  };
+
+  constructor(
+    @Inject(surfaceCostEventsFactoryToken)
+    queueEventsFactory: CreateWithEventFactory<JobInput, true>,
+  ) {
+    this.queueEvents = queueEventsFactory(this);
+  }
+
+  async createCompletedEvent(
+    eventData: EventData<JobInput, true>,
+  ): Promise<CreateApiEventDTO> {
+    const data = await eventData.data;
+    const kind = this.successEventsMapper.event;
+
+    return {
+      topic: data.scenarioId,
+      kind,
+      data: {},
+    };
+  }
+
+  async createFailedEvent(
+    eventData: EventData<JobInput, true>,
+  ): Promise<CreateApiEventDTO> {
+    const data = await eventData.data;
+    const kind = this.failEventsMapper.event;
+
+    return {
+      topic: data.scenarioId,
+      kind,
+      data: {},
+    };
+  }
+}
