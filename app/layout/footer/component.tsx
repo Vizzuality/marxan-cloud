1729d732ef843828effc6dfe58ff3baf30fc6410
anamontiaga <amontiaga@gmail.com>
2022-02-14 21:07:11 +0100

    feat: new footer

--- app/layout/footer/component.tsx
+++ app/layout/footer/component.tsx
@@ -4,51 +4,82 @@ import Link from 'next/link';
 
 import Wrapper from 'layout/wrapper';
 
+import Button from 'components/button';
+import Icon from 'components/icon';
+
+import TWITTER_FILLED_SVG from 'svgs/social/twitter-filled.svg?sprite';
+
 export interface FooterProps {
 
 }
 
 export const Footer: React.FC<FooterProps> = () => {
   return (
-    <div className="bg-gray-700">
+    <div className="py-24 bg-gray-700">
       <Wrapper>
-        <div className="w-full max-w-5xl py-10 mx-auto md:py-32">
-          <h2
-            className="text-4xl text-center md:text-left md:text-6xl font-heading"
-          >
-            Want to know more
-          </h2>
-          <h2
-            className="text-4xl text-center md:text-left md:text-6xl font-heading"
-          >
-            about Marxan?
-          </h2>
-          <div className="flex flex-col items-center mt-10 space-y-8 text-2xl md:space-y-0 md:space-x-8 font-heading md:flex-row md:mt-16">
-            <span>Explore more:</span>
-            <Link href="/about">
-              <a href="/about" className="text-gray-400 hover:text-gray-200">About</a>
+        <div className="flex justify-between w-full max-w-5xl pb-16 mx-auto space-x-36">
+          <div className="space-y-12">
+            <h2 className="text-2xl font-heading">
+              Reach out with comments
+              or suggestions.
+            </h2>
+
+            <Button
+              href="mailto:marxancloud@gmail.com"
+              size="lg"
+              target="_blank"
+              theme="secondary-alt"
+              type="submit"
+              className="w-full md:w-64"
+            >
+              Contact Us
+            </Button>
+
+          </div>
+
+          <div className="space-y-12">
+            <h2 className="text-2xl font-heading">
+              Join the Google Marxan group discussion forum.
+            </h2>
+            <Button
+              href="https://groups.google.com/g/marxan"
+              className="w-full md:w-64"
+              theme="secondary-alt"
+              size="lg"
+            >
+              MARXAN Google Group
+            </Button>
+          </div>
+        </div>
+        <div className="w-full h-px opacity-20" style={{ background: 'linear-gradient(to right, transparent, #ffffff, transparent)' }} />
+
+        <div className="flex justify-between w-full max-w-5xl pt-12 mx-auto">
+          <div>
+            <p className="text-base">Follow us on twitter and join in the conversation.</p>
+            <div className="flex items-center">
+              <Icon icon={TWITTER_FILLED_SVG} className="w-5 h-5 mr-2.5 text-primary-500" />
+              <a
+                className="text-primary-500 hover:underline"
+                type="button"
+                role="button"
+                href="https://twitter.com/marxan_planning?"
+                rel="noreferrer"
+                target="_blank"
+              >
+                @Marxan_Planning
+              </a>
+            </div>
+          </div>
+          <div className="flex items-center justify-center space-x-8 md:justify-start">
+            <Link href="/terms-of-use">
+              <a href="/terms-of-use" className="text-white hover:underline">Terms of use</a>
             </Link>
-            <Link href="/community">
-              <a href="/community" className="text-gray-400 hover:text-gray-200">Community</a>
+            <Link href="/privacy-policy">
+              <a href="/privacy-policy" className="text-white hover:underline">Privacy policy</a>
             </Link>
           </div>
         </div>
       </Wrapper>
-
-      {/* <Wrapper>
-        <div className="py-8 border-t-2 border-gray-600 border-opacity-25">
-          <div className="w-full max-w-5xl mx-auto">
-            <div className="flex items-center justify-center space-x-8 md:justify-start">
-              <Link href="/terms-of-use">
-                <a href="/terms-of-use" className="text-white hover:underline">Terms of use</a>
-              </Link>
-              <Link href="/privacy-policy">
-                <a href="/privacy-policy" className="text-white hover:underline">Privacy policy</a>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </Wrapper> */}
     </div>
   );
 };
