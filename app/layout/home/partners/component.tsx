37cef9932228bc003df7bfc96f2c0350cf19f3bf
anamontiaga <amontiaga@gmail.com>
2022-02-15 15:04:45 +0100

    styles: partners responsive

--- app/layout/home/partners/component.tsx
+++ app/layout/home/partners/component.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Wrapper from 'layout/wrapper';
+
 import Carousel from 'components/carousel';
 
 import { PARTNER_LOGOS } from './constants';
@@ -10,38 +12,41 @@ export interface PartnersListProps {
 
 export const PartnersList: React.FC<PartnersListProps> = () => {
   return (
-    <div className="relative bg-primary-50">
-      <div className="top-0 w-full h-px max-w-5xl mx-auto opacity-20" style={{ background: 'linear-gradient(to right, transparent, #000000, transparent)' }} />
+    <div className="relative border-t border-white bg-primary-50">
+      <Wrapper>
 
-      <div className="max-w-5xl pt-24 pb-6 mx-auto">
-        <Carousel
-          slides={PARTNER_LOGOS.map((pl) => {
-            const { id, title, logos } = pl;
-            return {
-              id,
-              content: (
-                <>
-                  <p className="text-2xl text-black font-heading">
-                    {title}
-                  </p>
-                  <div className="grid w-full grid-cols-4 py-14 gap-x-20">
-                    {logos.map((logo) => {
-                      const {
-                        id: logoId, alt, hyperlink, src,
-                      } = logo;
-                      return (
-                        <a href={hyperlink} key={logoId} rel="noreferrer" target="_blank" className="place-self-center">
-                          <img className="max-h-12" alt={alt} src={src} height="auto" width="auto" />
-                        </a>
-                      );
-                    })}
-                  </div>
-                </>
-              ),
-            };
-          })}
-        />
-      </div>
+        <div className="top-0 w-full h-px max-w-5xl mx-auto opacity-20" style={{ background: 'linear-gradient(to right, transparent, #000000, transparent)' }} />
+
+        <div className="max-w-5xl pt-24 pb-6 mx-auto">
+          <Carousel
+            slides={PARTNER_LOGOS.map((pl) => {
+              const { id, title, logos } = pl;
+              return {
+                id,
+                content: (
+                  <>
+                    <p className="text-lg text-black md:text-2xl font-heading">
+                      {title}
+                    </p>
+                    <div className="grid w-full grid-cols-1 md:grid-cols-4 py-14 md:gap-y-0 gap-y-36 md:gap-x-20">
+                      {logos.map((logo) => {
+                        const {
+                          id: logoId, alt, hyperlink, src,
+                        } = logo;
+                        return (
+                          <a href={hyperlink} key={logoId} rel="noreferrer" target="_blank" className="w-4/5 md:w-full">
+                            <img className="md:max-h-12" alt={alt} src={src} height="auto" width="auto" />
+                          </a>
+                        );
+                      })}
+                    </div>
+                  </>
+                ),
+              };
+            })}
+          />
+        </div>
+      </Wrapper>
 
     </div>
   );
