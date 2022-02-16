7f502fa71f9a689a4c2f5561657ac3d11dfd854a
Daute Rodríguez Rodríguez <daute@acidtango.com>
2022-02-16 12:16:00 +0000

    fix: resourceId for newly created imports is randomly generated

--- api/libs/utils/src/zip-file-extractor.ts
+++ api/libs/utils/src/zip-file-extractor.ts
@@ -6,11 +6,11 @@ export const extractFileFailed = Symbol('Extract file failed');
 
 export async function extractFile(
   readable: Readable,
-  fileName: RegExp,
+  fileRelativePath: string,
 ): Promise<Either<typeof extractFileFailed, string>> {
   return new Promise<Either<typeof extractFileFailed, string>>((resolve) => {
     readable
-      .pipe(unzipper.ParseOne(fileName))
+      .pipe(unzipper.ParseOne(new RegExp(fileRelativePath)))
       .on('entry', async (entry: unzipper.Entry) => {
         const buffer = await entry.buffer();
         resolve(right(buffer.toString()));
