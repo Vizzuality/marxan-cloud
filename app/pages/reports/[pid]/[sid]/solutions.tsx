1a004836e6dc33d4fe1ce754cbf4c179e7a0afcd
anamontiaga <amontiaga@gmail.com>
2022-02-17 00:15:40 +0100

    feat: add report header

--- app/pages/reports/[pid]/[sid]/solutions.tsx
+++ app/pages/reports/[pid]/[sid]/solutions.tsx
@@ -4,8 +4,6 @@ import { useRouter } from 'next/router';
 
 import { withProtection, withUser } from 'hoc/auth';
 
-import { format } from 'date-fns';
-
 import { useProjectUsers } from 'hooks/project-users';
 import { useProject } from 'hooks/projects';
 import { useScenarioPU, useScenario } from 'hooks/scenarios';
@@ -13,9 +11,19 @@ import { useWDPACategories } from 'hooks/wdpa';
 
 import Head from 'layout/head';
 import MetaIcons from 'layout/meta-icons';
+import ReportHeader from 'layout/scenarios/reports/header';
 import ScenarioReportsMap from 'layout/scenarios/reports/map';
 
 export const getServerSideProps = withProtection(withUser());
+
+const styles = {
+  page: {
+    minHeight: '210mm',
+    margin: '10mm auto',
+    padding: '33px 51px',
+    width: '297mm',
+  },
+};
 
 const SolutionsReport: React.FC = () => {
   const { query } = useRouter();
@@ -37,8 +45,6 @@ const SolutionsReport: React.FC = () => {
   } = useProjectUsers(pid);
 
   const contributors = projectUsers?.map((u) => u.user.displayName);
-
-  const projectOwner = projectUsers?.find((u) => u.roleName === 'project_owner').user.displayName;
 
   const {
     data: PUData,
@@ -71,84 +77,100 @@ const SolutionsReport: React.FC = () => {
 
       <MetaIcons />
 
-      <main
-        style={{
-          minHeight: '210mm', margin: '10mm auto', padding: '33px 51px', width: '297mm',
-        }}
+      <div
+        style={styles.page}
         className="flex flex-col h-full text-black bg-white"
       >
+        <ReportHeader page={1} totalPages={4} />
+
         {reportDataIsFetched && (
-          <>
-            <header className="w-full mb-12">
-              <div className="flex justify-between">
-                <div className="flex space-x-1 text-xs">
-                  <p className="font-semibold uppercase">
-                    Created by:
-                  </p>
-                  <p className="capitalize">{projectOwner}</p>
-                </div>
+          <div className="flex">
 
-                <p className="text-xs font-semibold ">
-                  Page 1/4
-                </p>
+            <section className="w-1/3 pt-6 space-y-8 text-xs">
+              <div>
+                <p className="font-semibold">Contributors</p>
+                <p>{contributors.join(', ')}</p>
               </div>
 
-              <h1 className="pb-6 text-2xl text-gray-500 font-heading">
-                {`${projectData.name}-${scenarioData.name}`}
-              </h1>
-
-              <div className="flex space-x-12 text-xxs">
-                <div className="flex space-x-1">
-                  <p className="font-semibold">Marxan platform version:</p>
-                  <p> V.0.0.1</p>
-                </div>
-                <div className="flex space-x-1">
-                  <p className="font-semibold">Date:</p>
-                  <p>{format(new Date().getTime(), 'MM/dd/yyyy')}</p>
-                </div>
+              <div>
+                <p className="font-semibold"> Features meeting targets:</p>
               </div>
-            </header>
 
-            <div className="flex">
+              <div>
+                <p className="font-semibold">Cost surface:</p>
+              </div>
 
-              <section className="w-1/3 pt-6 space-y-8 text-xs">
-                <div>
-                  <p className="font-semibold">Contributors</p>
-                  <p>{contributors.join(', ')}</p>
-                </div>
+              <div>
+                <p className="font-semibold">BLM</p>
+                <p>{scenarioData.metadata.marxanInputParameterFile.BLM || null}</p>
+              </div>
 
-                <div>
-                  <p className="font-semibold"> Features meeting targets:</p>
-                </div>
+              <div>
+                <p className="font-semibold">Protected Areas:</p>
+                <p>{protectedAreas.join(', ')}</p>
+              </div>
 
-                <div>
-                  <p className="font-semibold">Cost surface:</p>
-                </div>
+              <div>
+                <p className="font-semibold">No. of planning units</p>
+                <p>{`In: ${PUData.included.length || 0}`}</p>
+                <p>{`Out: ${PUData.excluded.length || 0}`}</p>
+              </div>
+            </section>
 
-                <div>
-                  <p className="font-semibold">BLM</p>
-                  <p>{scenarioData.metadata.marxanInputParameterFile.BLM || null}</p>
-                </div>
-
-                <div>
-                  <p className="font-semibold">Protected Areas:</p>
-                  <p>{protectedAreas.join(', ')}</p>
-                </div>
-
-                <div>
-                  <p className="font-semibold">No. of planning units</p>
-                  <p>{`In: ${PUData.included.length || 0}`}</p>
-                  <p>{`Out: ${PUData.excluded.length || 0}`}</p>
-                </div>
-              </section>
-
-              <section className="w-2/3" style={{ height: '552px' }}>
-                <ScenarioReportsMap />
-              </section>
-            </div>
-          </>
+            <section className="w-2/3" style={{ height: '552px' }}>
+              <ScenarioReportsMap />
+            </section>
+          </div>
         )}
-      </main>
+
+      </div>
+      <div
+        style={styles.page}
+        className="flex flex-col h-full text-black bg-white"
+      >
+        <ReportHeader page={2} totalPages={4} />
+
+        {reportDataIsFetched && (
+          <div className="flex">
+
+            <section className="w-1/3 pt-6 space-y-8 text-xs">
+              <div>
+                <p className="font-semibold">Contributors</p>
+                <p>{contributors.join(', ')}</p>
+              </div>
+
+              <div>
+                <p className="font-semibold"> Features meeting targets:</p>
+              </div>
+
+              <div>
+                <p className="font-semibold">Cost surface:</p>
+              </div>
+
+              <div>
+                <p className="font-semibold">BLM</p>
+                <p>{scenarioData.metadata.marxanInputParameterFile.BLM || null}</p>
+              </div>
+
+              <div>
+                <p className="font-semibold">Protected Areas:</p>
+                <p>{protectedAreas.join(', ')}</p>
+              </div>
+
+              <div>
+                <p className="font-semibold">No. of planning units</p>
+                <p>{`In: ${PUData.included.length || 0}`}</p>
+                <p>{`Out: ${PUData.excluded.length || 0}`}</p>
+              </div>
+            </section>
+
+            <section className="w-2/3" style={{ height: '552px' }}>
+              <ScenarioReportsMap />
+            </section>
+          </div>
+        )}
+
+      </div>
     </>
   );
 };
