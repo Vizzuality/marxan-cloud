597aea8a2a6d1f02ccc0ffbfb32e8a16f359ed4a
andrea rota <andrea.rota@vizzuality.com>
2022-02-18 17:07:04 +0000

    remove obsolete services from compose config

--- airflow/dags/exampleDag.py
+++
@@ -1,5 +0,0 @@
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
-with DAG('example_dag', start_date=datetime(2016, 1, 1)) as dag:
-    op = DummyOperator(task_id='op')
