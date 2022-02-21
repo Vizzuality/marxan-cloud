9087aa06c3786c5c4976fab089ba47401b31e3a2
andrea rota <andrea.rota@vizzuality.com>
2022-02-21 13:36:11 +0000

    unify PU geom enum step 4/5
    Sync enum name from api to geoprocessing.
    
    The geoprocessing "copy" of the enum was added later, with a different
    name. The migration in this commit syncs the enum names between the two
    services, picking the api side.

--- api/apps/geoprocessing/src/modules/planning-area/planning-units-grid/planning-units-grid.processor.ts
+++ api/apps/geoprocessing/src/modules/planning-area/planning-units-grid/planning-units-grid.processor.ts
@@ -38,12 +38,12 @@ export class PlanningUnitsGridProcessor {
             SELECT ST_SetSRID(
                      ST_GeomFromGeoJSON(features ->> 'geometry'),
                      4326)::geometry,
-                   $2::shape_type,
+                   $2::planning_unit_grid_shape,
                    $3
             FROM (
                    SELECT json_array_elements($1::json -> 'features') AS features
                  ) AS f
-            ON CONFLICT (the_geom_hash, type, COALESCE(project_id, '00000000-0000-0000-0000-000000000000')) DO UPDATE SET type = 'from_shapefile'::shape_type
+            ON CONFLICT (the_geom_hash, type, COALESCE(project_id, '00000000-0000-0000-0000-000000000000')) DO UPDATE SET type = 'from_shapefile'::planning_unit_grid_shape
             RETURNING "id", "project_id"
           `,
           [geoJson, PlanningUnitGridShape.FromShapefile, fakeProjectId],
