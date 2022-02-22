935ec7def2f2039c870cbce1d4d14abe16f06a76
anamontiaga <amontiaga@gmail.com>
2022-02-22 18:26:42 +0100

    fix: features  help beacon

--- app/layout/scenarios/edit/features/set-up/add/component.tsx
+++ app/layout/scenarios/edit/features/set-up/add/component.tsx
@@ -10,7 +10,6 @@ import { ScenarioSidebarTabs } from 'utils/tabs';
 import { useSelectedFeatures } from 'hooks/features';
 import { useScenario } from 'hooks/scenarios';
 
-import HelpBeacon from 'layout/help/beacon';
 import AddFeaturesModal from 'layout/scenarios/edit/features/set-up/add/add-modal';
 import ListFeatures from 'layout/scenarios/edit/features/set-up/add/list';
 
@@ -42,95 +41,59 @@ export const ScenariosSidebarEditFeatures: React.FC<ScenariosSidebarEditFeatures
 
   return (
     <div className="flex flex-col flex-grow w-full h-full overflow-hidden">
-      <HelpBeacon
-        id="scenarios-features"
-        title="Features"
-        subtitle="Manage features"
-        content={(
-          <div className="space-y-2">
-            <p>
-              Features are everything you want to include in
-              your conservation or land/sea use plan such as
-              species ranges, habitats or ecoregions.
-            </p>
-            <p>
-              The first step requires adding features.
-              There are public features available for use that
-              you can access directly
-              or you can upload your private features as a shapefile.
-            </p>
-            <p>
-              The second step of this section requires setting a
-              conservation target and feature penalty factor for
-              each feature.
-            </p>
-            <p>
-              <i>
-                Note on privacy: The features you upload will only
-                be accessible inside your project to you and your
-                contributors. They will not be shared with other users.
-              </i>
-            </p>
-
-          </div>
-        )}
-        modifiers={['flip']}
-        tooltipPlacement="left"
+      <motion.div
+        key="features"
+        className="flex flex-col min-h-0 overflow-hidden"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
       >
-        <motion.div
-          key="features"
-          className="flex flex-col min-h-0 overflow-hidden"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-        >
 
-          <header className="flex items-start justify-between flex-shrink-0">
-            <div>
-              <div className="flex items-baseline space-x-4">
-                <h2 className="text-lg font-medium font-heading">Features</h2>
-                <InfoButton>
-                  <div>
-                    <h4 className="font-heading text-lg mb-2.5">What are features?</h4>
-                    <div className="space-y-2">
-                      <p>
-                        Features are the important habitats, species, processes,
-                        activities, and discrete areas that you want to consider
-                        in your planning process. Common feature data formats are
-                        range maps, polygons, abundances, and continuous scale or
-                        probability of occurrence maps (e.g. 0-1). Features can
-                        include more than just ecological data but also be cultural
-                        and socio-economic areas like community fishing grounds
-                        or traditional-use areas, and other human activities and
-                        industries. Every feature must have a minimum target
-                        amount set.
-                        Some examples include:
-                      </p>
-                      <img src={FEATURE_SPECIES_IMG} alt="Feature-Range" />
-                      <img src={FEATURE_ABUND_IMG} alt="Feature-Abundance" />
-                      <img src={FEATURE_SOCIAL_IMG} alt="Feature-Social" />
-                    </div>
+        <header className="flex items-start justify-between flex-shrink-0">
+          <div>
+            <div className="flex items-baseline space-x-4">
+              <h2 className="text-lg font-medium font-heading">Features</h2>
+              <InfoButton>
+                <div>
+                  <h4 className="font-heading text-lg mb-2.5">What are features?</h4>
+                  <div className="space-y-2">
+                    <p>
+                      Features are the important habitats, species, processes,
+                      activities, and discrete areas that you want to consider
+                      in your planning process. Common feature data formats are
+                      range maps, polygons, abundances, and continuous scale or
+                      probability of occurrence maps (e.g. 0-1). Features can
+                      include more than just ecological data but also be cultural
+                      and socio-economic areas like community fishing grounds
+                      or traditional-use areas, and other human activities and
+                      industries. Every feature must have a minimum target
+                      amount set.
+                      Some examples include:
+                    </p>
+                    <img src={FEATURE_SPECIES_IMG} alt="Feature-Range" />
+                    <img src={FEATURE_ABUND_IMG} alt="Feature-Abundance" />
+                    <img src={FEATURE_SOCIAL_IMG} alt="Feature-Social" />
                   </div>
-                </InfoButton>
-              </div>
-
-              <div className="flex items-center mt-2 space-x-2">
-                <Icon icon={FEATURES_SVG} className="w-4 h-4 text-gray-400" />
-                <div className="text-xs uppercase font-heading">
-                  Features added:
-                  {' '}
-                  {selectedFeaturesData && <span className="ml-1 text-gray-400">{selectedFeaturesData.length}</span>}
                 </div>
-              </div>
+              </InfoButton>
             </div>
 
-            <AddFeaturesModal />
+            <div className="flex items-center mt-2 space-x-2">
+              <Icon icon={FEATURES_SVG} className="w-4 h-4 text-gray-400" />
+              <div className="text-xs uppercase font-heading">
+                Features added:
+                {' '}
+                {selectedFeaturesData && <span className="ml-1 text-gray-400">{selectedFeaturesData.length}</span>}
+              </div>
+            </div>
+          </div>
 
-          </header>
+          <AddFeaturesModal />
 
-          <ListFeatures />
+        </header>
 
-        </motion.div>
-      </HelpBeacon>
+        <ListFeatures />
+
+      </motion.div>
     </div>
   );
 };
