af47aea88cd3149d05a338ea3b1f24e3eb8ca8ac
Tiago Garcia <tiagojsag@gmail.com>
2022-02-22 14:27:49 +0100

    Additional node pools for k8s cluster

---
+++ infrastructure/modules/node_pool/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_kubernetes_cluster_node_pool" "node_pool" {
+  name                  = var.name
+  kubernetes_cluster_id = var.aks_cluster_id
+  vm_size               = var.vm_size
+  node_count            = var.node_count
+  enable_auto_scaling   = var.enable_auto_scaling
+  min_count             = var.min_node_count
+  max_count             = var.max_node_count
+
+  node_labels = var.node_labels
+}
