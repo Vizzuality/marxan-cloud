d7dfde9e5aa786dff88df5810597d72e0a98ed21
anamontiaga <amontiaga@gmail.com>
2022-02-23 18:35:33 +0100

    fix: remove refetch all features on list

--- app/layout/scenarios/edit/features/set-up/add/add-modal/uploader/component.tsx
+++ app/layout/scenarios/edit/features/set-up/add/add-modal/uploader/component.tsx
@@ -5,11 +5,8 @@ import React, {
 import { FEATURES_UPLOADER_MAX_SIZE } from 'constants/file-uploader-size-limits';
 import { useDropzone } from 'react-dropzone';
 import { Form as FormRFF, Field as FieldRFF } from 'react-final-form';
-import { useDispatch } from 'react-redux';
 
 import { useRouter } from 'next/router';
-
-import { getScenarioEditSlice } from 'store/slices/scenarios/edit';
 
 import cx from 'classnames';
 import { motion } from 'framer-motion';
@@ -42,13 +39,9 @@ export const ScenariosFeaturesAddUploader: React.FC<ScenariosFeaturesAddUploader
   const [successFile, setSuccessFile] = useState(null);
 
   const { query } = useRouter();
-  const { pid, sid } = query;
+  const { pid } = query;
 
   const { addToast } = useToasts();
-
-  const dispatch = useDispatch();
-  const scenarioSlice = getScenarioEditSlice(sid);
-  const { setCache } = scenarioSlice.actions;
 
   const uploadFeaturesShapefileMutation = useUploadFeaturesShapefile({
     requestConfig: {
@@ -116,7 +109,6 @@ export const ScenariosFeaturesAddUploader: React.FC<ScenariosFeaturesAddUploader
       onSuccess: ({ data: { data: g, id: shapefileId } }) => {
         setLoading(false);
         setSuccessFile({ ...successFile });
-        dispatch(setCache(Date.now()));
         onClose();
         addToast('success-upload-feature-shapefile', (
           <>
@@ -155,8 +147,6 @@ export const ScenariosFeaturesAddUploader: React.FC<ScenariosFeaturesAddUploader
     onClose,
     uploadFeaturesShapefileMutation,
     successFile,
-    dispatch,
-    setCache,
   ]);
 
   const {
