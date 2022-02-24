2a4452018011050114ecf6a0ab49b50ec4a65eeb
andrea rota <andrea.rota@vizzuality.com>
2022-02-24 11:59:37 +0000

    fire scenario.planningAreaProtectedCalculation.submitted much earlier
    We did originally fire the event below in
    `CalculatePlanningUnitsProtectionLevelHandler.execute()`, however this
    would be *after* some potentially expensive SQL queries had been run in
    `ScenarioPlanningUnitsProtectedStatusCalculatorService.calculatedProtectionStatusForPlanningUnitsIn()`,
    which would lead is some cases to a very long delay between the time an
    API user submits a request for `POST
    /api/scenarios/:id/protected-areas` and the time a
    `scenario.planningAreaProtectedCalculation.submitted/v1/alpha` event
    shows up in the scenario status data.
    
    However, as we're moving the firing of this event across a couple of
    cqrs event boundaries, I am less confident that we would consistently
    fire a `finished` or `failed` event paired with this `submitted` one in
    case of any unexpected errors. So, long story short, if something
    starts breaking horribly in how we deal with these
    `planningAreaProtectedCalculation` events, we should reassess this:
    probably keeping the event firing here, but adding more appropriately
    robust error handling, if needed.

--- api/apps/api/src/modules/scenarios/protected-area/selection/selection-update.service.ts
+++ api/apps/api/src/modules/scenarios/protected-area/selection/selection-update.service.ts
@@ -2,7 +2,7 @@ import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EventBus } from '@nestjs/cqrs';
-import { Either, right } from 'fp-ts/Either';
+import { Either, left, right } from 'fp-ts/Either';
 
 import { ProjectSnapshot } from '@marxan/projects';
 
@@ -13,6 +13,8 @@ import { SelectProtectedArea } from '../select-protected-area';
 import { ProtectedAreaKind } from '../protected-area.kind';
 import { ProtectedAreaUnlinked } from '../protected-area-unlinked';
 import { SelectionChangedEvent } from './selection-changed.event';
+import { API_EVENT_KINDS } from '@marxan/api-events';
+import { ApiEventsService } from '@marxan-api/modules/api-events';
 
 export const invalidProtectedAreaId = Symbol(`invalid protected area id`);
 export type ChangeProtectedAreasError = typeof invalidProtectedAreaId;
@@ -24,6 +26,7 @@ export class SelectionUpdateService {
     @InjectRepository(Scenario)
     protected readonly scenarios: Repository<Scenario>,
     private readonly events: EventBus,
+    private readonly apiEvents: ApiEventsService,
   ) {}
 
   async selectFor(
@@ -96,6 +99,36 @@ export class SelectionUpdateService {
     );
 
     if (idsToAdd.length > 0 || idsToRemove.length > 0) {
+      /**
+       * We did originally fire the event below in
+       * `CalculatePlanningUnitsProtectionLevelHandler.execute()`, however this
+       * would be *after* some potentially expensive SQL queries had been run in
+       * `ScenarioPlanningUnitsProtectedStatusCalculatorService.calculatedProtectionStatusForPlanningUnitsIn()`,
+       * which would lead is some cases to a very long delay between the time an
+       * API user submits a request for `POST
+       * /api/scenarios/:id/protected-areas` and the time a
+       * `scenario.planningAreaProtectedCalculation.submitted/v1/alpha` event
+       * shows up in the scenario status data.
+       *
+       * However, as we're moving the firing of this event across a couple of
+       * cqrs event boundaries, I am less confident that we would consistently
+       * fire a `finished` or `failed` event paired with this `submitted` one in
+       * case of any unexpected errors. So, long story short, if something
+       * starts breaking horribly in how we deal with these
+       * `planningAreaProtectedCalculation` events, we should reassess this:
+       * probably keeping the event firing here, but adding more appropriately
+       * robust error handling, if needed.
+       */
+      try {
+        await this.apiEvents.create({
+          kind:
+            API_EVENT_KINDS.scenario__planningAreaProtectedCalculation__submitted__v1__alpha1,
+          topic: scenario.id,
+        });
+      } catch (error: unknown) {
+        return left(invalidProtectedAreaId);
+      }
+
       this.events.publish(
         new SelectionChangedEvent(scenario.id, scenario.threshold, idsToAdd),
       );
