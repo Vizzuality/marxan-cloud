4d5b9e6b09af973b23a36bda9ad1a0ba1ea5457d
Daute Rodríguez Rodríguez <daute@acidtango.com>
2022-02-24 10:24:02 +0000

    fix: zip-file-extractor

--- api/libs/utils/src/zip-file-extractor.ts
+++ api/libs/utils/src/zip-file-extractor.ts
@@ -3,25 +3,28 @@ import { Readable } from 'stream';
 import { Parse, Entry } from 'unzipper';
 
 export const extractFileFailed = Symbol('Extract file failed');
+export const fileNotFound = Symbol('File not found');
 
 export async function extractFile(
   readable: Readable,
   fileRelativePath: string,
-): Promise<Either<typeof extractFileFailed, string>> {
-  return new Promise<Either<typeof extractFileFailed, string>>((resolve) => {
-    readable
-      .pipe(Parse())
-      .on('entry', async (entry: Entry) => {
-        if (entry.path !== fileRelativePath) {
-          entry.autodrain();
-          return;
-        }
+): Promise<Either<typeof extractFileFailed | typeof fileNotFound, string>> {
+  return new Promise<
+    Either<typeof extractFileFailed | typeof fileNotFound, string>
+  >(async (resolve) => {
+    const zip = readable.pipe(Parse({ forceStream: true })).on('error', () => {
+      resolve(left(extractFileFailed));
+    });
 
-        const buffer = await entry.buffer();
-        resolve(right(buffer.toString()));
-      })
-      .on('error', () => {
-        resolve(left(extractFileFailed));
-      });
+    for await (const entry of zip) {
+      if (entry.path !== fileRelativePath) {
+        entry.autodrain();
+        continue;
+      }
+      const buffer = await entry.buffer();
+      resolve(right(buffer.toString()));
+    }
+
+    resolve(left(fileNotFound));
   });
 }
