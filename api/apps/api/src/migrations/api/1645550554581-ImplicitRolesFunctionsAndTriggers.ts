a1aba2ef8ccc7305288bc59d78408b991b64e7ef
andrea rota <andrea.rota@vizzuality.com>
2022-03-01 12:01:59 +0000

    working wip of implicit scenario roles

--- api/apps/api/src/migrations/api/1645550554581-ImplicitRolesFunctionsAndTriggers.ts
+++ api/apps/api/src/migrations/api/1645550554581-ImplicitRolesFunctionsAndTriggers.ts
@@ -25,51 +25,53 @@ export class ImplicitRolesFunctionsAndTriggers1645550554581
           E as (
             SELECT scenario_id FROM users_scenarios us WHERE us.user_id = $1 AND is_implicit IS false
           ),
-          I as (
-            SELECT * FROM P EXCEPT SELECT * FROM E
-          ),
           C as (
             SELECT scenario_id FROM users_scenarios us WHERE us.user_id = $1 AND is_implicit IS true
           ),
-          -- @todo: this will need to be joined with the user role on each of the scenarios' parent project
           G as (
-            SELECT * FROM I EXCEPT SELECT * FROM C
+            SELECT * FROM P EXCEPT SELECT * FROM E
           ),
-		      J as (
-			      SELECT s.id, up.role_id as role_id FROM G * LEFT OUTER JOIN scenarios s ON G.id = s.id JOIN projects pro ON s.id = pro.id JOIN users_projects up ON up.project_id = pro.id AND up.user_id = $1
+		      GR as (
+			      SELECT s.id, up.role_id as role_id FROM G
+              LEFT OUTER JOIN scenarios s ON G.id = s.id
+              JOIN projects pro ON s.project_id = pro.id
+              JOIN users_projects up ON up.project_id = pro.id AND up.user_id = $1
           ),
           R as (
-            SELECT * FROM C EXCEPT SELECT * FROM I
+            SELECT * FROM C EXCEPT SELECT * FROM G
           )
-        SELECT array((SELECT (id, role_id)::role_on_entity FROM J)) AS grant_on, array((SELECT * FROM R)) AS revoke_on;
+        SELECT array((SELECT (id, regexp_replace(role_id, '^project_', 'scenario_'))::role_on_entity FROM GR)) AS grant_on, array((SELECT * FROM R)) AS revoke_on;
         $$ LANGUAGE 'sql';
     `);
 
     await queryRunner.query(`
-      CREATE OR REPLACE FUNCTION apply_scenario_acl_diff_for_user(_user_id uuid, grant_on role_on_entity[], revoke_on (uuid[] ) RETURNS void as $$
-      DECLARE
-        grant_on_scenario_id role_on_entity;
-        revoke_on_scenario_id uuid;
-      BEGIN
-        FOREACH grant_on_scenario IN array grant_on
-        LOOP
-          RAISE NOTICE 'Granting implicit % role on scenario % to user %', grant_on_scenario.role_id, grant_on_scenario.scenario_id, _user_id;
+    CREATE OR REPLACE FUNCTION apply_scenario_acl_diff_for_user(_user_id uuid, grant_on role_on_entity[], revoke_on uuid[]) RETURNS void as $$
+    DECLARE
+      grant_on_scenario role_on_entity;
+      revoke_on_scenario uuid;
+    BEGIN
+      FOREACH grant_on_scenario IN array grant_on
+      LOOP
+        RAISE NOTICE 'Granting implicit % role on scenario % to user %', grant_on_scenario.role_id, grant_on_scenario.entity_id, _user_id;
 
-          INSERT INTO users_scenarios
-            (user_id, scenario_id, role_id, is_implicit)
-            VALUES
-            (_user_id, grant_on_scenario.scenario_id, grant_on_scenario.role_id, true);
-        END LOOP;
+        -- first delete, then redo - this is so that we can easily reflect a
+        -- change on user role on the parent project
+        DELETE FROM users_scenarios WHERE user_id = _user_id AND scenario_id = grant_on_scenario.entity_id AND is_implicit IS true;
+        INSERT INTO users_scenarios
+          (user_id, scenario_id, role_id, is_implicit)
+          VALUES
+          (_user_id, grant_on_scenario.entity_id, grant_on_scenario.role_id, true);
+      END LOOP;
 
-        FOREACH revoke_on_scenario_id IN array revoke_on
-        LOOP
-          RAISE NOTICE 'Revoking implicit role on scenario % from user %', revoke_on_scenario_id, _user_id;
+      FOREACH revoke_on_scenario IN array revoke_on
+      LOOP
+        RAISE NOTICE 'Revoking implicit role on scenario % from user %', revoke_on_scenario, _user_id;
 
-          DELETE FROM users_scenarios us
-            WHERE us.user_id = _user_id AND scenario_id = revoke_on_scenario_id;
-        END LOOP;
-      END;
-      $$ LANGUAGE 'plpgsql';
+        DELETE FROM users_scenarios us
+          WHERE us.user_id = _user_id AND scenario_id = revoke_on_scenario;
+      END LOOP;
+    END;
+    $$ LANGUAGE 'plpgsql';
     `);
 
     await queryRunner.query(`
@@ -78,30 +80,24 @@ export class ImplicitRolesFunctionsAndTriggers1645550554581
       $$
       DECLARE
         scenario_id uuid;
+        user_id uuid;
         is_implicit boolean;
-        role_name varchar;
       BEGIN
-        -- define constants
-        -- set is_implicit to false as default
-        is_implicit := false;
-
-        -- set user_id and is_implicit from row being created or deleted,
+        -- set user_id from row being created or deleted,
         -- as applicable
-        IF TG_OP = 'INSERT' THEN
+        IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
           user_id := NEW.user_id;
-          IF TG_TABLE_NAME = 'users_projects' THEN
-            is_implicit := NEW.is_implicit;
-          END IF;
         ELSIF TG_OP = 'DELETE' THEN
             user_id := OLD.user_id;
-            IF TG_TABLE_NAME = 'users_projects' THEN
-              is_implicit := OLD.is_implicit;
-            END IF;
         END IF;
 
-        PERFORM apply_scenario_acl_diff_for_user(user_id, (select grant_on from compute_implicit_scenario_roles_for_user(user_id)));
+        PERFORM apply_scenario_acl_diff_for_user(
+          user_id,
+          (select grant_on from compute_implicit_scenario_roles_for_user(user_id)),
+          (select revoke_on from compute_implicit_scenario_roles_for_user(user_id))
+        );
 
-        IF TG_OP = 'INSERT' THEN
+        IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
           RETURN NEW;
         END IF;
 
@@ -137,12 +133,7 @@ export class ImplicitRolesFunctionsAndTriggers1645550554581
     await queryRunner.query(`
       -- triggers for implicit scenario roles
       CREATE OR REPLACE TRIGGER compute_implicit_scenario_roles_for_projects
-      AFTER INSERT OR DELETE ON users_projects
-      FOR EACH ROW
-      EXECUTE PROCEDURE manage_implicit_scenario_roles();
-
-      CREATE OR REPLACE TRIGGER compute_implicit_scenario_roles_for_scenarios
-      AFTER DELETE ON users_scenarios
+      AFTER INSERT OR UPDATE OR DELETE ON users_projects
       FOR EACH ROW
       EXECUTE PROCEDURE manage_implicit_scenario_roles();
 
