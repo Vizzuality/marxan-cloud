49edd6d9f851c6ca756578de427a81dd8242ce13
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-03-01 12:55:36 +0100

    Remove STORYBOOK_API_URL

--- app/services/roles/index.ts
+++ app/services/roles/index.ts
@@ -5,7 +5,7 @@ import { signOut } from 'next-auth/client';
 const dataFormatter = new Jsona();
 
 const ROLES = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL || process.env.STORYBOOK_API_URL}/api/v1/roles/projects`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/roles/projects`,
   headers: { 'Content-Type': 'application/json' },
   transformResponse: (data) => {
     try {
--- app/services/wdpa/index.ts
+++ app/services/wdpa/index.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
-import Jsona from 'jsona';
-
-const dataFormatter = new Jsona();
 
 const WDPA = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL || process.env.STORYBOOK_API_URL}/api/v1/scenarios`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/scenarios`,
   headers: { 'Content-Type': 'application/json' },
   transformResponse: (data) => {
     try {
