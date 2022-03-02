f386cb674dc1725b6f7cd57bc237335cfd24e74f
Ruben Vallejo <ralexanderv89@gmail.com>
2022-03-02 10:23:36 +0100

    feat: solve missing scenarios after trigger on tests

--- api/apps/api/test/project-scenarios.e2e-spec.ts
+++ api/apps/api/test/project-scenarios.e2e-spec.ts
@@ -51,6 +51,7 @@ describe('ScenariosModule (e2e)', () => {
   });
 
   it('Creating a scenario will fail because the user is a project viewer', async () => {
+    await fixtures.GivenViewerWasAddedToProject();
     const response = await fixtures.WhenCreatingAScenarioAsAProjectViewer();
     fixtures.ThenForbiddenIsReturned(response);
   });
@@ -137,6 +138,7 @@ async function getFixtures() {
   const scenarioViewerRole = ScenarioRoles.scenario_viewer;
 
   const projectContributorRole = ProjectRoles.project_contributor;
+  const projectViewerRole = ProjectRoles.project_viewer;
 
   const { projectId } = await GivenProjectExists(app, ownerToken);
 
@@ -152,6 +154,8 @@ async function getFixtures() {
 
   let scenarioId: string;
   const seedScenarioNames = [
+    'Example scenario 1 Project 1 Org 1',
+    'Example scenario 2 Project 1 Org 1',
     'Example scenario 1 Project 2 Org 2',
     'Example scenario 2 Project 2 Org 2',
   ];
@@ -196,6 +200,13 @@ async function getFixtures() {
         projectId,
         roleName: projectContributorRole,
         userId: contributorUserId,
+      }),
+
+    GivenViewerWasAddedToProject: async () =>
+      await userProjectsRepo.save({
+        projectId,
+        roleName: projectViewerRole,
+        userId: viewerUserId,
       }),
 
     GivenContributorWasAddedToScenario: async () =>
