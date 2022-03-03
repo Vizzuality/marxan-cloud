b81ae2e7c69960f6955431b27edc1ca75d080cd8
Daute Rodríguez Rodríguez <daute@acidtango.com>
2022-03-03 12:40:48 +0000

    feat: setup migration

---
+++ api/apps/geoprocessing/src/migrations/geoprocessing/1646308580694-AddProjectsPuTable.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddProjectsPuTable1646308580694 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+      CREATE TABLE projects_pu(
+        id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        project_id uuid NOT NULL,
+        puid int NOT NULL,
+        geom_id uuid NOT NULL,
+        geom_type planning_unit_grid_shape NOT NULL,
+        CONSTRAINT unique_project_puids UNIQUE (project_id, puid),
+        CONSTRAINT planning_units_geom_fk FOREIGN KEY (geom_id, geom_type) REFERENCES planning_units_geom(id, type) ON DELETE CASCADE
+      );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query('DROP TABLE projects_pu');
+  }
+}
