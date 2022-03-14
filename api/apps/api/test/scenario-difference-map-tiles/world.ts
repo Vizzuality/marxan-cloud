972816cecff100040a2ef17a755ff0c5806ab5d2
Alicia <alicia.arenzana@gmail.com>
2022-03-14 13:08:01 +0100

    update tests

--- api/apps/api/test/scenario-difference-map-tiles/world.ts
+++ api/apps/api/test/scenario-difference-map-tiles/world.ts
@@ -1,166 +1,64 @@
+import { INestApplication } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { INestApplication, Logger } from '@nestjs/common';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import * as request from 'supertest';
+import { v4 } from 'uuid';
 
-import { E2E_CONFIG } from '../e2e.config';
-import { Project } from '@marxan-api/modules/projects/project.api.entity';
-import { gunzip } from 'zlib';
+import { ScenariosPlanningUnitGeoEntity } from '@marxan/scenarios-planning-unit';
+import { DbConnections } from '@marxan-api/ormconfig.connections';
 
-import { GivenUserIsLoggedIn } from '../steps/given-user-is-logged-in';
-import { OrganizationsTestUtils } from '../utils/organizations.test.utils';
-import { PlanningUnitGridShape } from '@marxan/scenarios-planning-unit';
-import { decodeMvt } from '@marxan/utils/geo/decode-mvt';
-import { ProjectsTestUtils } from '../utils/projects.test.utils';
-import { PlanningArea } from '@marxan/planning-area-repository/planning-area.geo.entity';
+import { GivenScenarioPuDataExists } from '../steps/given-scenario-pu-data-exists';
+import { ScenariosTestUtils } from '../utils/scenarios.test.utils';
+import { ScenarioType } from '@marxan-api/modules/scenarios/scenario.api.entity';
+import { GivenProjectExists } from '../steps/given-project';
 
-export const createWorld = async (app: INestApplication) => {
-  const projectsRepo: Repository<Project> = app.get(
-    getRepositoryToken(Project),
-  );
-
-  const jwtToken = await GivenUserIsLoggedIn(app);
-
-  const organization = await OrganizationsTestUtils.createOrganization(
-    app,
-    jwtToken,
-    E2E_CONFIG.organizations.valid.minimal(),
-  );
-
-  const regularProject = await ProjectsTestUtils.createProject(app, jwtToken, {
-    ...E2E_CONFIG.projects.valid.minimalInGivenAdminArea({
-      countryCode: 'NAM',
-      adminAreaLevel1Id: 'NAM.8_1',
-      adminAreaLevel2Id: 'NAM.8.6_1',
-    }),
-    organizationId: organization.data.id,
+export const createWorld = async (app: INestApplication, jwt: string) => {
+  const { cleanup, projectId } = await GivenProjectExists(app, jwt, {
+    countryCode: 'BWA',
+    adminAreaLevel1Id: 'BWA.12_1',
+    adminAreaLevel2Id: 'BWA.12.1_1',
   });
+  const scenariosPuData: Repository<ScenariosPlanningUnitGeoEntity> = await app.get(
+    getRepositoryToken(
+      ScenariosPlanningUnitGeoEntity,
+      DbConnections.geoprocessingDB,
+    ),
+  );
 
-  const customPlanningAreaId = await request(app.getHttpServer())
-    .post(`/api/v1/projects/planning-area/shapefile`)
-    .set('Authorization', `Bearer ${jwtToken}`)
-    .attach(`file`, __dirname + `/test-planning-area.zip`)
-    .then((response) => response.body.id)
-    .catch((error) => {
-      Logger.error(error);
-      throw new Error(`[step] Could not upload planning area`);
-    });
+  const scenarioIdA = (
+    await ScenariosTestUtils.createScenario(app, jwt, {
+      name: `scenario-name-a`,
+      type: ScenarioType.marxan,
+      projectId,
+    })
+  ).data.id;
 
-  const customPlanningAreaGridId = await request(app.getHttpServer())
-    .post(`/api/v1/projects/planning-area/shapefile-grid`)
-    .set('Authorization', `Bearer ${jwtToken}`)
-    .attach(`file`, __dirname + `/test-grid-planning.zip`)
-    .then((response) => response.body.id)
-    .catch((error) => {
-      Logger.error(error);
-      throw new Error(`[step] Could not upload grid`);
-    });
-
-  const customProjectPlanningAreaGridId = await request(app.getHttpServer())
-    .post(`/api/v1/projects/planning-area/shapefile-grid`)
-    .set('Authorization', `Bearer ${jwtToken}`)
-    .attach(`file`, __dirname + `/test-grid-planning.zip`)
-    .then((response) => response.body.id)
-    .catch((error) => {
-      Logger.error(error);
-      throw new Error(`[step] Could not upload grid`);
-    });
-  const customProjectId = await ProjectsTestUtils.createProject(app, jwtToken, {
-    ...E2E_CONFIG.projects.valid.customArea({
-      planningAreaId: customProjectPlanningAreaGridId,
-      planningUnitGridShape: PlanningUnitGridShape.FromShapefile,
-    }),
-    organizationId: organization.data.id,
-  })
-    .then((response) => response.data.id)
-    .catch((error) => {
-      Logger.error(error);
-      throw new Error(`[step] Could not Create Custom area Project`);
-    });
+  const scenarioIdB = (
+    await ScenariosTestUtils.createScenario(app, jwt, {
+      name: `scenario-name-b`,
+      type: ScenarioType.marxan,
+      projectId,
+    })
+  ).data.id;
 
   return {
-    jwtToken,
-    organizationId: organization.data.id,
-    regularProjectId: regularProject.data.id,
-    customPlanningAreaId,
-    customPlanningAreaGridId,
-    customProjectId,
-
-    WhenRequestingTileForCustomArea: async (planningAreaId: string) =>
-      request(app.getHttpServer())
-        .get(
-          `/api/v1/projects/planning-area/${planningAreaId}/preview/tiles/9/189/291.mvt`,
-        )
-        .set('Authorization', `Bearer ${jwtToken}`)
-        .expect(200)
-        .responseType('blob')
-        .buffer()
-        .then((response) => response.body)
-        .catch((error) => {
-          Logger.error(error);
-          throw new Error(`[step] Could not Access tile preview grid tile`);
-        }),
-    WhenRequestingTileForCustomPlanningGrid: async (planningAreaId: string) =>
-      request(app.getHttpServer())
-        .get(
-          `/api/v1/projects/planning-area/${planningAreaId}/grid/preview/tiles/9/189/291.mvt`,
-        )
-        .set('Authorization', `Bearer ${jwtToken}`)
-        .expect(200)
-        .responseType('blob')
-        .buffer()
-        .then((response) => response.body)
-        .catch((error) => {
-          Logger.error(error);
-          throw new Error(`[step] Could not Access tile preview grid tile`);
-        }),
-    WhenRequestingTileForProjectPlanningArea: async (projectId: string) =>
-      request(app.getHttpServer())
-        .get(`/api/v1/projects/${projectId}/planning-area/tiles/9/189/291.mvt`)
-        .set('Authorization', `Bearer ${jwtToken}`)
-        .expect(200)
-        .responseType('blob')
-        .buffer()
-        .then((response) => response.body)
-        .catch((error) => {
-          Logger.error(error);
-          throw new Error(
-            `[step] Could not access project planning area tiles`,
-          );
-        }),
-    WhenRequestingTileForProjectPlanningGrid: async (projectId: string) =>
-      request(app.getHttpServer())
-        .get(`/api/v1/projects/${projectId}/grid/tiles/9/189/291.mvt`)
-        .set('Authorization', `Bearer ${jwtToken}`)
-        .expect(200)
-        .responseType('blob')
-        .buffer()
-        .then((response) => response.body)
-        .catch((error) => {
-          Logger.error(error);
-          throw new Error(`[step] Could not access project grid tiles`);
-        }),
-
-    ThenItContainsPlanningAreaTile: async (mvt: Buffer) => {
-      const tile = decodeMvt(mvt);
-      expect(tile.layers).toBeDefined();
-    },
-    ThenItContainsGridTile: async (mvt: Buffer) => {
-      const tile = decodeMvt(mvt);
-      expect(tile.layers).toBeDefined();
-    },
-    cleanup: async () => {
-      await projectsRepo.delete({
-        organization: {
-          id: organization.data.id,
-        },
-      });
-
-      await OrganizationsTestUtils.deleteOrganization(
+    scenarioId,
+    GivenScenarioPuDataExists: async () =>
+      (await GivenScenarioPuDataExists(scenariosPuData, scenarioIdA)).rows,
+    WhenChangingPlanningUnitInclusivityForRandomPu: async () =>
+      WhenChangingPlanningUnitInclusivity(app, scenarioIdA, jwt, [v4(), v4()]),
+    WhenChangingPlanningUnitInclusivityWithExistingPu: async () =>
+      WhenChangingPlanningUnitInclusivity(
         app,
-        jwtToken,
-        organization.data.id,
-      );
+        scenarioId,
+        jwt,
+        (await GivenScenarioPuDataExists(scenariosPuData, scenarioId)).rows.map(
+          (entity) => entity.id,
+        ),
+      ),
+    cleanup: async () => {
+      await ScenariosTestUtils.deleteScenario(app, jwt, scenarioIdA);
+      await ScenariosTestUtils.deleteScenario(app, jwt, scenarioIdB);
+      await cleanup();
     },
   };
 };
