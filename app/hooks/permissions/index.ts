01d997c7cfb903831be34835ef011d24586b8ce1
anamontiaga <amontiaga@gmail.com>
2022-03-14 13:45:17 +0100

    feat: add permissions on project list

--- app/hooks/permissions/index.ts
+++ app/hooks/permissions/index.ts
@@ -3,6 +3,21 @@ import { useMemo } from 'react';
 import { useMe } from 'hooks/me';
 import { useProjectUsers } from 'hooks/project-users';
 
+export function useProjectRole(projectId) {
+  const { data: me } = useMe();
+  const { data: projectUsers } = useProjectUsers(projectId);
+
+  const meId = me?.data?.id;
+
+  const projectRole = projectUsers?.find((r) => r.user.id === meId)?.roleName;
+
+  return useMemo(() => {
+    return {
+      data: projectRole,
+    };
+  }, [projectRole]);
+}
+
 export function useCanEditProject(projectId) {
   const { data: me } = useMe();
   const { data: projectUsers } = useProjectUsers(projectId);
@@ -19,6 +34,21 @@ export function useCanEditProject(projectId) {
 }
 
 export function useOwnsProject(projectId) {
+  const { data: me } = useMe();
+  const { data: projectUsers } = useProjectUsers(projectId);
+
+  const meId = me?.data?.id;
+
+  const isOwner = projectUsers?.find((r) => r.user.id === meId)?.roleName === 'project_owner';
+
+  return useMemo(() => {
+    return {
+      data: isOwner,
+    };
+  }, [isOwner]);
+}
+
+export function useOwnsScenario(projectId) {
   const { data: me } = useMe();
   const { data: projectUsers } = useProjectUsers(projectId);
 
