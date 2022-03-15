8db5f4d6611c401d756cb4f4dc3ab97774d91eb4
Aaron Perez <aarperper@gmail.com>
2022-03-15 13:36:27 +0000

    refactor: improves auth tests

--- api/apps/api/test/auth.e2e-spec.ts
+++ api/apps/api/test/auth.e2e-spec.ts
@@ -1,37 +1,26 @@
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { E2E_CONFIG } from './e2e.config';
-import { tearDown } from './utils/tear-down';
 import { bootstrapApplication } from './utils/api-application';
-
-afterAll(async () => {
-  await tearDown();
-});
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
-    try {
-      app = await bootstrapApplication();
-    } catch (e) {
-      console.error(e);
-    }
-  });
-
-  afterAll(async () => {
-    await Promise.all([app.close()]);
+  beforeEach(async () => {
+    app = await bootstrapApplication();
   });
 
   describe('Authentication', () => {
     it('Retrieves a JWT token when authenticating with valid credentials', async () => {
-      await request(app.getHttpServer())
+      const response = await request(app.getHttpServer())
         .post('/auth/sign-in')
         .send({
           username: E2E_CONFIG.users.basic.aa.username,
           password: E2E_CONFIG.users.basic.aa.password,
         })
         .expect(201);
+
+      expect(response.body.accessToken).toBeDefined();
     });
 
     it('Fails to authenticate a user with an incorrect password', async () => {
