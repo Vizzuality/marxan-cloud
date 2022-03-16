8139ab67e1b8e38678bd9c8074275e8e51231448
Alicia <alicia.arenzana@gmail.com>
2022-03-16 11:53:25 +0100

    geoprocessing compare tiles

---
+++ api/apps/geoprocessing/src/modules/scenarios/comparison-difference-tile/comparison-difference-tile-request.ts
@@ -0,0 +1,15 @@
+import { IsOptional, IsString } from 'class-validator';
+import { BBox } from 'geojson';
+import { TileRequest } from '@marxan/tiles';
+
+export class ScenarioComparisonTileRequest extends TileRequest {
+
+  @IsString()
+  scenarioIdA!: string;
+
+  @IsString()
+  scenarioIdB!: string;
+
+  @IsOptional()
+  bbox?: BBox;
+}
--- api/libs/tiles/src/index.ts
+++ api/libs/tiles/src/index.ts
@@ -1,2 +1,3 @@
 export { TilesOpenApi } from './tiles-api-doc';
 export { TileRequest } from './tile-request.dto';
+export { BaseTilesOpenApi } from './base-tiles-api-doc';
