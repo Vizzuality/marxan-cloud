9b32aa869f2a6b470588ebdbc902391db18bdb0e
Aaron Perez <aarperper@gmail.com>
2022-03-17 11:19:55 +0000

    refactor: improves api-events-security tests

--- api/apps/api/test/api-events/api-events-security.e2e-spec.ts
+++ api/apps/api/test/api-events/api-events-security.e2e-spec.ts
@@ -1,17 +1,12 @@
 import { INestApplication } from '@nestjs/common';
 import { bootstrapApplication } from '../utils/api-application';
 import * as request from 'supertest';
-import { tearDown } from '../utils/tear-down';
+import { AppConfig } from '@marxan-api/utils/config.utils';
 
 let app: INestApplication;
 
-beforeAll(async () => {
+beforeEach(async () => {
   app = await bootstrapApplication();
-});
-
-afterAll(async () => {
-  await Promise.all([app.close()]);
-  await tearDown();
 });
 
 describe(`when getting events without api-key`, () => {
@@ -25,12 +20,11 @@ describe(`when getting events without api-key`, () => {
 
 describe(`when getting events with valid api-key`, () => {
   it(`should allow the access`, async () => {
-    return request(app.getHttpServer())
+    const secret = AppConfig.get('auth.xApiKey.secret', '');
+
+    await request(app.getHttpServer())
       .get(`/api/v1/api-events`)
-      .set(
-        'X-Api-Key',
-        process.env.API_AUTH_X_API_KEY ?? 'sure it is valid in envs?',
-      )
+      .set('X-Api-Key', secret)
       .expect(200);
   });
 });
