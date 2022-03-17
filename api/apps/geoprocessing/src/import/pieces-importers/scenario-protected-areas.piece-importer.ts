9cbc2765d3e66a6da3280fcf643183b63d0f3048
Daute Rodríguez Rodríguez <daute@acidtango.com>
2022-03-17 15:57:09 +0000

    fix: scenario protected areas piece importer

--- api/apps/geoprocessing/src/import/pieces-importers/scenario-protected-areas.piece-importer.ts
+++ api/apps/geoprocessing/src/import/pieces-importers/scenario-protected-areas.piece-importer.ts
@@ -32,6 +32,62 @@ export class ScenarioProtectedAreasPieceImporter
     return piece === ClonePiece.ScenarioProtectedAreas;
   }
 
+  async getWdpaProtectedAreasIds(wdpa: number[]): Promise<string[]> {
+    if (wdpa.length === 0) return [];
+
+    const wdpaProtectedAreas: {
+      id: string;
+      wdpaid: number;
+    }[] = await this.geoprocessingEntityManager
+      .createQueryBuilder()
+      .select('id')
+      .addSelect('wdpaid')
+      .from(ProtectedArea, 'pa')
+      .where('wdpaid IN (:...wdpaIds)', { wdpaIds: wdpa })
+      .execute();
+
+    if (wdpaProtectedAreas.length !== wdpa.length) {
+      const wdpaProtectedAreasIds = wdpaProtectedAreas.map((pa) => pa.wdpaid);
+      const notFoundIds = wdpa.filter(
+        (id) => !wdpaProtectedAreasIds.includes(id),
+      );
+      const errorMessage = `WDPA protected areas not found: ${notFoundIds}`;
+      this.logger.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    return wdpaProtectedAreas.map((pa) => pa.id);
+  }
+
+  async getCustomProtectedAreasIds(
+    customProtectedAreas: ScenarioProtectedAreasContent['customProtectedAreas'],
+    projectId: string,
+  ): Promise<string[]> {
+    if (customProtectedAreas.length === 0) return [];
+
+    const qb = this.geoprocessingEntityManager
+      .createQueryBuilder()
+      .select('id')
+      .from(ProtectedArea, 'pa')
+      .where('project_id = :projectId', { projectId });
+
+    return Promise.all(
+      customProtectedAreas.map((customPa) => {
+        return new Promise<string>(async (resolve, reject) => {
+          const [protectedArea]: [{ id: string }] = await qb
+            .andWhere('ST_Equals(the_geom, :geom)', {
+              geom: Buffer.from(customPa.geom),
+            })
+            .execute();
+
+          if (protectedArea) resolve(protectedArea.id);
+
+          reject(new Error(`${customPa.name} custom protected area not found`));
+        });
+      }),
+    );
+  }
+
   async run(input: ImportJobInput): Promise<ImportJobOutput> {
     const {
       importResourceId: projectId,
@@ -76,51 +132,13 @@ export class ScenarioProtectedAreasPieceImporter
     );
     const protectedAreasIds: string[] = [];
 
-    const wdpaProtectedAreas: {
-      id: string;
-      wdpaid: number;
-    }[] = await this.geoprocessingEntityManager
-      .createQueryBuilder()
-      .select('id')
-      .addSelect('wdpaid')
-      .from(ProtectedArea, 'pa')
-      .where('wdpaid IN (:...wdpaIds)', { wdpaIds: wdpa })
-      .execute();
-
-    if (wdpaProtectedAreas.length !== wdpa.length) {
-      const wdpaProtectedAreasIds = wdpaProtectedAreas.map((pa) => pa.wdpaid);
-      const notFoundIds = wdpa.filter(
-        (id) => !wdpaProtectedAreasIds.includes(id),
-      );
-      const errorMessage = `WDPA protected areas not found: ${notFoundIds}`;
-      this.logger.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-
-    protectedAreasIds.push(...wdpaProtectedAreas.map((pa) => pa.id));
-
-    const qb = this.geoprocessingEntityManager
-      .createQueryBuilder()
-      .select('id')
-      .from(ProtectedArea, 'pa')
-      .where('project_id = :projectId', { projectId });
-
-    const customProtectedAreasIds = await Promise.all(
-      customProtectedAreas.map((customPa) => {
-        return new Promise<string>(async (resolve, reject) => {
-          const [protectedArea]: [{ id: string }] = await qb
-            .andWhere('ST_Equals(the_geom, :geom)', {
-              geom: Buffer.from(customPa.geom),
-            })
-            .execute();
-
-          if (protectedArea) resolve(protectedArea.id);
-
-          reject(new Error(`${customPa.name} custom protected area not found`));
-        });
-      }),
+    const wdpaProtectedAreasIds = await this.getWdpaProtectedAreasIds(wdpa);
+    const customProtectedAreasIds = await this.getCustomProtectedAreasIds(
+      customProtectedAreas,
+      projectId,
     );
 
+    protectedAreasIds.push(...wdpaProtectedAreasIds);
     protectedAreasIds.push(...customProtectedAreasIds);
 
     await this.apiEntityManager
