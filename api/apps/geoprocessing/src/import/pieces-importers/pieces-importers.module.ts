7ee2a8138fb7ccd1110b02fee48c3d827eecc362
Daute Rodríguez Rodríguez <94375891+aciddaute@users.noreply.github.com>
2022-03-21 09:25:03 +0000

    feat: scenario planning units data piece importer (#918)
    * feat: scenario planning units data piece importer
    
    * fix: missing cost data import

--- api/apps/geoprocessing/src/import/pieces-importers/pieces-importers.module.ts
+++ api/apps/geoprocessing/src/import/pieces-importers/pieces-importers.module.ts
@@ -2,12 +2,13 @@ import { geoprocessingConnections } from '@marxan-geoprocessing/ormconfig';
 import { FileRepositoryModule } from '@marxan/files-repository';
 import { Logger, Module, Scope } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PlanningAreaGadmPieceImporter } from './planning-area-gadm.piece-importer';
 import { PlanningAreaCustomPieceImporter } from './planning-area-custom.piece-importer';
-import { ProjectMetadataPieceImporter } from './project-metadata.piece-importer';
-import { ScenarioMetadataPieceImporter } from './scenario-metadata.piece-importer';
+import { PlanningAreaGadmPieceImporter } from './planning-area-gadm.piece-importer';
 import { PlanningUnitsGridPieceImporter } from './planning-units-grid.piece-importer';
 import { ProjectCustomProtectedAreasPieceImporter } from './project-custom-protected-areas.piece-importer';
+import { ProjectMetadataPieceImporter } from './project-metadata.piece-importer';
+import { ScenarioMetadataPieceImporter } from './scenario-metadata.piece-importer';
+import { ScenarioPlanningUnitsDataPieceImporter } from './scenario-planning-units-data.piece-importer';
 import { ScenarioProtectedAreasPieceImporter } from './scenario-protected-areas.piece-importer';
 
 @Module({
@@ -23,6 +24,7 @@ import { ScenarioProtectedAreasPieceImporter } from './scenario-protected-areas.
     PlanningUnitsGridPieceImporter,
     ProjectCustomProtectedAreasPieceImporter,
     ScenarioProtectedAreasPieceImporter,
+    ScenarioPlanningUnitsDataPieceImporter,
     { provide: Logger, useClass: Logger, scope: Scope.TRANSIENT },
   ],
 })
