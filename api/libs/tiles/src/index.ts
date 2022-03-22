17bf3e8ec0a998511b2689f8ab7f089f5d8d4f8e
andrea rota <andrea.rota@vizzuality.com>
2022-03-22 15:23:11 +0000

    add utility function for response headers for MVT tiles
    The recently added decorator won't really do as these headers are
    conditional (endpoint will send gzipped JSON in case of error, gzipped
    protobuf in case of success).

--- api/libs/tiles/src/index.ts
+++ api/libs/tiles/src/index.ts
@@ -2,3 +2,4 @@ export { TilesOpenApi } from './tiles-api-doc';
 export { TileRequest } from './tile-request.dto';
 export { BaseTilesOpenApi } from './base-tiles-openapi-doc.decorator';
 export { BaseTilesResponseHeaders } from './base-tiles-response-headers.decorator';
+export { setTileResponseHeadersForSuccessfulRequests } from './tiles-response-headers';
