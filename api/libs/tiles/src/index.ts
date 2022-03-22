2aef89d5afd23c1d3d5039c1a448024e58277af7
andrea rota <andrea.rota@vizzuality.com>
2022-03-22 11:58:39 +0000

    rename decorator file

--- api/libs/tiles/src/index.ts
+++ api/libs/tiles/src/index.ts
@@ -1,3 +1,3 @@
 export { TilesOpenApi } from './tiles-api-doc';
 export { TileRequest } from './tile-request.dto';
-export { BaseTilesOpenApi } from './base-tiles-api-doc';
+export { BaseTilesOpenApi } from './base-tiles-openapi-doc.decorator';
