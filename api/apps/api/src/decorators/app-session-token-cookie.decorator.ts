1af6218ed9509459af3b567d339add3d8b3f497a
andrea rota <andrea.rota@vizzuality.com>
2022-03-24 15:45:59 +0000

    pass token through as a proper cookie (secure and plain, for good measure)

--- api/apps/api/src/decorators/app-session-token-cookie.decorator.ts
+++ api/apps/api/src/decorators/app-session-token-cookie.decorator.ts
@@ -1,22 +1,32 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+const appSessionTokenCookieName = 'next-auth.session-token';
+const appSessionTokenCookieNameSecure = '__Secure-next-auth.session-token';
 /**
  * Extract NextJS session token from request Cookie header. This will typically
  * be useful for API endpoints that receive requests for the webshot service, as
  * the frontend app will supply a fresh app session token that is later used by
  * the webshot service to pass through the application's own auth.
  *
- * Use `__Secure-` cookie if available, fall back to plain if not. This may be
- * useful when working in local dev environments without HTTPS termination in
- * front of the frontend app
+ * Extract `__Secure-` cookie if available, fall back to plain if not: in
+ * practice, only one of these should be provided among the cookies received.
+ *
+ * As the combinations of webshot targets (`baseUrl`) and app from which a
+ * webshot request may originate will vary greatly between environments
+ * (especially local dev environments, or environments spread between Vercel and
+ * Azure, for example) we _then_ forward to the target app, via the webshot
+ * service, the same session token both as secure and non-secure cookie, to
+ * cover all possible setups.
  */
-export const AppSessionToken = createParamDecorator(
+export const AppSessionTokenCookie = createParamDecorator(
   (_data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return (
-      request.cookies?.['__Secure-next-auth.session-token'] ??
-      request.cookies?.['next-auth.session-token'] ??
-      undefined
-    );
+    const sessionToken =
+      request.cookies?.[appSessionTokenCookieNameSecure] ??
+      request.cookies?.[appSessionTokenCookieName];
+
+    return sessionToken
+      ? `${appSessionTokenCookieNameSecure}=${sessionToken}; ${appSessionTokenCookieName}=${sessionToken}`
+      : undefined;
   },
 );
