e74da999000720691e40be6df5564b9bda5e43d4
Daute Rodríguez Rodríguez <daute@acidtango.com>
2022-04-01 10:11:09 +0100

    ref: ClonePiece elements

--- api/libs/cloning/src/infrastructure/clone-piece-data/index.ts
+++ api/libs/cloning/src/infrastructure/clone-piece-data/index.ts
@@ -2,24 +2,51 @@ import { ComponentLocation, ResourceKind } from '../../domain';
 import { ClonePiece } from '../../domain/clone-piece';
 import { exportConfigRelativePath } from './export-config';
 import { featuresSpecificationRelativePath } from './features-specification';
-import { planningUnitsGridGeoJSONRelativePath } from './planning-units-grid-geojson';
 import { planningAreaCustomRelativePath } from './planning-area-custom';
 import { planningAreaCustomGeoJSONRelativePath } from './planning-area-custom-geojson';
 import { planningAreaGadmRelativePath } from './planning-area-gadm';
 import { planningUnitsGridRelativePath } from './planning-units-grid';
+import { planningUnitsGridGeoJSONRelativePath } from './planning-units-grid-geojson';
+import { projectCustomFeaturesRelativePath } from './project-custom-features';
 import { projectCustomProtectedAreasRelativePath } from './project-custom-protected-areas';
 import { projectMetadataRelativePath } from './project-metadata';
+import { scenarioFeaturesDataRelativePath } from './scenario-features-data';
 import { scenarioMetadataRelativePath } from './scenario-metadata';
-import { scenarioProtectedAreasRelativePath } from './scenario-protected-areas';
 import { scenarioPlanningUnitsDataRelativePath } from './scenario-planning-units-data';
+import { scenarioProtectedAreasRelativePath } from './scenario-protected-areas';
 import { scenarioRunResultsRelativePath } from './scenario-run-results';
-import { userUploadedFeaturesRelativePath } from './user-uploaded-features';
 
 export const exportOnlyClonePieces: ClonePiece[] = [
   ClonePiece.ExportConfig,
   ClonePiece.PlanningUnitsGridGeojson,
   ClonePiece.PlanningAreaCustomGeojson,
 ];
+
+type UriResolver = (
+  location: string,
+  scenarioPiecesExtraData?: { kind: ResourceKind; scenarioId: string },
+) => ComponentLocation[];
+
+function scenarioPieceUriResolver(relativePath: {
+  scenarioImport: string;
+  projectImport: (oldScenarioId: string) => string;
+}): UriResolver {
+  return (location, extraData) => {
+    if (!extraData)
+      throw new Error(
+        'It is not possible generate scenario pieces uris without export kind or old scenario id',
+      );
+
+    return [
+      new ComponentLocation(
+        location,
+        extraData.kind === ResourceKind.Project
+          ? relativePath.projectImport(extraData.scenarioId)
+          : relativePath.scenarioImport,
+      ),
+    ];
+  };
+}
 
 export const clonePieceImportOrder: Record<ClonePiece, number> = {
   // ExportConfig pieces should never be imported. When a import
@@ -38,24 +65,19 @@ export const clonePieceImportOrder: Record<ClonePiece, number> = {
   [ClonePiece.PlanningAreaCustom]: 1,
   [ClonePiece.PlanningUnitsGrid]: 1,
   [ClonePiece.ProjectCustomProtectedAreas]: 1,
+  [ClonePiece.ProjectCustomFeatures]: 1,
   //
   [ClonePiece.ScenarioProtectedAreas]: 2,
   [ClonePiece.ScenarioPlanningUnitsData]: 2,
+  [ClonePiece.ScenarioFeaturesData]: 2,
   //
   [ClonePiece.ScenarioRunResults]: 3,
   // Temporal import order
-  [ClonePiece.UserUploadedFeatures]: 4,
-  [ClonePiece.FeaturesSpecification]: 4,
+  [ClonePiece.FeaturesSpecification]: 10,
 };
 
 export class ClonePieceUrisResolver {
-  private static clonePieceUris: Record<
-    ClonePiece,
-    (
-      location: string,
-      scenarioPiecesExtraData?: { kind: ResourceKind; scenarioId: string },
-    ) => ComponentLocation[]
-  > = {
+  private static clonePieceUris: Record<ClonePiece, UriResolver> = {
     [ClonePiece.ExportConfig]: (location) => [
       new ComponentLocation(location, exportConfigRelativePath),
     ],
@@ -80,76 +102,27 @@ export class ClonePieceUrisResolver {
     [ClonePiece.ProjectCustomProtectedAreas]: (location) => [
       new ComponentLocation(location, projectCustomProtectedAreasRelativePath),
     ],
-    [ClonePiece.UserUploadedFeatures]: (location) => [
-      new ComponentLocation(location, userUploadedFeaturesRelativePath),
+    [ClonePiece.ProjectCustomFeatures]: (location) => [
+      new ComponentLocation(location, projectCustomFeaturesRelativePath),
     ],
     [ClonePiece.FeaturesSpecification]: (location) => [
       new ComponentLocation(location, featuresSpecificationRelativePath),
     ],
-    [ClonePiece.ScenarioMetadata]: (location, extraData) => {
-      if (!extraData)
-        throw new Error(
-          'It is not possible generate scenario pieces uris without export kind or old scenario id',
-        );
-
-      return [
-        new ComponentLocation(
-          location,
-          extraData.kind === ResourceKind.Project
-            ? scenarioMetadataRelativePath.projectImport(extraData.scenarioId)
-            : scenarioMetadataRelativePath.scenarioImport,
-        ),
-      ];
-    },
-    [ClonePiece.ScenarioPlanningUnitsData]: (location, extraData) => {
-      if (!extraData)
-        throw new Error(
-          'It is not possible generate scenario pieces uris without export kind or old scenario id',
-        );
-
-      return [
-        new ComponentLocation(
-          location,
-          extraData.kind === ResourceKind.Project
-            ? scenarioPlanningUnitsDataRelativePath.projectImport(
-                extraData.scenarioId,
-              )
-            : scenarioPlanningUnitsDataRelativePath.scenarioImport,
-        ),
-      ];
-    },
-    [ClonePiece.ScenarioProtectedAreas]: (location, extraData) => {
-      if (!extraData)
-        throw new Error(
-          'It is not possible generate scenario pieces uris without export kind or old scenario id',
-        );
-
-      return [
-        new ComponentLocation(
-          location,
-          extraData.kind === ResourceKind.Project
-            ? scenarioProtectedAreasRelativePath.projectImport(
-                extraData.scenarioId,
-              )
-            : scenarioProtectedAreasRelativePath.scenarioImport,
-        ),
-      ];
-    },
-    [ClonePiece.ScenarioRunResults]: (location, extraData) => {
-      if (!extraData)
-        throw new Error(
-          'It is not possible generate scenario pieces uris without export kind or old scenario id',
-        );
-
-      return [
-        new ComponentLocation(
-          location,
-          extraData.kind === ResourceKind.Project
-            ? scenarioRunResultsRelativePath.projectImport(extraData.scenarioId)
-            : scenarioRunResultsRelativePath.scenarioImport,
-        ),
-      ];
-    },
+    [ClonePiece.ScenarioMetadata]: scenarioPieceUriResolver(
+      scenarioMetadataRelativePath,
+    ),
+    [ClonePiece.ScenarioPlanningUnitsData]: scenarioPieceUriResolver(
+      scenarioPlanningUnitsDataRelativePath,
+    ),
+    [ClonePiece.ScenarioProtectedAreas]: scenarioPieceUriResolver(
+      scenarioProtectedAreasRelativePath,
+    ),
+    [ClonePiece.ScenarioFeaturesData]: scenarioPieceUriResolver(
+      scenarioFeaturesDataRelativePath,
+    ),
+    [ClonePiece.ScenarioRunResults]: scenarioPieceUriResolver(
+      scenarioRunResultsRelativePath,
+    ),
   };
 
   static resolveFor(
