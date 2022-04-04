3a27a73b04d76b5ec1b0c9214f56c348370518d8
Ruben Vallejo <ralexanderv89@gmail.com>
2022-04-04 17:56:11 +0200

    feat: include webshotConfig in startCalibrationDto

--- api/apps/api/src/modules/scenarios/dto/start-scenario-blm-calibration.dto.ts
+++ api/apps/api/src/modules/scenarios/dto/start-scenario-blm-calibration.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsOptional } from 'class-validator';
+import { IsArray, IsOptional, ValidateNested } from 'class-validator';
 import { IsValidRange } from '@marxan-api/decorators/is-valid-range.decorator';
+import { WebshotConfig } from '@marxan/webshot';
+import { Type } from 'class-transformer';
 
 export class StartScenarioBlmCalibrationDto {
   @ApiProperty({ example: [0.001, 100], required: false })
@@ -8,4 +10,9 @@ export class StartScenarioBlmCalibrationDto {
   @IsArray()
   @IsValidRange()
   range?: [number, number];
+
+  @ApiProperty()
+  @ValidateNested({ each: true })
+  @Type(() => WebshotConfig)
+  config!: WebshotConfig;
 }
