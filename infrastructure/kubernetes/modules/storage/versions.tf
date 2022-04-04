b5e6d5936d5f8fbcee15b6305022c61fa57e937d
andrea rota <andrea.rota@vizzuality.com>
2022-04-04 19:17:46 +0100

    add k8s_storage module with initial storage class for Azure Files CSI over NFS

---
+++ infrastructure/kubernetes/modules/storage/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.92.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.8.0"
+    }
+  }
+  required_version = "1.1.3"
+}
+
+provider "kubernetes" {
+  host                   = var.k8s_host
+  client_certificate     = var.k8s_client_certificate
+  client_key             = var.k8s_client_key
+  cluster_ca_certificate = var.k8s_cluster_ca_certificate
+}
