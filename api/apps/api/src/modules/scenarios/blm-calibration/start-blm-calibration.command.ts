c3a6d21c729f9dbab7c907228a0588d39a0539c8
Ruben Vallejo <ralexanderv89@gmail.com>
2022-04-04 12:58:38 +0200

    feat: adds png generation process to blm calibration

--- api/apps/api/src/modules/scenarios/blm-calibration/start-blm-calibration.command.ts
+++ api/apps/api/src/modules/scenarios/blm-calibration/start-blm-calibration.command.ts
@@ -1,9 +1,11 @@
+import { WebshotConfig } from '@marxan/webshot';
 import { Command } from '@nestjs-architects/typed-cqrs';
 
 export class StartBlmCalibration extends Command<void> {
   constructor(
     public readonly scenarioId: string,
     public readonly blmValues: number[],
+    public readonly config: WebshotConfig,
   ) {
     super();
   }
