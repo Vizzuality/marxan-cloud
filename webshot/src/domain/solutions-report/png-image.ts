72879e0d8bcfcdcf80e779902b7cddbd50ec186b
Ruben Vallejo <ralexanderv89@gmail.com>
2022-04-05 12:39:28 +0200

    feat: adds new endpoint for webshot service png image generation

---
+++ webshot/src/domain/solutions-report/png-image.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import puppeteer, { ScreenshotOptions } from "puppeteer";
+import { waitForReportReady } from "./wait-function";
+
+const appRouteTemplate = "/reports/:projectId/:scenarioId/blm?blmValue=:blmValue";
+
+export const generatePngImageFromBlmData = async (
+  req: Request,
+  res: Response
+) => {
+  const {
+    body: { baseUrl, cookie, screenshotOptions },
+  }: { body: { baseUrl: string; cookie: string; screenshotOptions: ScreenshotOptions } } =
+    req;
+
+  const {
+    params: { projectId, scenarioId, },
+  } = req;
+
+  const { query: { blmValue } } = req;
+
+  if (!(projectId || scenarioId || blmValue)) {
+    res.status(400).json({
+      error: `Invalid request: projectId (${projectId}), scenarioId (${scenarioId}) or blmValue (${blmValue}) were not provided.`,
+    });
+    return;
+  }
+
+  if (!baseUrl) {
+    res.status(400).json({ error: "No baseUrl was provided." });
+    return;
+  }
+
+  try {
+    new URL(baseUrl);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: `The provided baseUrl (${baseUrl} is not a valid URL.` });
+    return;
+  }
+
+  const pageUrl = `${baseUrl}${appRouteTemplate
+    .replace(":projectId", projectId)
+    .replace(":scenarioId", scenarioId)
+    .replace(":blmValue", blmValue)}`;
+
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+
+  /**
+   * The webshot service authenticates to the upstream frontend instance by
+   * passing through the cookie that it receives from the API. In practice, all
+   * that is needed is the `__Secure-next-auth.session-token` cookie (or
+   * `next-auth.session-token` in development environments where the frontend
+   * may not be running behind an HTTPS reverse proxy).
+   */
+  if (cookie) await page.setExtraHTTPHeaders({ cookie });
+
+  console.info(`Rendering ${pageUrl} as PNG`);
+  await page.goto(pageUrl);
+  await page.waitForFunction(waitForReportReady, { timeout: 30e3 });
+  const pageAsPng = await page.screenshot({ ...screenshotOptions, timeout: 30e3 });
+
+  await page.close();
+  await browser.close();
+
+  res.type("image/png");
+  res.end(pageAsPng);
+};
