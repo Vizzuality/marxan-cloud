0ad2a8983ab4f5bfe355ab6af25597a0ed7282fb
andrea rota <andrea.rota@vizzuality.com>
2022-04-06 20:52:21 +0100

    move pvc resource

--- infrastructure/kubernetes/modules/storage/main.tf
+++ infrastructure/kubernetes/modules/storage/main.tf
@@ -9,20 +9,3 @@ resource "kubernetes_storage_class" "azurefile_csi_nfs" {
   }
   mount_options = ["nconnect=8"]
 }
-
-resource "kubernetes_persistent_volume_claim" "backend_shared_spatial_data_storage" {
-  metadata {
-    # @debt use var
-    name      = "backend-shared-spatial-data-storage"
-    namespace = var.namespace
-  }
-  spec {
-    access_modes = ["ReadWriteMany"]
-    storage_class_name = var.backend_storage_class
-    resources {
-      requests = {
-        storage = var.backend_storage_size
-      }
-    }
-  }
-}
