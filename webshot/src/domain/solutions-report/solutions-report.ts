fcfba87fefedce99824cc0a14106f1f392d6d156
andrea rota <andrea.rota@vizzuality.com>
2022-04-07 12:35:11 +0100

    add passthrough of browser console to service logs

--- webshot/src/domain/solutions-report/solutions-report.ts
+++ webshot/src/domain/solutions-report/solutions-report.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import puppeteer, { PDFOptions } from "puppeteer";
 import { ReportOptions } from "./report-options.dto";
-import { waitForReportReady } from "../utils/wait-function";
+import { waitForReportReady } from "../../utils/wait-function";
+import { passthroughConsole } from "../../utils/passthrough-console.utils";
 
 const appRouteTemplate = "/reports/:projectId/:scenarioId/solutions?solutionId=:solutionId";
 
@@ -50,6 +51,8 @@ export const generateSummaryReportForScenario = async (
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
+  // Pass through browser console to our own service's console
+  page.on('console', passthroughConsole);
 
   /**
    * The webshot service authenticates to the upstream frontend instance by
