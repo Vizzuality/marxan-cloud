0b1f2cb0e48468d857d0edcae43c5f88bd56db35
Aaron Perez <aarperper@gmail.com>
2022-04-07 09:08:56 +0100

    wip

--- api/apps/geoprocessing/src/utils/config.utils.ts
+++ api/apps/geoprocessing/src/utils/config.utils.ts
@@ -12,6 +12,8 @@ export class AppConfig {
    * property does not exist.
    */
   static get<T>(property: string, defaultValue?: T): T {
+    console.dir(config, { depth: Infinity });
+
     if (config.has(property)) {
       return config.get(property);
     }
