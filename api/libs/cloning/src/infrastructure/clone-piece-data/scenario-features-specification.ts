d1e66dfaf78e982a87c72a6935fbfdaf603df8c4
Ángel Higuera Vaquero <angelhiguera@acidtango.com>
2022-04-07 14:36:28 +0200

    fix : add missing scenario features specification file

---
+++ api/libs/cloning/src/infrastructure/clone-piece-data/scenario-features-specification.ts
@@ -0,0 +1,70 @@
+import { SpecificationOperation } from '@marxan-api/modules/specification';
+import { SpecificationFeature } from '@marxan-api/modules/specification/adapters/specification-feature';
+import { FeatureSubSet } from '@marxan-api/modules/specification/domain';
+
+export const featuresSpecificationRelativePath = {
+  scenarioImport: `features-specification.json`,
+  projectImport: (oldScenarioId: string) =>
+    `scenarios/${oldScenarioId}/features-specification.json`,
+};
+
+export type FeatureElement = {
+  name: string;
+  isCustom: boolean;
+};
+
+export type FeatureNumberCalculated = {
+  featureId: number;
+  calculated: boolean;
+};
+export type FeatureIdCalculated = { featureId: string; calculated: boolean };
+
+export type FeaturesConfig = {
+  baseFeature: string;
+  againstFeature: string | null;
+  operation: SpecificationOperation;
+  featuresDetermined: boolean;
+  splitByProperty: string | null;
+  selectSubSets: FeatureSubSet[] | null;
+  features: FeatureNumberCalculated[];
+};
+export type ScenarioFeaturesSpecificationContent = {
+  raw: Record<string, any>;
+  draft: boolean;
+  configs: FeaturesConfig[];
+};
+
+export type ScenarioFeaturesSpecificationContentWithId = {
+  id: string;
+  raw: Record<string, any>;
+  draft: boolean;
+  configs: {
+    baseFeatureId: string;
+    againstFeatureId: string | null;
+    operation: SpecificationOperation;
+    featuresDetermined: boolean;
+    splitByProperty: string | null;
+    selectSubSets: FeatureSubSet[] | null;
+    features: SpecificationFeature[];
+  }[];
+};
+
+export function searchFeatureIdInObject(feature: any, results: string[]) {
+  Object.keys(feature).forEach((key) => {
+    if (typeof feature[key] === 'string' && key === 'featureId')
+      results.push(feature[key]);
+    if (typeof feature[key] === 'object')
+      searchFeatureIdInObject(feature[key], results);
+  });
+}
+export function parseFeatureIdInObject(
+  feature: any,
+  parseFeature: (feature: string) => string,
+) {
+  Object.keys(feature).forEach((key) => {
+    if (typeof feature[key] === 'string' && key === 'featureId')
+      feature[key] = parseFeature(feature[key]);
+    if (typeof feature[key] === 'object')
+      parseFeatureIdInObject(feature[key], parseFeature);
+  });
+}
