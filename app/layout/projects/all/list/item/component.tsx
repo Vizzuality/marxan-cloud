5b6f73c0bac25d481bf09d00a278a4b2c0707400
anamontiaga <amontiaga@gmail.com>
2022-04-12 16:20:59 +0200

    remove propagion stop on handle download

--- app/layout/projects/all/list/item/component.tsx
+++ app/layout/projects/all/list/item/component.tsx
@@ -93,8 +93,7 @@ export const Item: React.FC<ItemProps> = ({
     onClick(e);
   }, [onClick]);
 
-  const handleDownload = useCallback((e) => {
-    e.stopPropagation();
+  const handleDownload = useCallback(() => {
     projectDownloadMutation.mutate({ id, data: { scenarioIds } }, {
 
       onSuccess: () => {
