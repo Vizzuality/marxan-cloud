1d40abe9468902c0254df0a44d5724302e95c342
Ruben Vallejo <ralexanderv89@gmail.com>
2022-04-26 17:07:50 +0200

    feat: adds migration for export_id column in public projects

---
+++ api/apps/api/src/migrations/api/1650984916463-AddsExportIdToPublicProject.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddsExportIdToPublicProject1650984916463
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+          ALTER TABLE published_projects
+            ADD COLUMN export_id varchar;
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE published_projects
+                DROP COLUMN export_id;
+        `);
+  }
+}
