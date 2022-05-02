ce07ee3793cc925ec19320b38805759b8d129a86
Daute Rodríguez Rodríguez <daute@acidtango.com>
2022-05-02 11:40:54 +0100

    ref: manifest file service

--- api/apps/api/src/modules/clone/export/adapters/node-manifest-file-service.adapter.ts
+++ api/apps/api/src/modules/clone/export/adapters/node-manifest-file-service.adapter.ts
@@ -1,64 +1,31 @@
-import {
-  CloningFilesRepository,
-  GetFileError,
-  SaveFileError,
-} from '@marxan/cloning-files-repository';
-import {
-  manifestFileRelativePath,
-  signatureFileRelativePath,
-} from '@marxan/cloning/infrastructure/clone-piece-data/manifest-file';
-import { readableToBuffer } from '@marxan/utils';
 import { Inject, Injectable } from '@nestjs/common';
 import { spawnSync } from 'child_process';
 import { createSign, createVerify } from 'crypto';
 import { Either, left, right } from 'fp-ts/lib/Either';
-import { isLeft } from 'fp-ts/lib/These';
-import { Readable } from 'stream';
-import { FileDestination } from '../application/archive-creator.port';
 import {
+  CloningSigningSecret,
   integrityCheckFailed,
   invalidSignature,
   manifestFileGenerationError,
-  CloningSigningSecret,
   ManifestFileService,
   signatureFileGenerationError,
 } from '../application/manifest-file-service.port';
-import { ExportId } from '../domain';
 
 @Injectable()
 export class NodeManifestFileService implements ManifestFileService {
   private readonly algorithm = 'RSA-SHA256';
 
   constructor(
-    private readonly fileRepository: CloningFilesRepository,
     @Inject(CloningSigningSecret)
     private readonly cloningSigningSecret: string,
   ) {}
 
-  private async getManifestFileReadable(
-    exportFolder: string,
-  ): Promise<Either<typeof manifestFileGenerationError, Buffer>> {
-    const childProcess = spawnSync(
-      `find ${exportFolder} -type f -exec sha256sum {} \\;`,
-      { shell: true },
-    );
-
-    if (childProcess.error) {
-      return left(manifestFileGenerationError);
-    }
-
-    return right(childProcess.stdout);
-  }
-
   performIntegrityCheck(
-    exportId: ExportId,
+    manifestFilePath: string,
   ): Either<typeof integrityCheckFailed, true> {
-    const exportFolder = this.fileRepository.getFilesFolderFor(exportId.value);
-
-    const childProcess = spawnSync(
-      `sha256sum -c ${exportFolder}/manifest.txt`,
-      { shell: true },
-    );
+    const childProcess = spawnSync(`sha256sum -c ${manifestFilePath}`, {
+      shell: true,
+    });
 
     const stdout = childProcess.stdout.toString();
     const stderr = childProcess.stderr.toString();
@@ -72,92 +39,45 @@ export class NodeManifestFileService implements ManifestFileService {
   }
 
   async verifyManifestFileSignature(
-    manifestFileUri: string,
+    manifestFile: Buffer,
+    signatureFile: Buffer,
   ): Promise<Either<typeof invalidSignature, true>> {
-    const signatureFileUri = manifestFileUri.replace(
-      manifestFileRelativePath,
-      signatureFileRelativePath,
-    );
-
-    const manifestFile = await this.fileRepository.get(manifestFileUri);
-    const signatureFile = await this.fileRepository.get(signatureFileUri);
-    if (isLeft(signatureFile) || isLeft(manifestFile)) {
-      return left(invalidSignature);
-    }
-    const manifestFileBuffer = await readableToBuffer(manifestFile.right);
-    const signatureFileBuffer = await readableToBuffer(signatureFile.right);
-    const uploadedSignature = signatureFileBuffer.toString();
+    const uploadedSignature = signatureFile.toString();
 
     const verifier = createVerify(this.algorithm);
-    verifier.update(manifestFileBuffer);
+    verifier.update(manifestFile);
 
     return verifier.verify(this.cloningSigningSecret, uploadedSignature, 'hex')
       ? right(true)
       : left(invalidSignature);
   }
 
+  async generateManifestFileFor(
+    folderPath: string,
+  ): Promise<Either<typeof manifestFileGenerationError, Buffer>> {
+    const childProcess = spawnSync(
+      `find ${folderPath} -type f -exec sha256sum {} \\;`,
+      { shell: true },
+    );
+
+    if (childProcess.error) {
+      return left(manifestFileGenerationError);
+    }
+
+    return right(childProcess.stdout);
+  }
+
   async generateSignatureFileFor(
-    exportId: ExportId,
-    manifestFileUri: string,
-  ): Promise<
-    Either<
-      typeof signatureFileGenerationError | SaveFileError | GetFileError,
-      FileDestination
-    >
-  > {
+    manifestFile: Buffer,
+  ): Promise<Either<typeof signatureFileGenerationError, Buffer>> {
     try {
-      const manifestFile = await this.fileRepository.get(manifestFileUri);
-
-      if (isLeft(manifestFile)) {
-        return manifestFile;
-      }
-
-      const manifestFileBuffer = await readableToBuffer(manifestFile.right);
-
       const signer = createSign(this.algorithm);
-      signer.update(manifestFileBuffer);
+      signer.update(manifestFile);
       const result = signer.sign(this.cloningSigningSecret, 'hex');
 
-      const signatureFile = await this.fileRepository.saveCloningFile(
-        exportId.value,
-        Readable.from(result),
-        signatureFileRelativePath,
-      );
-
-      if (isLeft(signatureFile)) return signatureFile;
-
-      return right({
-        relativeDestination: signatureFileRelativePath,
-        uri: signatureFile.right,
-      });
+      return right(Buffer.from(result));
     } catch (err) {
       return left(signatureFileGenerationError);
     }
-  }
-
-  async generateManifestFileFor(
-    exportId: ExportId,
-  ): Promise<
-    Either<typeof manifestFileGenerationError | SaveFileError, FileDestination>
-  > {
-    const exportFilesFolder = this.fileRepository.getFilesFolderFor(
-      exportId.value,
-    );
-    const bufferOrError = await this.getManifestFileReadable(exportFilesFolder);
-
-    if (isLeft(bufferOrError)) return bufferOrError;
-
-    const manifestFile = await this.fileRepository.saveCloningFile(
-      exportId.value,
-      Readable.from(bufferOrError.right),
-      manifestFileRelativePath,
-    );
-
-    if (isLeft(manifestFile)) return manifestFile;
-
-    return right({
-      uri: manifestFile.right,
-      relativeDestination: manifestFileRelativePath,
-    });
   }
 }
--- api/apps/api/src/modules/clone/export/application/finalize-archive.handler.ts
+++ api/apps/api/src/modules/clone/export/application/finalize-archive.handler.ts
@@ -1,4 +1,11 @@
-import { manifestFileRelativePath } from '@marxan/cloning/infrastructure/clone-piece-data/manifest-file';
+import {
+  CloningFilesRepository,
+  SaveFileError,
+} from '@marxan/cloning-files-repository';
+import {
+  manifestFileRelativePath,
+  signatureFileRelativePath,
+} from '@marxan/cloning/infrastructure/clone-piece-data/manifest-file';
 import { isDefined } from '@marxan/utils';
 import { Logger } from '@nestjs/common';
 import {
@@ -7,12 +14,22 @@ import {
   EventPublisher,
   IInferredCommandHandler,
 } from '@nestjs/cqrs';
-import { isLeft } from 'fp-ts/Either';
+import { Either, isLeft, right } from 'fp-ts/Either';
+import { Readable } from 'stream';
 import { MarkExportAsFailed } from '../../infra/export/mark-export-as-failed.command';
-import { ArchiveCreator } from './archive-creator.port';
+import { ArchiveCreator, FileDestination } from './archive-creator.port';
 import { ExportRepository } from './export-repository.port';
 import { ExportId, FinalizeArchive } from './finalize-archive.command';
-import { ManifestFileService } from './manifest-file-service.port';
+import {
+  manifestFileGenerationError,
+  ManifestFileService,
+  signatureFileGenerationError,
+} from './manifest-file-service.port';
+
+type ManifestAndSignatureFilesUris = {
+  manifestFileDestination: FileDestination;
+  signatureFileDestination: FileDestination;
+};
 
 @CommandHandler(FinalizeArchive)
 export class FinalizeArchiveHandler
@@ -24,6 +41,7 @@ export class FinalizeArchiveHandler
     private readonly archiveCreator: ArchiveCreator,
     private readonly eventPublisher: EventPublisher,
     private readonly manifestFileService: ManifestFileService,
+    private readonly cloningFilesRepository: CloningFilesRepository,
     private readonly commandBus: CommandBus,
   ) {}
 
@@ -34,6 +52,67 @@ export class FinalizeArchiveHandler
     this.logger.error(error);
 
     await this.commandBus.execute(new MarkExportAsFailed(exportId));
+  }
+
+  private async generateManifestAndSignatureFiles(
+    exportId: string,
+  ): Promise<
+    Either<
+      | typeof manifestFileGenerationError
+      | typeof signatureFileGenerationError
+      | SaveFileError,
+      ManifestAndSignatureFilesUris
+    >
+  > {
+    const exportFolder = this.cloningFilesRepository.getFilesFolderFor(
+      exportId,
+    );
+
+    const manifestFile = await this.manifestFileService.generateManifestFileFor(
+      exportFolder,
+    );
+
+    if (isLeft(manifestFile)) {
+      return manifestFile;
+    }
+
+    const signatureFile = await this.manifestFileService.generateSignatureFileFor(
+      manifestFile.right,
+    );
+
+    if (isLeft(signatureFile)) {
+      return signatureFile;
+    }
+
+    const manifestFileUri = await this.cloningFilesRepository.saveCloningFile(
+      exportId,
+      Readable.from(manifestFile.right),
+      manifestFileRelativePath,
+    );
+
+    const signatureFileUri = await this.cloningFilesRepository.saveCloningFile(
+      exportId,
+      Readable.from(signatureFile.right),
+      signatureFileRelativePath,
+    );
+
+    if (isLeft(manifestFileUri)) {
+      return manifestFileUri;
+    }
+    if (isLeft(signatureFileUri)) {
+      return signatureFileUri;
+    }
+
+    return right({
+      manifestFileDestination: {
+        uri: manifestFileUri.right,
+        relativeDestination: manifestFileRelativePath,
+      },
+      signatureFileDestination: {
+        uri: signatureFileUri.right,
+        relativeDestination: signatureFileRelativePath,
+      },
+    });
   }
 
   async execute({ exportId }: FinalizeArchive): Promise<void> {
@@ -47,30 +126,21 @@ export class FinalizeArchiveHandler
       return;
     }
 
-    const manifestFile = await this.manifestFileService.generateManifestFileFor(
-      exportId,
+    const manifestAndSignatureFilesUris = await this.generateManifestAndSignatureFiles(
+      exportId.value,
     );
 
-    if (isLeft(manifestFile)) {
+    if (isLeft(manifestAndSignatureFilesUris)) {
       this.logErrorAndMarkExportAsFailed(
         exportId,
-        `${FinalizeArchiveHandler.name} could not generate manifest file for export with ID ${exportId.value}`,
+        `${FinalizeArchiveHandler.name} could not generate manifest and/or signature files`,
       );
       return;
     }
-
-    const signatureFile = await this.manifestFileService.generateSignatureFileFor(
-      exportId,
-      manifestFile.right.uri,
-    );
-
-    if (isLeft(signatureFile)) {
-      this.logErrorAndMarkExportAsFailed(
-        exportId,
-        `${FinalizeArchiveHandler.name} could not generate signature file for export with ID ${exportId.value}`,
-      );
-      return;
-    }
+    const {
+      manifestFileDestination,
+      signatureFileDestination,
+    } = manifestAndSignatureFilesUris.right;
 
     const pieces = exportInstance
       .toSnapshot()
@@ -84,7 +154,7 @@ export class FinalizeArchiveHandler
           uri: piece.uri,
           relativeDestination: piece.relativePath,
         }))
-        .concat(manifestFile.right, signatureFile.right),
+        .concat(manifestFileDestination, signatureFileDestination),
     );
 
     if (isLeft(archiveResult)) {
