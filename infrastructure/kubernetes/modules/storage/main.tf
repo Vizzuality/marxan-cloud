8e5fb618d7d69d1a95b9cb1f71d426ccbb68811b
andrea rota <andrea.rota@vizzuality.com>
2022-05-09 13:00:09 +0100

    switch to standard CIFS protocol
    Kinda unrelated to the current branch, but given that one of the
    suspects for the ongoing investigations about unzipper issues on
    files in temp storage is the underlying network protocol (one of the few
    differences between the k8s environment where things break sometimes,
    and the Compose environments where things work consistently), I am
    taking this chance to also, maybe temporarily, switch from NFS to CIFS
    for the existing shared volume.

--- infrastructure/kubernetes/modules/storage/main.tf
+++ infrastructure/kubernetes/modules/storage/main.tf
@@ -2,10 +2,12 @@ resource "kubernetes_storage_class" "azurefile_csi_temp_data" {
   metadata {
     name = var.temp_data_storage_class
   }
-  storage_provisioner = "file.csi.azure.com"
-  reclaim_policy      = "Delete"
-  parameters = {
-    protocol = "nfs"
+  storage_provisioner    = "file.csi.azure.com"
+  reclaim_policy         = "Delete"
+  allow_volume_expansion = true
+  mount_options = {
+    dir_mode  = "0640"
+    file_mode = "0640"
+    cache     = "strict" # In case kernel is < 3.7
   }
-  mount_options = ["nconnect=8"]
 }
