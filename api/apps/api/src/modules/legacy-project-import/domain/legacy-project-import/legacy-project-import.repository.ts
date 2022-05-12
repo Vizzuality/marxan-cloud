6e50a57c4a7468a7de5d93c6e98e85445b77abdb
Daute Rodríguez Rodríguez <94375891+aciddaute@users.noreply.github.com>
2022-05-12 15:20:47 +0100

    feat: legacy project imports repository (#1061)

---
+++ api/apps/api/src/modules/legacy-project-import/domain/legacy-project-import/legacy-project-import.repository.ts
@@ -0,0 +1,30 @@
+import { ResourceId } from '@marxan/cloning/domain';
+import { Either } from 'fp-ts/lib/Either';
+import { LegacyProjectImport } from './legacy-project-import';
+
+export const legacyProjectImportNotFound = Symbol(
+  'legacy project import not found',
+);
+
+export const legacyProjectImportSaveError = Symbol(
+  'legacy project import save error',
+);
+
+export type LegacyProjectImportRepositoryFindErrors = typeof legacyProjectImportNotFound;
+export type LegacyProjectImportRepositorySaveErrors = typeof legacyProjectImportSaveError;
+
+export abstract class LegacyProjectImportRepository {
+  abstract find(
+    projectId: ResourceId,
+  ): Promise<
+    Either<LegacyProjectImportRepositoryFindErrors, LegacyProjectImport>
+  >;
+
+  abstract save(
+    legacyProjectImport: LegacyProjectImport,
+  ): Promise<Either<LegacyProjectImportRepositorySaveErrors, true>>;
+
+  abstract transaction<T>(
+    code: (repo: LegacyProjectImportRepository) => Promise<T>,
+  ): Promise<T>;
+}
---
+++ api/apps/api/src/modules/legacy-project-import/infra/legacy-project-import-typeorm.repository.ts
@@ -0,0 +1,92 @@
+import { ResourceId } from '@marxan/cloning/domain';
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import { Either, left, right } from 'fp-ts/lib/Either';
+import { EntityManager, Repository } from 'typeorm';
+import { DbConnections } from '../../../ormconfig.connections';
+import { LegacyProjectImport } from '../domain/legacy-project-import/legacy-project-import';
+import {
+  legacyProjectImportNotFound,
+  LegacyProjectImportRepository,
+  legacyProjectImportSaveError,
+} from '../domain/legacy-project-import/legacy-project-import.repository';
+import { LegacyProjectImportComponentEntity } from './entities/legacy-project-import-component.api.entity';
+import { LegacyProjectImportFileEntity } from './entities/legacy-project-import-file.api.entity';
+import { LegacyProjectImportEntity } from './entities/legacy-project-import.api.entity';
+
+@Injectable()
+export class LegacyProjectImportTypeormRepository
+  implements LegacyProjectImportRepository {
+  private inTransaction = false;
+
+  constructor(
+    @InjectRepository(LegacyProjectImportEntity)
+    private readonly legacyProjectImportRepo: Repository<LegacyProjectImportEntity>,
+    @InjectRepository(LegacyProjectImportComponentEntity)
+    private readonly legacyProjectImportComponentRepo: Repository<LegacyProjectImportComponentEntity>,
+    @InjectRepository(LegacyProjectImportFileEntity)
+    private readonly legacyProjectImportFileRepo: Repository<LegacyProjectImportFileEntity>,
+    @InjectEntityManager(DbConnections.default)
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async find(
+    projectId: ResourceId,
+  ): Promise<Either<typeof legacyProjectImportNotFound, LegacyProjectImport>> {
+    const lock = this.inTransaction
+      ? { mode: 'pessimistic_write' as const }
+      : undefined;
+
+    const legacyProjectImport = await this.legacyProjectImportRepo.findOne({
+      where: { projectId: projectId.value },
+      lock,
+    });
+
+    if (!legacyProjectImport) return left(legacyProjectImportNotFound);
+
+    const pieces = await this.legacyProjectImportComponentRepo.find({
+      legacyProjectImportId: legacyProjectImport.id,
+    });
+    const files = await this.legacyProjectImportFileRepo.find({
+      legacyProjectImportId: legacyProjectImport.id,
+    });
+
+    legacyProjectImport.pieces = pieces;
+    legacyProjectImport.files = files;
+
+    return right(legacyProjectImport.toDomain());
+  }
+
+  async save(
+    legacyProjectImport: LegacyProjectImport,
+  ): Promise<Either<typeof legacyProjectImportSaveError, true>> {
+    try {
+      await this.legacyProjectImportRepo.save(
+        LegacyProjectImportEntity.fromSnapshot(
+          legacyProjectImport.toSnapshot(),
+        ),
+      );
+
+      return right(true);
+    } catch (err) {
+      return left(legacyProjectImportSaveError);
+    }
+  }
+
+  transaction<T>(
+    code: (repo: LegacyProjectImportRepository) => Promise<T>,
+  ): Promise<T> {
+    return this.entityManager.transaction((transactionEntityManager) => {
+      const transactionalRepository = new LegacyProjectImportTypeormRepository(
+        transactionEntityManager.getRepository(LegacyProjectImportEntity),
+        transactionEntityManager.getRepository(
+          LegacyProjectImportComponentEntity,
+        ),
+        transactionEntityManager.getRepository(LegacyProjectImportFileEntity),
+        transactionEntityManager,
+      );
+      transactionalRepository.inTransaction = true;
+      return code(transactionalRepository);
+    });
+  }
+}
