5802d49ca0aedcd34065f78dbf9ee211f24921f9
andrea rota <andrea.rota@vizzuality.com>
2022-05-13 11:05:06 +0100

    set 0777 permissions (as share is owned by root)

--- infrastructure/kubernetes/modules/storage/main.tf
+++ infrastructure/kubernetes/modules/storage/main.tf
@@ -6,9 +6,9 @@ resource "kubernetes_storage_class" "azurefile_csi_temp_data" {
   reclaim_policy         = "Delete"
   allow_volume_expansion = true
   mount_options = [
-    "dir_mode 0640",
-    "file_mode 0640",
-    "cache strict", # In case kernel is < 3.7
+    "dir_mode=0777",
+    "file_mode=0777",
+    "cache=strict", # In case kernel is < 3.7
   ]
 }
 
@@ -20,8 +20,8 @@ resource "kubernetes_storage_class" "azurefile_csi_cloning_data" {
   reclaim_policy      = "Retain"
   allow_volume_expansion = true
   mount_options = [
-    "dir_mode 0640",
-    "file_mode 0640",
-    "cache strict", # In case kernel is < 3.7
+    "dir_mode=0777",
+    "file_mode=0777",
+    "cache=strict", # In case kernel is < 3.7
   ]
 }
