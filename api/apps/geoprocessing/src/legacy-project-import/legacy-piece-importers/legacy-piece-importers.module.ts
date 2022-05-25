5372c250c5fb6e76922cee06d436647de38b3428
Daute Rodríguez Rodríguez <daute@acidtango.com>
2022-05-25 09:00:53 +0100

    feat: planning grid legacy piece importer

--- api/apps/geoprocessing/src/legacy-project-import/legacy-piece-importers/legacy-piece-importers.module.ts
+++ api/apps/geoprocessing/src/legacy-project-import/legacy-piece-importers/legacy-piece-importers.module.ts
@@ -1,7 +1,34 @@
-import { Module } from '@nestjs/common';
+import { ProjectsPuEntity } from '@marxan-jobs/planning-unit-geometry';
+import {
+  LegacyProjectImportFilesLocalRepository,
+  LegacyProjectImportFilesRepository,
+  LegacyProjectImportStoragePath,
+} from '@marxan/legacy-project-import';
+import { ShapefilesModule } from '@marxan/shapefile-converter';
+import { Logger, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfig } from '../../utils/config.utils';
+import { PlanningGridLegacyProjectPieceImporter } from './planning-grid.legacy-piece-importer';
 
 @Module({
-  imports: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([ProjectsPuEntity]), ShapefilesModule],
+  providers: [
+    Logger,
+    {
+      provide: LegacyProjectImportFilesRepository,
+      useClass: LegacyProjectImportFilesLocalRepository,
+    },
+    {
+      provide: LegacyProjectImportStoragePath,
+      useFactory: () => {
+        const path = AppConfig.get<string>(
+          'storage.cloningFileStorage.localPath',
+        );
+
+        return path.endsWith('/') ? path.substring(0, path.length - 1) : path;
+      },
+    },
+    PlanningGridLegacyProjectPieceImporter,
+  ],
 })
 export class LegacyPieceImportersModule {}
