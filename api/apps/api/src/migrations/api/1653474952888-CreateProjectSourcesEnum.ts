93633956910b069b190169117b21f70125468bcd
Ruben Vallejo <ralexanderv89@gmail.com>
2022-05-25 12:40:46 +0200

    feat: adds migration for column sources in projects, adds specific column enum

---
+++ api/apps/api/src/migrations/api/1653474952888-CreateProjectSourcesEnum.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateProjectSourcesEnum1653474952888
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    CREATE TYPE "project_sources_enum" AS ENUM(
+      'marxan_cloud',
+      'legacy_import'
+    );
+  `);
+    await queryRunner.query(`
+    ALTER TABLE projects
+        ADD COLUMN sources project_sources_enum not null default 'marxan_cloud';
+    );
+  `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    ALTER TABLE projects
+        DROP COLUMN sources;
+    `);
+    await queryRunner.query(`DROP TYPE "project_sources_enum"`);
+  }
+}
