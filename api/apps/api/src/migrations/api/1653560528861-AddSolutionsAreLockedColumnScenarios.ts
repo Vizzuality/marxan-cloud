225c126cb5b3df4a0cbe384ac8197197025a9366
Ruben Vallejo <ralexanderv89@gmail.com>
2022-05-26 15:03:48 +0200

    feat: adds column to scenarios and prevent marxan to be run if solutions are locked

---
+++ api/apps/api/src/migrations/api/1653560528861-AddSolutionsAreLockedColumnScenarios.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddSolutionsAreLockedColumnScenarios1653560528861
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    ALTER TABLE scenarios
+    ADD COLUMN solutions_are_locked boolean not null default false;
+  `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    ALTER TABLE scenarios
+    DROP COLUMN solutions_are_locked;
+  `);
+  }
+}
