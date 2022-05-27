ba76c5fede36b848d346a0e1e0212767650004bf
Ruben Vallejo <ralexanderv89@gmail.com>
2022-05-27 09:43:12 +0200

    feat: adds sources to project entity, adds sources info to project shell for legacy import

--- api/apps/api/src/modules/projects/project.api.entity.ts
+++ api/apps/api/src/modules/projects/project.api.entity.ts
@@ -31,6 +31,11 @@ export const projectResource: BaseServiceResource = {
   entitiesAllowedAsIncludes: ['scenarios', 'users'],
 };
 
+export enum ProjectSourcesEnum {
+  marxanCloud = 'marxan_cloud',
+  legacyImport = 'legacy_import',
+}
+
 @Entity('projects')
 export class Project extends TimeUserEntityMetadata {
   @ApiProperty()
@@ -130,6 +135,13 @@ export class Project extends TimeUserEntityMetadata {
   @ApiProperty()
   @OneToOne(() => ProjectBlm)
   projectBlm!: ProjectBlm;
+
+  @ApiProperty()
+  @Column('enum', {
+    name: 'sources',
+    enum: ProjectSourcesEnum,
+  })
+  sources?: string;
 
   /**
    * JSONB storage for non-relational attributes
