cd12290f589e65604d78abb166ac49555299a0ca
Ángel Higuera Vaquero <angelhiguera@acidtango.com>
2022-05-31 16:51:38 +0200

    feat: add input to legacy project import piece enum

---
+++ api/apps/api/src/migrations/api/1654005845323-AddInputPieceToLegacyProjectImportPieceEnum.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddInputPieceToLegacyProjectImportPieceEnum1654005845323
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TYPE legacy_project_import_piece ADD VALUE 'input'`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TYPE "legacy_project_import_piece_tmp" AS ENUM(
+        'planning-grid', 
+        'features', 
+        'scenario-pus-data', 
+        'features-specification', 
+        'solutions'
+      );
+    `);
+
+    await queryRunner.query(`
+      ALTER TABLE legacy_project_import_components
+      ALTER COLUMN kind TYPE legacy_project_import_piece_tmp USING kind::text::legacy_project_import_piece_tmp;
+    `);
+
+    await queryRunner.query(`
+      DROP TYPE legacy_project_import_piece;
+    `);
+    await queryRunner.query(`
+      ALTER TYPE legacy_project_import_piece_tmp RENAME TO legacy_project_import_piece;
+    `);
+  }
+}
