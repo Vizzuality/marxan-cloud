aaa9ae0b2005b476cf41e0d47d403034b3d21cd6
Ruben Vallejo <ralexanderv89@gmail.com>
2022-05-31 10:14:14 +0200

    feat: adds new column to scenario_features_data, modifies pvsprservice to use this data if not null

---
+++ api/apps/geoprocessing/src/migrations/geoprocessing/1653650018540-AddAmountFromLegacyProjectToScenarioFeaturesData.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddAmountFromLegacyProjectToScenarioFeaturesData1653916456540
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE scenario_features_data
+        ADD COLUMN amount_from_legacy_project int;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    ALTER TABLE scenario_features_data
+      DROP COLUMN amount_from_legacy_project;
+  `);
+  }
+}
