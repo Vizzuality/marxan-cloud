0d5c8d30e97948ecf42512acc49d71b56ca7b6e1
Ángel Higuera Vaquero <angelhiguera@acidtango.com>
2022-06-09 14:23:58 +0200

    drop on delete cascade on projects on legacy project import table

---
+++ api/apps/api/src/migrations/api/1654769225640-DropOnDeleteCascadeOnProjectsInLegacyProjectImportTable.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class DropOnDeleteCascadeOnProjectsLegacyProjectImport1654769225640
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE legacy_project_imports 
+        DROP CONSTRAINT legacy_project_imports_project_id_fkey;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE legacy_project_imports
+        ADD CONSTRAINT legacy_project_imports_project_id_fkey FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE CASCADE;
+    `);
+  }
+}
