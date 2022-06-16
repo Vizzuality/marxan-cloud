78e8f85e786e330330410dc201ca9744ae05678c
Ángel Higuera Vaquero <angelhiguera@acidtango.com>
2022-06-16 17:46:30 +0200

    feat: add delete unused resources cleanup worker and  processor

---
+++ api/apps/geoprocessing/src/modules/unused-resources-cleanup/delete-unused-resources/delete-unused-resources.module.ts
@@ -0,0 +1,39 @@
+import { geoprocessingConnections } from '@marxan-geoprocessing/ormconfig';
+import { ProjectsPuEntity } from '@marxan-jobs/planning-unit-geometry';
+import { BlmFinalResultEntity } from '@marxan/blm-calibration';
+import { ScenarioFeaturesData } from '@marxan/features';
+import { GeoFeatureGeometry } from '@marxan/geofeatures';
+import {
+  MarxanExecutionMetadataGeoEntity,
+  OutputScenariosPuDataGeoEntity,
+} from '@marxan/marxan-output';
+import { PlanningArea } from '@marxan/planning-area-repository/planning-area.geo.entity';
+import { ProtectedArea } from '@marxan/protected-areas';
+import { ScenariosPuPaDataGeo } from '@marxan/scenarios-planning-unit';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeleteProjectUnsusedReosurces } from './delete-project-unused-resources';
+import { DeleteScenarioUnsusedReosurces } from './delete-scenario-unused-resources';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([], geoprocessingConnections.apiDB),
+    TypeOrmModule.forFeature(
+      [
+        BlmFinalResultEntity,
+        ScenariosPuPaDataGeo,
+        OutputScenariosPuDataGeoEntity,
+        PlanningArea,
+        ProtectedArea,
+        GeoFeatureGeometry,
+        ProjectsPuEntity,
+        ScenarioFeaturesData,
+        MarxanExecutionMetadataGeoEntity,
+      ],
+      geoprocessingConnections.default,
+    ),
+  ],
+  providers: [DeleteProjectUnsusedReosurces, DeleteScenarioUnsusedReosurces],
+  exports: [DeleteProjectUnsusedReosurces, DeleteScenarioUnsusedReosurces],
+})
+export class UnusedResourcesCleanUpModule {}
---
+++ api/libs/unused-resources-cleanup/src/job-output.ts
@@ -0,0 +1,3 @@
+import { UnusedResourcesCleanupJobInput } from './job-input';
+
+export type UnusedResourcesCleanupJobOutput = UnusedResourcesCleanupJobInput;
