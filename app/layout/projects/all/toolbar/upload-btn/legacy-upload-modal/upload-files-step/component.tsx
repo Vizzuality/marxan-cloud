ff5fa44d2ba043645115e694e34378f051a1a5f1
anamontiaga <amontiaga@gmail.com>
2022-06-27 18:08:59 +0200

    MARXAN-1357: add info for uploading legacy project files

--- app/layout/projects/all/toolbar/upload-btn/legacy-upload-modal/upload-files-step/component.tsx
+++ app/layout/projects/all/toolbar/upload-btn/legacy-upload-modal/upload-files-step/component.tsx
@@ -174,7 +174,84 @@ export const UploadFilesStep: React.FC<UploadFilesStepProps> = ({
           return (
             <form onSubmit={handleSubmit}>
               <div className="p-9">
-                <h4 className="mb-5 text-lg text-black font-heading">Upload legacy project</h4>
+
+                <div className="flex space-x-2">
+                  <h4 className="mb-5 text-lg text-black font-heading">Upload legacy project</h4>
+                  <InfoButton
+                    theme="primary"
+                  >
+                    <div>
+                      <h4 className="font-heading text-lg mb-2.5">
+                        When uploading planning unit grid and input db
+                      </h4>
+                      <p>
+                        This may be the case when users are transitioning a legacy project to the
+                        Marxan MaPP platform (rather than making it available as an historical
+                        archive, for example).
+                      </p>
+                      <br />
+                      <ul className="pl-6 space-y-1 list-disc list-outside">
+                        <li>
+                          Users will not be able to add, remove or combine (split/stratification)
+                          features;
+                        </li>
+                        <li>
+                          the exact spatial distribution of features will not be available for
+                          display;
+                        </li>
+                        <li>
+                          planning unit lock status will be set from input `pu.dat`, but without
+                          first adding  spatial data for protected areas, users won&apos;t be able
+                          to rely on protected areas to set the default lock status of planning
+                          units.
+                        </li>
+                      </ul>
+                      <br />
+
+                      <h4 className="font-heading text-lg mb-2.5">
+                        When uploading planning unit grid, input db and output db
+                      </h4>
+                      <p>
+                        This may be the case when users intend to upload a historical project to be
+                        shared in  its archived state.
+                      </p>
+                      <br />
+                      <ul className="pl-6 space-y-1 list-disc list-outside">
+                        <li>
+                          dentical limitations to what was described in the previous use case will
+                          apply (in case users wish to continue working on the project within the
+                          Marxan MaPP platform);
+                        </li>
+                        <li>
+                          output data may be locked (i.e. running Marxan will be disallowed) if the
+                          user wishes to preserve a historical record of solutions calculated
+                          outside of the Marxan MaPP platform.
+                        </li>
+                      </ul>
+                      <br />
+
+                      <h4 className="font-heading text-lg mb-2.5">
+                        When uploading planning unit grid, input db, output db and feature data
+                      </h4>
+                      <p>
+                        This may be the case when users intend to transition to using Marxan MaPP
+                        for an existing project for which extensive source data is available; they
+                        may wish to showcase an historical record of the project as created
+                        outside of Marxan MaPP (by keeping the original output solutions intact),
+                        while working on a copy of the original project as a new scenario or set
+                        of scenarios, adding any further spatial data directly within the Marxan
+                        MaPP platform.
+                      </p>
+                      <br />
+                      <p>
+                        With the data supplied at project import stage, some limitations will still
+                        apply (for  example, default lock status from protected areas, until a
+                        protected area shapefile is uploaded in a cloned scenario) but the imported
+                        legacy project will largely be functional like a native Marxan MaPP project.
+                      </p>
+                    </div>
+                  </InfoButton>
+                </div>
 
                 <div className="space-y-5">
                   {LEGACY_FIELDS.map((f) => {
