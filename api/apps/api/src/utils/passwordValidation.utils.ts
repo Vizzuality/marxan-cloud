6431c071b24a890304c5b5b385e5d3145ee331d8
Ruben Vallejo <ralexanderv89@gmail.com>
2022-07-13 18:46:33 +0200

    feat: changes function name, moves entropy threshold to service

--- api/apps/api/src/utils/passwordValidation.utils.ts
+++ api/apps/api/src/utils/passwordValidation.utils.ts
@@ -1,12 +1,12 @@
 import { Either, left, right } from 'fp-ts/lib/Either';
 
-export const passwordEntropyValidation = (
+export const isStringEntropyHigherThan = (
+  entropyThreshold: number,
   password: string,
 ): Either<string, void> => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const entropyChecker = require('fast-password-entropy');
   const result = entropyChecker(password);
-  const entropyThreshold = 80;
   if (result < entropyThreshold) {
     return left('Weak Password. Please use a stronger password.');
   }
