46a15e5d0e298df7a0ca4577f67f7c57f92b7e66
Ruben Vallejo <ralexanderv89@gmail.com>
2022-07-14 12:53:43 +0200

    feat: adds cleanup for features_data inside cleanup service

---
+++ api/apps/geoprocessing/src/migrations/geoprocessing/1657795596079-CreateFeaturesDataCleanupPreparation.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateFeaturesDataCleanupPreparation1657795596079
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE features_data_cleanup_preparation (
+                feature_id uuid PRIMARY KEY
+              );`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            DROP TABLE features_data_cleanup_preparation;
+        `);
+  }
+}
