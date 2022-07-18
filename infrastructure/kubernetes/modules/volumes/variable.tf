97c7a2a8052da601db4b5761b69524d13f985e6f
Tiago Garcia <tiagojsag@gmail.com>
2022-07-18 11:47:16 +0200

    Simplify k8s variable management for TF

--- infrastructure/kubernetes/modules/volumes/variable.tf
+++ infrastructure/kubernetes/modules/volumes/variable.tf
@@ -1,23 +1,3 @@
-variable "k8s_host" {
-  description = "Hostname of the k8s cluster"
-  type        = string
-}
-
-variable "k8s_client_certificate" {
-  description = "Client certificate for the k8s cluster"
-  type        = string
-}
-
-variable "k8s_client_key" {
-  description = "Client key for the k8s cluster"
-  type        = string
-}
-
-variable "k8s_cluster_ca_certificate" {
-  description = "Cluster CA certificate for the k8s cluster"
-  type        = string
-}
-
 variable "namespace" {
   description = "The k8s namespace in which to deploy resources"
 }
