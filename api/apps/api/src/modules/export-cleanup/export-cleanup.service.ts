070ddeb724ff29fd3109774101b1c4b3f68bce52
Ruben Vallejo <ralexanderv89@gmail.com>
2022-07-20 14:52:22 +0200

    feat: use correct valid projects exports SQL

--- api/apps/api/src/modules/export-cleanup/export-cleanup.service.ts
+++ api/apps/api/src/modules/export-cleanup/export-cleanup.service.ts
@@ -5,7 +5,6 @@ import { EntityManager } from 'typeorm';
 import { ExportCleanup } from './export-cleanup';
 import { apiConnections } from '@marxan-api/ormconfig';
 import { AppConfig } from '@marxan-api/utils/config.utils';
-import { execSync } from 'child_process';
 
 const validityIntervalInHours = AppConfig.get<string>(
   'storage.cloningFileStorage.artifactValidityInHours',
@@ -23,8 +22,8 @@ export class ExportCleanupService implements ExportCleanup {
     const expiredProjectExportsIds = await this.apiEntityManager.query(`
       SELECT e.id FROM exports e
         WHERE e.resource_kind = 'project' AND 
-        WHERE e.created_at > NOW() - INTERVAL ${validityIntervalInHours} HOUR) AND
-        WHERE e.resource_id NOT IN (SELECT id FROM published_projects);
+        (AGE(NOW(), e.created_at) <  INTERVAL ${validityIntervalInHours} HOUR OR
+        e.resource_id NOT IN (SELECT id FROM published_projects));
     `);
 
     const expiredScenarioExportsIds = await this.apiEntityManager.query(`
@@ -49,7 +48,5 @@ export class ExportCleanupService implements ExportCleanup {
     );
 
     const expiredResourcesIds = await this.identifyExpiredResources();
-
-    // await this.deleteExpiredResources(expiredResourcesIds);
   }
 }
