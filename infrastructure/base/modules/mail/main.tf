6306c0a2cfd588d46729ae6c04d37292d98f9011
Tiago Garcia <tiagojsag@gmail.com>
2022-07-22 14:37:42 +0200

    Additional cleanup on TF code

--- infrastructure/base/modules/mail/main.tf
+++ infrastructure/base/modules/mail/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_dns_txt_record" "sparkpost_dkim_txt_record" {
   ttl                 = 300
   name                = var.dkim_name
   record {
-    value =var.dkim_value
+    value = var.dkim_value
   }
 
   tags = var.project_tags
--- infrastructure/kubernetes/modules/key_vault/outputs.tf
+++ infrastructure/kubernetes/modules/key_vault/outputs.tf
@@ -2,7 +2,3 @@ output "key_vault_id" {
   value       = azurerm_key_vault.key_vault.id
   description = "Azure key vault id"
 }
-
-output current_client_config {
-    value = data.azurerm_client_config.current
-}
