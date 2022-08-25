2ac76ce2cd943686b7ccc084ab3dc97c4b0692f0
andrea rota <andrea.rota@vizzuality.com>
2022-08-25 15:06:44 +0200

    narrow ability to accept nonsense (fixes: MARXAN-1764)

--- api/apps/api/src/modules/projects/dto/create.project.dto.ts
+++ api/apps/api/src/modules/projects/dto/create.project.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
+  registerDecorator,
   IsAlpha,
   IsEnum,
   IsNumber,
@@ -9,8 +10,41 @@ import {
   IsUppercase,
   IsUUID,
   Length,
+  ValidationOptions,
+  ValidationArguments,
 } from 'class-validator';
 import { PlanningUnitGridShape } from '@marxan/scenarios-planning-unit';
+import { isUndefined } from 'lodash';
+
+export function EitherCustomPlanningAreaOrGadmAreaMustBeSetButNotBoth(
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'eitherCustomPlanningAreaOrGadmAreaMustBeSetButNotBoth',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(_value: any, args: ValidationArguments) {
+          const {
+            countryId,
+            adminAreaLevel1Id,
+            adminAreaLevel2Id,
+            planningAreaId,
+          } = args.object as CreateProjectDTO;
+
+          // xor
+          return (
+            (isUndefined(countryId) &&
+              isUndefined(adminAreaLevel1Id) &&
+              isUndefined(adminAreaLevel2Id)) != isUndefined(planningAreaId)
+          );
+        },
+      },
+    });
+  };
+}
 
 /**
  * @todo We have this dto partially duplicated in the geoprocessing service
@@ -32,6 +66,10 @@ export class CreateProjectDTO {
   @ApiPropertyOptional()
   @IsUUID()
   @IsOptional()
+  @EitherCustomPlanningAreaOrGadmAreaMustBeSetButNotBoth({
+    message:
+      'Either a custom planning area id or a GADM area must be set, but not both',
+  })
   planningAreaId?: string;
 
   @ApiPropertyOptional({
@@ -42,16 +80,28 @@ export class CreateProjectDTO {
   @IsUppercase()
   @Length(3, 3)
   @IsOptional()
+  @EitherCustomPlanningAreaOrGadmAreaMustBeSetButNotBoth({
+    message:
+      'Either a custom planning area id or a GADM area must be set, but not both',
+  })
   countryId?: string;
 
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @EitherCustomPlanningAreaOrGadmAreaMustBeSetButNotBoth({
+    message:
+      'Either a custom planning area id or a GADM area must be set, but not both',
+  })
   adminAreaLevel1Id?: string;
 
   @ApiPropertyOptional()
   @IsString()
   @IsOptional()
+  @EitherCustomPlanningAreaOrGadmAreaMustBeSetButNotBoth({
+    message:
+      'Either a custom planning area id or a GADM area must be set, but not both',
+  })
   adminAreaLevel2Id?: string;
 
   @ApiPropertyOptional()
