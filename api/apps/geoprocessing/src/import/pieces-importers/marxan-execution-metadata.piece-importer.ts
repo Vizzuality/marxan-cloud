b5149a0f5a300c742f7f85da69fafb1300174e7e
andrea rota <andrea.rota@vizzuality.com>
2022-08-31 15:28:30 +0200

    filter out marxan run metadata without corresponding input/output folders; chunk inserts
    Also skip tests that rely on the previous behaviour.

--- api/apps/geoprocessing/src/import/pieces-importers/marxan-execution-metadata.piece-importer.ts
+++ api/apps/geoprocessing/src/import/pieces-importers/marxan-execution-metadata.piece-importer.ts
@@ -1,11 +1,11 @@
 import { geoprocessingConnections } from '@marxan-geoprocessing/ormconfig';
+import { CHUNK_SIZE_FOR_BATCH_GEODB_OPERATIONS } from '@marxan-geoprocessing/utils/chunk-size-for-batch-geodb-operations';
 import { ClonePiece, ImportJobInput, ImportJobOutput } from '@marxan/cloning';
 import { CloningFilesRepository } from '@marxan/cloning-files-repository';
 import { ComponentLocationSnapshot } from '@marxan/cloning/domain';
 import {
   isMarxanExecutionMetadataFolderRelativePath,
   MarxanExecutionMetadataContent,
-  MarxanExecutionMetadataFolderType,
   marxanExecutionMetadataRelativePath,
 } from '@marxan/cloning/infrastructure/clone-piece-data/marxan-execution-metadata';
 import { MarxanExecutionMetadataGeoEntity } from '@marxan/marxan-output';
@@ -114,30 +114,96 @@ export class MarxanExecutionMetadataPieceImporter
     await this.entityManager
       .getRepository(MarxanExecutionMetadataGeoEntity)
       .save(
-        marxanExecutionMetadata.map((metadata) => {
-          const inputZip = buffers[`${metadata.id}-input`];
-          const outputZip = buffers[`${metadata.id}-output`];
+        marxanExecutionMetadata
+          /**
+           * The original implementation threw errors if we didn't have the
+           * input folder for a given Marxan run (execution) or if we didn't
+           * have the output folder while the metadata said we should have it.
+           *
+           * I am not entirely sure about the rationale for that, looking back
+           * at this now: I think we can "just" skip any Marxan runs for which
+           * we may have metadata in `marxanExecutionMetadata`, but no
+           * corresponding input or output folders. In practice we only show in
+           * the app the solutions of the latest Marxan run, so importing (and
+           * before that, exporting) metadata of previous runs may not be useful
+           * in practice.
+           *
+           * So I think we can ignore marxan execution metadata rows with
+           * missing input/output folders: I am not sure why we would have any
+           * metadata for a Marxan run _but_ no related input or output folders,
+           * as these are stored alongside run metadata in the
+           * `(geodb)marxan_execution_metadata` table, but nevertheless, this
+           * was a real issue until we introduced this filter, leading to some
+           * imports to fail, where they would have been otherwise fully valid
+           * (moreover, the marxan run metadata that _did_ import even after
+           * adding this filter was enough to show solutions).
+           *
+           * In practice this may be an issue only if force-importing on an
+           * instance Marxan projects that have been exported from another
+           * instance (this would only be possible at all if sharing the signing
+           * key between the two instances, which should only be done on
+           * development instances for triaging purposes, and if the export
+           * makes no reference to platform-wide features or protected areas).
+           *
+           * The worst that may happen is that a scenario is imported without
+           * marxan run metadata, leading to having to re-run Marxan.
+           *
+           * @debt Review this once the export/import flow is thoroughly tested
+           * and blessed as stable - we may then remove again the filter and
+           * restore the original, stricter erroring out in case of unexpectedly
+           * missing data ()
+           */
+          .filter((metadata) => {
+            const inputZip = buffers[`${metadata.id}-input`];
+            const outputZip = buffers[`${metadata.id}-output`];
 
-          if (!inputZip) {
-            const errorMessage = `uris array doesn't contain uri for metadata input folder for execution ${metadata.id}`;
-            this.logger.error(errorMessage);
-            throw new Error(errorMessage);
-          }
-          if (metadata.includesOutputFolder && !outputZip) {
-            const errorMessage = `uris array doesn't contain uri for metadata output folder for execution ${metadata.id}`;
-            this.logger.error(errorMessage);
-            throw new Error(errorMessage);
-          }
+            if (!inputZip) {
+              this.logger.warn(
+                `uris array doesn't contain uri for metadata input folder for execution ${metadata.id}`,
+              );
+            }
+            if (metadata.includesOutputFolder && !outputZip) {
+              this.logger.warn(
+                `uris array doesn't contain uri for metadata output folder for execution ${metadata.id}`,
+              );
+            }
 
-          return {
-            scenarioId,
-            stdOutput: metadata.stdOutput,
-            stdError: metadata.stdError,
-            inputZip,
-            outputZip,
-            failed: metadata.failed,
-          };
-        }),
+            return inputZip && metadata.includesOutputFolder ? outputZip : true;
+          })
+          .map((metadata) => {
+            const inputZip = buffers[`${metadata.id}-input`];
+            const outputZip = buffers[`${metadata.id}-output`];
+
+            /**
+             * @debt These checks should never trigger an exception as long as
+             * the filter above is in place. See comment about the filter for
+             * full rationale.
+             *
+             * These checks are left here so that if going back to stricter
+             * checks in the future, all that will be needed will be removing
+             * the filter above.
+             */
+            if (!inputZip) {
+              const errorMessage = `uris array doesn't contain uri for metadata input folder for execution ${metadata.id}`;
+              this.logger.error(errorMessage);
+              throw new Error(errorMessage);
+            }
+            if (metadata.includesOutputFolder && !outputZip) {
+              const errorMessage = `uris array doesn't contain uri for metadata output folder for execution ${metadata.id}`;
+              this.logger.error(errorMessage);
+              throw new Error(errorMessage);
+            }
+
+            return {
+              scenarioId,
+              stdOutput: metadata.stdOutput,
+              stdError: metadata.stdError,
+              inputZip,
+              outputZip,
+              failed: metadata.failed,
+            };
+          }),
+        { chunk: CHUNK_SIZE_FOR_BATCH_GEODB_OPERATIONS },
       );
 
     return {
