c3e5c6657e1c1875b651a3ec65ab1e845780b102
anamontiaga <amontiaga@gmail.com>
2022-10-07 18:13:59 +0200

    documentation component

--- app/layout/help/documentation/component.tsx
+++ app/layout/help/documentation/component.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Icon from 'components/icon';
 
+import DOCUMENTATION_HOVER_SVG from 'svgs/ui/documentation-hover.svg?sprite';
 import DOCUMENTATION_SVG from 'svgs/ui/documentation.svg?sprite';
 
 export const DocumentationLink = () => {
+  const [isHover, setIsHover] = useState(false);
   return (
 
-    <div
-      className="fixed z-50 p-2 right-2.5 bottom-32"
+    <a
+      className="fixed z-50 p-2 right-1 bottom-28"
+      href="https://marxansolutions.org"
+      target="_blank"
+      rel="noopener noreferrer"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
-      <Icon className="flex-shrink-0 w-6 h-6 fill-red-500" icon={DOCUMENTATION_SVG} />
-    </div>
+      <div className="px-1 pb-5 border-b border-gray-500">
+        <Icon className="flex-shrink-0 w-6 h-6" icon={isHover ? DOCUMENTATION_HOVER_SVG : DOCUMENTATION_SVG} />
+      </div>
+      {isHover && (
+
+        <div className="absolute px-2 py-px text-black bg-white top-2 right-12 z-60 rounded-xl">
+          <p className="font-sans text-sm whitespace-nowrap">
+            Marxan&apos;s documentation
+          </p>
+        </div>
+      )}
+    </a>
+
   );
 };
 
