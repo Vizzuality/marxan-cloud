89aab1d0feefcead142888201352af5680097ef6
andrea rota <andrea.rota@vizzuality.com>
2022-10-20 13:37:25 +0100

    add missing variables/arguments

--- infrastructure/remote-state/outputs.tf
+++ infrastructure/remote-state/outputs.tf
@@ -1,7 +1,4 @@
 output "storage_account_primary_access_key" {
   value = data.azurerm_storage_account.storage_account.primary_access_key
-}
-
-output "storage_account_name" {
-  value = data.azurerm_storage_account.storage_account.name
+  sensitive = true
 }
