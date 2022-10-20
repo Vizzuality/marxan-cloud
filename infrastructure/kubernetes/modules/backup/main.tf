4ce1c8625ed604e7c7b36575ce1327ff361a4139
andrea rota <andrea.rota@vizzuality.com>
2022-10-20 13:38:03 +0100

    structure cronjob manifest properly

--- infrastructure/kubernetes/modules/backup/main.tf
+++ infrastructure/kubernetes/modules/backup/main.tf
@@ -9,7 +9,7 @@ resource "kubernetes_cron_job_v1" "cloning_storage_backup_cronjob" {
     schedule                      = var.schedule
     starting_deadline_seconds     = 60
 
-    template {
+    job_template {
       metadata {
         labels = {
           name = "cloning-storage-backup"
@@ -17,56 +17,61 @@ resource "kubernetes_cron_job_v1" "cloning_storage_backup_cronjob" {
       }
 
       spec {
-        volume {
-          name = "shared-cloning-storage"
-          persistent_volume_claim {
-            claim_name = var.cloning_pvc_name
-          }
-        }
-
-        container {
-          name    = "cloning-storage-backup-agent"
-          image   = "restic/restic:0.14.0"
-          command = [ "/bin/sh" ]
-          args = ["-c", "restic init ; restic backup ${var.backup_source} ; restic --prune forget ${var.restic_forget_cli_parameters} ; restic check"]
-
-          volume_mount {
-            mount_path  = var.cloning_volume_mount_path
-            name        = "shared-cloning-storage"
-          }
-
-          env {
-            name  = "AZURE_ACCOUNT_NAME"
-            value = var.azure_storage_account_name
-          }
-
-          env {
-            name = "AZURE_ACCOUNT_KEY"
-            value_from {
-              secret_key_ref {
-                name = "api"
-                key  = "AZURE_STORAGE_ACCOUNT_KEY"
+        template {
+          metadata {}
+          spec {
+            volume {
+              name = "shared-cloning-storage"
+              persistent_volume_claim {
+                claim_name = var.cloning_pvc_name
               }
             }
-          }
 
-          env {
-            name = "RESTIC_PASSWORD"
-            value_from {
-              secret_key_ref {
-                name = "api"
-                key  = "CLONING_STORAGE_BACKUP_RESTIC_PASSWORD"
+            container {
+              name    = "cloning-storage-backup-agent"
+              image   = "restic/restic:0.14.0"
+              command = [ "/bin/sh" ]
+              args = ["-c", "restic init ; restic backup ${var.cloning_volume_mount_path} ; restic --prune forget ${var.restic_forget_cli_parameters} ; restic check"]
+
+              volume_mount {
+                mount_path  = var.cloning_volume_mount_path
+                name        = "shared-cloning-storage"
+              }
+
+              env {
+                name  = "AZURE_ACCOUNT_NAME"
+                value = var.azure_storage_account_name
+              }
+
+              env {
+                name = "AZURE_ACCOUNT_KEY"
+                value_from {
+                  secret_key_ref {
+                    name = "api"
+                    key  = "AZURE_STORAGE_ACCOUNT_KEY"
+                  }
+                }
+              }
+
+              env {
+                name = "RESTIC_PASSWORD"
+                value_from {
+                  secret_key_ref {
+                    name = "api"
+                    key  = "CLONING_STORAGE_BACKUP_RESTIC_PASSWORD"
+                  }
+                }
+              }
+
+              env {
+                name = "RESTIC_REPOSITORY"
+                value = var.restic_repository
               }
             }
-          }
 
-          env {
-            name = "RESTIC_REPOSITORY"
-            value = var.restic_repository
+            restart_policy = "Never"
           }
         }
-
-        restart_policy = "Never"
       }
     }
   }
