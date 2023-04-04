46721f8aa27517bb58b38a4287aaf16154032d09
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2023-04-04 11:03:24 +0200

    cypress@12

--- app/cypress/integration/home__test__.js
+++
@@ -1,9 +0,0 @@
-// context('Home page', () => {
-//   beforeEach(() => {
-//     cy.visit('/');
-//   });
-
-//   it('should verify the title of home page', () => {
-//     cy.title().should('eq', 'Home');
-//   });
-// });
