6134d37eee17466ad27a7144c746f162978c12ab
Yulia Belyakova <yuliya.beliakova@gmail.com>
2023-04-24 11:08:50 +0200

    Refactor DataSource initialization for postgres version check util

--- api/apps/api/src/api-data-source.ts
+++
@@ -1,7 +0,0 @@
-import { DataSource } from 'typeorm';
-import { apiConnections } from '@marxan-api/ormconfig';
-
-export const newApiMigrationDataSource: DataSource = new DataSource({
-  ...apiConnections.default,
-  name: 'PostgresUtils',
-});
--- api/apps/api/src/utils/postgresql.utils.ts
+++ api/apps/api/src/utils/postgresql.utils.ts
@@ -10,14 +10,13 @@ export class PostgreSQLUtils {
    * Check if the PostgreSQL server we are connected to is version 13 or higher.
    */
   static async version13Plus(): Promise<boolean> {
-    await newApiMigrationDataSource.initialize();
+    // Here we do not need to initialize DataSource again, because it is happening internally when starting the migration process
     const postgresqlMajorVersion = await newApiMigrationDataSource
       .query('show server_version')
       .then((result: [{ server_version: string }]) => {
         return result[0].server_version.split('.')[0];
       })
       .then((majorVersion: string) => Number(majorVersion));
-    await newApiMigrationDataSource.destroy();
     return postgresqlMajorVersion >= 13;
   }
 }
