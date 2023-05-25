88cad7ff891875ccb588a62bd8bee34d07d54b69
andrea rota <andrea.rota@vizzuality.com>
2023-05-25 13:17:01 +0100

    update remoteObject to current Puppeteer API

--- webshot/src/utils/passthrough-console.utils.ts
+++ webshot/src/utils/passthrough-console.utils.ts
@@ -3,5 +3,5 @@ import { inspect } from 'util';
 
 export const passthroughConsole = async (msg: puppeteer.ConsoleMessage) => {
   for (let i = 0; i < msg.args().length; ++i)
-    console.debug(inspect(msg.args()[i]?._remoteObject, undefined, 5));
+    console.debug(inspect(msg.args()[i]?.remoteObject, undefined, 5));
 };
