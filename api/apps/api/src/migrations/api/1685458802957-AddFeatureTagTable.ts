adc2e8430ccecc44f96ed4f364be34943714101f
Kevin <kew.sanchez@gmail.com>
2023-06-01 17:46:39 +0200

    feat(Feature Tags): New Trigger to update news tags based on existing ones
    In order to mantain capitalization consistency, a new trigger function is added, that will check for existing capizalition equivalent tag of the incoming new tag, and use that one instead if found.

--- api/apps/api/src/migrations/api/1685458802957-AddFeatureTagTable.ts
+++ api/apps/api/src/migrations/api/1685458802957-AddFeatureTagTable.ts
@@ -11,17 +11,44 @@ export class AddFeatureTagTable1685458802957 implements MigrationInterface {
     );`);
 
     await queryRunner.query(`
-      CREATE UNIQUE INDEX unique_project_feature ON project_feature_tags (project_id, feature_id);
+      CREATE UNIQUE INDEX single_tag_on_project_features ON project_feature_tags (project_id, feature_id);
     `);
 
+    // A trigger that will update the tag of the incoming insert to use a capitalization equivalent tag that already exists
+    // for the same project
     await queryRunner.query(`
-      CREATE UNIQUE INDEX unique_project_tag ON project_feature_tags (project_id, LOWER(tag));
+    CREATE OR REPLACE FUNCTION tr_equivalent_capitalization_tag()
+    RETURNS TRIGGER AS $BODY$
+    DECLARE
+      equivalent_tag project_feature_tags.tag%type ;
+      equivalent_tag_counter integer;
+    BEGIN
+      select count(*) into equivalent_tag_counter from project_feature_tags pft where LOWER(pft.tag) = LOWER(NEW.tag);
+      IF equivalent_tag_counter >= 1 THEN
+        select tag into equivalent_tag from project_feature_tags pft where LOWER(pft.tag) = LOWER(NEW.tag) limit 1;
+        NEW.tag = equivalent_tag;
+      ELSE
+      END IF;
+
+      RETURN NEW;
+    end
+    $BODY$ LANGUAGE plpgsql;
+
+    DROP TRIGGER IF EXISTS tr_project_feature_tags_insert ON project_feature_tags;
+
+    CREATE TRIGGER tr_project_feature_tags_insert
+    BEFORE INSERT ON project_feature_tags
+    FOR EACH ROW EXECUTE
+    PROCEDURE tr_equivalent_capitalization_tag();
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX unique_project_tag`);
-    await queryRunner.query(`DROP INDEX unique_project_feature`);
-    await queryRunner.query(`DROP TABLE project_feature_tags`);
+    // Indexes will be implicitly dropped with the table
+    await queryRunner.query(`
+        DROP TRIGGER IF EXISTS tr_project_feature_tags_insert on project_feature_tags;
+        DROP FUNCTION IF EXISTS tr_equivalent_capitalization_tag();
+        DROP TABLE project_feature_tags;
+    `);
   }
 }
