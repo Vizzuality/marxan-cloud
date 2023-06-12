ad59079ffe889fba604d4b15d61a7ec052657c1f
Yulia Belyakova <yuliya.beliakova@gmail.com>
2023-06-12 20:06:40 +0200

    Update webshot service mock for tests

--- api/apps/api/test/utils/webshot.service-fake.ts
+++ api/apps/api/test/utils/webshot.service-fake.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Readable } from 'stream';
-import { left, right } from 'fp-ts/Either';
-import { unknownPdfWebshotError } from '@marxan/webshot';
+import { Either, left, right } from 'fp-ts/Either';
+import {
+  unknownPdfWebshotError,
+  unknownPngWebshotError,
+  WebshotPngConfig,
+} from '@marxan/webshot';
 import * as fs from 'fs';
+import { lastValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class FakeWebshotService {
-  constructor() {}
+  constructor(private readonly httpService: HttpService) {}
 
   async getScenarioFrequencyComparisonMap() {
     try {
@@ -20,6 +26,33 @@ export class FakeWebshotService {
       return right(stream);
     } catch (error) {
       return left(unknownPdfWebshotError);
+    }
+  }
+
+  async getPublishedProjectsImage(
+    scenarioId: string,
+    projectId: string,
+    config: WebshotPngConfig,
+    webshotUrl: string,
+  ): Promise<Either<typeof unknownPngWebshotError, string>> {
+    try {
+      const pngBuffer = await lastValueFrom(
+        this.httpService.post(
+          `${webshotUrl}/projects/${projectId}/scenarios/${scenarioId}/published-projects/frequency`,
+          config,
+          { responseType: 'arraybuffer' },
+        ),
+      )
+        .then((response) => response.data)
+        .catch((error) => {
+          throw new Error(error);
+        });
+
+      const pngBase64String = Buffer.from(pngBuffer).toString('base64');
+
+      return right(pngBase64String);
+    } catch (error) {
+      return left(unknownPngWebshotError);
     }
   }
 }
