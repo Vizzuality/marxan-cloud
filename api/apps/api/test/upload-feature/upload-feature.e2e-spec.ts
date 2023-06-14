c03e5d0c66d785956dfa59be5c3584bd58327a2f
Kevin <kew.sanchez@gmail.com>
2023-06-14 16:05:52 +0200

    feat(Feature Tags): Extends the upload feature shapefile endpoint to accept tags

--- api/apps/api/test/upload-feature/upload-feature.e2e-spec.ts
+++ api/apps/api/test/upload-feature/upload-feature.e2e-spec.ts
@@ -1,5 +1,7 @@
 import { FixtureType } from '@marxan/utils/tests/fixture-type';
 import { getFixtures } from './upload-feature.fixtures';
+import { tagMaxlength } from '@marxan-api/modules/geo-feature-tags/dto/update-geo-feature-tag.dto';
+import { HttpStatus } from '@nestjs/common';
 
 let fixtures: FixtureType<typeof getFixtures>;
 
@@ -12,6 +14,79 @@ afterEach(async () => {
 });
 
 test(`custom feature upload`, async () => {
-  const result = await fixtures.WhenUploadingCustomFeature();
-  await fixtures.ThenGeoFeaturesAreCreated(result);
+  const name = 'someFeature';
+  const description = 'someDescrip';
+
+  const result = await fixtures.WhenUploadingCustomFeature(name, description);
+
+  await fixtures.ThenGeoFeaturesAreCreated(result, name, description);
+});
+
+test(`if tagging info is included in DTO but invalid, then error is returned and everything is rolled back`, async () => {
+  // ARRANGE
+  const name = 'invalidFeat';
+  const description = 'invalidDesc';
+  const invalidTag1 = 'some\ntag';
+  const invalidTag2 = `t${'a'.repeat(tagMaxlength + 1)}g`;
+
+  // ACT / ASSERT
+  const result1 = await fixtures.WhenUploadingCustomFeature(
+    name,
+    description,
+    invalidTag1,
+  );
+  expect(result1.status).toBe(HttpStatus.BAD_REQUEST);
+  fixtures.ThenInvalidTagErrorWasReturned(result1);
+  await fixtures.ThenNoGeoFeatureIsCreated(result1, name);
+
+  const result2 = await fixtures.WhenUploadingCustomFeature(
+    name,
+    description,
+    invalidTag2,
+  );
+  expect(result2.status).toBe(HttpStatus.BAD_REQUEST);
+  fixtures.ThenMaxLengthErrorWasReturned(result2);
+  await fixtures.ThenNoGeoFeatureIsCreated(result2, name);
+});
+
+test(`if tagging info is included in DTO and valid, created feature should be tagged properly`, async () => {
+  // ARRANGE
+  const name = 'someFeature';
+  const description = 'someDescrip';
+  const tag = 'someTag';
+
+  // ACT
+  const result = await fixtures.WhenUploadingCustomFeature(
+    name,
+    description,
+    tag,
+  );
+
+  // ASSERT
+  await fixtures.ThenGeoFeaturesAreCreated(result, name, description);
+  await fixtures.ThenGeoFeatureTagIsCreated(name, tag);
+});
+
+test(`if there is already an existing feature with a tag that has equivalent capitalization to the one included in the custom feature DTO, the existing one will be used for the new feature`, async () => {
+  // ARRANGE
+  const equivalentTag = 'some-Tag';
+  const name = 'someFeature';
+  const description = 'someDescrip';
+
+  const featureWithEquivalentTagId = await fixtures.GivenFeatureOnProject(
+    'equivalentTagFeature',
+  );
+  await fixtures.GivenTagOnFeature(featureWithEquivalentTagId, equivalentTag);
+
+  // ACT
+  const result = await fixtures.WhenUploadingCustomFeature(
+    name,
+    description,
+    'SomE-TAG',
+  );
+
+  // ASSERT
+  await fixtures.ThenGeoFeaturesAreCreated(result, name, description);
+  await fixtures.ThenGeoFeatureTagIsCreated(name, equivalentTag);
+  // TODO Check for update of last_modified_at for all affected tag rows for project when implemented
 });
--- api/apps/api/test/upload-feature/upload-feature.fixtures.ts
+++ api/apps/api/test/upload-feature/upload-feature.fixtures.ts
@@ -8,6 +8,8 @@ import { Repository } from 'typeorm';
 import * as request from 'supertest';
 import { GeoFeatureGeometry } from '@marxan/geofeatures';
 import { DbConnections } from '@marxan-api/ormconfig.connections';
+import { GeoFeatureTag } from '@marxan-api/modules/geo-feature-tags/geo-feature-tag.api.entity';
+import { tagMaxlength } from '@marxan-api/modules/geo-feature-tags/dto/update-geo-feature-tag.dto';
 
 export const getFixtures = async () => {
   const app = await bootstrapApplication();
@@ -23,14 +25,15 @@ export const getFixtures = async () => {
       name: `Organization ${Date.now()}`,
     },
   );
-  const customFeatureName = `User custom feature ${Date.now()}`;
-  const customFeatureDesc = `User custom feature desc`;
 
   const geoFeaturesApiRepo: Repository<GeoFeature> = app.get(
     getRepositoryToken(GeoFeature),
   );
   const geoFeatureDataRepo: Repository<GeoFeatureGeometry> = app.get(
     getRepositoryToken(GeoFeatureGeometry, DbConnections.geoprocessingDB),
+  );
+  const geoFeatureTagRepo: Repository<GeoFeatureTag> = app.get(
+    getRepositoryToken(GeoFeatureTag),
   );
 
   return {
@@ -51,29 +54,108 @@ export const getFixtures = async () => {
       await cleanup();
       await app.close();
     },
-    WhenUploadingCustomFeature: async () =>
-      request(app.getHttpServer())
+
+    // ARRANGE
+    GivenFeatureOnProject: async (featureName: string) => {
+      const results: {
+        id: string;
+      }[] = await geoFeaturesApiRepo.query(`INSERT INTO features
+            (feature_class_name, alias, description, property_name, intersection, project_id, creation_status, created_by)
+          VALUES
+            ('${featureName}', 'alias_${featureName}', null, ' name', null, '${projectId}', 'created', (SELECT id FROM users WHERE email = 'aa@example.com'))
+          RETURNING id;
+        `);
+
+      return results[0].id;
+    },
+
+    GivenTagOnFeature: async (featureId: string, tag: string) =>
+      await geoFeatureTagRepo.query(`INSERT INTO project_feature_tags
+            (project_id, feature_id, tag)
+          VALUES
+            ('${projectId}', '${featureId}', '${tag}' ) `),
+
+    // ACT
+    WhenUploadingCustomFeature: async (
+      name: string,
+      description: string,
+      tagName?: string,
+    ) => {
+      const dto: any = {
+        name,
+        description,
+      };
+      if (tagName) {
+        dto.tagName = tagName;
+      }
+      return request(app.getHttpServer())
         .post(`/api/v1/projects/${projectId}/features/shapefile`)
         .set('Authorization', `Bearer ${token}`)
         .attach(`file`, __dirname + `/wetlands.zip`)
-        .field({
-          name: customFeatureName,
-          description: customFeatureDesc,
-        }),
-    ThenGeoFeaturesAreCreated: async (result: request.Response) => {
+        .field(dto);
+    },
+
+    // ASSERT
+    ThenMaxLengthErrorWasReturned: (response: request.Response) => {
+      const error: any =
+        response.body.errors[0].meta.rawError.response.message[0];
+      expect(error).toContain(
+        `A tag should not be longer than ${tagMaxlength} characters`,
+      );
+    },
+
+    ThenInvalidTagErrorWasReturned: (response: request.Response) => {
+      const error: any =
+        response.body.errors[0].meta.rawError.response.message[0];
+      expect(error).toContain(`A tag cannot contain line breaks`);
+    },
+
+    ThenNoGeoFeatureIsCreated: async (
+      result: request.Response,
+      name: string,
+    ) => {
+      const features = await geoFeaturesApiRepo.find({
+        where: {
+          projectId,
+          featureClassName: name,
+        },
+      });
+      expect(features.length).toBe(0);
+    },
+    ThenGeoFeatureTagIsCreated: async (name: string, tag: string) => {
+      const features = await geoFeaturesApiRepo.find({
+        where: {
+          projectId,
+          featureClassName: name,
+        },
+      });
+
+      const featureTags = await geoFeatureTagRepo.find({
+        where: { projectId, featureId: features[0].id },
+      });
+      expect(featureTags).toHaveLength(1);
+      expect(featureTags[0].tag).toEqual(tag);
+    },
+
+    ThenGeoFeaturesAreCreated: async (
+      result: request.Response,
+      name: string,
+      description: string,
+    ) => {
       expect(result.body).toEqual({
         success: true,
       });
       const features = await geoFeaturesApiRepo.find({
         where: {
           projectId,
+          featureClassName: name,
         },
       });
       expect(features).toEqual([
         {
           id: expect.any(String),
-          featureClassName: customFeatureName,
-          description: customFeatureDesc,
+          featureClassName: name,
+          description,
           alias: null,
           propertyName: null,
           intersection: null,
