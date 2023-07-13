dbebca4e7948bac532327a37c76a9db32c755a95
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2023-07-13 14:05:20 +0200

    layout redesign for project/scenario pages

---
+++ app/layout/project/sidebar/index.tsx
@@ -0,0 +1,48 @@
+import { PropsWithChildren, useCallback, useState } from 'react';
+
+import Icon from 'components/icon';
+import { cn } from 'utils/cn';
+
+import ARROW_RIGHT_SVG from 'svgs/ui/arrow-right.svg?sprite';
+
+export const Sidebar = ({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string }>): JSX.Element => {
+  const [isOpen, setSidebar] = useState(true);
+
+  const handleSidebar = useCallback(() => {
+    setSidebar((prevState) => !prevState);
+  }, []);
+
+  return (
+    <aside
+      className={cn({
+        'absolute z-20 flex h-full w-[550px] rounded-r-2xl bg-black px-8 py-6 transition-transform':
+          true,
+        '-translate-x-full': !isOpen,
+        [className]: !!className,
+      })}
+    >
+      {children}
+      <button
+        type="button"
+        onClick={handleSidebar}
+        className={cn({
+          'absolute left-full top-6 z-20 -translate-x-1/2 rounded-lg bg-black px-2 py-3': true,
+          'translate-x-3': !isOpen,
+        })}
+      >
+        <Icon
+          icon={ARROW_RIGHT_SVG}
+          className={cn({
+            'h-3 w-3 transition-transform': true,
+            'rotate-180': isOpen,
+          })}
+        />
+      </button>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- app/layout/projects/new/form/types.ts
+++
@@ -1,3 +0,0 @@
-export default interface ProjectFormProps {
-  reset?: () => void;
-}
