a32d1b54cc51730cf0b0097007ea0195472c4020
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2023-07-18 18:01:32 +0200

    basic organisation for sidebar content

--- app/pages/projects/[pid]/index.tsx
+++ app/pages/projects/[pid]/index.tsx
@@ -1,9 +1,14 @@
+import { useRouter } from 'next/router';
+
 import { withProtection, withUser } from 'hoc/auth';
 import { withProject } from 'hoc/projects';
 
 import MetaIcons from 'layout/meta-icons';
 import ProjectLayout from 'layout/project';
 import Sidebar from 'layout/project/sidebar';
+import InventoryPanelCostSurface from 'layout/project/sidebar/project/inventory-panel/cost-surface';
+import InventoryPanelFeatures from 'layout/project/sidebar/project/inventory-panel/features';
+import InventoryPanelProtectedAreas from 'layout/project/sidebar/project/inventory-panel/protected-areas';
 import ProjectHeader from 'layout/projects/show/header';
 import ProjectMap from 'layout/projects/show/map';
 import ProjectScenarios from 'layout/projects/show/scenarios';
@@ -14,6 +19,9 @@ import ProjectTitle from 'layout/title/project-title';
 export const getServerSideProps = withProtection(withUser(withProject()));
 
 const ShowProjectsPage = (): JSX.Element => {
+  const { query } = useRouter();
+  const { tab } = query as { tab: 'features' | 'protected-areas' | 'cost-surface' };
+
   return (
     <Protected>
       <ProjectTitle title="" />
@@ -21,7 +29,10 @@ const ShowProjectsPage = (): JSX.Element => {
       <ProjectLayout className="relative z-10">
         <Sidebar className="flex-col">
           <ProjectHeader />
-          <ProjectScenarios />
+          {tab === 'features' && <InventoryPanelFeatures />}
+          {tab === 'protected-areas' && <InventoryPanelProtectedAreas />}
+          {tab === 'cost-surface' && <InventoryPanelCostSurface />}
+          {!tab && <ProjectScenarios />}
         </Sidebar>
         <ProjectStatus />
         <ProjectMap />
