f9cc697a0d7fe29540059bb8913a8c45e1153baf
anamontiaga <amontiaga@gmail.com>
2023-07-20 18:28:43 +0200

    add scenario types

---
+++ app/layout/project/sidebar/project/scenarios-list/scenario-item/constants.tsx
@@ -0,0 +1,58 @@
+export const SCENARIO_STATES = {
+  'run-running': {
+    text: 'Running Scenario',
+    styles: 'text-white',
+  },
+  'run-failure': {
+    text: 'Fail Running Scenario',
+    styles: 'text-red-500',
+  },
+  'run-done': {
+    text: 'Run Scenario',
+    styles: 'text-blue-500',
+  },
+  'calibration-running': {
+    text: 'Running Calibration',
+    styles: 'text-white',
+  },
+  'calibration-failure': {
+    text: 'Fail Running Calibration',
+    styles: 'text-red-500',
+  },
+  'pa-running': {
+    text: 'Running PA percentage',
+    styles: 'text-white',
+  },
+  'pa-failure': {
+    text: 'Fail PA percentage',
+    styles: 'text-red-500',
+  },
+  'pu-running': {
+    text: 'Running PU inclusion',
+    styles: 'text-white',
+  },
+  'pu-failure': {
+    text: 'Fail PU inclusion',
+    styles: 'text-red-500',
+  },
+  'features-running': {
+    text: 'Running Features',
+    styles: 'text-white',
+  },
+  'features-failure': {
+    text: 'Fail Features',
+    styles: 'text-red-500',
+  },
+  'clone-running': {
+    text: 'Running cloning',
+    styles: 'text-white',
+  },
+  'clone-failure': {
+    text: 'Fail cloning scenario',
+    styles: 'text-red-500',
+  },
+  draft: {
+    text: 'Edited',
+    styles: 'text-gray-400',
+  },
+};
