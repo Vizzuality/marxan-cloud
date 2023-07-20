4e9fdea8a66a8d08cf16ed786b3ea1e2df0ae88b
anamontiaga <amontiaga@gmail.com>
2023-07-20 16:11:35 +0200

    remove inventory panel sections components

--- app/components/new-layout/uploader/types.ts
+++
@@ -1,9 +0,0 @@
-export interface UploaderProps {
-  id?: string;
-  open?: boolean;
-  caption?: string;
-  disabled?: boolean;
-  children: React.ReactNode | React.ReactNode[];
-  onOpen: () => void;
-  onClose: () => void;
-}
--- app/layout/project/sidebar/project/inventory-panel/protected-areas/index.tsx
+++ app/layout/project/sidebar/project/inventory-panel/protected-areas/index.tsx
@@ -1,20 +1,5 @@
-// import ProtectedAreasUploader from 'layout/project/sidebar/project/inventory-panel/protected-areas/protected-areas-uploader';
-
 const InventoryPanelProtectedAreas = (): JSX.Element => {
-  return (
-    <div className="flex items-start justify-between rounded-xl bg-gray-700 p-6">
-      <div className="flex w-full items-start justify-between">
-        <div>
-          <h3 className="font-sans text-xs font-semibold text-blue-500">Inventory Panel</h3>
-
-          <div className="flex items-baseline space-x-2">
-            <h4 className="font-heading text-lg font-medium text-white">Protected Areas</h4>
-          </div>
-        </div>
-        {/* <ProtectedAreasUploader /> */}
-      </div>
-    </div>
-  );
+  return <div>InventoryPanelProtectedAreas</div>;
 };
 
 export default InventoryPanelProtectedAreas;
