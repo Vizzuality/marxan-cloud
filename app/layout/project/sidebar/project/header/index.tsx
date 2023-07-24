d04b7426f636683d7d113f7aa29b6e0dea1ee365
anamontiaga <amontiaga@gmail.com>
2023-07-24 16:58:00 +0200

    add under moderation component to new layout

--- app/layout/project/sidebar/project/header/index.tsx
+++ app/layout/project/sidebar/project/header/index.tsx
@@ -1,11 +1,15 @@
 import Contributors from 'layout/project/sidebar/project/header/contributors';
 import ProjectButton from 'layout/project/sidebar/project/header/project-button';
 import ProjectTitle from 'layout/project/sidebar/project/header/title';
+import UnderModeration from 'layout/project/sidebar/project/header/under-moderation';
 
 const InventoryProjectHeader = (): JSX.Element => {
   return (
     <div className="mb-6 flex items-start justify-between">
-      <ProjectTitle />
+      <div>
+        <UnderModeration />
+        <ProjectTitle />
+      </div>
       <div className="mt-4 flex items-center space-x-5">
         <Contributors />
         <ProjectButton />
