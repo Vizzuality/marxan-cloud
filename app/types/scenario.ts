ca3e46816fa307c18d3acc014ff12d95b4a0791b
anamontiaga <amontiaga@gmail.com>
2023-07-24 15:43:37 +0200

    fix Scenario types

--- app/types/scenario.ts
+++ app/types/scenario.ts
@@ -1,19 +1,43 @@
+import { Job } from 'types/job';
+
 export interface Scenario {
   id: string;
-  name: string;
-  warnings: boolean;
-  progress?: number;
-  lastUpdate: string;
+  projectId: number;
+  boundaryLengthModifier: number;
+  description: string;
   jobs?: Record<string, any>[];
-  runStatus: 'created' | 'running' | 'done' | 'failure';
+  lastUpdate: string;
   lock?: Record<string, any>;
   lastUpdateDistance: string;
-  className?: string;
-  ranAtLeastOnce: boolean;
+  name: string;
   numberOfRuns: number;
-  boundaryLengthModifier: number;
-  metadata: any;
+  metadata: {
+    marxanInputParameterFile: {
+      BESTSCORE: number;
+      BLM: number;
+      CLUMPTYPE: number;
+      COOLFAC: number;
+      COSTTHRESH: number;
+      HEURTYPE: number;
+      ITIMPTYPE: number;
+      MISSLEVEL: number;
+      NUMITNS: number;
+      NUMREPS: number;
+      NUMTEMP: number;
+      PROP: number;
+      RANDSEED: number;
+      RUNMODE: number;
+      STARTTEMP: number;
+      THRESHPEN1: number;
+      THRESHPEN2: number;
+    };
+    scenarioEditingMetadata: any;
+  };
+  progress?: number;
+  projectScenarioId?: number;
+  protectedAreaFilterByIds?: number[];
+  ranAtLeastOnce: boolean;
+  runStatus: Job['status'];
   solutionsAreLocked: boolean;
   wdpaThreshold: number;
-  wdpaIucnCategories: string[];
 }
