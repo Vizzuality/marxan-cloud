6b81de4deb3e712d34458723386e891cf14a86c9
anamontiaga <amontiaga@gmail.com>
2023-08-01 18:25:13 +0200

    add breadcrumbs

--- app/pages/projects/[pid]/index.tsx
+++ app/pages/projects/[pid]/index.tsx
@@ -15,14 +15,15 @@ import ProjectMap from 'layout/projects/show/map';
 import ProjectStatus from 'layout/projects/show/status';
 import Protected from 'layout/protected';
 import ProjectTitle from 'layout/title/project-title';
+import { Tab } from 'types/navigation';
 
-import Breadcrumb from 'components/breadcrumb';
+import Breadcrumbs from 'components/new-layout/breadcrumbs';
 
 export const getServerSideProps = withProtection(withUser(withProject()));
 
 const ShowProjectsPage = (): JSX.Element => {
-  const { push, query } = useRouter();
-  const { tab } = query as { tab: 'features' | 'protected-areas' | 'cost-surface' };
+  const { query } = useRouter();
+  const { tab } = query as { tab: Tab };
 
   return (
     <Protected>
@@ -30,13 +31,7 @@ const ShowProjectsPage = (): JSX.Element => {
       <MetaIcons />
       <ProjectLayout className="relative z-10">
         <Sidebar className="flex-col">
-          <Breadcrumb
-            onClick={() => {
-              push('/projects');
-            }}
-          >
-            Dashboard
-          </Breadcrumb>
+          <Breadcrumbs />
 
           <InventoryProjectHeader />
 
