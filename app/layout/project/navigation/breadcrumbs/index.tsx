fb94a03194d17c8c347f154c529cfaf01177d997
anamontiaga <amontiaga@gmail.com>
2023-08-02 09:40:01 +0200

    feedback

--- app/layout/project/navigation/breadcrumbs/index.tsx
+++ app/layout/project/navigation/breadcrumbs/index.tsx
@@ -1,52 +1,47 @@
-import React from 'react';
-
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import { useProject } from 'hooks/projects';
 import { useScenario } from 'hooks/scenarios';
 
+import Icon from 'components/icon';
 import { cn } from 'utils/cn';
 
+import ARROW_RIGHT_SVG from 'svgs/ui/arrow-right.svg?sprite';
+
 export const Breadcrumbs = ({ className }: { className?: string }): JSX.Element => {
-  const { pathname, push, query } = useRouter();
+  const { pathname, query } = useRouter();
   const { pid, sid } = query as { pid: string; sid: string };
   const projectQuery = useProject(pid);
   const scenarioQuery = useScenario(sid);
 
   return (
-    <nav className="flex font-sans text-xs text-white" aria-label="Breadcrumb">
-      <ol role="list" className="flex items-center space-x-2">
+    <nav className="flex text-xs text-white" aria-label="Breadcrumb">
+      <ol
+        className={cn({
+          'flex items-center space-x-2': true,
+          [className]: !!className,
+        })}
+      >
         <li>
-          <button
-            onClick={() => push('/projects')}
+          <Link
+            href="/projects"
             className={cn({
               'flex h-6 items-center overflow-hidden hover:text-primary-500 focus:outline-none':
                 true,
               'font-semibold text-primary-500': pathname === '/projects',
-              [className]: !!className,
             })}
           >
             Dashboard
-          </button>
+          </Link>
         </li>
         {pid && (
           <li>
             <div className="flex max-w-[220px] items-center">
-              <svg
-                className="h-5 w-5 flex-shrink-0 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <Icon className="mb-0.5 h-2 w-2 flex-shrink-0 text-gray-400" icon={ARROW_RIGHT_SVG} />
 
-              <button
-                onClick={() => push(`/projects/${pid}`)}
+              <Link
+                href={`/projects/${pid}`}
                 className={cn({
                   'ml-2 overflow-hidden overflow-ellipsis whitespace-nowrap hover:text-primary-500':
                     true,
@@ -54,25 +49,14 @@ export const Breadcrumbs = ({ className }: { className?: string }): JSX.Element 
                 })}
               >
                 {projectQuery.data?.name}
-              </button>
+              </Link>
             </div>
           </li>
         )}
         {sid && (
           <li>
             <div className="flex max-w-[200px] items-center">
-              <svg
-                className="h-5 w-5 flex-shrink-0 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <Icon className="mb-0.5 h-2 w-2 flex-shrink-0 text-gray-400" icon={ARROW_RIGHT_SVG} />
               <div
                 className={cn({
                   'ml-2 overflow-hidden overflow-ellipsis whitespace-nowrap': true,
