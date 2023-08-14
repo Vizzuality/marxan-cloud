e01842d87057aaf7fe92bd3900be23bcc47d2616
anamontiaga <amontiaga@gmail.com>
2023-08-14 13:03:26 +0200

    add to edit bulk

--- app/layout/project/sidebar/project/inventory-panel/features/modals/edit-bulk/index.tsx
+++ app/layout/project/sidebar/project/inventory-panel/features/modals/edit-bulk/index.tsx
@@ -177,7 +177,10 @@ const EditBulkModal = ({
                             className="h-10 w-full rounded-md border border-gray-300 px-3 text-gray-800 focus:border-none focus:outline-none focus:ring-1 focus:ring-blue-500"
                             placeholder="Type to pick or create tag..."
                             value={fprops.input.value}
-                            onFocus={() => setTagsMenuOpen(true)}
+                            onFocus={() => {
+                              setTagsMenuOpen(true);
+                              form.change('tag', '');
+                            }}
                             onBlur={() => setTagIsDone(true)}
                             onKeyDown={handleKeyPress}
                           />
