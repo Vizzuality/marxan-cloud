99f833601831140eb18b66e15e0ca4a011d4c2f7
anamontiaga <amontiaga@gmail.com>
2023-08-14 16:13:42 +0200

    add upload tabs component

---
+++ app/layout/project/sidebar/project/inventory-panel/features/modals/upload/upload-tabs/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { cn } from 'utils/cn';
+
+const BUTTON_COMMON_CLASSES =
+  'relative py-2 transition after:absolute after:bottom-0 after:left-1/2 after:h-0.5 after:w-0 after:-translate-x-1/2 after:transform after:bg-primary-500 after:transition-all focus:outline-none text-black/85';
+const BUTTON_ACTIVE_CLASSES = 'after:w-full';
+const BUTTON_INACTIVE_CLASSES = 'text-gray-300';
+
+type UploadFeatureMode = 'shapefile' | 'csv';
+
+export const UploadFeatureTabs = ({
+  mode,
+  onChange,
+}: {
+  mode: UploadFeatureMode;
+  onChange: (mode: UploadFeatureMode) => void;
+}): JSX.Element => {
+  return (
+    <div className="flex w-full space-x-4 text-xs font-medium text-black">
+      <button
+        type="button"
+        className={cn({
+          [BUTTON_COMMON_CLASSES]: true,
+          [BUTTON_ACTIVE_CLASSES]: mode === 'shapefile',
+          [BUTTON_INACTIVE_CLASSES]: mode !== 'shapefile',
+        })}
+        onClick={() => onChange('shapefile')}
+      >
+        Shapefile
+      </button>
+
+      <button
+        type="button"
+        className={cn({
+          [BUTTON_COMMON_CLASSES]: true,
+          [BUTTON_ACTIVE_CLASSES]: mode === 'csv',
+          [BUTTON_INACTIVE_CLASSES]: mode !== 'csv',
+        })}
+        onClick={() => onChange('csv')}
+      >
+        CSV
+      </button>
+    </div>
+  );
+};
+
+export default UploadFeatureTabs;
