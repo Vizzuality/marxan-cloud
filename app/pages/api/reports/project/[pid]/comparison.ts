038ceb23a82770e5c677913e5fbf1952e48fae75
anamontiaga <amontiaga@gmail.com>
2023-08-17 13:53:44 +0200

    await scenario data to campare

--- app/pages/api/reports/project/[pid]/comparison.ts
+++ app/pages/api/reports/project/[pid]/comparison.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import SCENARIOS from 'services/scenarios';
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const baseUrl = req.headers.origin;
 
@@ -10,20 +12,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   console.log('sid1', sid1, 'sid2', sid2);
 
-  // await SCENARIOS.request({
-  //   method: 'POST',
-  //   url: `/${sid}/calibration`,
-  //   headers: {
-  //     ...(req?.headers?.authorization && { Authorization: req.headers.authorization }),
-  //     'Content-Type': 'application/json',
-  //     Cookie: req?.headers?.cookie,
-  //   },
-  //   data: {
-  //     config: {
-  //       baseUrl,
-  //     },
-  //   },
-  // });
+  await SCENARIOS.request({
+    method: 'POST',
+    url: `/${sid1}`,
+    headers: {
+      ...(req?.headers?.authorization && { Authorization: req.headers.authorization }),
+      'Content-Type': 'application/json',
+      Cookie: req?.headers?.cookie,
+    },
+    data: {
+      config: {
+        baseUrl,
+      },
+    },
+  });
 
   res.status(200).send({});
 }
