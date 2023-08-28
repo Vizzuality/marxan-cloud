d0d613d466f887058af5240fb8ac563752b12d6d
Kevin <kew.sanchez@gmail.com>
2023-08-28 16:19:35 +0200

    feat(CostSurface): Adds new endpoint to update a Cost Surface
    For now, only the name is updated

--- api/apps/api/test/project/project-cost-surface.controller.acl.e2e-spec.ts
+++ api/apps/api/test/project/project-cost-surface.controller.acl.e2e-spec.ts
@@ -29,3 +29,32 @@ describe('Upload Cost Surface Shapefile', () => {
     await fixtures.ThenCostSurfaceWasNotCreated(costSurfaceName);
   });
 });
+
+describe('Upload Cost Surface Shapefile', () => {
+  beforeEach(async () => {
+    fixtures = await getProjectCostSurfaceControllerFixtures();
+  });
+
+  it(`should not update CostSurface API entity if the user doesn't have permissions to edit the project`, async () => {
+    // ARRANGE
+    const projectId = await fixtures.GivenProject('someProject', [
+      ProjectRoles.project_viewer,
+    ]);
+    const originalName = 'costSuperCoolName';
+    const costSurface = await fixtures.GivenCostSurfaceMetadataForProject(
+      projectId,
+      originalName,
+    );
+
+    // ACT
+    const response = await fixtures.WhenUpdatingCostSurfaceForProject(
+      projectId,
+      costSurface.id,
+      'someNewName',
+    );
+
+    // ASSERT
+    await fixtures.ThenProjectNotEditableErrorWasReturned(response, projectId);
+    await fixtures.ThenCostSurfaceAPIEntityWasNotUpdated(costSurface);
+  });
+});
