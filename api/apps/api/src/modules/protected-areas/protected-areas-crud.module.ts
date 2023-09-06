63b274c7c37601fa658a5641e7e4f82db1468367
yulia-bel <yuliya.beliakova@gmail.com>
2023-09-06 12:53:47 +0200

    Update protected-areas-crud module

--- api/apps/api/src/modules/protected-areas/protected-areas-crud.module.ts
+++ api/apps/api/src/modules/protected-areas/protected-areas-crud.module.ts
@@ -8,14 +8,15 @@ import { ProxyService } from '@marxan-api/modules/proxy/proxy.service';
 import { DbConnections } from '@marxan-api/ormconfig.connections';
 import { Scenario } from '../scenarios/scenario.api.entity';
 import { SelectionGetterModule } from '@marxan-api/modules/scenarios/protected-area/getter/selection-getter.module';
-import {SelectionGetterModule} from "@marxan-api/modules/scenarios/protected-area/getter/selection-getter.module";
 import { ProjectAclModule } from '@marxan-api/modules/access-control/projects-acl/project-acl.module';
+import { ProjectsModule } from "@marxan-api/modules/projects/projects.module";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ProtectedArea], DbConnections.geoprocessingDB),
     TypeOrmModule.forFeature([Scenario]),
     forwardRef(() => SelectionGetterModule),
+    forwardRef(() => ProjectsModule),
     ProjectAclModule,
   ],
   providers: [ProtectedAreasCrudService, ProxyService],
