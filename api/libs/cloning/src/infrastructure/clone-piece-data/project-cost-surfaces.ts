9f5d5e0f02d60d573b8ceea33d8edb805b00b1ad
yulia-bel <yuliya.beliakova@gmail.com>
2023-09-19 14:47:40 +0200

    Remove unused code

--- api/libs/cloning/src/infrastructure/clone-piece-data/project-cost-surfaces.ts
+++ api/libs/cloning/src/infrastructure/clone-piece-data/project-cost-surfaces.ts
@@ -1,17 +1,4 @@
-import { GeometrySource } from '@marxan/geofeatures';
-
 export const projectCostSurfacesRelativePath = 'cost-surfaces.json';
-
-// @debt JobStatus is a duplicated type as it is declared here and in
-// api/src/modules/scenarios/scenario.api.entity.ts
-export enum JobStatus {
-  draft = 'draft',
-  created = 'created',
-  running = 'running',
-  canceled = 'canceled',
-  done = 'done',
-  failure = 'failure',
-}
 
 type CostSurfaceData = {
   cost: number;
