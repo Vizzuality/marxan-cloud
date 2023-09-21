1339054303678d42671c43f2dd3836856bedac47
yulia-bel <yuliya.beliakova@gmail.com>
2023-09-21 16:59:11 +0200

    Add ProjectCostSurfacesPieceExporter to pieces-exporter module

--- api/apps/geoprocessing/src/export/pieces-exporters/pieces-exporters.module.ts
+++ api/apps/geoprocessing/src/export/pieces-exporters/pieces-exporters.module.ts
@@ -30,6 +30,9 @@ import { ScenarioOutputFolderPieceExporter } from './scenario-output-folder.piec
 import { ScenarioPlanningUnitsDataPieceExporter } from './scenario-planning-units-data.piece-exporter';
 import { ScenarioProtectedAreasPieceExporter } from './scenario-protected-areas.piece-exporter';
 import { ScenarioRunResultsPieceExporter } from './scenario-run-results.piece-exporter';
+import {
+  ProjectCostSurfacesPieceExporter
+} from "@marxan-geoprocessing/export/pieces-exporters/project-cost-surfaces.piece-exporter";
 
 @Module({
   imports: [
@@ -52,6 +55,7 @@ import { ScenarioRunResultsPieceExporter } from './scenario-run-results.piece-ex
     ExportConfigProjectPieceExporter,
     ExportConfigScenarioPieceExporter,
     ProjectCustomFeaturesPieceExporter,
+    ProjectCostSurfacesPieceExporter,
     PlanningAreaGadmPieceExporter,
     PlanningAreaCustomPieceExporter,
     PlanningAreaCustomGeojsonPieceExporter,
