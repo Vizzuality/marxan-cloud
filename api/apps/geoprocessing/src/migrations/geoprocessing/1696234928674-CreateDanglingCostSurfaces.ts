ec173a483e9141d70b7c5a713729a9fdd00e15b7
yulia-bel <yuliya.beliakova@gmail.com>
2023-10-02 11:29:13 +0200

    Add dangling cost surfaces garbage collector

---
+++ api/apps/geoprocessing/src/migrations/geoprocessing/1696234928674-CreateDanglingCostSurfaces.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateDanglingCostSurfaces1696234928674 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`
+            CREATE TABLE dangling_cost_surfaces (
+                cost_surface_id uuid PRIMARY KEY
+              );`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`
+            DROP TABLE dangling_cost_surfaces;
+        `);
+    }
+
+}
