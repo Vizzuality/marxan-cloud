47c40708ddff4cb9aa4147c129682d699e2751b2
Kevin <kew.sanchez@gmail.com>
2023-10-31 09:47:41 +0100

    test(FeatureAmounts): Adapts some tests to account for feature amounts calculations on feature shapefile upload

--- api/apps/geoprocessing/test/steps/given-pu-geometries-exists.ts
+++ api/apps/geoprocessing/test/steps/given-pu-geometries-exists.ts
@@ -10,14 +10,35 @@ export const GivenPuGeometriesExists = async (
   const result: PlanningUnitsGeom[] = await entityManager.query(
     `
       INSERT INTO planning_units_geom(type, the_geom)
-      VALUES 
-        ($1, ST_GeomFromText('POINT(1 1)',4326)), 
-        ($1, ST_GeomFromText('POINT(2 2)',4326)), 
+      VALUES
+        ($1, ST_GeomFromText('POINT(1 1)',4326)),
+        ($1, ST_GeomFromText('POINT(2 2)',4326)),
         ($1, ST_GeomFromText('POINT(3 3)',4326))
       ON CONFLICT (the_geom_hash, type) DO UPDATE SET type = $1
       RETURNING *
   `,
     [shape],
+  );
+
+  return result;
+};
+
+export const GivenPuSquareGridGeometryExists = async (
+  entityManager: EntityManager,
+  size: number,
+  xMin: number,
+  yMin: number,
+  xMax: number,
+  yMax: number,
+  shape: PlanningUnitGridShape,
+): Promise<PlanningUnitsGeom[]> => {
+  const result: PlanningUnitsGeom[] = await entityManager.query(
+    `INSERT INTO planning_units_geom (type, the_geom)
+      SELECT $1 as type, grid.geom as the_geom
+      FROM ( select geom from ST_SquareGrid( $2, ST_MakeEnvelope( $3, $4, $5, $6, 4326 ) ) ) grid
+      ON CONFLICT (the_geom_hash, type) DO UPDATE SET type = $1
+      RETURNING *`,
+    [shape, size, xMin, yMin, xMax, yMax],
   );
 
   return result;
