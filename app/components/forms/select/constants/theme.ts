be51fdc87a9b05d7b6f4a033112c72a69f0c5cda
Andrés González <andres.gonzalez@vizzuality.com>
2024-01-15 16:34:29 +0100

    select: fixes color for ligh themes

--- app/components/forms/select/constants/theme.ts
+++ app/components/forms/select/constants/theme.ts
@@ -20,7 +20,7 @@ export const SELECT_THEME = {
   light: {
     container: 'text-gray-700 bg-white ring-1 ring-gray-100 rounded-3xl',
     open: 'ring-2 ring-primary-400 bg-white text-gray-700 rounded-xl',
-    closed: 'text-gray-100',
+    closed: 'text-gray-900',
     prefix: {
       base: 'text-gray-900',
     },
@@ -38,17 +38,17 @@ export const SELECT_THEME = {
   'light-square': {
     container: 'text-gray-700 bg-white ring-1 ring-gray-100 rounded',
     open: 'ring-1 ring-primary-400 bg-white text-gray-700 rounded',
-    closed: 'text-gray-100',
+    closed: 'text-gray-900',
     prefix: {
       base: 'text-gray-900',
     },
     icon: {
-      closed: 'text-gray-700',
+      closed: 'text-gray-900',
       open: 'text-primary-500 transform rotate-180',
       disabled: 'text-gray-100',
     },
     item: {
-      base: 'text-sm text-gray-100',
+      base: 'text-sm text-gray-900',
       highlighted: 'text-sm bg-gray-200 text-gray-900',
       disabled: 'text-sm opacity-50 pointer-events-none',
     },
