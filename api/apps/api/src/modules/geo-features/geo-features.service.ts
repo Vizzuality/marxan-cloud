19e44cab23961789bc6796a815b19e7c1fd76f51
andrea rota <andrea.rota@vizzuality.com>
2025-03-11 13:39:12 +0000

    chore(api): narrow selection of feature data via materialized splits [MRXN23-543]

--- api/apps/api/src/modules/geo-features/geo-features.service.ts
+++ api/apps/api/src/modules/geo-features/geo-features.service.ts
@@ -908,11 +908,25 @@ export class GeoFeaturesService extends AppBaseService<
       )
       .then((result) => result.map((i: { id: string }) => i.id));
 
+    const projectSpecificFeaturesFromGeoprocessingOperations =
+      await this.geoFeaturesRepository
+        .query(
+          `
+          SELECT id FROM features
+            WHERE
+              project_id = $1
+              AND
+              geoprocessing_ops_hash IS NOT NULL;
+          `,
+          [projectId],
+        )
+        .then((result) => result.map((i: { id: string }) => i.id));
+
     /**
      * Then narrow down the list of features relevant to the project to those
      * that effectively intersect the project's bbox.
      */
-    const geoFeaturesWithinProjectBbox =
+    const geoFeaturesWithinProjectBbox: string[] =
       await this.geoFeaturesGeometriesRepository
         .createQueryBuilder('geoFeatureGeometries')
         .select('"geoFeatureGeometries"."feature_id"', 'featureId')
@@ -940,7 +954,10 @@ export class GeoFeaturesService extends AppBaseService<
           throw new Error(error);
         });
 
-    return geoFeaturesWithinProjectBbox;
+    return [
+      ...geoFeaturesWithinProjectBbox,
+      ...projectSpecificFeaturesFromGeoprocessingOperations,
+    ];
   }
 
   private async createFeature(
